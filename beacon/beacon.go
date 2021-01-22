8ad3d8220e6e589bf476dbd72f620104e448ea5e
Roman Behma <begmaroman@gmail.com>
2021-01-22 16:53:11 +0200

    Implemented base logic for submitting attestation

--- beacon/beacon.go
+++ beacon/beacon.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
+	ikeymanager "github.com/prysmaticlabs/prysm/validator/keymanager"
 )
 
 // Beacon represents the behavior of the beacon node connector
@@ -12,5 +13,5 @@ type Beacon interface {
 	StreamDuties(ctx context.Context, pubKey []byte) (<-chan *ethpb.DutiesResponse_Duty, error)
 
 	// SubmitAttestation submits attestation fo the given slot using the given public key
-	SubmitAttestation(ctx context.Context, slot uint64, duty *ethpb.DutiesResponse_Duty) error
+	SubmitAttestation(ctx context.Context, slot uint64, duty *ethpb.DutiesResponse_Duty, keyManager ikeymanager.IKeymanager) error
 }
