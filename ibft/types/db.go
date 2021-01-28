78286ad8d91dd040a82405f74d744e49d2458a8e
Alon Muroch <alonmuroch@gmail.com>
2021-01-28 17:17:13 +0200

    db

--- ibft/types/db.go
+++ ibft/types/db.go
@@ -1,5 +1,7 @@
 package types
 
-// Persister is an interface for persisting chain data
-type Persister interface {
+// DB is an interface for persisting chain data
+type DB interface {
+	SavePrepareJustification(lambda []byte, round uint64, msg *Message, signature []byte, signers []uint64)
+	SaveDecidedRound(lambda []byte, msg *Message, signature []byte, signers []uint64)
 }
