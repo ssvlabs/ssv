df6c9105a066352e0b352b3a6ed6b337d6de08ae
Alon Muroch <alonmuroch@gmail.com>
2021-02-01 12:26:54 +0200

    previous

--- ibft/proto/generate.go
+++ ibft/proto/generate.go
@@ -2,3 +2,4 @@ package proto
 
 //go:generate protoc -I $GOPATH/src/github.com/prysmaticlabs/ethereumapis --proto_path=$GOPATH/src:. --go_out=$GOPATH/src $GOPATH/src/github.com/bloxapp/ssv/ibft/proto/msgs.proto
 //go:generate protoc -I $GOPATH/src/github.com/prysmaticlabs/ethereumapis --proto_path=$GOPATH/src:. --go_out=$GOPATH/src $GOPATH/src/github.com/bloxapp/ssv/ibft/proto/params.proto
+//go:generate protoc -I $GOPATH/src/github.com/prysmaticlabs/ethereumapis --proto_path=$GOPATH/src:. --go_out=$GOPATH/src $GOPATH/src/github.com/bloxapp/ssv/ibft/proto/state.proto
