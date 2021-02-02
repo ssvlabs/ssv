23069fac06f805b410f12991a32b16105bdb6af0
Alon Muroch <alonmuroch@gmail.com>
2021-02-02 16:00:09 +0200

    merge fix

--- ibft/valueImpl/validation/validation.go
+++ ibft/valueImpl/validation/validation.go
@@ -1,20 +1,33 @@
 package validation
 
-import "github.com/bloxapp/ssv/ibft/valueImpl"
+import (
+	"encoding/json"
+
+	"github.com/bloxapp/ssv/ibft/valueImpl"
+	"go.uber.org/zap"
+)
 
 // validationConsensus implements valueImpl.ValueImplementation interface
 type validationConsensus struct {
+	logger   *zap.Logger
 	inputVal *InputValue
 }
 
 // New is the constructor of validationConsensus
-func New(inputVal *InputValue) valueImpl.ValueImplementation {
+func New(logger *zap.Logger, inputVal *InputValue) valueImpl.ValueImplementation {
 	return &validationConsensus{
+		logger:   logger,
 		inputVal: inputVal,
 	}
 }
 
 func (c *validationConsensus) ValidateValue(value []byte) error {
 	// TODO: Implement
+	actualData, err := json.Marshal(c.inputVal)
+	if err != nil {
+		return err
+	}
+
+	c.logger.Info("got validation request", zap.String("given_input", string(value)), zap.String("origin_input", string(actualData)))
 	return nil
 }
