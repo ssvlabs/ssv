69f05f28e3853053fc023382760231303869b2dd
Roman Behma <begmaroman@gmail.com>
2021-02-02 15:42:33 +0200

    Minor fixes

--- ibft/consensus/validation/validation.go
+++ ibft/consensus/validation/validation.go
@@ -1,20 +1,34 @@
 package validation
 
-import "github.com/bloxapp/ssv/ibft/consensus"
+import (
+	"encoding/json"
+
+	"go.uber.org/zap"
+
+	"github.com/bloxapp/ssv/ibft/consensus"
+)
 
 // validationConsensus implements consensus.Consensus interface
 type validationConsensus struct {
+	logger   *zap.Logger
 	inputVal *InputValue
 }
 
 // New is the constructor of validationConsensus
-func New(inputVal *InputValue) consensus.Consensus {
+func New(logger *zap.Logger, inputVal *InputValue) consensus.Consensus {
 	return &validationConsensus{
+		logger:   logger,
 		inputVal: inputVal,
 	}
 }
 
 func (c *validationConsensus) ValidateValue(value []byte) error {
 	// TODO: Implement
+	actualData, err := json.Marshal(c.inputVal)
+	if err != nil {
+		return err
+	}
+
+	c.logger.Info("got validation request", zap.String("given_input", string(value)), zap.String("origin_input", string(actualData)))
 	return nil
 }
