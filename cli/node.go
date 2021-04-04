005479767cc72686834018e0ce5e821184647edc
Lior Rutenberg <liorr@blox.io>
2021-04-04 15:25:23 +0300

    add flag to node

--- cli/node.go
+++ cli/node.go
@@ -172,6 +172,7 @@ func init() {
 	flags.AddValidatorKeyFlag(startNodeCmd)
 	flags.AddBeaconAddrFlag(startNodeCmd)
 	flags.AddNetworkFlag(startNodeCmd)
+	flags.AddDiscoveryFlag(startNodeCmd)
 	flags.AddConsensusFlag(startNodeCmd)
 	flags.AddNodeIDKeyFlag(startNodeCmd)
 	flags.AddSignatureCollectionTimeFlag(startNodeCmd)
