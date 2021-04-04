68deb5c41ce07ccb9052a6ee439d8e9901220a34
nivBlox <niv@blox.io>
2021-04-04 12:31:27 +0300

    enr add

--- cli/node.go
+++ cli/node.go
@@ -75,11 +75,11 @@ var startNodeCmd = &cobra.Command{
 
 		cfg := p2p.Config{
 			Local:             false,
-			BootstrapNodeAddr: []string{"enr:-LK4QMIAfHA47rJnVBaGeoHwXOrXcCNvUaxFiDEE2VPCxQ40cu_k2hZsGP6sX9xIQgiVnI72uxBBN7pOQCo5d9izhkcBh2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__________gmlkgnY0gmlwhH8AAAGJc2VjcDI1NmsxoQJu41tZ3K8fb60in7AarjEP_i2zv35My_XW_D_t6Y1fJ4N0Y3CCE4iDdWRwgg-g"},
+			BootstrapNodeAddr: []string{"enr:-LK4QBEZXm0QMAZREBPNB1FpPeIhpdFUCStO1beZ6XKDPZE2ahOn1K0BH8Iv53oh2s1bjRKAPpma7aKlrsyHFl2Wu-4Bh2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__gmlkgnY0gmlwhArCIRuJc2VjcDI1NmsxoQO8KQz5L1UEXzEr-CXFFq1th0eG6gopbdul2OQVMuxfMoN0Y3CCE4iDdWRwgg-g"},
 			UdpPort:           12000 + int(nodeID),
 			TcpPort:           13000 + int(nodeID),
 			TopicName:         validatorKey,
-			HostAddress:       "127.0.0.1",
+			//HostAddress:       "127.0.0.1",
 		}
 		peer, err := p2p.New(cmd.Context(), logger, &cfg)
 		if err != nil {
