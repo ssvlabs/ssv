c161b2d342d6bc17e1d6e6018386e689441db357
Alon Muroch <alonmuroch@gmail.com>
2021-05-05 16:59:56 +0300

    [WIP] sync storage

--- storage/inmem/inmem.go
+++ storage/inmem/inmem.go
@@ -14,10 +14,32 @@ func New() collections.Iibft {
 	return &inMemStorage{}
 }
 
-func (s *inMemStorage) SavePrepared(signedMsg *proto.SignedMessage) {
-	// TODO: Implement
+// SaveCurrentInstance saves the state for the current running (not yet decided) instance
+func (s *inMemStorage) SaveCurrentInstance(state *proto.State) error {
+	return nil
 }
 
-func (s *inMemStorage) SaveDecided(signedMsg *proto.SignedMessage) {
-	// TODO: Implement
+// GetCurrentInstance returns the state for the current running (not yet decided) instance
+func (s *inMemStorage) GetCurrentInstance(pk []byte) (*proto.State, error) {
+	return nil, nil
+}
+
+// SaveDecided saves a signed message for an ibft instance with decided justification
+func (s *inMemStorage) SaveDecided(signedMsg *proto.SignedMessage) error {
+	return nil
+}
+
+// GetDecided returns a signed message for an ibft instance which decided by identifier
+func (s *inMemStorage) GetDecided(pk []byte, seqNumber uint64) (*proto.SignedMessage, error) {
+	return nil, nil
+}
+
+// SaveHighestDecidedInstance saves a signed message for an ibft instance which is currently highest
+func (s *inMemStorage) SaveHighestDecidedInstance(signedMsg *proto.SignedMessage) error {
+	return nil
+}
+
+// GetHighestDecidedInstance gets a signed message for an ibft instance which is the highest
+func (s *inMemStorage) GetHighestDecidedInstance(pk []byte) (*proto.SignedMessage, error) {
+	return nil, nil
 }
