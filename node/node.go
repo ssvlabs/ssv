408ceab59ec59e535d2b47f8c0131f5e8c397175
nivBlox <niv@blox.io>
2021-05-12 12:31:58 +0300

    update duty stream fix

--- node/node.go
+++ node/node.go
@@ -110,7 +110,7 @@ func (n *ssvNode) InformObserver(data interface{}) {
 		}
 
 		// update stream duties
-		n.startStreamDuties(n.context)
+		n.startStreamDuties()
 	}
 }
 
@@ -122,58 +122,62 @@ func (n *ssvNode) GetObserverID() string {
 
 // Start implements Node interface
 func (n *ssvNode) Start() error {
-	n.setupValidators(n.context)
+	n.setupValidators()
 	n.startValidators()
-	n.startStreamDuties(n.context)
+	n.startStreamDuties()
 
-	for duty := range n.streamDuties {
-		go func(duty *ethpb.DutiesResponse_Duty) {
-			slots := collectSlots(duty)
-			if len(slots) == 0 {
-				n.logger.Debug("no slots found for the given duty")
-				return
-			}
-
-			for _, slot := range slots {
-				if slot < n.getEpochFirstSlot(n.genesisEpoch) {
-					// wait until genesis epoch starts
-					n.logger.Debug("skipping slot, lower than genesis", zap.Uint64("genesis_slot", n.getEpochFirstSlot(n.genesisEpoch)), zap.Uint64("slot", slot))
-					continue
+	for {
+		select {
+		case duty := <-n.streamDuties:
+			go func(duty *ethpb.DutiesResponse_Duty) {
+				slots := collectSlots(duty)
+				if len(slots) == 0 {
+					n.logger.Debug("no slots found for the given duty")
+					return
 				}
-				go func(slot uint64) {
-					// execute task if slot already began and not pass 1 epoch
-					currentSlot := uint64(n.getCurrentSlot())
-					if slot >= currentSlot && slot-currentSlot <= n.dutySlotsLimit {
-						prevIdentifier := ibft.FirstInstanceIdentifier()
-						pubKey := &bls.PublicKey{}
-						if err := pubKey.Deserialize(duty.PublicKey); err != nil {
-							n.logger.Error("Failed to deserialize pubkey from duty")
-						}
-						v := n.validatorsMap[pubKey.SerializeToHexStr()]
-						n.logger.Info("starting duty processing start for slot",
-							zap.Uint64("committee_index", duty.GetCommitteeIndex()),
-							zap.Uint64("slot", slot))
-						go v.ExecuteDuty(n.context, prevIdentifier, slot, duty)
-					} else {
-						n.logger.Info("scheduling duty processing start for slot",
-							zap.Time("start_time", n.getSlotStartTime(slot)),
-							zap.Uint64("committee_index", duty.GetCommitteeIndex()),
-							zap.Uint64("slot", slot))
 
-						if err := n.slotQueue.Schedule(duty.PublicKey, slot, duty); err != nil {
-							n.logger.Error("failed to schedule slot")
-						}
+				for _, slot := range slots {
+					if slot < n.getEpochFirstSlot(n.genesisEpoch) {
+						// wait until genesis epoch starts
+						n.logger.Debug("skipping slot, lower than genesis", zap.Uint64("genesis_slot", n.getEpochFirstSlot(n.genesisEpoch)), zap.Uint64("slot", slot))
+						continue
 					}
-				}(slot)
-			}
-		}(duty)
+					go func(slot uint64) {
+						// execute task if slot already began and not pass 1 epoch
+						currentSlot := uint64(n.getCurrentSlot())
+						if slot >= currentSlot && slot-currentSlot <= n.dutySlotsLimit {
+							prevIdentifier := ibft.FirstInstanceIdentifier()
+							pubKey := &bls.PublicKey{}
+							if err := pubKey.Deserialize(duty.PublicKey); err != nil {
+								n.logger.Error("Failed to deserialize pubkey from duty")
+							}
+							n.logger.Info("TEST", zap.String("key", pubKey.SerializeToHexStr()))
+							v := n.validatorsMap[pubKey.SerializeToHexStr()]
+							n.logger.Info("starting duty processing start for slot",
+								zap.Uint64("committee_index", duty.GetCommitteeIndex()),
+								zap.Uint64("slot", slot))
+							go v.ExecuteDuty(n.context, prevIdentifier, slot, duty)
+						} else {
+							n.logger.Info("scheduling duty processing start for slot",
+								zap.Time("start_time", n.getSlotStartTime(slot)),
+								zap.Uint64("committee_index", duty.GetCommitteeIndex()),
+								zap.Uint64("slot", slot))
+
+							if err := n.slotQueue.Schedule(duty.PublicKey, slot, duty); err != nil {
+								n.logger.Error("failed to schedule slot")
+							}
+						}
+					}(slot)
+				}
+			}(duty)
+		}
 	}
 
 	return nil
 }
 
 // setupValidators for each validatorShare with proper ibft wrappers
-func (n *ssvNode) setupValidators(ctx context.Context) {
+func (n *ssvNode) setupValidators() {
 	validatorShares, err := n.validatorStorage.GetAllValidatorShares()
 	if err != nil {
 		n.logger.Fatal("Failed to get all validator shares", zap.Error(err))
@@ -181,7 +185,7 @@ func (n *ssvNode) setupValidators(ctx context.Context) {
 
 	res := make(map[string]*validator.Validator)
 	for _, validatorShare := range validatorShares {
-		res[validatorShare.ValidatorPK.SerializeToHexStr()] = validator.New(ctx, n.logger, validatorShare, n.ibftStorage, n.network, n.ethNetwork, n.beacon, validator.Options{
+		res[validatorShare.ValidatorPK.SerializeToHexStr()] = validator.New(n.context, n.logger, validatorShare, n.ibftStorage, n.network, n.ethNetwork, n.beacon, validator.Options{
 			SlotQueue:                  n.slotQueue,
 			SignatureCollectionTimeout: n.signatureCollectionTimeout,
 		})
@@ -201,13 +205,13 @@ func (n *ssvNode) startValidators() {
 }
 
 // startStreamDuties start to stream duties from the beacon chain
-func (n *ssvNode) startStreamDuties(ctx context.Context) {
+func (n *ssvNode) startStreamDuties() {
 	var pubKeys [][]byte
 	var err error
 	for _, val := range n.validatorsMap {
 		pubKeys = append(pubKeys, val.ValidatorShare.ValidatorPK.Serialize())
 	}
-	n.streamDuties, err = n.beacon.StreamDuties(ctx, pubKeys)
+	n.streamDuties, err = n.beacon.StreamDuties(n.context, pubKeys)
 	if err != nil {
 		n.logger.Error("failed to open duties stream", zap.Error(err))
 	}
