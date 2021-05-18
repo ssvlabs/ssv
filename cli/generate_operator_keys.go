d69e2b8c2113a08b3f1de24dcd9d0b0bea45f455
nivBlox <niv@blox.io>
2021-05-18 14:03:41 +0300

    extract operator key to func + testing

--- cli/generate_operator_keys.go
+++ cli/generate_operator_keys.go
@@ -1,14 +1,12 @@
 package cli
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"encoding/pem"
 	"github.com/bloxapp/ssv/utils/logex"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+
+	"github.com/bloxapp/ssv/utils/operator_keys"
 )
 
 // generateOperatorKeysCmd is the command to generate operator private/public keys
@@ -18,65 +16,14 @@ var generateOperatorKeysCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		logger := logex.Build(RootCmd.Short, zapcore.DebugLevel)
 
-		// generate random private key (secret)
-		sk, err := rsa.GenerateKey(rand.Reader, 2048)
-		if err != nil {
-			logger.Error("Failed to generate operator private key", zap.Error(err))
-			return
+		pk, sk, err := operator_keys.GenerateKeys()
+		if err != nil{
+			logger.Fatal("Failed to generate operator keys", zap.Error(err))
 		}
-		// retrieve public key from the newly generated secret
-		pk := &sk.PublicKey
-
-		// convert to bytes
-		skPem := pem.EncodeToMemory(
-			&pem.Block{
-				Type:  "RSA PRIVATE KEY",
-				Bytes: x509.MarshalPKCS1PrivateKey(sk),
-			},
-		)
-		pkBytes, err := x509.MarshalPKIXPublicKey(pk)
-		if err != nil {
-			logger.Error("Failed to marshal public key", zap.Error(err))
-			return
-		}
-		pkPem := pem.EncodeToMemory(
-			&pem.Block{
-				Type:  "RSA PUBLIC KEY",
-				Bytes: pkBytes,
-			},
-		)
-
-		logger.Info("generated private key (base64)", zap.Any("sk", skPem))
-		logger.Info("generated public key (base64)", zap.Any("pk", pkPem))
+		logger.Info("generated public key (base64)", zap.Any("pk", pk))
+		logger.Info("generated private key (base64)", zap.Any("sk", sk))
 	},
 }
-
-//func convertPemToPrivateKey(logger *zap.Logger) *rsa.PrivateKey {
-//	block, _ := pem.Decode([]byte("-----BEGIN RSA PRIVATE KEY-----\nMIICXQIBAAKBgQDMQ6q8kgHF2LEhGmnlaoKGIxJx2Bh/fb0vFTq0DPeeJgCNeYPJ\n2ClQ316p+EMBjXAY7bC81Y2u/FGubuAzfH0JC9ENnxnlBopnSYcCzNuxRe/MwIrs\nrLc1krHNpgFMYrTmsPjtB6cmA454SfdYO1lvcxUxCOdNQZc3PwxK8EHyoQIDAQAB\nAoGATiTU/K8e3oG3weJJAOtuY8KnG8aAGMYRyiFlA9yyHl6Ld5Q1RtLbe4T4wi2n\n9MAXUnIcWyGXwonk9caVHx1Q96TysDgkoALEsmq0so/bSgQGC53aUwrZEVdiwph4\nT0F+hLl8ov5eynHuiYMHFRgJdhHoco4NNKzCqZ0MX+KBVsECQQD/e0Mn6BLhPBOK\no0fCkwYP20CsRPeU35gvUNEfak8FWz1fbFaf7n3d40j6lyABVQx2EjEDsZGhN6jl\n1dq/awhLAkEAzK3LVdFZgOh+t8rDU1T77DyTjexCJpXQ2E89mHL4MzlAMLKwRL31\nQAr3QiMJWTxHQ0aW6EvCjLGqc3Q5JY31QwJBAMM2z4jFtu9t9UyhGSsfJqmlEhTQ\nGhIii+nTqgeENt9T6WBpqwNHu9t5WYFJSsZZ00zA97znyOxUWHVOZHiRc2MCQQCP\nTof1uDSQmzhN+vuzluckSm2NiwPt/CtTqHeaC7VYOBeHgTUFjHLwujzQ47Mh9aB3\nrC7wykqXM7YCTDfO4Yv9AkBP2igHYwm9Ly88sg4EOt+FvVhqzwjzldfotDFbb6Ly\ngECRZsqYkgW3Xzu74ZOs+L+jC3fmI4zYNSx3M4Vufy55\n-----END RSA PRIVATE KEY-----\n"))
-//	enc:= x509.IsEncryptedPEMBlock(block)
-//	b := block.Bytes
-//	if enc{
-//		var err error
-//		b, err = x509.DecryptPEMBlock(block, nil)
-//		if err != nil {
-//			logger.Error("Failed to decrypt operator private key", zap.Error(err))
-//		}
-//	}
-//	parsedSk, err := x509.ParsePKCS1PrivateKey(b)
-//	if err != nil {
-//		logger.Error("Failed to parse operator private key", zap.Error(err))
-//		return nil
-//	}
-//	return parsedSk
-//}
-//
-//func decrypteHash(logger *zap.Logger, sk *rsa.PrivateKey, hash []byte) {
-//	dectyptedKey, err := rsa.DecryptPKCS1v15(rand.Reader, sk, hash)
-//	if err != nil {
-//		logger.Error("Failed to decrypt key", zap.Error(err))
-//	}
-//	logger.Info("decrypted key", zap.Any("key", string(dectyptedKey)))
-//}
 
 func init() {
 	RootCmd.AddCommand(generateOperatorKeysCmd)
---
+++ utils/operator_keys/operator_keys.go
@@ -0,0 +1,81 @@
+package operator_keys
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"github.com/pkg/errors"
+)
+
+var keySize = 2048
+
+// GenerateKeys using rsa random generate keys and return []byte bas64
+func GenerateKeys() ([]byte, []byte, error) {
+	// generate random private key (secret)
+	sk, err := rsa.GenerateKey(rand.Reader, keySize)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "Failed to generate operator private key")
+	}
+	// retrieve public key from the newly generated secret
+	pk := &sk.PublicKey
+
+	// convert to bytes
+	skPem := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: x509.MarshalPKCS1PrivateKey(sk),
+		},
+	)
+	pkBytes, err := x509.MarshalPKIXPublicKey(pk)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "Failed to marshal public key")
+	}
+	pkPem := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PUBLIC KEY",
+			Bytes: pkBytes,
+		},
+	)
+	return pkPem, skPem, nil
+}
+
+func DecodeKey(skPemBase64 string, hashBase64 string) (string, error) {
+	sk, err := convertPemToPrivateKey(skPemBase64)
+	if err != nil{
+		return "", errors.Wrap(err, "Failed to decrypt operator private key")
+	}
+	return decryptHash(sk, hashBase64)
+}
+
+func convertPemToPrivateKey(skPemBase64 string) (*rsa.PrivateKey, error) {
+	skPem, err := base64.StdEncoding.DecodeString(skPemBase64)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to decode base64")
+	}
+	block, _ := pem.Decode(skPem)
+	enc:= x509.IsEncryptedPEMBlock(block)
+	b := block.Bytes
+	if enc{
+		var err error
+		b, err = x509.DecryptPEMBlock(block, nil)
+		if err != nil {
+			return nil, errors.Wrap(err, "Failed to decrypt operator private key")
+		}
+	}
+	parsedSk, err := x509.ParsePKCS1PrivateKey(b)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to parse operator private key")
+	}
+	return parsedSk, nil
+}
+
+func decryptHash(sk *rsa.PrivateKey, hashBase64 string) (string, error) {
+	hash, _ := base64.StdEncoding.DecodeString(hashBase64)
+	decryptedKey, err := rsa.DecryptPKCS1v15(rand.Reader, sk, hash)
+	if err != nil {
+		return "", errors.Wrap(err, "Failed to decrypt key")
+	}
+	return string(decryptedKey), nil
+}
