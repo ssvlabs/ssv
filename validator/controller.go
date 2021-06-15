1b451403febf8fbb579b6be2a92300c1d097a0ee
amir-blox <83904651+amir-blox@users.noreply.github.com>
2021-06-15 12:50:51 +0300

    Print validators upon setup (#122)
    * print validators upon setup

--- validator/controller.go
+++ validator/controller.go
@@ -18,6 +18,7 @@ import (
 	"go.uber.org/zap"
 	"strings"
 	"time"
+	"fmt"
 )
 
 // ControllerOptions for controller struct creation
@@ -110,6 +111,7 @@ func (c *controller) setupValidators() map[string]*Validator {
 
 	res := make(map[string]*Validator)
 	for _, validatorShare := range validatorsShare {
+		printValidatorShare(c.logger, validatorShare)
 		res[validatorShare.PublicKey.SerializeToHexStr()] = New(Options{
 			Context:                    c.context,
 			SignatureCollectionTimeout: c.signatureCollectionTimeout,
@@ -243,3 +245,15 @@ func (c *controller) serializeValidatorAddedEvent(validatorAddedEvent eth1.Valid
 	validatorShare.Committee = ibftCommittee
 	return &validatorShare
 }
+
+func printValidatorShare(logger *zap.Logger, validatorShare *validatorstorage.Share) {
+	var committee []string
+	for _, c := range validatorShare.Committee {
+		committee = append(committee, fmt.Sprintf(`[IbftId=%d, PK=%x]`, c.IbftId, c.Pk))
+	}
+	logger.Debug("setup validator",
+		zap.String("pubKey", validatorShare.PublicKey.SerializeToHexStr()),
+		zap.Uint64("nodeID", validatorShare.NodeID),
+		zap.Strings("committee", committee))
+}
+
