0e40528b5d1a3981a9694417e5032357c8a69e2a
Amir Yahalom <amir@blox.io>
2021-06-16 14:52:26 +0300

    update gitlabci and fix lint issue

--- validator/duty_execution.go
+++ validator/duty_execution.go
@@ -209,15 +209,14 @@ func (v *Validator) ExecuteDuty(ctx context.Context, slot uint64, duty *ethpb.Du
 
 	visitedRoles := map[string]bool{}
 	for _, role := range roles {
+		if visitedRoles[role.String()] {
+			logger.Debug("skipping redundant role",
+				zap.String("role", role.String()))
+			continue
+		}
+		visitedRoles[role.String()] = true
 		go func(role beacon.Role) {
 			l := logger.With(zap.String("role", role.String()))
-			if visitedRoles[role.String()] {
-				l.Debug("skipping redundant role",
-					zap.String("role", role.String()))
-				return
-			} else {
-				visitedRoles[role.String()] = true
-			}
 			l.Debug("starting duty role")
 
 			signaturesCount, decidedValue, identifier, err := v.comeToConsensusOnInputValue(ctx, logger, slot, role, duty)
