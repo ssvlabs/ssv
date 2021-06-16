4efc061768fb8b778924c40aa3d67bca12cd22ff
Amir Yahalom <amir@blox.io>
2021-06-16 20:01:56 +0300

    remove log

--- ibft/pipeline/auth/msg_validator_pk.go
+++ ibft/pipeline/auth/msg_validator_pk.go
@@ -11,7 +11,7 @@ import (
 func ValidatePKs(pk []byte) pipeline.Pipeline {
 	return pipeline.WrapFunc("validator PK", func(signedMessage *proto.SignedMessage) error {
 		if len(signedMessage.Message.ValidatorPk) != 48 || !bytes.Equal(pk, signedMessage.Message.ValidatorPk) {
-			return errors.Errorf("invalid message validator PK: %x, signedMessage.Message.ValidatorPk: %x", pk, signedMessage.Message.ValidatorPk)
+			return errors.New("invalid message validator PK")
 		}
 		return nil
 	})
