6d3553776125a11a1293d4292523e68bdff2171c
Alon Muroch <alonmuroch@gmail.com>
2021-07-05 10:17:19 +0300

    proto gen fix

--- ibft/proto/params.go
+++ ibft/proto/params.go
@@ -5,7 +5,7 @@ import "time"
 //DefaultConsensusParams returns the default round change duration time
 func DefaultConsensusParams() *InstanceConfig {
 	return &InstanceConfig{
-		RoundChangeDuration:   int64(time.Second * 3),
-		LeaderPreprepareDelay: int64(time.Second * 1),
+		RoundChangeSeconds:           int64(time.Second * 3),
+		LeaderPreprepareDelaySeconds: int64(time.Second * 1),
 	}
 }
