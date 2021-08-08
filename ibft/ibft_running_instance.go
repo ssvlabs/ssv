da72b6e884c25d779e477f1d2394f4a849e716dd
Amir Yahalom <amir@blox.io>
2021-08-08 11:43:06 +0300

    check if current instance is nil

--- ibft/ibft_running_instance.go
+++ ibft/ibft_running_instance.go
@@ -24,6 +24,10 @@ func (i *ibftImpl) startInstanceWithOptions(instanceOpts InstanceOptions, value 
 instanceLoop:
 	for {
 		stage := <-stageChan
+		if i.currentInstance == nil {
+			i.logger.Debug("new stage but instance is already empty", zap.Any("stage", stage))
+			break instanceLoop
+		}
 		exit, e := i.instanceStageChange(stage)
 		if e != nil {
 			err = e
