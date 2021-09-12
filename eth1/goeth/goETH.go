629b46705261c5685aa3aa3b5363744eb1dbe8c7
amir-blox <83904651+amir-blox@users.noreply.github.com>
2021-09-12 08:26:32 +0300

    Fix ETH1 sync  (#300)
    * use batches to sync eth1 events

--- eth1/goeth/goETH.go
+++ eth1/goeth/goETH.go
@@ -24,7 +24,8 @@ import (
 )
 
 const (
-	healthCheckTimeout = 10 * time.Second
+	healthCheckTimeout        = 10 * time.Second
+	blocksInBatch      uint64 = 100000
 )
 
 type eth1NodeStatus int32
@@ -254,31 +255,74 @@ func (ec *eth1Client) syncSmartContractsEvents(fromBlock *big.Int) error {
 	if err != nil {
 		return errors.Wrap(err, "failed to parse ABI interface")
 	}
+	currentBlock, err := ec.conn.BlockNumber(ec.ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get current block")
+	}
+	var logs []types.Log
+	var nSuccess int
+	for {
+		var toBlock *big.Int
+		if currentBlock-fromBlock.Uint64() > blocksInBatch {
+			toBlock = big.NewInt(int64(fromBlock.Uint64() + blocksInBatch))
+		} else { // no more batches are required -> setting toBlock to nil
+			toBlock = nil
+		}
+		_logs, _nSuccess, err := ec.fetchAndProcessEvents(fromBlock, toBlock, contractAbi)
+		if err != nil {
+			return errors.Wrap(err, "failed to get events")
+		}
+		nSuccess += _nSuccess
+		logs = append(logs, _logs...)
+		if toBlock == nil { // finished
+			break
+		}
+		fromBlock = toBlock
+	}
+
+	ec.logger.Debug("finished syncing registry contract",
+		zap.Int("total events", len(logs)), zap.Int("total success", nSuccess))
+	// publishing SyncEndedEvent so other components could track the sync
+	ec.fireEvent(types.Log{}, eth1.SyncEndedEvent{Logs: logs, Success: nSuccess == len(logs)})
+
+	return nil
+}
+
+func (ec *eth1Client) fetchAndProcessEvents(fromBlock, toBlock *big.Int, contractAbi abi.ABI) ([]types.Log, int, error) {
+	logger := ec.logger.With(zap.Int64("fromBlock", fromBlock.Int64()))
+	if toBlock != nil {
+		logger = logger.With(zap.Int64("toBlock", toBlock.Int64()))
+	}
+	logger.Debug("fetching event logs")
+
 	contractAddress := common.HexToAddress(ec.registryContractAddr)
 	query := ethereum.FilterQuery{
 		Addresses: []common.Address{contractAddress},
 		FromBlock: fromBlock,
 	}
+	if toBlock != nil {
+		query.ToBlock = toBlock
+	}
 	logs, err := ec.conn.FilterLogs(ec.ctx, query)
 	if err != nil {
-		return errors.Wrap(err, "failed to get event logs")
+		return nil, 0, errors.Wrap(err, "failed to get event logs")
 	}
-	nResults := len(logs)
-	ec.logger.Debug(fmt.Sprintf("got event logs, number of results: %d", nResults))
+	nSuccess := len(logs)
+	logger = logger.With(zap.Int("results", len(logs)))
+	logger.Debug("got event logs")
 
 	for _, vLog := range logs {
 		err := ec.handleEvent(vLog, contractAbi)
 		if err != nil {
-			nResults--
+			nSuccess--
 			ec.logger.Error("Failed to handle event during sync", zap.Error(err))
 			continue
 		}
 	}
-	ec.logger.Debug(fmt.Sprintf("%d event logs were received and parsed successfully", nResults))
-	// publishing SyncEndedEvent so other components could track the sync
-	ec.fireEvent(types.Log{}, eth1.SyncEndedEvent{Logs: logs, Success: nResults == len(logs)})
+	logger.Debug("event logs were received and parsed successfully",
+		zap.Int("successCount", nSuccess))
 
-	return nil
+	return logs, nSuccess, nil
 }
 
 func (ec *eth1Client) handleEvent(vLog types.Log, contractAbi abi.ABI) error {
