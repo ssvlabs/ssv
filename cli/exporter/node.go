189e3c60c5904d4a11b76b6dd291bf564ba7b188
Alon Muroch <alonmuroch@gmail.com>
2021-09-21 08:41:31 +0300

    added beacon node to exporter

--- cli/exporter/node.go
+++ cli/exporter/node.go
@@ -3,6 +3,8 @@ package exporter
 import (
 	"crypto/rsa"
 	"fmt"
+	"github.com/bloxapp/ssv/beacon"
+	"github.com/bloxapp/ssv/beacon/goclient"
 	global_config "github.com/bloxapp/ssv/cli/config"
 	"github.com/bloxapp/ssv/eth1"
 	"github.com/bloxapp/ssv/eth1/goeth"
@@ -28,6 +30,7 @@ type config struct {
 	DBOptions                  basedb.Options `yaml:"db"`
 	P2pNetworkConfig           p2p.Config     `yaml:"p2p"`
 	ETH1Options                eth1.Options   `yaml:"eth1"`
+	ETH2Options                beacon.Options `yaml:"eth2"`
 
 	WsAPIPort         int    `yaml:"WebSocketAPIPort" env:"WS_API_PORT" env-default:"14000" env-description:"port of exporter WS api"`
 	MetricsAPIPort    int    `yaml:"MetricsAPIPort" env:"METRICS_API_PORT" env-description:"port of metrics api"`
@@ -95,8 +98,18 @@ var StartExporterNodeCmd = &cobra.Command{
 			Logger.Fatal("failed to create eth1 client", zap.Error(err))
 		}
 
+		// TODO Not refactored yet Start
+		cfg.ETH2Options.Context = cmd.Context()
+		cfg.ETH2Options.Logger = Logger
+		cfg.ETH2Options.Graffiti = []byte("BloxStaking")
+		beaconClient, err := goclient.New(cfg.ETH2Options)
+		if err != nil {
+			Logger.Fatal("failed to create beacon go-client", zap.Error(err))
+		}
+
 		exporterOptions := new(exporter.Options)
 		exporterOptions.Eth1Client = eth1Client
+		exporterOptions.Beacon = beaconClient
 		exporterOptions.Logger = Logger
 		exporterOptions.Network = network
 		exporterOptions.DB = db
@@ -109,6 +122,7 @@ var StartExporterNodeCmd = &cobra.Command{
 		exporterNode = exporter.New(*exporterOptions)
 
 		metrics.WaitUntilHealthy(Logger, eth1Client, "eth1 node")
+		metrics.WaitUntilHealthy(Logger, beaconClient, "beacon node")
 
 		if err := exporterNode.StartEth1(eth1.HexStringToSyncOffset(cfg.ETH1Options.ETH1SyncOffset)); err != nil {
 			Logger.Fatal("failed to start eth1", zap.Error(err))
