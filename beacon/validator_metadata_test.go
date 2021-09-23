05f3007297b9aa115ab26d185135e7aab7c7acc7
Amir Yahalom <amir@blox.io>
2021-09-23 19:39:26 +0300

    align test

--- beacon/validator_metadata_test.go
+++ beacon/validator_metadata_test.go
@@ -1,10 +1,10 @@
 package beacon
 
 import (
+	"encoding/hex"
 	v1 "github.com/attestantio/go-eth2-client/api/v1"
 	spec "github.com/attestantio/go-eth2-client/spec/phase0"
 	"github.com/bloxapp/ssv/utils/logex"
-	"github.com/herumi/bls-eth-go-binary/bls"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 	"strings"
@@ -50,28 +50,29 @@ func TestUpdateValidatorsMetadata(t *testing.T) {
 	pks := []string{
 		"a17bb48a3f8f558e29d08ede97d6b7b73823d8dc2e0530fe8b747c93d7d6c2755957b7ffb94a7cec830456fd5492ba19",
 		"a0cf5642ed5aa82178a5f79e00292c5b700b67fbf59630ce4f542c392495d9835a99c826aa2459a67bc80867245386c6",
+		"8bafb7165f42e1179f83b7fcd8fe940e60ed5933fac176fdf75a60838c688eaa3b57717be637bde1d5cebdadb8e39865",
 	}
 
-	pk1 := bls.PublicKey{}
-	require.Nil(t, pk1.DeserializeHexStr(pks[0]))
-	blsPubKey1 := spec.BLSPubKey{}
-	copy(blsPubKey1[:], pk1.Serialize())
-
-	pk2 := bls.PublicKey{}
-	require.Nil(t, pk2.DeserializeHexStr(pks[1]))
-	blsPubKey2 := spec.BLSPubKey{}
-	copy(blsPubKey2[:], pk2.Serialize())
+	decodeds := make([][]byte, len(pks))
+	blsPubKeys := make([]spec.BLSPubKey, len(pks))
+	for i, pk := range pks {
+		blsPubKey := spec.BLSPubKey{}
+		decoded, _ := hex.DecodeString(pk)
+		copy(blsPubKey[:], decoded[:])
+		decodeds[i] = decoded
+		blsPubKeys[i] = blsPubKey
+	}
 
 	data := map[spec.BLSPubKey]*v1.Validator{}
-	data[blsPubKey1] = &v1.Validator{
+	data[blsPubKeys[0]] = &v1.Validator{
 		Index:     spec.ValidatorIndex(210961),
 		Status:    v1.ValidatorStateWithdrawalPossible,
-		Validator: &spec.Validator{PublicKey: blsPubKey1},
+		Validator: &spec.Validator{PublicKey: blsPubKeys[0]},
 	}
-	data[blsPubKey2] = &v1.Validator{
+	data[blsPubKeys[1]] = &v1.Validator{
 		Index:     spec.ValidatorIndex(213820),
 		Status:    v1.ValidatorStateActiveOngoing,
-		Validator: &spec.Validator{PublicKey: blsPubKey2},
+		Validator: &spec.Validator{PublicKey: blsPubKeys[1]},
 	}
 	bc := NewMockBeacon(map[uint64][]*Duty{}, data)
 
@@ -83,7 +84,7 @@ func TestUpdateValidatorsMetadata(t *testing.T) {
 		require.True(t, meta.Index == spec.ValidatorIndex(210961) || meta.Index == spec.ValidatorIndex(213820))
 		atomic.AddUint64(&updateCount, 1)
 	}
-	err := UpdateValidatorsMetadata([][]byte{pk1.Serialize(), pk2.Serialize()}, storage, bc, onUpdated)
+	err := UpdateValidatorsMetadata([][]byte{blsPubKeys[0][:], blsPubKeys[1][:], blsPubKeys[2][:]}, storage, bc, onUpdated)
 	require.Nil(t, err)
 	require.Equal(t, uint64(2), updateCount)
 	require.Equal(t, 2, storage.(*mockValidatorMetadataStorage).Size())
