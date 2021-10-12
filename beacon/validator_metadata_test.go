81eb66870201f0a568721d3217f2b5e519bb1e95
Amir Yahalom <amir@blox.io>
2021-10-12 18:41:50 +0300

    add tests

--- beacon/validator_metadata_test.go
+++ beacon/validator_metadata_test.go
@@ -5,11 +5,14 @@ import (
 	v1 "github.com/attestantio/go-eth2-client/api/v1"
 	spec "github.com/attestantio/go-eth2-client/spec/phase0"
 	"github.com/bloxapp/ssv/utils/logex"
+	"github.com/bloxapp/ssv/utils/tasks"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 	"strings"
+	"sync"
 	"sync/atomic"
 	"testing"
+	"time"
 )
 
 func init() {
@@ -88,4 +91,61 @@ func TestUpdateValidatorsMetadata(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, uint64(2), updateCount)
 	require.Equal(t, 2, storage.(*mockValidatorMetadataStorage).Size())
+}
+
+func TestBatch(t *testing.T) {
+	pks := []string{
+		"a17bb48a3f8f558e29d08ede97d6b7b73823d8dc2e0530fe8b747c93d7d6c2755957b7ffb94a7cec830456fd5492ba19",
+		"a0cf5642ed5aa82178a5f79e00292c5b700b67fbf59630ce4f542c392495d9835a99c826aa2459a67bc80867245386c6",
+		"8bafb7165f42e1179f83b7fcd8fe940e60ed5933fac176fdf75a60838c688eaa3b57717be637bde1d5cebdadb8e39865",
+		"aaafb7165f42e1179f83b7fcd8fe940e60ed5933fac176fdf75a60838c688eaa3b57717be637bde1d5cebdadb8e39865",
+		"bbbfb7165f42e1179f83b7fcd8fe940e60ed5933fac176fdf75a60838c688eaa3b57717be637bde1d5cebdadb8e39865",
+		"cccfb7165f42e1179f83b7fcd8fe940e60ed5933fac176fdf75a60838c688eaa3b57717be637bde1d5cebdadb8e39865",
+	}
+
+	decodeds := make([][]byte, 0)
+	blsPubKeys := make([]spec.BLSPubKey, len(pks))
+	for i, pk := range pks {
+		blsPubKey := spec.BLSPubKey{}
+		decoded, _ := hex.DecodeString(pk)
+		copy(blsPubKey[:], decoded[:])
+		decodeds = append(decodeds, decoded)
+		blsPubKeys[i] = blsPubKey
+	}
+
+	t.Run("multiple batches", func(t *testing.T) {
+		called := make([][]byte, 0)
+		var wg sync.WaitGroup
+		wg.Add(2)
+		batch(decodeds, tasks.NewExecutionQueue(time.Millisecond), func(pks [][]byte) func() error {
+			defer wg.Done()
+			require.True(t, len(pks) > 0)
+			called = append(called, pks...)
+			return nil
+		}, 4)
+		wg.Wait()
+		require.Equal(t, len(pks), len(called))
+	})
+
+	t.Run("single batch", func(t *testing.T) {
+		called := make([][]byte, 0)
+		var wg sync.WaitGroup
+		wg.Add(1)
+		batch(decodeds, tasks.NewExecutionQueue(time.Millisecond), func(pks [][]byte) func() error {
+			defer wg.Done()
+			require.Equal(t, len(decodeds), len(pks))
+			called = append(called, pks...)
+			return nil
+		}, 25)
+		wg.Wait()
+		require.Equal(t, len(pks), len(called))
+	})
+
+	t.Run("no items", func(t *testing.T) {
+		batch(make([][]byte, 0), tasks.NewExecutionQueue(time.Millisecond), func(pks [][]byte) func() error {
+			t.Fail()
+			return nil
+		}, 4)
+		time.Sleep(10 * time.Millisecond) // to be sure the function has finished
+	})
 }
--- beacon/validator_metdata.go
+++ beacon/validator_metdata.go
@@ -123,25 +123,26 @@ func UpdateValidatorsMetadataBatch(pubKeys [][]byte,
 	bc Beacon,
 	onUpdated OnUpdated,
 	batchSize int) {
-
-	n := float64(len(pubKeys))
-	batches := int(math.Ceil(n / float64(batchSize)))
-	if batches == 0 {
-		// nothing to update..
-		return
-	}
-	start := 0
-	end := int(math.Min(n, float64(start+batchSize)))
-
-	batchTask := func(pks [][]byte) func() error {
+	batch(pubKeys, queue, func(pks [][]byte) func() error {
 		return func() error {
 			return UpdateValidatorsMetadata(pks, collection, bc, onUpdated)
 		}
-	}
+	}, batchSize)
+}
 
-	for i := 0; i <= batches; i++ {
+type batchTask func(pks [][]byte) func() error
+
+func batch(pubKeys [][]byte, queue tasks.Queue, task batchTask, batchSize int) {
+	n := float64(len(pubKeys))
+	// in case the amount of public keys is lower than the batch size
+	batchSize = int(math.Min(n, float64(batchSize)))
+	batches := int(math.Ceil(n / float64(batchSize)))
+	start := 0
+	end := int(math.Min(n, float64(batchSize)))
+
+	for i := 0; i < batches; i++ {
 		// run task
-		queue.Queue(batchTask(pubKeys[start:end]))
+		queue.Queue(task(pubKeys[start:end]))
 		// reset start and end
 		start = end
 		end = int(math.Min(n, float64(start+batchSize)))
