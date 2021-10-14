8633962fd7596c1116e82304b2254e7f64912c3d
amir-blox <83904651+amir-blox@users.noreply.github.com>
2021-10-14 19:14:01 +0300

    Set exporter as direct peer (#377)
    * setup exporter as direct peer
    
    * add pending status back
    
    * extract exporter-peer-id to config

--- validator/metrics.go
+++ validator/metrics.go
@@ -87,9 +87,9 @@ func ReportValidatorStatus(pk string, meta *beacon.ValidatorMetadata, logger *za
 	} else if !meta.Activated() {
 		logger.Debug("validator not activated")
 		metricsValidatorStatus.WithLabelValues(pk).Set(float64(validatorStatusNotActivated))
-		//} else if meta.Pending() {
-		//	logger.Debug("validator pending")
-		//	metricsValidatorStatus.WithLabelValues(pk).Set(float64(validatorStatusPending))
+	} else if meta.Pending() {
+		logger.Debug("validator pending")
+		metricsValidatorStatus.WithLabelValues(pk).Set(float64(validatorStatusPending))
 	} else if meta.Index == 0 {
 		logger.Debug("validator index not found")
 		metricsValidatorStatus.WithLabelValues(pk).Set(float64(validatorStatusNoIndex))
@@ -111,7 +111,7 @@ var (
 	validatorStatusExiting      validatorStatus = 5
 	validatorStatusSlashed      validatorStatus = 6
 	validatorStatusNotFound     validatorStatus = 7
-	//validatorStatusPending      validatorStatus = 8
+	validatorStatusPending      validatorStatus = 8
 )
 
 var (
