1b0884d9b0b8ad3dbcc0f6641aee170c78fcde2b
Amir Yahalom <amir@blox.io>
2021-10-18 16:47:11 +0300

    send role on new decided message

--- exporter/ibft/decided_reader.go
+++ exporter/ibft/decided_reader.go
@@ -214,7 +214,10 @@ func validateMsg(msg *proto.SignedMessage, identifier string) error {
 func newDecidedNetworkMsg(msg *proto.SignedMessage, pk string) api.NetworkMessage {
 	return api.NetworkMessage{Msg: api.Message{
 		Type:   api.TypeDecided,
-		Filter: api.MessageFilter{PublicKey: pk, From: int64(msg.Message.SeqNumber), To: int64(msg.Message.SeqNumber)},
+		Filter: api.MessageFilter{
+			PublicKey: pk,
+			From: int64(msg.Message.SeqNumber), To: int64(msg.Message.SeqNumber),
+			Role: api.RoleAttester},
 		Data:   []*proto.SignedMessage{msg},
 	}, Conn: nil}
 }
