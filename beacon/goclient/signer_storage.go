fce023b0c1a59f821839bd572d1cf65850ccebaa
Alon Muroch <alonmuroch@gmail.com>
2021-10-22 09:20:13 +0300

    signer db - account funcs

--- beacon/goclient/signer_storage.go
+++ beacon/goclient/signer_storage.go
@@ -2,8 +2,10 @@ package goclient
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/bloxapp/eth2-key-manager/core"
 	"github.com/bloxapp/eth2-key-manager/encryptor"
+	"github.com/bloxapp/eth2-key-manager/wallets"
 	"github.com/bloxapp/eth2-key-manager/wallets/hd"
 	"github.com/bloxapp/ssv/storage/basedb"
 	"github.com/google/uuid"
@@ -13,19 +15,17 @@ import (
 )
 
 const (
-	prefix     = "signer_data-"
-	walletPath = "wallet"
+	prefix         = "signer_data-"
+	walletPrefix   = prefix + "wallet-"
+	walletPath     = "wallet"
+	accountsPrefix = prefix + "accounts-"
+	accountsPath   = "accounts_%s"
 )
-
-func storagePrefix(network core.Network) []byte {
-	return []byte(prefix + network)
-}
 
 type signerStorage struct {
 	db      basedb.IDb
 	network core.Network
 	lock    sync.RWMutex
-	prefix  []byte
 }
 
 func newSignerStorage(db basedb.IDb, network core.Network) *signerStorage {
@@ -33,8 +33,11 @@ func newSignerStorage(db basedb.IDb, network core.Network) *signerStorage {
 		db:      db,
 		network: network,
 		lock:    sync.RWMutex{},
-		prefix:  storagePrefix(network),
 	}
+}
+
+func (s *signerStorage) objPrefix(obj string) []byte {
+	return []byte(string(s.network) + obj)
 }
 
 // Name returns storage name.
@@ -57,7 +60,7 @@ func (s *signerStorage) SaveWallet(wallet core.Wallet) error {
 		return errors.Wrap(err, "failed to marshal wallet")
 	}
 
-	return s.db.Set(s.prefix, []byte(walletPath), data)
+	return s.db.Set(s.objPrefix(walletPrefix), []byte(walletPath), data)
 }
 
 // OpenWallet returns nil,err if no wallet was found
@@ -66,7 +69,7 @@ func (s *signerStorage) OpenWallet() (core.Wallet, error) {
 	defer s.lock.RUnlock()
 
 	// get wallet bytes
-	obj, found, err := s.db.Get(s.prefix, []byte(walletPath))
+	obj, found, err := s.db.Get(s.objPrefix(walletPrefix), []byte(walletPath))
 	if !found {
 		return nil, errors.New("could not find wallet")
 	}
@@ -74,7 +77,7 @@ func (s *signerStorage) OpenWallet() (core.Wallet, error) {
 		return nil, errors.Wrap(err, "failed to open wallet")
 	}
 	if obj.Value == nil || len(obj.Value) == 0 {
-		return nil, errors.Wrap(err, "failed to open wallet")
+		return nil, errors.New("failed to open wallet")
 	}
 
 	// decode
@@ -91,20 +94,48 @@ func (s *signerStorage) OpenWallet() (core.Wallet, error) {
 func (s *signerStorage) ListAccounts() ([]core.ValidatorAccount, error) {
 	s.lock.RLock()
 	defer s.lock.RUnlock()
-	return nil, nil
+
+	objs, err := s.db.GetAllByCollection(s.objPrefix(accountsPrefix))
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to get val share")
+	}
+
+	ret := make([]core.ValidatorAccount, 0)
+	for _, obj := range objs {
+		acc, err := s.decodeAccount(obj.Value)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to list accounts")
+		}
+
+		ret = append(ret, acc)
+	}
+
+	return ret, nil
 }
 
 // SaveAccount saves the given account
 func (s *signerStorage) SaveAccount(account core.ValidatorAccount) error {
 	s.lock.Lock()
 	defer s.lock.Unlock()
-	return nil
+
+	data, err := json.Marshal(account)
+	if err != nil {
+		return errors.Wrap(err, "failed to marshal account")
+	}
+
+	key := fmt.Sprintf(accountsPath, account.ID().String())
+
+	return s.db.Set(s.objPrefix(accountsPrefix), []byte(key), data)
 }
 
 // DeleteAccount deletes account by uuid
 func (s *signerStorage) DeleteAccount(accountID uuid.UUID) error {
 	s.lock.Lock()
 	defer s.lock.Unlock()
+
+	key := fmt.Sprintf(accountsPath, accountID.String())
+	s.db.Set(s.objPrefix(accountsPrefix), []byte(key), nil)
+
 	return nil
 }
 
@@ -112,7 +143,33 @@ func (s *signerStorage) DeleteAccount(accountID uuid.UUID) error {
 func (s *signerStorage) OpenAccount(accountID uuid.UUID) (core.ValidatorAccount, error) {
 	s.lock.RLock()
 	defer s.lock.RUnlock()
-	return nil, nil
+
+	key := fmt.Sprintf(accountsPath, accountID.String())
+
+	// get account bytes
+	obj, found, err := s.db.Get(s.objPrefix(accountsPrefix), []byte(key))
+	if !found {
+		return nil, errors.New("could not find account")
+	}
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to open account")
+	}
+	return s.decodeAccount(obj.Value)
+}
+
+func (s *signerStorage) decodeAccount(byts []byte) (core.ValidatorAccount, error) {
+	if byts == nil || len(byts) == 0 {
+		return nil, errors.New("failed to open wallet")
+	}
+
+	// decode
+	var ret *wallets.HDAccount
+	if err := json.Unmarshal(byts, &ret); err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal HD account object")
+	}
+	ret.SetContext(&core.WalletContext{Storage: s})
+
+	return ret, nil
 }
 
 // SetEncryptor sets the given encryptor to the wallet.
--- beacon/goclient/signer_storage_test.go
+++ beacon/goclient/signer_storage_test.go
@@ -1,6 +1,8 @@
 package goclient
 
 import (
+	"encoding/hex"
+	"fmt"
 	"github.com/bloxapp/eth2-key-manager/core"
 	"github.com/bloxapp/eth2-key-manager/encryptor"
 	"github.com/bloxapp/eth2-key-manager/encryptor/keystorev4"
@@ -13,6 +15,11 @@ import (
 	"go.uber.org/zap"
 	"testing"
 )
+
+func _byteArray(input string) []byte {
+	res, _ := hex.DecodeString(input)
+	return res
+}
 
 func getWalletStorage(t *testing.T) core.Storage {
 	options := basedb.Options{
@@ -41,6 +48,58 @@ func testWallet(t *testing.T) (core.Wallet, core.Storage) {
 	require.NoError(t, err)
 
 	return wallet, storage
+}
+
+func TestOpeningAccounts(t *testing.T) {
+	wallet, storage := testWallet(t)
+	defer storage.(*signerStorage).db.Close()
+	seed := _byteArray("0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1fff")
+
+	for i := 0; i < 10; i++ {
+		testName := fmt.Sprintf("adding and fetching account: %d", i)
+		t.Run(testName, func(t *testing.T) {
+			// create
+			a, err := wallet.CreateValidatorAccount(seed, nil)
+			require.NoError(t, err)
+
+			// open
+			a1, err := wallet.AccountByPublicKey(hex.EncodeToString(a.ValidatorPublicKey()))
+			require.NoError(t, err)
+
+			a2, err := wallet.AccountByID(a.ID())
+			require.NoError(t, err)
+
+			// verify
+			for _, fetchedAccount := range []core.ValidatorAccount{a1, a2} {
+				require.Equal(t, a.ID().String(), fetchedAccount.ID().String())
+				require.Equal(t, a.Name(), fetchedAccount.Name())
+				require.Equal(t, a.ValidatorPublicKey(), fetchedAccount.ValidatorPublicKey())
+				require.Equal(t, a.WithdrawalPublicKey(), fetchedAccount.WithdrawalPublicKey())
+			}
+		})
+	}
+}
+
+func TestDeleteAccount(t *testing.T) {
+	_, storage := testWallet(t)
+	defer storage.(*signerStorage).db.Close()
+
+	accts, err := storage.ListAccounts()
+	require.NoError(t, err)
+	require.Len(t, accts, 1)
+
+	require.NoError(t, storage.DeleteAccount(accts[0].ID()))
+	acc, err := storage.OpenAccount(accts[0].ID())
+	require.EqualError(t, err, "failed to open wallet")
+	require.Nil(t, acc)
+}
+
+func TestNonExistingWallet(t *testing.T) {
+	storage := getWalletStorage(t)
+	w, err := storage.OpenWallet()
+	require.NotNil(t, err)
+	require.EqualError(t, err, "could not find wallet")
+	require.Nil(t, w)
 }
 
 func TestWalletStorage(t *testing.T) {
