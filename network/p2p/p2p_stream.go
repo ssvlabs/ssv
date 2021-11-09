910e472c94ba5a34ce1c8bb06797be55bfaa846c
Amir Yahalom <amir@blox.io>
2021-11-09 11:50:24 +0200

    change log to network trace

--- network/p2p/p2p_stream.go
+++ network/p2p/p2p_stream.go
@@ -10,7 +10,8 @@ import (
 // syncStreamHandler sets a stream handler for the host to process streamed messages
 func (n *p2pNetwork) syncStreamHandler() {
 	n.host.SetStreamHandler(syncStreamProtocol, func(stream core.Stream) {
-		n.logger.Debug("syncStreamHandler start")
+		n.trace("syncStreamHandler start")
+
 		netSyncStream := NewSyncStream(stream)
 
 		// read msg
@@ -20,14 +21,12 @@ func (n *p2pNetwork) syncStreamHandler() {
 			return
 		}
 
-		n.logger.Debug("syncStreamHandler buf", zap.ByteString("buf", buf))
-
 		cm, err := n.fork.DecodeNetworkMsg(buf)
 		if err != nil {
 			n.logger.Error("could not parse stream", zap.Error(err))
 			return
 		}
-		n.logger.Debug("syncStreamHandler decoded", zap.Any("cm", cm))
+		n.trace("syncStreamHandler decoded", zap.Any("cm", cm))
 		n.propagateSyncMsg(cm, netSyncStream)
 	})
 }
