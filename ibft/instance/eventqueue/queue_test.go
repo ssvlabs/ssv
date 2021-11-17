8c4eb3589921b3a1e99b2e593021bf5f6f567ec2
amir-blox <83904651+amir-blox@users.noreply.github.com>
2021-11-17 16:14:31 +0200

    refactor event queue (#435)

--- ibft/instance/eventqueue/queue_test.go
+++ ibft/instance/eventqueue/queue_test.go
@@ -2,6 +2,7 @@ package eventqueue
 
 import (
 	"github.com/stretchr/testify/require"
+	"sync/atomic"
 	"testing"
 )
 
@@ -11,7 +12,7 @@ func TestQueue(t *testing.T) {
 		q := New()
 
 		require.Nil(t, q.Pop())
-		require.True(t, q.Add(func() {}))
+		require.True(t, q.Add(NewEvent(func() {})))
 		require.NotNil(t, q.Pop())
 		require.Nil(t, q.Pop())
 	})
@@ -19,11 +20,11 @@ func TestQueue(t *testing.T) {
 	t.Run("add multiple", func(t *testing.T) {
 		q := New()
 
-		require.True(t, q.Add(func() {}))
-		require.True(t, q.Add(func() {}))
-		require.True(t, q.Add(func() {}))
-		require.True(t, q.Add(func() {}))
-		require.True(t, q.Add(func() {}))
+		require.True(t, q.Add(NewEvent(func() {})))
+		require.True(t, q.Add(NewEvent(func() {})))
+		require.True(t, q.Add(NewEvent(func() {})))
+		require.True(t, q.Add(NewEvent(func() {})))
+		require.True(t, q.Add(NewEvent(func() {})))
 		require.NotNil(t, q.Pop())
 		require.NotNil(t, q.Pop())
 		require.NotNil(t, q.Pop())
@@ -33,13 +34,25 @@ func TestQueue(t *testing.T) {
 	})
 
 	t.Run("clear and stop", func(t *testing.T) {
+		var drained uint32
 		q := New()
 
-		require.True(t, q.Add(func() {}))
+		cancel := func() {
+			atomic.AddUint32(&drained, uint32(1))
+		}
+		require.True(t, q.Add(NewEventWithCancel(func() {}, cancel)))
+		require.True(t, q.Add(NewEventWithCancel(func() {}, cancel)))
+		require.True(t, q.Add(NewEventWithCancel(func() {}, cancel)))
 		q.ClearAndStop()
 		require.Nil(t, q.Pop())
-		require.False(t, q.Add(func() {}))
+		require.False(t, q.Add(NewEventWithCancel(func() {}, cancel)))
 		require.Nil(t, q.Pop())
+		// sleep so cancel functions will be executed
+		for {
+			if atomic.LoadUint32(&drained) >= uint32(3) {
+				return
+			}
+		}
 	})
 
 }
