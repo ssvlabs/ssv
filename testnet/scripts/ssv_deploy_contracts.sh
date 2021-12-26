e500a55f44b1ab360a65b07154facc8cbe7aebbe
Amir Yahalom <amir@blox.io>
2021-12-26 11:04:02 +0200

    fix contracts deployment

--- testnet/scripts/ssv_deploy_contracts.sh
+++ testnet/scripts/ssv_deploy_contracts.sh
@@ -9,6 +9,9 @@ function install_ssv_network() {
 
 cd "$SSV_TESTNET_DIR" && touch ssv-deploy.log
 cd ssv-network || install_ssv_network
-GANACHE_MNEMONIC=$(cat "$LH_DIR/scripts/local_testnet/vars.env" | grep -E -o "^ETH1_NETWORK_MNEMONIC=(.+)" | sed 's/ETH1_NETWORK_MNEMONIC=//g') \
-GAS_PRICE="0x0" GANACHE_ETH_NODE_URL="http://$LH_TESTNET_IP:8545" \
-  npx hardhat run scripts/ssv-deploy-test.ts --network ganache > ../ssv-deploy.log
+#GANACHE_MNEMONIC=$(cat "$LH_DIR/scripts/local_testnet/vars.env" | grep -E -o "^ETH1_NETWORK_MNEMONIC=(.+)" | sed 's/ETH1_NETWORK_MNEMONIC=//g') \
+#GAS_PRICE="0x0" GANACHE_ETH_NODE_URL="http://$LH_TESTNET_IP:8545" \
+GAS_PRICE="0x0" MINIMUM_BLOCKS_BEFORE_LIQUIDATION=100 OPERATOR_MAX_FEE_INCREASE=3 \
+  npx hardhat run scripts/ssv-deploy-test.ts --network localhost > ssv-deploy.log
+
+export SSV_REG_ADDR=$(cat ssv-deploy.log | grep "SSVRegistry:" | grep -E -o "(0x.+)$")
