5696b82a832d9326f9b8941811d8af9732e04131
nivBlox <niv@blox.io>
2022-01-04 19:28:24 +0200

    deploy exporter stage

--- ibft/controller/mediator.go
+++ ibft/controller/mediator.go
@@ -45,6 +45,6 @@ func (m *Mediator) redirect(ibftType network.NetworkMsg, readerHandler func(publ
 	publicKey, role := format.IdentifierUnformat(string(msg.Message.Lambda)) // TODO need to support multi role types
 	if reader, ok := readerHandler(publicKey); ok {
 		m.logger.Debug("redirect msg", zap.String("publicKey", publicKey), zap.String("role", role), zap.String("type", ibftType.String()))
-		reader.GetMsgResolver(ibftType)(msg)
+		err := reader.GetMsgResolver(ibftType)(msg)
 	}
 }
