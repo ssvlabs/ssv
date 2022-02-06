649c29a83194e7c4092c0633c245c945ae439855
Amir Yahalom <amir@blox.io>
2022-02-06 19:01:35 +0200

    enhance race test

--- ibft/instance/roundtimer/timer_test.go
+++ ibft/instance/roundtimer/timer_test.go
@@ -59,15 +59,43 @@ func TestRoundTimer_Kill(t *testing.T) {
 }
 
 func TestRoundTimer_Race(t *testing.T) {
-	timer := New(context.Background(), zaptest.NewLogger(t))
-	timer.Reset(time.Millisecond * 4)
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		time.Sleep(time.Millisecond * 5)
-		timer.Kill()
-	}()
-	wg.Wait()
-	require.True(t, <-timer.ResultChan())
+	// this test checks for end cases where kill is called right before/after the timer has elapsed.
+	// in the first case, reset is called first and therefore timer should end with positive result
+	// in the second case, the timer is killed and should end with negative result
+
+	t.Run("reset_wins", func(t *testing.T) {
+		timer := New(context.Background(), zaptest.NewLogger(t))
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			timer.Reset(time.Millisecond * 4)
+		}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			time.Sleep(time.Millisecond * 5)
+			timer.Kill()
+		}()
+		wg.Wait()
+		require.True(t, <-timer.ResultChan())
+	})
+
+	t.Run("kill_wins", func(t *testing.T) {
+		timer := New(context.Background(), zaptest.NewLogger(t))
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			timer.Reset(time.Millisecond * 5)
+		}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			time.Sleep(time.Millisecond * 4)
+			timer.Kill()
+		}()
+		wg.Wait()
+		require.False(t, <-timer.ResultChan())
+	})
 }
