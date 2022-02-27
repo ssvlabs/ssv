d4015cfd3df492a3505d72a9d5eabd5861f05d96
Amir Yahalom <amir@blox.io>
2022-02-27 09:46:57 +0200

    extract common utilities

---
+++ network/commons/addr_utils.go
@@ -0,0 +1,43 @@
+package commons
+
+import (
+	"fmt"
+	"github.com/libp2p/go-libp2p-core/peer"
+	ma "github.com/multiformats/go-multiaddr"
+	"github.com/pkg/errors"
+	"github.com/prysmaticlabs/prysm/network"
+	"net"
+	"time"
+)
+
+// IPAddr returns the external IP address
+func IPAddr() (net.IP, error) {
+	ip, err := network.ExternalIP()
+	if err != nil {
+		return nil, errors.Wrap(err, "could not get IPv4 address")
+	}
+	return net.ParseIP(ip), nil
+}
+
+// CheckAddress checks that some address is accessible and returns error accordingly
+func CheckAddress(addr string) error {
+	conn, err := net.DialTimeout("tcp", addr, time.Second*10)
+	if err != nil {
+		return errors.Wrap(err, "IP address is not accessible")
+	}
+	if err := conn.Close(); err != nil {
+		return errors.Wrap(err, "could not close connection")
+	}
+	return nil
+}
+
+func BuildMultiAddress(ipAddr, protocol string, port uint, id peer.ID) (ma.Multiaddr, error) {
+	parsedIP := net.ParseIP(ipAddr)
+	if parsedIP.To4() == nil && parsedIP.To16() == nil {
+		return nil, errors.Errorf("invalid ip address provided: %s", ipAddr)
+	}
+	if parsedIP.To4() != nil {
+		return ma.NewMultiaddr(fmt.Sprintf("/ip4/%s/%s/%d/p2p/%s", ipAddr, protocol, port, id.String()))
+	}
+	return ma.NewMultiaddr(fmt.Sprintf("/ip6/%s/%s/%d/p2p/%s", ipAddr, protocol, port, id.String()))
+}
---
+++ network/commons/defaults.go
@@ -0,0 +1,7 @@
+package commons
+
+const (
+	DefaultIP  = "127.0.0.1"
+	DefaultTCP = 12001
+	DefaultUDP = 13001
+)
