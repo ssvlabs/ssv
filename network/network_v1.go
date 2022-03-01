8d5a82b2ebde829ad5185d34f37bd7f152de7d18
Amir Yahalom <amir@blox.io>
2022-03-01 18:42:25 +0200

    added network v1 interface and protocol message

---
+++ network/network_v1.go
@@ -0,0 +1,66 @@
+package network
+
+import "github.com/bloxapp/ssv/protocol"
+
+// MessageRouter is accepting network messages and route them to the corresponding (internal) components
+type MessageRouter interface {
+	// Route routes the given message
+	Route(message protocol.SSVMessage)
+}
+
+// MsgValidationResult helps other components to report message validation with a generic results scheme
+type MsgValidationResult int32
+
+const (
+	// ValidationAccept is the result of a valid message
+	ValidationAccept MsgValidationResult = iota
+	// ValidationIgnore is the result in case we want to ignore the validation
+	ValidationIgnore
+	// ValidationRejectLow is the result for invalid message, with low severity (e.g. late message)
+	ValidationRejectLow
+	// ValidationRejectMedium is the result for invalid message, with medium severity (e.g. wrong height)
+	ValidationRejectMedium
+	// ValidationRejectHigh is the result for invalid message, with high severity (e.g. invalid signature)
+	ValidationRejectHigh
+)
+
+// ValidationReporter is the interface for reporting on message validation results
+type ValidationReporter interface {
+	// ReportValidation reports the result for the given message
+	ReportValidation(message protocol.SSVMessage, res MsgValidationResult)
+}
+
+// SubscriberV1 is the interface for subscribing to topics
+type SubscriberV1 interface {
+	// Subscribe subscribes to validator subnet
+	Subscribe(pk protocol.ValidatorPK) error
+	// UseMessageRouter registers a message router to handle incoming messages
+	UseMessageRouter(router MessageRouter)
+}
+
+// BroadcasterV1 is the interface for broadcasting messages
+type BroadcasterV1 interface {
+	// Broadcast publishes the message to all peers in subnet
+	Broadcast(message protocol.SSVMessage) error
+}
+
+// SyncerV1 is the interface for syncing messages
+type SyncerV1 interface {
+	// LastState fetches last state from other peers
+	LastState(mid protocol.MessageID) ([]protocol.SSVMessage, error)
+	// GetHistory sync the given range from other peers
+	GetHistory(mid protocol.MessageID, from, to uint64) ([]protocol.SSVMessage, error)
+	// LastChangeRound fetches last change round message from other peers
+	LastChangeRound(mid protocol.MessageID) ([]protocol.SSVMessage, error)
+	// Respond will respond on some stream with the given results
+	// TODO: consider using a different approach or encapsulate the stream id
+	Respond(streamID string, results []protocol.SSVMessage) error
+}
+
+// NetworkV1 is a facade interface that provides the entire functionality of the different network interfaces
+type NetworkV1 interface {
+	ValidationReporter
+	SubscriberV1
+	BroadcasterV1
+	SyncerV1
+}
---
+++ protocol/message.go
@@ -0,0 +1,137 @@
+package protocol
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/hex"
+	"encoding/json"
+	"github.com/bloxapp/ssv/beacon"
+	"github.com/pkg/errors"
+)
+
+type MsgType uint32
+
+const (
+	// SSVConsensusMsgType are all QBFT consensus related messages
+	SSVConsensusMsgType MsgType = iota
+	// SSVSyncMsgType are all QBFT sync messages
+	SSVSyncMsgType
+	// SSVPostConsensusMsgType are all partial signatures sent after consensus
+	SSVPostConsensusMsgType
+)
+
+// ValidatorPK is an eth2 validator public key
+type ValidatorPK []byte
+
+// MessageIDBelongs returns true if message ID belongs to validator
+func (vid ValidatorPK) MessageIDBelongs(msgID MessageID) bool {
+	toMatch := msgID[:len(vid)]
+	return bytes.Equal(vid, toMatch)
+}
+
+// MessageID is used to identify and route messages to the right validator and DutyRunner
+type MessageID []byte
+
+func NewMessageID(pk []byte, role beacon.RoleType) MessageID {
+	roleByts := make([]byte, 4)
+	binary.LittleEndian.PutUint32(roleByts, uint32(role))
+	return append(pk, roleByts...)
+}
+
+func (msgID MessageID) GetRoleType() beacon.RoleType {
+	roleByts := msgID[len(msgID)-4:]
+	return beacon.RoleType(binary.LittleEndian.Uint32(roleByts))
+}
+
+func (msgID MessageID) String() string {
+	return hex.EncodeToString(msgID)
+}
+
+type MessageEncoder interface {
+	// Encode returns a msg encoded bytes or error
+	Encode() ([]byte, error)
+	// Decode returns error if decoding failed
+	Decode(data []byte) error
+}
+
+// SSVMessage is the main message passed within the SSV network, it can contain different types of messages (QBTF, Sync, etc.)
+type SSVMessage struct {
+	MsgType MsgType
+	MsgID   MessageID
+	Data    []byte
+}
+
+func (msg *SSVMessage) GetType() MsgType {
+	return msg.MsgType
+}
+
+// GetID returns a unique msg ID that is used to identify to which validator should the message be sent for processing
+func (msg *SSVMessage) GetID() MessageID {
+	return msg.MsgID
+}
+
+// GetData returns message Data as byte slice
+func (msg *SSVMessage) GetData() []byte {
+	return msg.Data
+}
+
+// Encode returns a msg encoded bytes or error
+func (msg *SSVMessage) Encode() ([]byte, error) {
+	m := make(map[string]string)
+
+	d, err := json.Marshal(msg.MsgType)
+	if err != nil {
+		return nil, errors.Wrap(err, "MsgType marshaling failed")
+	}
+	m["type"] = hex.EncodeToString(d)
+
+	if msg.MsgID != nil {
+		m["id"] = hex.EncodeToString(msg.MsgID)
+	}
+
+	if msg.Data != nil {
+		d, err := json.Marshal(msg.Data)
+		if err != nil {
+			return nil, errors.Wrap(err, "Data marshaling failed")
+		}
+		m["Data"] = hex.EncodeToString(d)
+	}
+	return json.Marshal(m)
+}
+
+// Decode returns error if decoding failed
+func (msg *SSVMessage) Decode(data []byte) error {
+	m := make(map[string]string)
+	if err := json.Unmarshal(data, &m); err != nil {
+		return errors.Wrap(err, "could not unmarshal SSVMessage")
+	}
+
+	d, err := hex.DecodeString(m["type"])
+	if err != nil {
+		return errors.Wrap(err, "SSVMessage decode string failed")
+	}
+	if err := json.Unmarshal(d, &msg.MsgType); err != nil {
+		return errors.Wrap(err, "could not unmarshal MsgType")
+	}
+
+	if val, ok := m["id"]; ok {
+		d, err := hex.DecodeString(val)
+		if err != nil {
+			return errors.Wrap(err, "msg id decode string failed")
+		}
+		msg.MsgID = d
+	}
+
+	if val, ok := m["Data"]; ok {
+		msg.Data = make([]byte, 0)
+		d, err := hex.DecodeString(val)
+		if err != nil {
+			return errors.Wrap(err, "Data decode string failed")
+		}
+		if err := json.Unmarshal(d, &msg.Data); err != nil {
+			msg.Data = nil
+			return errors.Wrap(err, "could not unmarshal Data")
+		}
+	}
+	return nil
+}
