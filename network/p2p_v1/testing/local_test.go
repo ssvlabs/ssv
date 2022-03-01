c6e8373823f7e0936dc02e68e0466b89a1cfc5c7
Amir Yahalom <amir@blox.io>
2022-03-01 09:06:11 +0200

    reduce timeouts in local net

--- network/p2p_v1/testing/local_test.go
+++ network/p2p_v1/testing/local_test.go
@@ -5,7 +5,6 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 	"testing"
-	"time"
 )
 
 func TestNewLocalNet(t *testing.T) {
@@ -14,13 +13,13 @@ func TestNewLocalNet(t *testing.T) {
 	defer cancel()
 	//logger := zaptest.NewLogger(t)
 	logger := zap.L()
+	t.Logf("creating %d keys", n)
 	identities, err := CreateKeys(n)
 	require.NoError(t, err)
+	t.Logf("creating local mdns network with %d nodes", n)
 	ln, err := NewLocalNet(ctx, logger, identities...)
 	require.NoError(t, err)
 	require.Len(t, ln.Nodes, n)
-
-	<-time.After(time.Second)
 }
 
 func TestNewLocalDiscV5Net(t *testing.T) {
@@ -29,12 +28,12 @@ func TestNewLocalDiscV5Net(t *testing.T) {
 	defer cancel()
 	//logger := zaptest.NewLogger(t)
 	logger := zap.L()
+	t.Logf("creating %d keys", n)
 	identities, err := CreateKeys(n)
 	require.NoError(t, err)
+	t.Logf("creating local discv5 network with %d nodes and bootnode", n)
 	ln, err := NewLocalDiscV5Net(ctx, logger, identities...)
 	require.NoError(t, err)
 	require.NotNil(t, ln.Bootnode)
 	require.Len(t, ln.Nodes, n)
-
-	<-time.After(time.Second)
 }
