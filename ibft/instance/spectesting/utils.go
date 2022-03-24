c3a8227f5c4f627278b5079ff6ee0dc111c6af43
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2022-03-24 16:36:20 +0400

    remove pre-generated keys from ibft/instance/spectesting/utils.go

--- ibft/instance/spectesting/utils.go
+++ ibft/instance/spectesting/utils.go
@@ -1,7 +1,6 @@
 package spectesting
 
 import (
-	"encoding/hex"
 	"encoding/json"
 	"testing"
 
@@ -16,34 +15,21 @@ import (
 	"github.com/bloxapp/ssv/ibft/proto"
 	"github.com/bloxapp/ssv/network/local"
 	"github.com/bloxapp/ssv/network/msgqueue"
+	"github.com/bloxapp/ssv/utils/blskeygen"
 	"github.com/bloxapp/ssv/utils/dataval/bytesval"
 	"github.com/bloxapp/ssv/utils/threshold"
 	"github.com/bloxapp/ssv/validator/storage"
 )
 
-func _byteArray(input string) []byte {
-	res, _ := hex.DecodeString(input)
-	return res
+const keysCount = 4
+
+func init() {
+	threshold.Init()
 }
 
 var (
-	// RefPk is a testing PK of RefSk
-	refPk = _byteArray("a9cf360aa15fb1d1d30ee2b578dc5884823c19661886ae8b892775ccb3bd96b7d7345569a2aa0b14e4d015c54a6a0c54")
-
-	// RefSplitShares is RefSk split into 4 shares
-	refSplitShares = [][]byte{ // sk split to 4: 2c083f2c8fc923fa2bd32a70ab72b4b46247e8c1f347adc30b2f8036a355086c
-		_byteArray("1a1b411e54ebb0973dc0f133c8b192cc4320fd464cbdcfe3be38b77f821f30bc"),
-		_byteArray("6a93d37661cfe9cbaff9f051f2dd1d1995905932375e09357be1a50f7f4de323"),
-		_byteArray("3596a78e633ad5071c0a77bb16b1a391b21ab47fb32ba1ba442a48e89ae11f9f"),
-		_byteArray("62ff0c0cac676cd9e866377f4772d63f403b5734c02351701712a308d4d8e632"),
-	}
-	// RefSplitSharesPubKeys is the PK for RefSplitShares
-	refSplitSharesPubKeys = [][]byte{
-		_byteArray("84d90424a5511e3741ac3c99ee1dba39007a290410e805049d0ae40cde74191d785d7848f08b2dfb99b742ebfe846e3b"),
-		_byteArray("b6ac738a09a6b7f3fb4f85bac26d8965f6329d431f484e8b43633f7b7e9afce0085bb592ea90df6176b2f2bd97dfd7f3"),
-		_byteArray("a261c25548320f1aabfc2aac5da3737a0b8bbc992a5f4f937259d22d39fbf6ebf8ec561720de3a04f661c9772fcace96"),
-		_byteArray("85dd2d89a3e320995507c46320f371dc85eb16f349d1c56d71b58663b5b6a5fd390fcf41cf9098471eb5437fd95be1ac"),
-	}
+	refSK, refPK = blskeygen.GenBLSKeyPair()
+	splitKeys, _ = threshold.Create(refSK.Serialize(), keysCount-1, keysCount)
 )
 
 // PrePrepareMsg constructs and signs a pre-prepare msg
@@ -212,33 +198,27 @@ func TestNodes() map[uint64]*proto.Node {
 
 // TestValidatorPK returns ref validator pk
 func TestValidatorPK() *bls.PublicKey {
-	threshold.Init()
-	ret := &bls.PublicKey{}
-	if err := ret.Deserialize(refPk); err != nil {
-		panic(err)
-	}
-	return ret
+	return refPK
 }
 
 // TestPKs PKS for TestSKs
 func TestPKs() [][]byte {
 	return [][]byte{
-		refSplitSharesPubKeys[0],
-		refSplitSharesPubKeys[1],
-		refSplitSharesPubKeys[2],
-		refSplitSharesPubKeys[3],
+		splitKeys[1].GetPublicKey().Serialize(),
+		splitKeys[2].GetPublicKey().Serialize(),
+		splitKeys[3].GetPublicKey().Serialize(),
+		splitKeys[4].GetPublicKey().Serialize(),
 	}
 }
 
 // TestSKs returns reference test shares for SSV
 func TestSKs() []*bls.SecretKey {
-	threshold.Init()
 	ret := make([]*bls.SecretKey, 4)
 	for i, skByts := range [][]byte{
-		refSplitShares[0],
-		refSplitShares[1],
-		refSplitShares[2],
-		refSplitShares[3],
+		splitKeys[1].Serialize(),
+		splitKeys[2].Serialize(),
+		splitKeys[3].Serialize(),
+		splitKeys[4].Serialize(),
 	} {
 		sk := &bls.SecretKey{}
 		if err := sk.Deserialize(skByts); err != nil {
