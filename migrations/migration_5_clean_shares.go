e9a3c95485298bd9d42e47d1d429ae1a30416882
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2022-04-12 19:36:11 +0300

    include cleaning exporter registry data

--- migrations/migration_5_clean_shares.go
+++ migrations/migration_5_clean_shares.go
@@ -2,22 +2,17 @@ package migrations
 
 import (
 	"context"
-	"github.com/pkg/errors"
 )
 
 var migrationCleanValidatorRegistryData = Migration{
 	Name: "migration_5_clean_validator_registry_data",
 	Run: func(ctx context.Context, opt Options, key []byte) error {
-		nodeStorage := opt.nodeStorage()
-		err := nodeStorage.CleanRegistryData()
-		if err != nil {
-			return errors.Wrap(err, "could not clean node registry data")
-		}
-
-		validatorStorage := opt.validatorStorage()
-		err = validatorStorage.CleanRegistryData()
-		if err != nil {
-			return errors.Wrap(err, "could not clean validator registry data")
+		stores := opt.getRegistryStores()
+		for _, store := range stores {
+			err := store.CleanRegistryData()
+			if err != nil {
+				return err
+			}
 		}
 		return opt.Db.Set(migrationsPrefix, key, migrationCompleted)
 	},
