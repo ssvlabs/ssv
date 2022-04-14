8c7afdbb67546f36e9031e788d58bde562e24986
nivBlox <niv@blox.io>
2022-04-14 16:31:19 +0300

    km

--- beacon/goclient/key_manager.go
+++ beacon/goclient/key_manager.go
@@ -1,7 +1,8 @@
 package goclient
 
 import (
-	"github.com/bloxapp/ssv/ibft/proto"
+	"github.com/bloxapp/ssv/protocol/v1/message"
+
 	"github.com/herumi/bls-eth-go-binary/bls"
 )
 
@@ -9,6 +10,6 @@ func (gc *goClient) AddShare(shareKey *bls.SecretKey) error {
 	return gc.keyManager.AddShare(shareKey)
 }
 
-func (gc *goClient) SignIBFTMessage(message *proto.Message, pk []byte) ([]byte, error) {
+func (gc *goClient) SignIBFTMessage(message *message.ConsensusMessage, pk []byte) ([]byte, error) {
 	return gc.keyManager.SignIBFTMessage(message, pk)
 }
