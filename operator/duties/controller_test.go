3a69f1b89815d91e5dc734432414ddfbebd2d8c9
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2022-04-16 13:34:56 +0400

    use gomock in operator/duties

--- operator/duties/controller_test.go
+++ operator/duties/controller_test.go
@@ -2,40 +2,56 @@ package duties
 
 import (
 	"context"
-	spec "github.com/attestantio/go-eth2-client/spec/phase0"
-	"github.com/bloxapp/eth2-key-manager/core"
-	"github.com/bloxapp/ssv/protocol/v1/blockchain/beacon"
-	types "github.com/prysmaticlabs/eth2-types"
-	"github.com/stretchr/testify/require"
-	"go.uber.org/zap"
 	"sync"
 	"testing"
 	"time"
+
+	spec "github.com/attestantio/go-eth2-client/spec/phase0"
+	"github.com/golang/mock/gomock"
+	types "github.com/prysmaticlabs/eth2-types"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+
+	"github.com/bloxapp/eth2-key-manager/core"
+	"github.com/bloxapp/ssv/operator/duties/mocks"
+	"github.com/bloxapp/ssv/protocol/v1/blockchain/beacon"
 )
 
 func TestDutyController_ListenToTicker(t *testing.T) {
-	f := fetcherMock{}
 	var wg sync.WaitGroup
-	ctrl := &dutyController{
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockExecutor := mocks.NewMockDutyExecutor(mockCtrl)
+	mockExecutor.EXPECT().ExecuteDuty(gomock.Any()).DoAndReturn(func(duty *beacon.Duty) error {
+		require.NotNil(t, duty)
+		require.True(t, duty.Slot > 0)
+		wg.Done()
+		return nil
+	}).AnyTimes()
+
+	mockFetcher := mocks.NewMockDutyFetcher(mockCtrl)
+	mockFetcher.EXPECT().GetDuties(gomock.Any()).DoAndReturn(func(slot uint64) ([]beacon.Duty, error) {
+		return []beacon.Duty{{Slot: spec.Slot(slot), PubKey: spec.BLSPubKey{}}}, nil
+	}).AnyTimes()
+
+	dutyCtrl := &dutyController{
 		logger: zap.L(), ctx: context.Background(), ethNetwork: beacon.NewNetwork(core.PraterNetwork),
-		executor: execWithWaitGroup(t, &wg), fetcher: &f, genesisEpoch: 0, dutyLimit: 32,
+		executor: mockExecutor,
+		fetcher:  mockFetcher,
 	}
+
 	cn := make(chan types.Slot)
 
 	secPerSlot = 2
 	defer func() {
 		secPerSlot = 12
 	}()
-	currentSlot := ctrl.ethNetwork.EstimatedCurrentSlot()
-	duties := map[types.Slot][]beacon.Duty{}
-	duties[currentSlot] = []beacon.Duty{
-		{Slot: spec.Slot(currentSlot), PubKey: spec.BLSPubKey{}},
-	}
-	duties[currentSlot+1] = []beacon.Duty{
-		{Slot: spec.Slot(currentSlot + 1), PubKey: spec.BLSPubKey{}},
-	}
-	f.results = duties
-	go ctrl.listenToTicker(cn)
+
+	currentSlot := dutyCtrl.ethNetwork.EstimatedCurrentSlot()
+
+	go dutyCtrl.listenToTicker(cn)
 	wg.Add(2)
 	go func() {
 		cn <- currentSlot
@@ -66,36 +82,12 @@ func TestDutyController_GetCurrentSlot(t *testing.T) {
 	d := dutyController{logger: zap.L(), ethNetwork: beacon.NewNetwork(core.PraterNetwork)}
 
 	slot := d.ethNetwork.EstimatedCurrentSlot()
-	require.Greater(t, slot, int64(646855))
+	require.Greater(t, slot, types.Slot(646855))
 }
 
 func TestDutyController_GetEpochFirstSlot(t *testing.T) {
 	d := dutyController{logger: zap.L(), ethNetwork: beacon.NewNetwork(core.PraterNetwork)}
 
 	slot := d.getEpochFirstSlot(20203)
-	require.Equal(t, uint64(646496), slot)
-}
-
-type executorMock struct {
-	t  *testing.T
-	wg *sync.WaitGroup
-}
-
-func (e *executorMock) ExecuteDuty(duty *beacon.Duty) error {
-	require.NotNil(e.t, duty)
-	require.True(e.t, duty.Slot > 0)
-	e.wg.Done()
-	return nil
-}
-
-func execWithWaitGroup(t *testing.T, wg *sync.WaitGroup) DutyExecutor {
-	return &executorMock{t, wg}
-}
-
-type fetcherMock struct {
-	results map[types.Slot][]beacon.Duty
-}
-
-func (f *fetcherMock) GetDuties(slot uint64) ([]beacon.Duty, error) {
-	return f.results[types.Slot(slot)], nil
+	require.EqualValues(t, 646496, slot)
 }
