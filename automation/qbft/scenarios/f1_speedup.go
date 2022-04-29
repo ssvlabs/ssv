76be5a47089f8ead93ff2c186d4ef253bc96a6a9
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2022-04-30 00:02:26 +0400

    create f1 speedup scenario

---
+++ automation/qbft/scenarios/f1_speedup.go
@@ -0,0 +1,171 @@
+package scenarios
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/bloxapp/ssv/automation/commons"
+	"github.com/bloxapp/ssv/network"
+	"github.com/bloxapp/ssv/protocol/v1/blockchain/beacon"
+	"github.com/bloxapp/ssv/protocol/v1/message"
+	"github.com/bloxapp/ssv/protocol/v1/qbft/controller"
+	ibftinstance "github.com/bloxapp/ssv/protocol/v1/qbft/instance"
+	"github.com/bloxapp/ssv/protocol/v1/validator"
+	"github.com/herumi/bls-eth-go-binary/bls"
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+)
+
+type f1SpeedupScenario struct {
+	logger     *zap.Logger
+	sks        map[uint64]*bls.SecretKey
+	share      *beacon.Share
+	validators []validator.IValidator
+}
+
+// NewF1SpeedupScenario creates a f1Speedup scenario instance
+func NewF1SpeedupScenario(logger *zap.Logger) Scenario {
+	return &f1SpeedupScenario{logger: logger}
+}
+
+func (r *f1SpeedupScenario) NumOfOperators() int {
+	return 4
+}
+
+func (r *f1SpeedupScenario) NumOfExporters() int {
+	return 0
+}
+
+func (r *f1SpeedupScenario) Name() string {
+	return "f1Speedup"
+}
+
+func (r *f1SpeedupScenario) PreExecution(ctx *ScenarioContext) error {
+	share, sks, validators, err := commons.CreateShareAndValidators(ctx.Ctx, r.logger, ctx.LocalNet, ctx.KeyManagers, ctx.Stores)
+	if err != nil {
+		return errors.Wrap(err, "could not create share")
+	}
+	// save all references
+	r.validators = validators
+	r.sks = sks
+	r.share = share
+
+	routers := make([]*router, r.NumOfOperators())
+
+	loggerFactory := func(who string) *zap.Logger {
+		logger := zap.L().With(zap.String("who", who))
+		return logger
+	}
+
+	for i, node := range ctx.LocalNet.Nodes {
+		routers[i] = &router{
+			logger:      loggerFactory(fmt.Sprintf("msgRouter-%d", i)),
+			controllers: r.validators[i].(*validator.Validator).Ibfts(),
+		}
+		node.UseMessageRouter(routers[i])
+	}
+
+	return nil
+}
+
+func (r *f1SpeedupScenario) Execute(ctx *ScenarioContext) error {
+	if len(r.sks) == 0 || r.share == nil {
+		return errors.New("pre-execution failed")
+	}
+
+	var startErr error
+
+	// init ibfts
+	var wg sync.WaitGroup
+	for i := uint64(1); i <= uint64(3); i++ {
+		if i <= 2 {
+			wg.Add(1)
+			go func(node validator.IValidator, net network.P2PNetwork) {
+				if err := r.initNode(node, net); err != nil {
+					fmt.Printf("error initializing ibft")
+				}
+				wg.Done()
+			}(r.validators[i-1], ctx.LocalNet.Nodes[i-1])
+		} else {
+			go func(node validator.IValidator, net network.P2PNetwork) {
+				time.Sleep(time.Second * 13)
+				if err := r.initNode(node, net); err != nil {
+					fmt.Printf("error initializing ibft")
+				}
+				if err := r.startNode(node); err != nil {
+					fmt.Printf("error starting ibft")
+				}
+			}(r.validators[i-1], ctx.LocalNet.Nodes[i-1])
+		}
+	}
+
+	r.logger.Info("waiting for nodes to init")
+	wg.Wait()
+
+	r.logger.Info("start instances")
+	for i := uint64(1); i <= uint64(r.NumOfOperators()); i++ {
+		wg.Add(1)
+		go func(node validator.IValidator) {
+			defer wg.Done()
+			if err := r.startNode(node); err != nil {
+				fmt.Printf("error starting ibft")
+			}
+		}(r.validators[i-1])
+	}
+
+	return startErr
+}
+
+func (r *f1SpeedupScenario) PostExecution(ctx *ScenarioContext) error {
+	for i := range ctx.Stores {
+		msgs, err := ctx.Stores[i].GetDecided(message.NewIdentifier(r.share.PublicKey.Serialize(), message.RoleTypeAttester), message.Height(0), message.Height(0))
+		if err != nil {
+			return err
+		}
+		if len(msgs) < 3 {
+			return fmt.Errorf("node-%d didn't sync all messages", i)
+		}
+	}
+
+	return nil
+}
+
+func (r *f1SpeedupScenario) initNode(val validator.IValidator, net network.P2PNetwork) error {
+	if err := net.Subscribe(val.GetShare().PublicKey.Serialize()); err != nil {
+		return errors.Wrap(err, "failed to subscribe topic")
+	}
+
+	ibftc := val.(*validator.Validator).Ibfts()[message.RoleTypeAttester]
+
+	if err := ibftc.Init(); err != nil {
+		if err == controller.ErrAlreadyRunning {
+			r.logger.Debug("ibft init is already running")
+			return nil
+		}
+		r.logger.Error("could not initialize ibft instance", zap.Error(err))
+		return err
+	}
+
+	return nil
+}
+
+func (r *f1SpeedupScenario) startNode(val validator.IValidator) error {
+	ibftc := val.(*validator.Validator).Ibfts()[message.RoleTypeAttester]
+
+	res, err := ibftc.StartInstance(ibftinstance.ControllerStartInstanceOptions{
+		Logger:    r.logger,
+		SeqNumber: 1,
+		Value:     []byte("value"),
+	})
+
+	if err != nil {
+		return err
+	} else if !res.Decided {
+		return errors.New("instance could not decide")
+	} else {
+		r.logger.Info("decided with value", zap.String("decided value", string(res.Msg.Message.Data)))
+	}
+
+	return nil
+}
