34383a07eb1f8cd8c510b32c7ac40bf6667715ac
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2022-05-05 22:11:39 +0400

    fix qbft/instance/msgcont/inmem

--- protocol/v1/qbft/instance/msgcont/inmem/inmem_test.go
+++ protocol/v1/qbft/instance/msgcont/inmem/inmem_test.go
@@ -2,11 +2,15 @@ package inmem
 
 import (
 	"encoding/json"
+	"testing"
+
 	"github.com/bloxapp/ssv/ibft/proto"
 	"github.com/bloxapp/ssv/network"
+	v0 "github.com/bloxapp/ssv/network/forks/v0"
+	"github.com/bloxapp/ssv/protocol/v1/message"
+
 	"github.com/herumi/bls-eth-go-binary/bls"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 func changeRoundDataToBytes(input *proto.ChangeRoundData) []byte {
@@ -126,7 +130,9 @@ func TestFindPartialChangeRound(t *testing.T) {
 		t.Run(test.name, func(tt *testing.T) {
 			c := New(3, 2)
 			for _, msg := range test.msgs {
-				c.AddMessage(msg.SignedMessage, nil)
+				v1, err := v0.ToSignedMessageV1(msg.SignedMessage)
+				require.NoError(tt, err)
+				c.AddMessage(v1, nil)
 			}
 
 			found, lowest := c.PartialChangeRoundQuorum(1)
@@ -138,72 +144,72 @@ func TestFindPartialChangeRound(t *testing.T) {
 
 func TestMessagesContainer_OverrideMessages(t *testing.T) {
 	c := New(3, 2)
-	c.AddMessage(&proto.SignedMessage{
-		Message: &proto.Message{
-			Round:  1,
-			Lambda: nil,
-			Value:  []byte{1, 1, 1, 1},
+	c.AddMessage(&message.SignedMessage{
+		Message: &message.ConsensusMessage{
+			Round:      1,
+			Identifier: nil,
+			Data:       []byte{1, 1, 1, 1},
 		},
 		Signature: nil,
-		SignerIds: []uint64{1, 4},
+		Signers:   []message.OperatorID{1, 4},
 	}, nil)
-	c.AddMessage(&proto.SignedMessage{
-		Message: &proto.Message{
-			Round:  1,
-			Lambda: nil,
-			Value:  []byte{1, 1, 1, 1},
+	c.AddMessage(&message.SignedMessage{
+		Message: &message.ConsensusMessage{
+			Round:      1,
+			Identifier: nil,
+			Data:       []byte{1, 1, 1, 1},
 		},
 		Signature: nil,
-		SignerIds: []uint64{2, 3},
+		Signers:   []message.OperatorID{2, 3},
 	}, nil)
 
-	c.OverrideMessages(&proto.SignedMessage{
-		Message: &proto.Message{
-			Round:  1,
-			Lambda: nil,
-			Value:  []byte{1, 1, 1, 1},
+	c.OverrideMessages(&message.SignedMessage{
+		Message: &message.ConsensusMessage{
+			Round:      1,
+			Identifier: nil,
+			Data:       []byte{1, 1, 1, 1},
 		},
 		Signature: nil,
-		SignerIds: []uint64{1},
+		Signers:   []message.OperatorID{1},
 	}, nil)
 	require.Len(t, c.ReadOnlyMessagesByRound(1), 1)
 }
 
 func TestMessagesContainer_AddMessage(t *testing.T) {
 	c := New(3, 2)
-	c.AddMessage(&proto.SignedMessage{
-		Message: &proto.Message{
-			Round:  1,
-			Lambda: nil,
-			Value:  []byte{1, 1, 1, 1},
+	c.AddMessage(&message.SignedMessage{
+		Message: &message.ConsensusMessage{
+			Round:      1,
+			Identifier: nil,
+			Data:       []byte{1, 1, 1, 1},
 		},
 		Signature: nil,
-		SignerIds: []uint64{1, 2, 3, 4},
+		Signers:   []message.OperatorID{1, 2, 3, 4},
 	}, nil)
 
 	require.Len(t, c.ReadOnlyMessagesByRound(1), 1)
 	require.Len(t, c.ReadOnlyMessagesByRound(2), 0)
 
 	// try to add duplicate
-	c.AddMessage(&proto.SignedMessage{
-		Message: &proto.Message{
-			Round:  1,
-			Lambda: nil,
-			Value:  []byte{1, 1, 1, 1},
+	c.AddMessage(&message.SignedMessage{
+		Message: &message.ConsensusMessage{
+			Round:      1,
+			Identifier: nil,
+			Data:       []byte{1, 1, 1, 1},
 		},
 		Signature: nil,
-		SignerIds: []uint64{4, 5},
+		Signers:   []message.OperatorID{4, 5},
 	}, nil)
 	require.Len(t, c.ReadOnlyMessagesByRound(1), 1)
 	require.Len(t, c.ReadOnlyMessagesByRound(2), 0)
-	c.AddMessage(&proto.SignedMessage{
-		Message: &proto.Message{
-			Round:  1,
-			Lambda: nil,
-			Value:  []byte{1, 1, 1, 1},
+	c.AddMessage(&message.SignedMessage{
+		Message: &message.ConsensusMessage{
+			Round:      1,
+			Identifier: nil,
+			Data:       []byte{1, 1, 1, 1},
 		},
 		Signature: nil,
-		SignerIds: []uint64{4},
+		Signers:   []message.OperatorID{4},
 	}, nil)
 	require.Len(t, c.ReadOnlyMessagesByRound(1), 1)
 	require.Len(t, c.ReadOnlyMessagesByRound(2), 0)
@@ -211,45 +217,45 @@ func TestMessagesContainer_AddMessage(t *testing.T) {
 
 func TestMessagesContainer_ReadOnlyMessagesByRound(t *testing.T) {
 	c := New(3, 2)
-	c.AddMessage(&proto.SignedMessage{
-		Message: &proto.Message{
-			Round:  1,
-			Lambda: nil,
-			Value:  []byte{1, 1, 1, 1},
+	c.AddMessage(&message.SignedMessage{
+		Message: &message.ConsensusMessage{
+			Round:      1,
+			Identifier: nil,
+			Data:       []byte{1, 1, 1, 1},
 		},
 		Signature: nil,
-		SignerIds: []uint64{1, 2, 3, 4},
+		Signers:   []message.OperatorID{1, 2, 3, 4},
 	}, nil)
-	c.AddMessage(&proto.SignedMessage{
-		Message: &proto.Message{
-			Round:  1,
-			Lambda: nil,
-			Value:  []byte{1, 1, 1, 1},
+	c.AddMessage(&message.SignedMessage{
+		Message: &message.ConsensusMessage{
+			Round:      1,
+			Identifier: nil,
+			Data:       []byte{1, 1, 1, 1},
 		},
 		Signature: nil,
-		SignerIds: []uint64{5},
+		Signers:   []message.OperatorID{5},
 	}, nil)
 
 	msgs := c.ReadOnlyMessagesByRound(1)
 	require.EqualValues(t, 1, msgs[0].Message.Round)
 	require.EqualValues(t, 1, msgs[1].Message.Round)
-	require.EqualValues(t, []byte{1, 1, 1, 1}, msgs[0].Message.Value)
-	require.EqualValues(t, []byte{1, 1, 1, 1}, msgs[1].Message.Value)
-	require.EqualValues(t, []uint64{1, 2, 3, 4}, msgs[0].SignerIds)
-	require.EqualValues(t, []uint64{5}, msgs[1].SignerIds)
+	require.EqualValues(t, []byte{1, 1, 1, 1}, msgs[0].Message.Data)
+	require.EqualValues(t, []byte{1, 1, 1, 1}, msgs[1].Message.Data)
+	require.EqualValues(t, []message.OperatorID{1, 2, 3, 4}, msgs[0].Signers)
+	require.EqualValues(t, []message.OperatorID{5}, msgs[1].Signers)
 }
 
 func TestMessagesContainer_QuorumAchieved(t *testing.T) {
 	c := New(3, 2)
-	c.AddMessage(&proto.SignedMessage{
-		Message: &proto.Message{
-			Round:  1,
-			Lambda: nil,
-			Value:  []byte{1, 1, 1, 1},
+	c.AddMessage(&message.SignedMessage{
+		Message: &message.ConsensusMessage{
+			Round:      1,
+			Identifier: nil,
+			Data:       []byte{1, 1, 1, 1},
 		},
 		Signature: nil,
-		SignerIds: []uint64{1, 2, 3},
-	}, nil)
+		Signers:   []message.OperatorID{1, 2, 3},
+	}, []byte{1, 1, 1, 1})
 	res, _ := c.QuorumAchieved(1, []byte{1, 1, 1, 1})
 	require.True(t, res)
 	res, _ = c.QuorumAchieved(0, []byte{1, 1, 1, 1})
@@ -257,26 +263,26 @@ func TestMessagesContainer_QuorumAchieved(t *testing.T) {
 	res, _ = c.QuorumAchieved(1, []byte{1, 1, 1, 0})
 	require.False(t, res)
 
-	c.AddMessage(&proto.SignedMessage{
-		Message: &proto.Message{
-			Round:  2,
-			Lambda: nil,
-			Value:  []byte{1, 1, 1, 1},
+	c.AddMessage(&message.SignedMessage{
+		Message: &message.ConsensusMessage{
+			Round:      2,
+			Identifier: nil,
+			Data:       []byte{1, 1, 1, 1},
 		},
 		Signature: nil,
-		SignerIds: []uint64{1, 2},
-	}, nil)
+		Signers:   []message.OperatorID{1, 2},
+	}, []byte{1, 1, 1, 1})
 	res, _ = c.QuorumAchieved(2, []byte{1, 1, 1, 1})
 	require.False(t, res)
-	c.AddMessage(&proto.SignedMessage{
-		Message: &proto.Message{
-			Round:  2,
-			Lambda: nil,
-			Value:  []byte{1, 1, 1, 1},
+	c.AddMessage(&message.SignedMessage{
+		Message: &message.ConsensusMessage{
+			Round:      2,
+			Identifier: nil,
+			Data:       []byte{1, 1, 1, 1},
 		},
 		Signature: nil,
-		SignerIds: []uint64{3},
-	}, nil)
+		Signers:   []message.OperatorID{3},
+	}, []byte{1, 1, 1, 1})
 	res, _ = c.QuorumAchieved(2, []byte{1, 1, 1, 1})
 	require.True(t, res)
 	res, _ = c.QuorumAchieved(3, []byte{1, 1, 1, 1})
