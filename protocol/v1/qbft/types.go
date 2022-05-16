51fcddc0d9cc8cb242b6268d21d59a91bee90a2a
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2022-05-17 00:22:40 +0400

    avoid panic if state is not initialized

--- protocol/v1/qbft/types.go
+++ protocol/v1/qbft/types.go
@@ -2,8 +2,10 @@ package qbft
 
 import (
 	"encoding/json"
-	"github.com/bloxapp/ssv/protocol/v1/message"
+
 	"go.uber.org/atomic"
+
+	"github.com/bloxapp/ssv/protocol/v1/message"
 )
 
 type RoundState int32
@@ -93,8 +95,11 @@ func (s *State) UnmarshalJSON(data []byte) error {
 }
 
 func (s *State) GetHeight() message.Height {
-	height := s.Height.Load().(message.Height)
-	return height
+	if height, ok := s.Height.Load().(message.Height); ok {
+		return height
+	}
+
+	return message.Height(0)
 }
 
 func NewHeight(height message.Height) atomic.Value {
@@ -104,8 +109,10 @@ func NewHeight(height message.Height) atomic.Value {
 }
 
 func (s *State) GetRound() message.Round {
-	round := s.Round.Load().(message.Round)
-	return round
+	if round, ok := s.Round.Load().(message.Round); ok {
+		return round
+	}
+	return message.Round(0)
 }
 
 func NewRound(round message.Round) atomic.Value {
@@ -115,8 +122,11 @@ func NewRound(round message.Round) atomic.Value {
 }
 
 func (s *State) GetPreparedRound() message.Round {
-	round := s.PreparedRound.Load().(message.Round)
-	return round
+	if round, ok := s.PreparedRound.Load().(message.Round); ok {
+		return round
+	}
+
+	return message.Round(0)
 }
 
 func (s *State) SetRound(newRound message.Round) {
@@ -124,18 +134,26 @@ func (s *State) SetRound(newRound message.Round) {
 }
 
 func (s *State) GetIdentifier() message.Identifier {
-	identifier := s.Identifier.Load().(message.Identifier)
-	return identifier
+	if identifier, ok := s.Identifier.Load().(message.Identifier); ok {
+		return identifier
+	}
+
+	return nil
 }
 
 func (s *State) GetInputValue() []byte {
-	inputValue := s.InputValue.Load().([]byte)
-	return inputValue
+	if inputValue, ok := s.InputValue.Load().([]byte); ok {
+		return inputValue
+	}
+	return nil
 }
 
 func (s *State) GetPreparedValue() []byte {
-	value := s.PreparedValue.Load().([]byte)
-	return value
+	if value, ok := s.PreparedValue.Load().([]byte); ok {
+		return value
+	}
+
+	return nil
 }
 
 // InstanceConfig is the configuration of the instance
