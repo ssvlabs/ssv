705df41559748ca52635c1238508ef5fb7e66363
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2022-05-16 21:57:26 +0400

    fix protocol/v1/validator/validator_test.go

--- protocol/v1/validator/validator_test.go
+++ protocol/v1/validator/validator_test.go
@@ -1,13 +1,23 @@
 package validator
 
-//
-//import (
-//	"github.com/stretchr/testify/require"
-//	"testing"
-//)
-//
-//func TestIdentifierTest(t *testing.T) {
-//	node := testingValidator(t, true, 4, []byte{1, 2, 3, 4})
-//	require.True(t, node.oneOfIBFTIdentifiers([]byte{1, 2, 3, 4}))
-//	require.False(t, node.oneOfIBFTIdentifiers([]byte{1, 2, 3, 3}))
-//}
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIdentifierTest(t *testing.T) {
+	node := testingValidator(t, true, 4, []byte{1, 2, 3, 4})
+	require.True(t, oneOfIBFTIdentifiers(node, []byte{1, 2, 3, 4}))
+	require.False(t, oneOfIBFTIdentifiers(node, []byte{1, 2, 3, 3}))
+}
+
+func oneOfIBFTIdentifiers(v *Validator, toMatch []byte) bool {
+	for _, i := range v.ibfts {
+		if bytes.Equal(i.GetIdentifier(), toMatch) {
+			return true
+		}
+	}
+	return false
+}
