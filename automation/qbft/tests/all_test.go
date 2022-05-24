59c8d0bcef7b03bc6c586e2abd6f2d8cd0293cc8
Amir Yahalom <amir@blox.io>
2022-05-24 17:08:17 +0300

    added qbft simulation tests

---
+++ automation/qbft/tests/all_test.go
@@ -0,0 +1,21 @@
+package tests
+
+import (
+	"github.com/bloxapp/ssv/automation/qbft/runner"
+	"github.com/bloxapp/ssv/automation/qbft/scenarios"
+	"github.com/bloxapp/ssv/utils/logex"
+	"go.uber.org/zap/zapcore"
+	"testing"
+)
+
+func Test_Automation_QBFTScenarios(t *testing.T) {
+	logger := logex.Build("simulation", zapcore.DebugLevel, nil)
+	scenariosToRun := []string{
+		scenarios.OnForkV1Scenario,
+	}
+
+	for _, s := range scenariosToRun {
+		scenario := scenarios.NewScenario(s, logger)
+		runner.Start(logger, scenario, runner.QBFTScenarioBootstrapper())
+	}
+}
