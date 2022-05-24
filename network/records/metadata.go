af972d710a2ac8fb173bddfe6b25026d3133b635
Amir Yahalom <amir@blox.io>
2022-05-24 12:53:28 +0300

    node info (WIP)
    node info will replace `Identity` as the struct
    we use for handshaking with other peers.
    
    changes:
    
    - added node info record
    (based on github.com/libp2p/go-libp2p-core/record)
    - added metadata

---
+++ network/records/metadata.go
@@ -0,0 +1,65 @@
+package records
+
+import (
+	"encoding/json"
+	"github.com/pkg/errors"
+)
+
+// NodeMetadata holds node's general information
+type NodeMetadata struct {
+	// NodeVersion is the ssv-node version, it is a required field
+	NodeVersion string
+	// ExecutionNode is the "name/version" of the eth1 node
+	ExecutionNode string
+	// ConsensusNode is the "name/version" of the beacon node
+	ConsensusNode string
+	// OperatorID holds a hash of the operator public key, based on operator key
+	OperatorID string
+}
+
+// Encode encodes the metadata into bytes
+// TODO: switch to SSZ
+func (nm *NodeMetadata) Encode() ([]byte, error) {
+	ser := newSerializable(
+		nm.NodeVersion,
+		nm.ConsensusNode,
+		nm.ExecutionNode,
+		nm.OperatorID,
+	)
+
+	return json.Marshal(ser)
+}
+
+// Decode decodes a raw payload into metadata
+// TODO: switch to SSZ
+func (nm *NodeMetadata) Decode(data []byte) error {
+	var ser serializable
+
+	if err := json.Unmarshal(data, &ser); err != nil {
+		return err
+	}
+
+	nm.NodeVersion = ""
+	nm.ConsensusNode = ""
+	nm.ExecutionNode = ""
+	nm.OperatorID = ""
+
+	if len(ser.Entries) < 1 {
+		return errors.New("not enough entries in node metadata, node version is required")
+	}
+	nm.NodeVersion = ser.Entries[0]
+	if len(ser.Entries) < 2 {
+		return nil
+	}
+	nm.ConsensusNode = ser.Entries[1]
+	if len(ser.Entries) < 3 {
+		return nil
+	}
+	nm.ExecutionNode = ser.Entries[2]
+	if len(ser.Entries) < 4 {
+		return nil
+	}
+	nm.OperatorID = ser.Entries[3]
+
+	return nil
+}
