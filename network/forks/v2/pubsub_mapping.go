6e00e1b3e852364d1df7faebda80f6cde7114fae
nivBlox <niv@blox.io>
2022-06-16 13:59:49 +0300

    fix lint

--- network/forks/v2/pubsub_mapping.go
+++ network/forks/v2/pubsub_mapping.go
@@ -4,11 +4,15 @@ import (
 	"encoding/hex"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 const (
 	// UnknownSubnet is used when a validator public key is invalid
 	UnknownSubnet = "unknown"
+	decidedTopic  = "decided"
+
+	topicPrefix = "ssv.v1"
 )
 
 // SubnetsCount returns the subnet count for v1
@@ -19,6 +23,21 @@ func (v2 *ForkV2) ValidatorTopicID(pkByts []byte) []string {
 	pkHex := hex.EncodeToString(pkByts)
 	subnet := validatorSubnet(pkHex)
 	return []string{topicOf(subnet)}
+}
+
+// GetTopicFullName returns the topic full name, including prefix
+func (v2 *ForkV2) GetTopicFullName(baseName string) string {
+	return fmt.Sprintf("%s.%s", topicPrefix, baseName)
+}
+
+// GetTopicBaseName return the base topic name of the topic, w/o ssv prefix
+func (v2 *ForkV2) GetTopicBaseName(topicName string) string {
+	return strings.Replace(topicName, fmt.Sprintf("%s.", topicPrefix), "", 1)
+}
+
+// DecidedTopic returns decided topic name for v1
+func (v2 *ForkV2) DecidedTopic() string {
+	return decidedTopic
 }
 
 // topicOf returns the topic for the given subnet
