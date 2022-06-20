afe710d876fef341547d524e43db690555d6ccc2
Amir Yahalom <amir@blox.io>
2022-06-20 15:32:32 +0300

    bind new decided handler

--- cli/operator/node.go
+++ cli/operator/node.go
@@ -3,6 +3,8 @@ package operator
 import (
 	"context"
 	"fmt"
+	"github.com/bloxapp/ssv/exporter/api"
+	"github.com/bloxapp/ssv/protocol/v1/message"
 	"log"
 	"net/http"
 	"time"
@@ -18,7 +20,6 @@ import (
 	global_config "github.com/bloxapp/ssv/cli/config"
 	"github.com/bloxapp/ssv/eth1"
 	"github.com/bloxapp/ssv/eth1/goeth"
-	"github.com/bloxapp/ssv/exporter/api"
 	ssv_identity "github.com/bloxapp/ssv/identity"
 	"github.com/bloxapp/ssv/migrations"
 	"github.com/bloxapp/ssv/monitoring/metrics"
@@ -203,13 +204,17 @@ var StartNodeCmd = &cobra.Command{
 			Logger.Fatal("failed to create eth1 client", zap.Error(err))
 		}
 
+		if cfg.WsAPIPort != 0 {
+			ws := api.NewWsServer(cmd.Context(), Logger, nil, http.NewServeMux(), cfg.WithPing)
+			cfg.SSVOptions.WS = ws
+			cfg.SSVOptions.WsAPIPort = cfg.WsAPIPort
+			cfg.SSVOptions.ValidatorOptions.NewDecidedHandler = func(msg *message.SignedMessage) {
+				ws.BroadcastFeed().Send(api.NewDecidedAPIMsg(msg))
+			}
+		}
+
 		validatorCtrl := validator.NewController(cfg.SSVOptions.ValidatorOptions)
 		cfg.SSVOptions.ValidatorController = validatorCtrl
-
-		if cfg.WsAPIPort != 0 {
-			cfg.SSVOptions.WS = api.NewWsServer(cmd.Context(), Logger, nil, http.NewServeMux(), cfg.WithPing)
-			cfg.SSVOptions.WsAPIPort = cfg.WsAPIPort
-		}
 
 		operatorNode = operator.New(cfg.SSVOptions)
 
--- exporter/api/msg.go
+++ exporter/api/msg.go
@@ -1,5 +1,10 @@
 package api
 
+import (
+	"encoding/hex"
+	"github.com/bloxapp/ssv/protocol/v1/message"
+)
+
 // Message represents an exporter message
 type Message struct {
 	// Type is the type of message
@@ -8,6 +13,20 @@ type Message struct {
 	Filter MessageFilter `json:"filter"`
 	// Values holds the results, optional as it's relevant for response
 	Data interface{} `json:"data,omitempty"`
+}
+
+// NewDecidedAPIMsg creates a new message from the given message
+func NewDecidedAPIMsg(msg *message.SignedMessage) Message {
+	return Message{
+		Type: TypeDecided,
+		Filter: MessageFilter{
+			PublicKey: hex.EncodeToString(msg.Message.Identifier.GetValidatorPK()),
+			From:      uint64(msg.Message.Height),
+			To:        uint64(msg.Message.Height),
+			Role:      DutyRole(msg.Message.Identifier.GetRoleType().String()),
+		},
+		Data: []*message.SignedMessage{msg},
+	}
 }
 
 // MessageFilter is a criteria for query in request messages and projection in responses
