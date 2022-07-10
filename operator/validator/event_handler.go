c2915ee594aa0e8554033a9fedd57661591ea80c
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2022-07-10 19:46:38 +0300

    fix operator remove handle event

--- operator/validator/event_handler.go
+++ operator/validator/event_handler.go
@@ -159,7 +159,7 @@ func (c *controller) handleOperatorRemovedEvent(
 	}
 	if !found {
 		return nil, &abiparser.MalformedEventError{
-			Err: errors.New("could not find operator's data"),
+			Err: errors.New("could not find operator data"),
 		}
 	}
 
@@ -169,7 +169,7 @@ func (c *controller) handleOperatorRemovedEvent(
 		}
 	}
 
-	shares, err := c.collection.GetOperatorValidatorShares(c.operatorPubKey, false)
+	shares, err := c.collection.GetOperatorValidatorShares(od.PublicKey, false)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not get all operator validator shares")
 	}
@@ -260,6 +260,7 @@ func (c *controller) handleAccountEnabledEvent(
 
 	for _, share := range shares {
 		if share.IsOperatorShare(c.operatorPubKey) {
+			operatorSharePubKeys = append(operatorSharePubKeys, share.PublicKey.SerializeToHexStr())
 			share.Liquidated = false
 
 			// save validator data
