a2abd750f32bca6afe15e4078126f1bbedcf8e33
Amir Yahalom <amir@blox.io>
2022-07-11 17:24:01 +0300

    modify queues size

--- network/topics/pubsub.go
+++ network/topics/pubsub.go
@@ -22,9 +22,9 @@ const (
 // the following are kept in vars to allow flexibility (e.g. in tests)
 var (
 	// validationQueueSize is the size that we assign to the validation queue
-	validationQueueSize = 512
+	validationQueueSize = 256
 	// outboundQueueSize is the size that we assign to the outbound message queue
-	outboundQueueSize = 32
+	outboundQueueSize = 128
 	// scoreInspectInterval is the interval for performing score inspect, which goes over all peers scores
 	scoreInspectInterval = time.Minute
 )
