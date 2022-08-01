8e3301be05348dd188b23dd3ca42128980df6957
amir-blox <83904651+amir-blox@users.noreply.github.com>
2022-08-01 13:48:11 +0300

    Network Fixes (#653)
    * created ID Service only if it wasn't created by libp2p Host
    
    * increased gossip degree parameters for better propagation of message
    
    * modified iwant/ihave cache
    
    * increased `validateThrottle` (4096)
    
    * modified subscriptions limit (128 subnets + 1 decided topic)

--- network/topics/gossipsub_params.go
+++ network/topics/gossipsub_params.go
@@ -7,22 +7,22 @@ import (
 
 var (
 	// gsD topic stable mesh target count
-	gsD = 5
+	gsD = 8
 	// gsDlo topic stable mesh low watermark
-	gsDlo = 3
+	gsDlo = 6
 	// gsDhi topic stable mesh high watermark
-	gsDhi = 9
+	gsDhi = 12
 
 	// gsMaxIHaveLength is max number for ihave messages to send
 	// decreased the default (5000) to avoid ihave floods
-	gsMaxIHaveLength = 1000
+	gsMaxIHaveLength = 1500
 	// gsMaxIHaveMessages is the max number for ihave message to accept from a peer within a heartbeat
 	// increased default (10) to avoid multiple ihave messages
 	gsMaxIHaveMessages = 32
 	// gsMcacheLen number of windows to retain full messages in cache for `IWANT` responses
-	gsMcacheLen = 100
+	gsMcacheLen = 6
 	// gsMcacheGossip number of windows to gossip about
-	gsMcacheGossip = 3
+	gsMcacheGossip = 4
 
 	// heartbeat interval frequency of heartbeat, milliseconds
 	gsHeartbeatInterval = 700 * time.Millisecond
