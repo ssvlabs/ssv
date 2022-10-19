8becd3cf39944f8d8624e96fbe2c75405a515a6d
Amir Yahalom <amir@blox.io>
2022-10-19 16:52:01 +0300

    added adapter for network (broadcast and sync)

---
+++ protocol/v2/ssv/validator/spec_adapters.go
@@ -0,0 +1,100 @@
+package ssv
+
+import (
+	"fmt"
+	qbft2 "github.com/bloxapp/ssv-spec/qbft"
+	"github.com/bloxapp/ssv-spec/ssv"
+	"github.com/bloxapp/ssv-spec/types"
+	protcolp2p "github.com/bloxapp/ssv/protocol/v1/p2p"
+	"github.com/patrickmn/go-cache"
+	"github.com/pkg/errors"
+	"time"
+)
+
+func newNetworkAdapter(net protcolp2p.Network) *networkAdapter {
+	return &networkAdapter{broadcaster: net, syncer: net, results: cache.New(time.Minute*10, time.Minute*12)}
+}
+
+func NewSSVNetworkAdapter(net protcolp2p.Network) ssv.Network {
+	return newNetworkAdapter(net)
+}
+
+func NewQBFTNetworkAdapter(net protcolp2p.Network) qbft2.Network {
+	return newNetworkAdapter(net)
+}
+
+type networkAdapter struct {
+	broadcaster protcolp2p.Broadcaster
+	syncer protcolp2p.Syncer
+
+	results *cache.Cache
+}
+
+type SyncResults []protcolp2p.SyncResult
+
+func (na *networkAdapter) SyncHighestDecided(identifier []byte) error {
+	mid := types.MessageID{}
+	copy(mid[:], identifier)
+	res, err := na.syncer.LastDecided(mid)
+	if err != nil {
+		return err
+	}
+
+	k := fmt.Sprintf("sync-%x", identifier)
+	if _, ok := na.results.Get(k); !ok {
+		na.results.SetDefault(k, res)
+	}
+	return nil
+}
+
+func (na *networkAdapter) SyncHighestRoundChange(identifier []byte, height qbft2.Height) error {
+	mid := types.MessageID{}
+	copy(mid[:], identifier)
+
+	res, err := na.syncer.LastChangeRound(mid, height)
+	if err != nil {
+		return err
+	}
+
+	k := fmt.Sprintf("sync-cr-%x", identifier)
+	if _, ok := na.results.Get(k); !ok {
+		na.results.SetDefault(k, res)
+	}
+	return nil
+}
+
+func (na *networkAdapter) Broadcast(msg types.Encoder) error {
+	m, ok := msg.(*types.SSVMessage)
+	if !ok {
+		return errors.New("invalid message structure")
+	}
+	if m == nil {
+		return errors.New("empty message")
+	}
+	return na.broadcaster.Broadcast(*m)
+}
+
+func (na *networkAdapter) BroadcastDecided(msg types.Encoder) error {
+	return na.Broadcast(msg)
+}
+
+func (na *networkAdapter) GetDecidedResults(identifier []byte) SyncResults {
+	return na.getResults("sync", identifier)
+}
+
+func (na *networkAdapter) GetChangeRoundResults(identifier []byte) SyncResults {
+	return na.getResults("sync-cr", identifier)
+}
+
+func (na *networkAdapter) getResults(prefix string, identifier []byte) SyncResults {
+	k := fmt.Sprintf("%s-%x", prefix, identifier)
+	res, ok := na.results.Get(k)
+	if !ok {
+		return nil
+	}
+	results, ok := res.(SyncResults)
+	if !ok {
+		return nil
+	}
+	return results
+}
