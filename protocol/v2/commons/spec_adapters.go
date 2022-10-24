d74cc70bd69f5237f82dfbe28e23a79fd3cfdf3f
Amir Yahalom <amir@blox.io>
2022-10-24 17:11:22 +0300

    enhance network adapter

--- protocol/v2/commons/spec_adapters.go
+++ protocol/v2/commons/spec_adapters.go
@@ -11,13 +11,14 @@ import (
 	"github.com/bloxapp/ssv/protocol/v1/blockchain/beacon"
 	protcolp2p "github.com/bloxapp/ssv/protocol/v1/p2p"
 	qbftstorage "github.com/bloxapp/ssv/protocol/v1/qbft/storage"
+	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/patrickmn/go-cache"
 	"github.com/pkg/errors"
 	"time"
 )
 
 func newNetworkAdapter(net protcolp2p.Network) *networkAdapter {
-	return &networkAdapter{broadcaster: net, syncer: net, results: cache.New(time.Minute*10, time.Minute*12)}
+	return &networkAdapter{subscriber: net, broadcaster: net, syncer: net, results: cache.New(time.Minute*10, time.Minute*12)}
 }
 
 func NewSSVNetworkAdapter(net protcolp2p.Network) ssv.Network {
@@ -29,6 +30,7 @@ func NewQBFTNetworkAdapter(net protcolp2p.Network) qbft2.Network {
 }
 
 type networkAdapter struct {
+	subscriber 	protcolp2p.Subscriber
 	broadcaster protcolp2p.Broadcaster
 	syncer      protcolp2p.Syncer
 
@@ -81,6 +83,18 @@ func (na *networkAdapter) Broadcast(msg types.Encoder) error {
 
 func (na *networkAdapter) BroadcastDecided(msg types.Encoder) error {
 	return na.Broadcast(msg)
+}
+
+func (na *networkAdapter) Subscribe(pk types.ValidatorPK) error {
+	return na.subscriber.Subscribe(pk)
+}
+
+func (na *networkAdapter) Unsubscribe(pk types.ValidatorPK) error {
+	return na.subscriber.Unsubscribe(pk)
+}
+
+func (na *networkAdapter) Peers(pk types.ValidatorPK) ([]peer.ID, error) {
+	return na.subscriber.Peers(pk)
 }
 
 func (na *networkAdapter) GetDecidedResults(identifier []byte) SyncResults {
