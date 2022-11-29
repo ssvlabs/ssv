c7d80065c715a3f152c438aa966cd86d7293d5e1
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2022-11-30 00:16:33 +0300

    Fix test links

--- protocol/v2/qbft/spectest/qbft_mapping_test.go
+++ protocol/v2/qbft/spectest/qbft_mapping_test.go
@@ -29,7 +29,7 @@ func TestQBFTMapping(t *testing.T) {
 	filePath := path + "/" + fileName
 	jsonTests, err := os.ReadFile(filePath)
 	if err != nil {
-		resp, err := http.Get("https://raw.githubusercontent.com/bloxapp/ssv-spec/main/qbft/spectest/generate/tests.json")
+		resp, err := http.Get("https://raw.githubusercontent.com/bloxapp/ssv-spec/7e96c8b781915faaa12d29eba94e702445bd5945/qbft/spectest/generate/tests.json")
 		require.NoError(t, err)
 
 		defer func() {
