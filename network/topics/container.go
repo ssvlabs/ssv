461249dd0285d4869968d4841ebac97691183d3a
Amir Yahalom <amir@blox.io>
2022-12-19 15:04:38 +0200

    pubsub: refactor topic container

---
+++ network/topics/container.go
@@ -0,0 +1,102 @@
+package topics
+
+import (
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"sync"
+)
+
+type onTopicJoined func(ps *pubsub.PubSub, topic *pubsub.Topic)
+
+type topicsContainer struct {
+	ps *pubsub.PubSub
+
+	onTopicJoined onTopicJoined
+
+	subLock *sync.RWMutex
+	lock    *sync.RWMutex
+	topics  map[string]*pubsub.Topic
+	subs    map[string]*pubsub.Subscription
+}
+
+func newTopicsContainer(ps *pubsub.PubSub, onTopicJoined onTopicJoined) *topicsContainer {
+	return &topicsContainer{
+		ps:            ps,
+		onTopicJoined: onTopicJoined,
+		subLock:       &sync.RWMutex{},
+		lock:          &sync.RWMutex{},
+		topics:        make(map[string]*pubsub.Topic),
+		subs:          make(map[string]*pubsub.Subscription),
+	}
+}
+
+func (tc *topicsContainer) Get(name string) *pubsub.Topic {
+	topic, _ := tc.Join(name)
+	return topic
+}
+
+func (tc *topicsContainer) Leave(name string) error {
+	tc.lock.Lock()
+	topic, ok := tc.topics[name]
+	if ok {
+		delete(tc.topics, name)
+	}
+	tc.lock.Unlock()
+	if topic == nil {
+		return nil
+	}
+
+	return topic.Close()
+}
+
+func (tc *topicsContainer) Join(name string, opts ...pubsub.TopicOpt) (*pubsub.Topic, error) {
+	tc.lock.Lock()
+	defer tc.lock.Unlock()
+
+	topic, ok := tc.topics[name]
+	if !ok {
+		t, err := tc.ps.Join(name, opts...)
+		if err != nil {
+			return nil, err
+		}
+		tc.onTopicJoined(tc.ps, t)
+		topic = t
+		tc.topics[name] = t
+	}
+
+	return topic, nil
+}
+
+func (tc *topicsContainer) Unsubscribe(name string) {
+	tc.subLock.Lock()
+	defer tc.subLock.Unlock()
+
+	sub, ok := tc.subs[name]
+	if !ok {
+		return
+	}
+	delete(tc.subs, name)
+	sub.Cancel()
+}
+
+func (tc *topicsContainer) Subscribe(name string, opts ...pubsub.SubOpt) (*pubsub.Subscription, error) {
+	tc.subLock.Lock()
+	defer tc.subLock.Unlock()
+
+	_, ok := tc.subs[name]
+	if ok {
+		return nil, nil
+	}
+
+	topic, err := tc.Join(name)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := topic.Subscribe(opts...)
+	if err != nil {
+		return nil, err
+	}
+	tc.subs[name] = s
+
+	return s, nil
+}
