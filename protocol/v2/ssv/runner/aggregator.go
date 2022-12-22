d2d1be1ea69da29fc845cade18271f7b01d180be
nivBlox <niv@blox.io>
2022-12-22 18:26:35 +0200

    aggregate panic fix

--- protocol/v2/ssv/runner/aggregator.go
+++ protocol/v2/ssv/runner/aggregator.go
@@ -89,7 +89,9 @@ func (r *AggregatorRunner) ProcessPreConsensus(signedMsg *specssv.SignedPartialS
 	if err != nil {
 		return errors.Wrap(err, "failed to submit aggregate and proof")
 	}
-
+	if res == nil {
+		return errors.New("selection proof is nil")
+	}
 	input := &spectypes.ConsensusData{
 		Duty:              duty,
 		AggregateAndProof: res,
