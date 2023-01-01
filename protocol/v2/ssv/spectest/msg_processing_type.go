e6b4471b0613865b5aaf73a051246c2f29793995
nivBlox <niv@blox.io>
2023-01-01 11:21:32 +0200

    spec test fix

--- protocol/v2/ssv/spectest/msg_processing_type.go
+++ protocol/v2/ssv/spectest/msg_processing_type.go
@@ -2,6 +2,7 @@ package spectest
 
 import (
 	"encoding/hex"
+	"github.com/bloxapp/ssv-spec/qbft"
 	ssvtesting "github.com/bloxapp/ssv/protocol/v2/ssv/testing"
 	"github.com/bloxapp/ssv/protocol/v2/ssv/validator"
 	"testing"
@@ -34,7 +35,7 @@ func (test *MsgProcessingSpecTest) TestName() string {
 func RunMsgProcessing(t *testing.T, test *MsgProcessingSpecTest) {
 	v := ssvtesting.BaseValidator(spectestingutils.KeySetForShare(test.Runner.GetBaseRunner().Share))
 	v.DutyRunners[test.Runner.GetBaseRunner().BeaconRoleType] = test.Runner
-	// v.Network = test.Runner.GetNetwork() // TODO need to align
+	v.Network = test.Runner.GetNetwork().(qbft.Network) // TODO need to align
 
 	var lastErr error
 	if !test.DontStartDuty {
@@ -54,50 +55,10 @@ func RunMsgProcessing(t *testing.T, test *MsgProcessingSpecTest) {
 	}
 
 	// test output message
-	net := v.Network.(specssv.Network)
-	broadcastedMsgs := net.(*spectestingutils.TestingNetwork).BroadcastedMsgs
-	if len(broadcastedMsgs) > 0 {
-		index := 0
-		for _, msg := range broadcastedMsgs {
-			if msg.MsgType != spectypes.SSVPartialSignatureMsgType {
-				continue
-			}
+	test.compareOutputMsgs(t, v)
 
-			msg1 := &specssv.SignedPartialSignatureMessage{}
-			require.NoError(t, msg1.Decode(msg.Data))
-			msg2 := test.OutputMessages[index]
-			require.Len(t, msg1.Message.Messages, len(msg2.Message.Messages))
-
-			// messages are not guaranteed to be in order so we map them and then test all roots to be equal
-			roots := make(map[string]string)
-			for i, partialSigMsg2 := range msg2.Message.Messages {
-				r2, err := partialSigMsg2.GetRoot()
-				require.NoError(t, err)
-				if _, found := roots[hex.EncodeToString(r2)]; !found {
-					roots[hex.EncodeToString(r2)] = ""
-				} else {
-					roots[hex.EncodeToString(r2)] = hex.EncodeToString(r2)
-				}
-
-				partialSigMsg1 := msg1.Message.Messages[i]
-				r1, err := partialSigMsg1.GetRoot()
-				require.NoError(t, err)
-
-				if _, found := roots[hex.EncodeToString(r1)]; !found {
-					roots[hex.EncodeToString(r1)] = ""
-				} else {
-					roots[hex.EncodeToString(r1)] = hex.EncodeToString(r1)
-				}
-			}
-			for k, v := range roots {
-				require.EqualValues(t, k, v, "missing output msg")
-			}
-
-			index++
-		}
-
-		require.Len(t, test.OutputMessages, index)
-	}
+	// test beacon broadcasted msgs
+	test.compareBroadcastedBeaconMsgs(t)
 
 	// post root
 	postRoot, err := test.Runner.GetRoot()
