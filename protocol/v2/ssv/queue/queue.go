a62d2bb5fad060affe7df951104ca2d73d924333
Amir Y <83904651+amir-blox@users.noreply.github.com>
2023-01-19 19:36:20 +0200

    New Msg Q (#801)
    * https://github.com/bloxapp/ssv/pull/761
    
    * # Conflicts:
    #	go.mod
    #	go.sum
    #	protocol/v2/qbft/roundtimer/timer.go
    #	protocol/v2/ssv/msgqueue/metrics.go
    #	protocol/v2/ssv/msgqueue/post_consensus_msg.go
    #	protocol/v2/ssv/msgqueue/queue.go
    #	protocol/v2/ssv/msgqueue/signed_msg.go
    #	protocol/v2/ssv/msgqueue/sort.go
    #	protocol/v2/ssv/validator/msgqueue_consumer.go
    #	protocol/v2/ssv/validator/startup.go
    #	protocol/v2/ssv/validator/validator.go
    
    # Conflicts:
    #	protocol/v2/ssv/validator/validator.go
    
    * refactor q: change to lock free list
    
    * set higher priority of proposal over prepare
    
    * fix nil deref
    
    * avoid error when timeout happens after instance decided
    
    * fix internal type for event type msg
    
    * benchmark
    
    * redundent past round check remove
    
    * remove race condition old check
    
    This reverts commit 2d54939c1185f5788af7bf33d8bef8f42c878cc7.
    
    Co-authored-by: nivBlox <niv@blox.io>

---
+++ protocol/v2/ssv/queue/queue.go
@@ -0,0 +1,134 @@
+package queue
+
+import (
+	"github.com/bloxapp/ssv-spec/types"
+	"sync/atomic"
+	"unsafe"
+)
+
+// Filter is a function that returns true if the given message should be included.
+type Filter func(*DecodedSSVMessage) bool
+
+// Pop removes a message from the queue
+type Pop func()
+
+// FilterRole returns a Filter which returns true for messages whose BeaconRole matches the given role.
+func FilterRole(role types.BeaconRole) Filter {
+	return func(msg *DecodedSSVMessage) bool {
+		return msg != nil && msg.MsgID.GetRoleType() == role
+	}
+}
+
+// Queue objective is to receive messages and pop the right msg by to specify priority.
+type Queue interface {
+	// Push inserts a message to the queue
+	Push(*DecodedSSVMessage)
+	// Pop removes & returns the highest priority message which matches the given filter.
+	// Returns nil if no message is found.
+	Pop(MessagePrioritizer) *DecodedSSVMessage
+	// IsEmpty checks if the q is empty
+	IsEmpty() bool
+}
+
+// PriorityQueue implements Queue, it manages a lock-free linked list of DecodedSSVMessage.
+// Implemented using atomic CAS (CompareAndSwap) operations to handle multiple goroutines that add/pop messages.
+type PriorityQueue struct {
+	head unsafe.Pointer
+}
+
+// New initialized a PriorityQueue with the given MessagePrioritizer.
+// If prioritizer is nil, the messages will be returned in the order they were pushed.
+func New() Queue {
+	// nolint
+	h := unsafe.Pointer(&msgItem{})
+	return &PriorityQueue{
+		head: h,
+	}
+}
+
+func (q *PriorityQueue) Push(msg *DecodedSSVMessage) {
+	// nolint
+	n := &msgItem{value: unsafe.Pointer(msg), next: q.head}
+	// nolint
+	added := cas(&q.head, q.head, unsafe.Pointer(n))
+	if added {
+		metricMsgQRatio.WithLabelValues(msg.MsgID.String()).Inc()
+	}
+}
+
+func (q *PriorityQueue) Pop(prioritizer MessagePrioritizer) *DecodedSSVMessage {
+	res := q.pop(prioritizer)
+	if res != nil {
+		metricMsgQRatio.WithLabelValues(res.MsgID.String()).Dec()
+	}
+	return res
+}
+
+func (q *PriorityQueue) IsEmpty() bool {
+	h := load(&q.head)
+	if h == nil {
+		return true
+	}
+	return h.Value() == nil
+}
+
+func (q *PriorityQueue) pop(prioritizer MessagePrioritizer) *DecodedSSVMessage {
+	var h, beforeHighest, previous *msgItem
+	currentP := q.head
+
+	for currentP != nil {
+		current := load(&currentP)
+		val := current.Value()
+		if val != nil {
+			if h == nil || prioritizer.Prior(val, h.Value()) {
+				if previous != nil {
+					beforeHighest = previous
+				}
+				h = current
+			}
+		}
+		previous = current
+		currentP = current.NextP()
+	}
+
+	if h == nil {
+		return nil
+	}
+
+	if beforeHighest != nil {
+		cas(&beforeHighest.next, beforeHighest.NextP(), h.NextP())
+	} else {
+		atomic.StorePointer(&q.head, h.NextP())
+	}
+
+	return h.Value()
+}
+
+func load(p *unsafe.Pointer) *msgItem {
+	return (*msgItem)(atomic.LoadPointer(p))
+}
+
+func cas(p *unsafe.Pointer, old, new unsafe.Pointer) bool {
+	return atomic.CompareAndSwapPointer(p, old, new)
+}
+
+// msgItem is an item in the linked list that is used by PriorityQueue
+type msgItem struct {
+	value unsafe.Pointer //*DecodedSSVMessage
+	next  unsafe.Pointer
+}
+
+// Value returns the underlaying value
+func (i *msgItem) Value() *DecodedSSVMessage {
+	return (*DecodedSSVMessage)(atomic.LoadPointer(&i.value))
+}
+
+// Next returns the next item in the list
+func (i *msgItem) Next() *msgItem {
+	return (*msgItem)(atomic.LoadPointer(&i.next))
+}
+
+// NextP returns the next item's pointer
+func (i *msgItem) NextP() unsafe.Pointer {
+	return atomic.LoadPointer(&i.next)
+}
--- protocol/v2/ssv/validator/timer.go
+++ protocol/v2/ssv/validator/timer.go
@@ -5,6 +5,7 @@ import (
 	specqbft "github.com/bloxapp/ssv-spec/qbft"
 	spectypes "github.com/bloxapp/ssv-spec/types"
 	"github.com/bloxapp/ssv/protocol/v2/message"
+	"github.com/bloxapp/ssv/protocol/v2/ssv/queue"
 	"github.com/bloxapp/ssv/protocol/v2/types"
 	"github.com/pkg/errors"
 	"go.uber.org/zap"
@@ -13,16 +14,22 @@ import (
 func (v *Validator) onTimeout(identifier spectypes.MessageID, height specqbft.Height) func() {
 	return func() {
 		dr := v.DutyRunners[identifier.GetRoleType()]
-		if !dr.HasRunningDuty() {
+		hasDuty := dr.HasRunningDuty()
+		if !hasDuty {
 			return
 		}
 
 		msg, err := v.createTimerMessage(identifier, height)
 		if err != nil {
-			v.logger.Warn("failed to create timer msg", zap.Error(err))
+			v.logger.Debug("failed to create timer msg", zap.Error(err))
 			return
 		}
-		v.Queues[identifier.GetRoleType()].Add(msg)
+		dec, err := queue.DecodeSSVMessage(msg)
+		if err != nil {
+			v.logger.Debug("failed to decode timer msg", zap.Error(err))
+			return
+		}
+		v.Queues[identifier.GetRoleType()].Q.Push(dec)
 	}
 }
 
