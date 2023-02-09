6f79f32b2a82f9ad9e7b67767689c308739fff47
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-02-09 17:55:34 +0400

    Change buckets for consensus duration

--- protocol/v2/ssv/runner/metrics/metrics.go
+++ protocol/v2/ssv/runner/metrics/metrics.go
@@ -14,7 +14,7 @@ var (
 	metricsConsensusDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
 		Name:    "ssv_validator_consensus_duration_seconds",
 		Help:    "Consensus duration (seconds)",
-		Buckets: []float64{0.5, 1, 2, 3, 4, 10},
+		Buckets: []float64{0.02, 0.05, 0.1, 0.2, 0.5, 1, 5},
 	}, []string{"pubKey", "role"})
 	metricsPreConsensusDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
 		Name:    "ssv_validator_pre_consensus_duration_seconds",
