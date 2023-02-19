efee1bbef08e5d142d735f85fb5ef36db895d45c
moshe-blox <moshe@blox.io>
2023-02-19 14:35:54 +0200

    fix: only delete prepare messages below last quorum round

--- protocol/v2/qbft/instance/compact.go
+++ protocol/v2/qbft/instance/compact.go
@@ -2,6 +2,7 @@ package instance
 
 import (
 	specqbft "github.com/bloxapp/ssv-spec/qbft"
+	spectypes "github.com/bloxapp/ssv-spec/types"
 )
 
 // Compact trims the given qbft.State down to the minimum required
@@ -13,23 +14,23 @@ import (
 //
 // This helps reduce the state's memory footprint.
 func Compact(state *specqbft.State, decidedMessage *specqbft.SignedMessage) {
-	// compactContainer(state.ProposeContainer, state.Round, state.Decided)
-	// compactContainer(state.PrepareContainer, state.Round, state.Decided)
+	compactContainer(state.ProposeContainer, state.Round, state.Decided)
+	compactContainer(state.PrepareContainer, state.LastPreparedRound, state.Decided)
 	compactContainer(state.RoundChangeContainer, state.Round, state.Decided)
 
 	// Only discard commit messages if the whole committee decided,
 	// otherwise just trim down to the current round (and up).
-	// var signers []spectypes.OperatorID
-	// if decidedMessage != nil {
-	// 	signers = decidedMessage.Signers
-	// } else if state.Decided {
-	// 	signers, _ = state.CommitContainer.LongestUniqueSignersForRoundAndValue(state.Round, state.DecidedValue)
-	// }
-	// wholeCommitteeDecided := len(signers) == len(state.Share.Committee)
-	// compactContainer(state.CommitContainer, state.Round, wholeCommitteeDecided)
+	var signers []spectypes.OperatorID
+	if decidedMessage != nil {
+		signers = decidedMessage.Signers
+	} else if state.Decided {
+		signers, _ = state.CommitContainer.LongestUniqueSignersForRoundAndValue(state.Round, state.DecidedValue)
+	}
+	wholeCommitteeDecided := len(signers) == len(state.Share.Committee)
+	compactContainer(state.CommitContainer, state.Round, wholeCommitteeDecided)
 }
 
-func compactContainer(container *specqbft.MsgContainer, round specqbft.Round, discard bool) {
+func compactContainer(container *specqbft.MsgContainer, currentRound specqbft.Round, discard bool) {
 	switch {
 	case container == nil || len(container.Msgs) == 0:
 		// Empty already.
@@ -39,7 +40,7 @@ func compactContainer(container *specqbft.MsgContainer, round specqbft.Round, di
 	default:
 		// Trim down to the current round (and up).
 		for r := range container.Msgs {
-			if r < round {
+			if r < currentRound {
 				delete(container.Msgs, r)
 			}
 		}
