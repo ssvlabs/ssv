496f2d49c143beb537f718afb19e0f2b6cce1eed
moshe-blox <moshe@blox.io>
2023-02-22 17:54:35 +0200

    moved `compactInstanceIfNeeded` to a separate file

--- protocol/v2/ssv/runner/runner.go
+++ protocol/v2/ssv/runner/runner.go
@@ -150,17 +150,6 @@ func (b *BaseRunner) baseConsensusMsgProcessing(runner Runner, msg *specqbft.Sig
 	return true, decidedValue, nil
 }
 
-// Compacts the given message's associated instance if it's either...
-//   - Got a decided: to discard messages that are no longer needed. (proposes, prepares and sometimes commits)
-//   - Advanced a round: to discard messages from previous rounds. (otherwise it might grow indefinitely)
-func (b *BaseRunner) compactInstanceIfNeeded(msg *specqbft.SignedMessage) {
-	if inst := b.QBFTController.StoredInstances.FindInstance(msg.Message.Height); inst != nil {
-		if controller.IsDecidedMsg(b.Share, msg) || msg.Message.MsgType == specqbft.RoundChangeMsgType {
-			instance.Compact(inst.State, msg)
-		}
-	}
-}
-
 // basePostConsensusMsgProcessing is a base func that all runner implementation can call for processing a post-consensus msg
 func (b *BaseRunner) basePostConsensusMsgProcessing(runner Runner, signedMsg *specssv.SignedPartialSignatureMessage) (bool, [][]byte, error) {
 	if err := b.ValidatePostConsensusMsg(runner, signedMsg); err != nil {
