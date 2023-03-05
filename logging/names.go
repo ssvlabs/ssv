215b645dfaf638f22b40364354d908b30e3dc2f1
Yehor Sereda <yehor@blox.io>
2023-03-05 16:59:17 +0200

    WIP

---
+++ logging/names.go
@@ -0,0 +1,10 @@
+package logging
+
+const (
+	MetricsHandlerComponent = "MetricsHandler"
+	Eth1Component           = "Eth1"
+	WsServerComponent       = "WsServer"
+	P2PNetworkComponent     = "P2PNetwork"
+	OperatorComponent       = "Operator"
+	ValidatorComponent      = "Validator"
+)
