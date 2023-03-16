b4f506a7c450ee50b15223910e3458d93820ceca
moshe-blox <moshe@blox.io>
2023-03-16 16:54:50 +0200

    properly measure goroutine time

--- bounded/bounded.go
+++ bounded/bounded.go
@@ -34,16 +34,30 @@ func init() {
 			zap.L().Debug("bounded goroutine average time", zap.Float64("time_ms", time.Duration(counter.Rate()).Seconds()))
 		}
 	}()
+
+	// Measure the time it takes to start a goroutine.
+	go func() {
+		for {
+			start := time.Now()
+			go func() {
+				d := time.Since(start)
+				if d >= time.Millisecond*1 {
+					zap.L().Debug("TRACE:goroutineLag", zap.Int64("tookMillis", d.Milliseconds()), zap.Bool("very_long", d > time.Millisecond*20))
+				}
+			}()
+			time.Sleep(time.Millisecond * 100)
+		}
+	}()
 }
 
 var counter = ratecounter.NewAvgRateCounter(60 * time.Second)
 
 // Run runs the function f in a goroutine.
 func Run(f func() error) error {
-	start := time.Now()
-	defer func() {
-		counter.Incr(int64(time.Since(start)))
-	}()
+	// start := time.Now()
+	// defer func() {
+	// 	counter.Incr(int64(time.Since(start)))
+	// }()
 	out := make(chan error)
 	in <- job{f, out}
 	return <-out
