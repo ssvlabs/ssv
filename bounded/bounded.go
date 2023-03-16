717b9ebeabb31c9cbefbfc3e338065fb0a19d3c5
moshe-blox <moshe@blox.io>
2023-03-16 16:58:20 +0200

    report goroutine avg start time

--- bounded/bounded.go
+++ bounded/bounded.go
@@ -31,26 +31,35 @@ func init() {
 	go func() {
 		t := time.NewTicker(1 * time.Second)
 		for range t.C {
-			zap.L().Debug("bounded goroutine average time", zap.Float64("time_ms", time.Duration(counter.Rate()).Seconds()))
+			zap.L().Debug("bounded cgo average time", zap.Float64("time_ms", time.Duration(runCounter.Rate()).Seconds()))
 		}
 	}()
 
 	// Measure the time it takes to start a goroutine.
+	var goroutineCounter = ratecounter.NewAvgRateCounter(60 * time.Second)
+	var printRateTicker = time.NewTicker(5 * time.Second)
 	go func() {
 		for {
 			start := time.Now()
 			go func() {
 				d := time.Since(start)
+				goroutineCounter.Incr(int64(d))
 				if d >= time.Millisecond*1 {
 					zap.L().Debug("TRACE:goroutineLag", zap.Int64("tookMillis", d.Milliseconds()), zap.Bool("very_long", d > time.Millisecond*20))
 				}
 			}()
 			time.Sleep(time.Millisecond * 100)
+
+			select {
+			case <-printRateTicker.C:
+				zap.L().Debug("goroutine average start time", zap.Float64("time_ms", time.Duration(goroutineCounter.Rate()).Seconds()))
+			default:
+			}
 		}
 	}()
 }
 
-var counter = ratecounter.NewAvgRateCounter(60 * time.Second)
+var runCounter = ratecounter.NewAvgRateCounter(60 * time.Second)
 
 // Run runs the function f in a goroutine.
 func Run(f func() error) error {
