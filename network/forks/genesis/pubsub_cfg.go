d1be5162c984829ea89e186834895d4ed5dd427e
moshe-blox <moshe@blox.io>
2023-03-21 18:18:45 +0200

    deploy xxhash msgid

--- network/forks/genesis/pubsub_cfg.go
+++ network/forks/genesis/pubsub_cfg.go
@@ -1,11 +1,10 @@
 package genesis
 
 import (
-	"crypto/sha256"
-	"hash"
-	"sync"
+	"encoding/binary"
 
 	"github.com/bloxapp/ssv/network/forks"
+	"github.com/cespare/xxhash/v2"
 )
 
 // MsgID returns msg_id for the given message
@@ -14,35 +13,13 @@ func (genesis *ForkGenesis) MsgID() forks.MsgIDFunc {
 		if len(msg) == 0 {
 			return ""
 		}
-		h := Sha256Hash(msg)
-		return string(h[20:])
+		var b [12]byte
+		binary.LittleEndian.PutUint64(b[:], xxhash.Sum64(msg))
+		return string(b[:])
 	}
 }
 
 // Subnets returns the subnets count for this fork
 func (genesis *ForkGenesis) Subnets() int {
 	return int(subnetsCount)
-}
-
-// sha256Pool holds a pool of hash.Hash
-// it should be used to perform sha256 hash
-var sha256Pool = sync.Pool{New: func() interface{} {
-	return sha256.New()
-}}
-
-// Sha256Hash does sha256 on the given input
-func Sha256Hash(data []byte) [32]byte {
-	h, ok := sha256Pool.Get().(hash.Hash)
-	if !ok {
-		h = sha256.New()
-	}
-	defer sha256Pool.Put(h)
-	h.Reset()
-
-	var b [32]byte
-	// #nosec G104
-	h.Write(data)
-	h.Sum(b[:0])
-
-	return b
 }
