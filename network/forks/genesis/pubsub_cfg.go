20620c61b3bec4edfbb00053bda8197d41350c01
moshe-blox <moshe@blox.io>
2023-03-21 13:32:08 +0200

    fix

--- network/forks/genesis/pubsub_cfg.go
+++ network/forks/genesis/pubsub_cfg.go
@@ -13,7 +13,7 @@ func (genesis *ForkGenesis) MsgID() forks.MsgIDFunc {
 		if len(msg) == 0 {
 			return ""
 		}
-		var b [20]byte
+		var b [12]byte
 		binary.LittleEndian.PutUint64(b[:], xxhash.Sum64(msg))
 		return string(b[:])
 	}
