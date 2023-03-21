478c1893fa8c9c4979a53dc5bb8d238a7b66da4d
moshe-blox <moshe@blox.io>
2023-03-21 14:46:57 +0200

    cleanups

--- bounded/bounded.go
+++ bounded/bounded.go
@@ -1,13 +1,11 @@
 package bounded
 
 import (
+	"log"
 	"os"
 	"runtime"
 	"strconv"
 	"sync"
-	"time"
-
-	"go.uber.org/zap"
 )
 
 var outChanPool = sync.Pool{
@@ -24,11 +22,6 @@ type job struct {
 var in = make(chan job, 1024)
 
 func init() {
-	// TODO: undo
-	if err := os.Setenv("SSV_NUM_CPU", "3"); err != nil {
-		panic(err)
-	}
-
 	// Get the number of available CPUs.
 	//
 	// SSV_NUM_CPU is a useful override when running in environments
@@ -64,14 +57,7 @@ func init() {
 	}
 
 	// Log the number of CPUs and CGO goroutines.
-	go func() {
-		time.Sleep(3 * time.Second)
-		zap.L().Debug("tuning GOMAXPROCS and CGO goroutines",
-			zap.Int("num_cpu", numCPU),
-			zap.Int("cgoroutines", cgoroutines),
-			zap.Int("GOMAXPROCS", goMaxProcs),
-		)
-	}()
+	log.Printf("tuning GOMAXPROCS and CGO goroutines (num_cpu=%d, cgoroutines=%d, GOMAXPROCS=%d)", numCPU, cgoroutines, goMaxProcs)
 }
 
 // CGO runs the given function in a goroutine dedicated to CGO calls,
@@ -81,11 +67,9 @@ func init() {
 // to a fixed number of goroutines with locked OS threads, thereby
 // reducing the number of OS threads that CGO creates and destroyes.
 func CGO(f func()) {
-	f()
+	out := outChanPool.Get().(chan struct{})
+	defer outChanPool.Put(out)
 
-	// out := outChanPool.Get().(chan struct{})
-	// defer outChanPool.Put(out)
-
-	// in <- job{f, out}
-	// <-out
+	in <- job{f, out}
+	<-out
 }
