9649a919e3766702255796e7197a1c2650f82f14
moshe-blox <moshe@blox.io>
2023-03-22 12:38:54 +0200

    removed legacy sha256

--- network/forks/genesis/pubsub_cfg.go
+++ network/forks/genesis/pubsub_cfg.go
@@ -1,10 +1,7 @@
 package genesis
 
 import (
-	"crypto/sha256"
 	"encoding/binary"
-	"hash"
-	"sync"
 
 	"github.com/bloxapp/ssv/network/forks"
 	"github.com/cespare/xxhash/v2"
@@ -16,8 +13,6 @@ func (genesis *ForkGenesis) MsgID() forks.MsgIDFunc {
 		if len(msg) == 0 {
 			return ""
 		}
-		// h := Sha256Hash(msg)
-		// return string(h[20:])
 		b := make([]byte, 12)
 		binary.LittleEndian.PutUint64(b, xxhash.Sum64(msg))
 		return string(b)
@@ -27,27 +22,4 @@ func (genesis *ForkGenesis) MsgID() forks.MsgIDFunc {
 // Subnets returns the subnets count for this fork
 func (genesis *ForkGenesis) Subnets() int {
 	return int(subnetsCount)
-}
-
-// sha256Pool holds a pool of hash.Hash
-// it should be used to perform sha256 hash
-var sha256Pool = sync.Pool{New: func() interface{} {
-	return sha256.New()
-}}
-
-// Sha256Hash does sha256 on the given input
-func Sha256Hash(data []byte) [32]byte {
-	h, ok := sha256Pool.Get().(hash.Hash)
-	if !ok {
-		h = sha256.New()
-	}
-	defer sha256Pool.Put(h)
-	h.Reset()
-
-	var b [32]byte
-	// #nosec G104
-	h.Write(data)
-	h.Sum(b[:0])
-
-	return b
 }
