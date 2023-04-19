4be0b71eb3f0c631dcad52debf384755d0d8a751
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-04-19 19:58:28 +0400

    Print fee recipient when init share

--- operator/validator/validators_map.go
+++ operator/validator/validators_map.go
@@ -120,5 +120,8 @@ func printShare(s *types.SSVShare, logger *zap.Logger, msg string) {
 	logger.Debug(msg,
 		fields.PubKey(s.ValidatorPubKey),
 		zap.Uint64("node_id", s.OperatorID),
-		zap.Strings("committee", committee))
+		zap.Strings("committee", committee),
+		zap.String("FeeRecipientAddress", hex.EncodeToString(s.FeeRecipientAddress[:])),
+		zap.String("FeeRecipient", hex.EncodeToString(s.FeeRecipient[:])),
+	)
 }
