19956ffe9cbdf6d8f95cb76539bb322da041444d
Yehor Sereda <yehor@blox.io>
2023-05-16 12:03:29 +0300

    add more signed node info unit tests

--- network/records/signed_node_info_test.go
+++ network/records/signed_node_info_test.go
@@ -67,3 +67,53 @@ func TestSignedNodeInfo_Seal_Consume(t *testing.T) {
 
 	require.True(t, reflect.DeepEqual(sni, parsedRec))
 }
+
+func TestSignedNodeInfo_Marshal_Unmarshal(t *testing.T) {
+	nodeInfo := &NodeInfo{
+		ForkVersion: forksprotocol.GenesisForkVersion,
+		NetworkID:   "testnet",
+		Metadata: &NodeMetadata{
+			NodeVersion:   "v0.1.12",
+			ExecutionNode: "geth/x",
+			ConsensusNode: "prysm/x",
+			OperatorID:    "xxx",
+			Subnets:       "some-subnets",
+		},
+	}
+
+	_, senderPrivateKeyPem, err := rsaencryption.GenerateKeys()
+	require.NoError(t, err)
+
+	senderPrivateKey, err := rsaencryption.ConvertPemToPrivateKey(string(senderPrivateKeyPem))
+	require.NoError(t, err)
+
+	senderBase64PublicKeyPem, err := rsaencryption.ExtractPublicKey(senderPrivateKey)
+	require.NoError(t, err)
+
+	handshakeData := HandshakeData{
+		SenderPeerID:    peer.ID("1.1.1.1"),
+		RecipientPeerID: peer.ID("2.2.2.2"),
+		Timestamp:       time.Now().Round(time.Second),
+		SenderPubicKey:  []byte(senderBase64PublicKeyPem),
+	}
+	hashed := handshakeData.Hash()
+
+	hashedHandshakeData := hashed[:]
+
+	signature, err := rsa.SignPKCS1v15(nil, senderPrivateKey, crypto.SHA256, hashedHandshakeData)
+	require.NoError(t, err)
+
+	sni := &SignedNodeInfo{
+		NodeInfo:      nodeInfo,
+		HandshakeData: handshakeData,
+		Signature:     signature,
+	}
+
+	data, err := sni.MarshalRecord()
+	require.NoError(t, err)
+
+	parsedRec := &SignedNodeInfo{}
+	require.NoError(t, parsedRec.UnmarshalRecord(data))
+
+	require.True(t, reflect.DeepEqual(sni, parsedRec))
+}
