d8605473a23a7a3a1105994be51ca8365aab6d8a
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-05-23 15:40:23 +0400

    Adjust network configs

--- networkconfig/main.go
+++ networkconfig/main.go
@@ -13,10 +13,13 @@ var Mainnet = spectypes.BeaconNetwork{
 	SSV: spectypes.SSVParams{
 		Domain:                 spectypes.GenesisMainnet,
 		ForkVersion:            [4]byte{0, 0, 0, 0},
+		GenesisEpoch:           1,
 		DefaultSyncOffset:      new(big.Int).SetInt64(8661727),
 		DepositContractAddress: "0x00000000219ab540356cBB839Cbe05303d7705Fa",
 		GenesisValidatorsRoot:  "4b363db94e286120d76eb905340fdd4e54bfe9f06bf33ff6cf5ad27f511bfe95",
-		GenesisEpoch:           1,
+		Bootnodes:              []string{
+			// TODO: fill
+		},
 	},
 	ETH: spectypes.ETHParams{
 		NetworkName:      string(core.MainNetwork),
