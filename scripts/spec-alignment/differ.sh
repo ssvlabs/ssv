d457621394a9dc0164f16ec5b5962819c27f4eb8
moshe-blox <moshe@blox.io>
2023-05-30 12:32:45 +0300

    action upload diff

---
+++ scripts/spec-alignment/differ.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get the path to the ssv-spec folder.
+SPEC_PATH=$(go mod download -json github.com/bloxapp/ssv-spec | jq -r .Dir)
+
+# Run Differ.
+differ \
+    --config ./differ.config.yaml \
+    --output ./output.diff \
+    ../../ \
+    $SPEC_PATH
