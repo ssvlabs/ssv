5988246f525a55c68f5974d56bb6ffcb42eb24dc
moshe-blox <moshe@blox.io>
2023-06-04 16:34:38 +0300

    fix k8s scripts

--- .k8/production/prater/scripts/deploy-boot-nodes.sh
+++ .k8/production/prater/scripts/deploy-boot-nodes.sh
@@ -103,8 +103,8 @@ fi
   #done
 #fi
 
-if [[ -d .k8/prater/ ]]; then
-  for file in $(ls -A1 .k8/prater/); do
+if [[ -d .k8/production/prater/ ]]; then
+  for file in $(ls -A1 .k8/production/prater/); do
    sed -i -e "s|REPLACE_NAMESPACE|${NAMESPACE}|g" \
           -e "s|REPLACE_DOCKER_REPO|${DOCKERREPO}|g" \
           -e "s|REPLACE_REPLICAS|${REPLICAS}|g" \
@@ -113,7 +113,7 @@ if [[ -d .k8/prater/ ]]; then
           -e "s|REPLACE_HEALTH_IMAGE|${HEALTH_CHECK_IMAGE}|g" \
           -e "s|REPLACE_NODES_CPU_LIMIT|${NODES_CPU_LIMIT}|g" \
           -e "s|REPLACE_NODES_MEM_LIMIT|${NODES_MEM_LIMIT}|g" \
-	  -e "s|REPLACE_IMAGETAG|${IMAGETAG}|g" ".k8/prater/${file}" || exit 1
+	  -e "s|REPLACE_IMAGETAG|${IMAGETAG}|g" ".k8/production/prater/${file}" || exit 1
   done
 fi
 
