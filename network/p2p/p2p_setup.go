badae2255282540bc2eb7a5220ea3eb204ba571e
moshe-blox <moshe@blox.io>
2023-06-04 12:30:41 +0300

    fix

--- network/p2p/p2p_setup.go
+++ network/p2p/p2p_setup.go
@@ -15,7 +15,7 @@ import (
 	rcmgr "github.com/libp2p/go-libp2p/p2p/host/resource-manager"
 	"github.com/libp2p/go-libp2p/p2p/protocol/identify"
 	"github.com/pkg/errors"
-	"github.com/prysmaticlabs/prysm/async"
+	"github.com/prysmaticlabs/prysm/v4/async"
 	"go.uber.org/zap"
 
 	"github.com/bloxapp/ssv/logging"
