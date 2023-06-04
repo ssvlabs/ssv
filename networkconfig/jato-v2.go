de900607fb78dc09b83e85e4c53d9b8d8207baf6
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2023-06-04 15:25:19 +0300

    align to v0.3.2-rc.0 prod contract

--- networkconfig/jato-v2.go
+++ networkconfig/jato-v2.go
@@ -9,15 +9,12 @@ import (
 )
 
 var JatoV2 = NetworkConfig{
-	Name:         "jato-v2",
-	Beacon:       beacon.NewNetwork(spectypes.PraterNetwork),
-	Domain:       spectypes.V3Testnet,
-	GenesisEpoch: 205852,
-
-	// TODO: should be adjusted to the actual offset once we have a contract
-	ETH1SyncOffset: new(big.Int).SetInt64(9015219),
-	// TODO: should be adjusted to the actual contract address once we have a contract
-	RegistryContractAddr: "0x01",
+	Name:                 "jato-v2",
+	Beacon:               beacon.NewNetwork(spectypes.PraterNetwork),
+	Domain:               spectypes.V3Testnet,
+	GenesisEpoch:         205852,
+	ETH1SyncOffset:       new(big.Int).SetInt64(9120510),
+	RegistryContractAddr: "0x9d3F908cB3b132379A97b0E0f8171F0B42756E28",
 	Bootnodes: []string{
 		"enr:-Li4QMwFYXASubP23bEmzbm5bGEtTZdbYqSQz-K12Hl4NZZlcJi2zE8oZ5LErjnvQ3qlyl2_flDIcIVOhX5mb9UPpv-GAYh8cjRLh2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__________gmlkgnY0gmlwhCLdu_SJc2VjcDI1NmsxoQO0oKeSpS0TfgDof9xIGhUTLgnjEg91kVKA5fufSt_CQIN0Y3CCE4mDdWRwgg-h",
 	},
