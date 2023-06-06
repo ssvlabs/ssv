82b6990a822dcb21e829cbfd1a37204a7e492992
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-06-06 19:32:01 +0400

    network/discovery: return error instead of negative value in nsToSubnet

--- network/discovery/subnets.go
+++ network/discovery/subnets.go
@@ -1,27 +1,30 @@
 package discovery
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/bloxapp/ssv/utils/format"
 )
 
-var regPool = format.NewRegexpPool("\\w+:bloxstaking\\.ssv\\.(\\d+)")
+var (
+	regPool     = format.NewRegexpPool("\\w+:bloxstaking\\.ssv\\.(\\d+)")
+	errNotFound = fmt.Errorf("not found")
+)
 
 // nsToSubnet converts the given topic to subnet
-// TODO: return other value than zero upon failure?
-func nsToSubnet(ns string) int {
+func nsToSubnet(ns string) (int, error) {
 	r, done := regPool.Get()
 	defer done()
 	found := r.FindStringSubmatch(ns)
 	if len(found) != 2 {
-		return -1
+		return 0, errNotFound
 	}
 	val, err := strconv.ParseUint(found[1], 10, 64)
 	if err != nil {
-		return -1
+		return 0, err
 	}
-	return int(val)
+	return int(val), nil
 }
 
 // isSubnet checks if the given string is a subnet string
--- network/discovery/subnets_test.go
+++ network/discovery/subnets_test.go
@@ -8,33 +8,45 @@ import (
 
 func TestNsToSubnet(t *testing.T) {
 	tests := []struct {
-		name     string
-		ns       string
-		expected int
-		isSubnet bool
+		name        string
+		ns          string
+		expected    int
+		expectedErr string
+		isSubnet    bool
 	}{
 		{
 			"dummy string",
 			"xxx",
-			-1,
+			0,
+			errNotFound.Error(),
 			false,
 		},
 		{
 			"invalid int",
 			"floodsub:bloxstaking.ssv.xxx",
-			-1,
+			0,
+			errNotFound.Error(),
 			false,
 		},
 		{
 			"invalid",
 			"floodsub:ssv.1",
-			-1,
+			0,
+			errNotFound.Error(),
 			false,
 		},
 		{
 			"valid",
 			"floodsub:bloxstaking.ssv.21",
 			21,
+			"",
+			true,
+		},
+		{
+			"uint overflow",
+			"floodsub:bloxstaking.ssv.99999999999999999999",
+			0,
+			`strconv.ParseUint: parsing "99999999999999999999": value out of range`,
 			true,
 		},
 	}
@@ -43,7 +55,14 @@ func TestNsToSubnet(t *testing.T) {
 		test := test
 		t.Run(test.name, func(t *testing.T) {
 			require.Equal(t, test.isSubnet, isSubnet(test.ns))
-			require.Equal(t, test.expected, nsToSubnet(test.ns))
+
+			subnet, err := nsToSubnet(test.ns)
+			if test.expectedErr == "" {
+				require.NoError(t, err)
+			} else {
+				require.EqualError(t, err, test.expectedErr)
+			}
+			require.Equal(t, test.expected, subnet)
 		})
 	}
 }
