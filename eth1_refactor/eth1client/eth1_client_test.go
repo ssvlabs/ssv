20684e18019ae9ed3c75402751986f4e50eca183
pavelkrolevets <pavelkrolevets@gmail.com>
2023-06-27 00:06:15 +0300

    update contract and fill blocks with events

--- eth1_refactor/eth1client/eth1_client_test.go
+++ eth1_refactor/eth1client/eth1_client_test.go
@@ -32,7 +32,7 @@ var (
 
 	testBalance = big.NewInt(2e18)
 
-	contractAddr = ethcommon.HexToAddress("0xdB7d6AB1f17c6b31909aE466702703dAEf9269Cf")
+	contractAddr = ethcommon.HexToAddress("0x3A220f351252089D385b29beca14e27F204c296A")
 )
 
 var genesis = &core.Genesis{
@@ -43,46 +43,18 @@ var genesis = &core.Genesis{
 	BaseFee:   big.NewInt(params.InitialBaseFee),
 }
 
-var testTx1 = types.MustSignNewTx(testKey, types.LatestSigner(genesis.Config), &types.LegacyTx{
-	Nonce:    0,
-	Value:    big.NewInt(12),
-	GasPrice: big.NewInt(params.InitialBaseFee),
-	Gas:      params.TxGas,
-	To:       &ethcommon.Address{2},
-})
+/*
+Example contract to test event emission:
 
-var testTx2 = types.MustSignNewTx(testKey, types.LatestSigner(genesis.Config), &types.LegacyTx{
-	Nonce:    1,
-	Value:    big.NewInt(0),
-	GasPrice: big.NewInt(params.InitialBaseFee),
-	Gas:      100000,
-	Data:     ethcommon.FromHex(abiBin),
-})
+	pragma solidity >=0.7.0 <0.9.0;
+	contract Callable {
+		event Called();
+		function Call() public { emit Called(); }
+	}
+*/
+const callableAbi = "[{\"anonymous\":false,\"inputs\":[],\"name\":\"Called\",\"type\":\"event\"},{\"inputs\":[],\"name\":\"Call\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
 
-var testTx3 = types.MustSignNewTx(testKey, types.LatestSigner(genesis.Config), &types.LegacyTx{
-	Nonce:    2,
-	Value:    big.NewInt(0),
-	GasPrice: big.NewInt(params.InitialBaseFee),
-	Gas:      100000,
-	To:       &contractAddr,
-	Data:     ethcommon.FromHex("0xa69b6ed0000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000010100000000000000000000000000000000000000000000000000000000000000"),
-})
-
-// the following is based on this contract:
-//
-//	 contract T {
-//	 	event received(address sender, uint amount, bytes memo);
-//	 	event receivedAddr(address sender);
-//
-//	 	function receive(bytes calldata memo) external payable returns (string memory res) {
-//	 		emit received(msg.sender, msg.value, memo);
-//	 		emit receivedAddr(msg.sender);
-//			return "hello world";
-//	 	}
-//	 }
-const abiJSON = `[ { "constant": false, "inputs": [ { "name": "memo", "type": "bytes" } ], "name": "receive", "outputs": [ { "name": "res", "type": "string" } ], "payable": true, "stateMutability": "payable", "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "sender", "type": "address" }, { "indexed": false, "name": "amount", "type": "uint256" }, { "indexed": false, "name": "memo", "type": "bytes" } ], "name": "received", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "sender", "type": "address" } ], "name": "receivedAddr", "type": "event" } ]`
-const abiBin = `0x608060405234801561001057600080fd5b506102a0806100206000396000f3fe60806040526004361061003b576000357c010000000000000000000000000000000000000000000000000000000090048063a69b6ed014610040575b600080fd5b6100b76004803603602081101561005657600080fd5b810190808035906020019064010000000081111561007357600080fd5b82018360208201111561008557600080fd5b803590602001918460018302840111640100000000831117156100a757600080fd5b9091929391929390505050610132565b6040518080602001828103825283818151815260200191508051906020019080838360005b838110156100f75780820151818401526020810190506100dc565b50505050905090810190601f1680156101245780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b60607f75fd880d39c1daf53b6547ab6cb59451fc6452d27caa90e5b6649dd8293b9eed33348585604051808573ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001848152602001806020018281038252848482818152602001925080828437600081840152601f19601f8201169050808301925050509550505050505060405180910390a17f46923992397eac56cf13058aced2a1871933622717e27b24eabc13bf9dd329c833604051808273ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390a16040805190810160405280600b81526020017f68656c6c6f20776f726c6400000000000000000000000000000000000000000081525090509291505056fea165627a7a72305820ff0c57dad254cfeda48c9cfb47f1353a558bccb4d1bc31da1dae69315772d29e0029`
-const deployedCode = `60806040526004361061003b576000357c010000000000000000000000000000000000000000000000000000000090048063a69b6ed014610040575b600080fd5b6100b76004803603602081101561005657600080fd5b810190808035906020019064010000000081111561007357600080fd5b82018360208201111561008557600080fd5b803590602001918460018302840111640100000000831117156100a757600080fd5b9091929391929390505050610132565b6040518080602001828103825283818151815260200191508051906020019080838360005b838110156100f75780820151818401526020810190506100dc565b50505050905090810190601f1680156101245780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b60607f75fd880d39c1daf53b6547ab6cb59451fc6452d27caa90e5b6649dd8293b9eed33348585604051808573ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001848152602001806020018281038252848482818152602001925080828437600081840152601f19601f8201169050808301925050509550505050505060405180910390a17f46923992397eac56cf13058aced2a1871933622717e27b24eabc13bf9dd329c833604051808273ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390a16040805190810160405280600b81526020017f68656c6c6f20776f726c6400000000000000000000000000000000000000000081525090509291505056fea165627a7a72305820ff0c57dad254cfeda48c9cfb47f1353a558bccb4d1bc31da1dae69315772d29e0029`
+const callableBin = "6080604052348015600f57600080fd5b5060998061001e6000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c806334e2292114602d575b600080fd5b60336035565b005b7f81fab7a4a0aa961db47eefc81f143a5220e8c8495260dd65b1356f1d19d3c7b860405160405180910390a156fea2646970667358221220029436d24f3ac598ceca41d4d712e13ced6d70727f4cdc580667de66d2f51d8b64736f6c63430008010033"
 
 func TestEth1Client(t *testing.T) {
 	const testTimeout = 100 * time.Millisecond
@@ -95,7 +67,7 @@ func TestEth1Client(t *testing.T) {
 	defer httpsrv.Close()
 
 	addr := "ws:" + strings.TrimPrefix(httpsrv.URL, "http:")
-	// contractAddr := ethcommon.HexToAddress(networkconfig.TestNetwork.RegistryContractAddr)
+
 	logger := zaptest.NewLogger(t)
 
 	client := New(addr, contractAddr, WithLogger(logger))
@@ -126,7 +98,7 @@ func TestEth1Client(t *testing.T) {
 
 func newTestBackend(t *testing.T) (*node.Node, []*types.Block) {
 	// Generate test chain.
-	blocks := generateTestChain()
+	blocks := generateTestChain(t)
 
 	// Create node
 	n, err := node.New(&node.Config{})
@@ -159,14 +131,35 @@ func newTestBackend(t *testing.T) (*node.Node, []*types.Block) {
 	return n, blocks
 }
 
-func generateTestChain() []*types.Block {
+func generateTestChain(t *testing.T) []*types.Block {
 	generate := func(i int, g *core.BlockGen) {
 		g.OffsetTime(5)
 		g.SetExtra([]byte("test"))
+		if i == 0 {
+			return
+		}
 		if i == 1 {
-			g.AddTx(testTx1)
-			g.AddTx(testTx2)
-			g.AddTx(testTx3)
+			tx := types.MustSignNewTx(testKey, types.LatestSigner(genesis.Config), &types.LegacyTx{
+				Nonce:    uint64(i-1),
+				Value:    big.NewInt(0),
+				GasPrice: big.NewInt(params.InitialBaseFee),
+				Gas:      100000,
+				Data: ethcommon.FromHex(callableBin),
+			})
+			g.AddTx(tx)
+			t.Log("Tx hash", tx.Hash().Hex())
+		} else {
+			to := ethcommon.HexToAddress("0x3A220f351252089D385b29beca14e27F204c296A")
+			tx := types.MustSignNewTx(testKey, types.LatestSigner(genesis.Config), &types.LegacyTx{
+				To: &to,
+				Nonce:    uint64(i-1),
+				Value:    big.NewInt(0),
+				GasPrice: big.NewInt(params.InitialBaseFee),
+				Gas:      100000,
+				Data: ethcommon.FromHex("0x34e22921"),
+			})
+			g.AddTx(tx)
+			t.Log("Tx hash", tx.Hash().Hex())
 		}
 	}
 	_, blocks, _ := core.GenerateChainWithGenesis(genesis, ethash.NewFaker(), 20, generate)
