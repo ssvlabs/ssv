adbdbb3643857cb2a502b7bb982fff9092f3ac1c
moshe-blox <moshe@blox.io>
2023-07-02 17:42:19 +0300

    fix

--- api/node/handler.go
+++ api/node/handler.go
@@ -24,16 +24,15 @@ type peerJSON struct {
 func (h *Handler) Peers(w http.ResponseWriter, r *http.Request) error {
 	peers := h.Network.Peers()
 	resp := make([]peerJSON, len(peers))
-	for _, id := range peers {
-		record := peerJSON{
+	for i, id := range peers {
+		resp[i] = peerJSON{
 			ID:            id,
 			Connectedness: h.Network.Connectedness(id).String(),
 			Subnets:       h.PeersIndex.GetPeerSubnets(id).String(),
 		}
 		for _, addr := range h.Network.Peerstore().Addrs(id) {
-			record.Addresses = append(record.Addresses, addr.String())
+			resp[i].Addresses = append(resp[i].Addresses, addr.String())
 		}
-		resp = append(resp, record)
 	}
 	render.JSON(w, r, resp)
 	return nil
