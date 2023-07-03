40f015693e8d1ebd084b0429a8b180a9c275e28b
pavelkrolevets <pavelkrolevets@gmail.com>
2023-07-03 17:16:11 +0300

    update test Client and EventDispatcher

--- eth/executionclient/execution_client_test.go
+++ eth/executionclient/execution_client_test.go
@@ -16,6 +16,8 @@ import (
 	"github.com/ethereum/go-ethereum/eth"
 	"github.com/ethereum/go-ethereum/eth/ethconfig"
 	"github.com/ethereum/go-ethereum/eth/filters"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/miner"
 	"github.com/ethereum/go-ethereum/node"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/rpc"
@@ -33,108 +35,15 @@ var (
 	testBalance = big.NewInt(2e18)
 
 	contractAddr = ethcommon.HexToAddress("0x3A220f351252089D385b29beca14e27F204c296A")
+
+	genesis = &core.Genesis{
+		Config:    params.AllEthashProtocolChanges,
+		Alloc:     core.GenesisAlloc{testAddr: {Balance: testBalance}},
+		ExtraData: []byte("test genesis"),
+		Timestamp: 9000,
+		BaseFee:   big.NewInt(params.InitialBaseFee),
+	}
 )
-
-var genesis = &core.Genesis{
-	Config:    params.AllEthashProtocolChanges,
-	Alloc:     core.GenesisAlloc{testAddr: {Balance: testBalance}},
-	ExtraData: []byte("test genesis"),
-	Timestamp: 9000,
-	BaseFee:   big.NewInt(params.InitialBaseFee),
-}
-
-func newTestBackend(t *testing.T, done <-chan interface{}, blockStream <-chan []*types.Block, delay time.Duration) (*node.Node, <-chan []*types.Block) {
-	processedStream := make(chan []*types.Block)
-	// Create node
-	n, err := node.New(&node.Config{})
-
-	if err != nil {
-		t.Fatalf("can't create new node: %v", err)
-	}
-	// Create Ethereum Service
-	config := &ethconfig.Config{Genesis: genesis}
-	ethservice, err := eth.New(n, config)
-	if err != nil {
-		t.Fatalf("can't create new ethereum service: %v", err)
-	}
-
-	// Add required APIs
-	filterSystem := filters.NewFilterSystem(ethservice.APIBackend, filters.Config{})
-	n.RegisterAPIs([]rpc.API{{
-		Namespace: "eth",
-		Service:   filters.NewFilterAPI(filterSystem, false),
-	}})
-
-	// Start eth1 node
-	if err := n.Start(); err != nil {
-		t.Fatalf("can't start test node: %v", err)
-	}
-
-	go func() {
-		defer close(processedStream)
-		select {
-		case <-done:
-			return
-		case blocks := <-blockStream:
-			// Batch block processing because events are fired only after all blocks in the batch processed
-			// for _, block := range blocks {
-			// 	var blocksToProcess []*types.Block
-			// 	blocksToProcess = append(blocksToProcess, block)
-			// 	if _, err := ethservice.BlockChain().InsertChain(blocksToProcess); err != nil {
-			// 		return
-			// 	}
-			// 	processedStream <- blocksToProcess
-			// 	time.Sleep(delay)
-			// }
-			if _, err := ethservice.BlockChain().InsertChain(blocks); err != nil {
-				return
-			}
-			processedStream <- blocks
-		}
-	}()
-	return n, processedStream
-}
-
-// Generate blocks with transactions
-func generateInitialTestChain(t *testing.T, done <-chan interface{}, blockStream chan []*types.Block, n int) {
-	generate := func(i int, g *core.BlockGen) {
-		g.OffsetTime(5)
-		g.SetExtra([]byte("test"))
-		if i == 0 {
-			return
-		}
-		// Add contract deployment to the firs block
-		if i == 1 {
-			tx := types.MustSignNewTx(testKey, types.LatestSigner(genesis.Config), &types.LegacyTx{
-				Nonce:    uint64(i - 1),
-				Value:    big.NewInt(0),
-				GasPrice: big.NewInt(params.InitialBaseFee),
-				Gas:      100000,
-				Data:     ethcommon.FromHex(callableBin),
-			})
-			g.AddTx(tx)
-		} else {
-			// Transactions to Callable contract
-			tx := types.MustSignNewTx(testKey, types.LatestSigner(genesis.Config), &types.LegacyTx{
-				To:       &contractAddr,
-				Nonce:    uint64(i - 1),
-				Value:    big.NewInt(0),
-				GasPrice: big.NewInt(params.InitialBaseFee),
-				Gas:      100000,
-				// Call to function Call() which emits event Called()
-				Data:     ethcommon.FromHex("0x34e22921"),
-			})
-			g.AddTx(tx)
-		}
-	}
-	_, blocks, _ := core.GenerateChainWithGenesis(genesis, ethash.NewFaker(), n, generate)
-	go func() {
-		select {
-		case <-done:
-		case blockStream <- blocks:
-		}
-	}()
-}
 
 /*
 Example contract to test event emission:
@@ -145,7 +54,6 @@ Example contract to test event emission:
 		function Call() public { emit Called(); }
 	}
 */
-const callableAbi = "[{\"anonymous\":false,\"inputs\":[],\"name\":\"Called\",\"type\":\"event\"},{\"inputs\":[],\"name\":\"Call\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
 
 const callableBin = "6080604052348015600f57600080fd5b5060998061001e6000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c806334e2292114602d575b600080fd5b60336035565b005b7f81fab7a4a0aa961db47eefc81f143a5220e8c8495260dd65b1356f1d19d3c7b860405160405180910390a156fea2646970667358221220029436d24f3ac598ceca41d4d712e13ced6d70727f4cdc580667de66d2f51d8b64736f6c63430008010033"
 
@@ -162,6 +70,12 @@ func TestFetchHistoricalLogs(t *testing.T) {
 
 	backend, processedStream := newTestBackend(t, done, blockStream, time.Microsecond)
 
+	// Generate test chain before we read historical logs
+	generateInitialTestChain(t, done, blockStream, 1008)
+	for blocks := range processedStream {
+		t.Log("Processed blocks: ", len(blocks))
+	}
+
 	// Create JSON-RPC handler
 	rpcServer, _ := backend.RPCHandler()
 	// Expose handler on a test server with ws open
@@ -169,11 +83,24 @@ func TestFetchHistoricalLogs(t *testing.T) {
 	defer rpcServer.Stop()
 	defer httpsrv.Close()
 
+	// Check if the contract is deployes successfuly with a standard eth1 client
+	ec, err := backend.Attach()
+	require.NoError(t, err)
+	cl := ethclient.NewClient(ec)
+	receipt, err := cl.TransactionReceipt(ctx, ethcommon.HexToHash("0x0a854d0edf6b757240d5ef2cbfc3fe355525ad1656bf7ce0fbcfa27077a1246a"))
+	require.NoError(t, err)
+	require.Equal(t, uint64(1), receipt.Status)
+	contracctCode, err := cl.CodeAt(ctx, receipt.ContractAddress, nil)
+	require.NoError(t, err)
+	if len(contracctCode) == 0 {
+		t.Fatal("got code for account that does not have contract code")
+	}
+
 	addr := "ws:" + strings.TrimPrefix(httpsrv.URL, "http:")
 
 	logger := zaptest.NewLogger(t)
 
-	client := New(addr, contractAddr, WithLogger(logger))
+	client := New(addr, receipt.ContractAddress, WithLogger(logger))
 
 	require.NoError(t, client.Connect(ctx))
 
@@ -181,11 +108,6 @@ func TestFetchHistoricalLogs(t *testing.T) {
 	require.NoError(t, err)
 	require.True(t, ready)
 
-	// Generate test chain before we read historical logs
-	generateInitialTestChain(t, done, blockStream, 1008)
-	for blocks := range processedStream {
-		t.Log("Processed blocks: ", len(blocks))
-	}
 	// Fetch all logs history starting from block 0
 	logs, lastBlock, err := client.FetchHistoricalLogs(ctx, 0)
 	require.NoError(t, err)
@@ -208,7 +130,7 @@ func TestStreamLogs(t *testing.T) {
 	blockStream := make(chan []*types.Block)
 	defer close(blockStream)
 	// Create sim instance with a delay between block execution
-	backend, processedStream := newTestBackend(t, done, blockStream, time.Microsecond * 50)
+	backend, processedStream := newTestBackend(t, done, blockStream, time.Microsecond*50)
 
 	rpcServer, _ := backend.RPCHandler()
 	httpsrv := httptest.NewServer(rpcServer.WebsocketHandler([]string{"*"}))
@@ -248,4 +170,89 @@ func TestStreamLogs(t *testing.T) {
 	require.NoError(t, client.Close())
 	require.NoError(t, backend.Close())
 
+}
+
+func newTestBackend(t *testing.T, done <-chan interface{}, blockStream <-chan []*types.Block, delay time.Duration) (*node.Node, <-chan []*types.Block) {
+	processedStream := make(chan []*types.Block)
+	// Create node
+	n, err := node.New(&node.Config{})
+
+	if err != nil {
+		t.Fatalf("can't create new node: %v", err)
+	}
+	// Create Ethereum Service
+	config := &ethconfig.Config{Genesis: genesis, Miner: miner.DefaultConfig}
+	ethservice, err := eth.New(n, config)
+	if err != nil {
+		t.Fatalf("can't create new ethereum service: %v", err)
+	}
+
+	// Add required APIs
+	filterSystem := filters.NewFilterSystem(ethservice.APIBackend, filters.Config{})
+	n.RegisterAPIs([]rpc.API{{
+		Namespace: "eth",
+		Service:   filters.NewFilterAPI(filterSystem, false),
+	}})
+
+	// Start eth1 node
+	if err := n.Start(); err != nil {
+		t.Fatalf("can't start test node: %v", err)
+	}
+
+	go func() {
+		defer close(processedStream)
+		select {
+		case <-done:
+			return
+		case blocks := <-blockStream:
+			if _, err := ethservice.BlockChain().InsertChain(blocks); err != nil {
+				t.Fatal(err)
+				return
+			}
+			processedStream <- blocks
+		}
+	}()
+	return n, processedStream
+}
+
+// Generate blocks with transactions
+func generateInitialTestChain(t *testing.T, done <-chan interface{}, blockStream chan []*types.Block, n int) {
+
+	generate := func(i int, g *core.BlockGen) {
+		g.OffsetTime(5)
+		g.SetExtra([]byte("test"))
+		if i == 0 {
+			return
+		}
+		// Add contract deployment to the firs block
+		if i == 1 {
+			tx := types.MustSignNewTx(testKey, types.LatestSigner(genesis.Config), &types.LegacyTx{
+				Nonce:    uint64(i - 1),
+				Value:    big.NewInt(0),
+				GasPrice: big.NewInt(params.InitialBaseFee),
+				Gas:      300000,
+				Data:     ethcommon.FromHex(callableBin),
+			})
+			g.AddTx(tx)
+		} else {
+			// Transactions to contract
+			tx := types.MustSignNewTx(testKey, types.LatestSigner(genesis.Config), &types.LegacyTx{
+				To:       &contractAddr,
+				Nonce:    uint64(i - 1),
+				Value:    big.NewInt(0),
+				GasPrice: big.NewInt(params.InitialBaseFee),
+				Gas:      302916,
+				// Call to function which emits event
+				Data: ethcommon.FromHex("0x34e22921"),
+			})
+			g.AddTx(tx)
+		}
+	}
+	_, blocks, _ := core.GenerateChainWithGenesis(genesis, ethash.NewFaker(), n, generate)
+	go func() {
+		select {
+		case <-done:
+		case blockStream <- blocks:
+		}
+	}()
 }
