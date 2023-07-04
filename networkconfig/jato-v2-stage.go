8944d9bd2ae215c013ab16609637fc9d04eef204
moshe-blox <89339422+moshe-blox@users.noreply.github.com>
2023-07-04 13:44:20 +0300

    Contract Upgrade for `jato-v2-stage` (#1041)
    * deploy new contract

--- networkconfig/jato-v2-stage.go
+++ networkconfig/jato-v2-stage.go
@@ -13,8 +13,8 @@ var JatoV2Stage = NetworkConfig{
 	Beacon:               beacon.NewNetwork(spectypes.PraterNetwork),
 	Domain:               [4]byte{0x00, 0x00, 0x30, 0x12},
 	GenesisEpoch:         152834,
-	ETH1SyncOffset:       new(big.Int).SetInt64(9109142),
-	RegistryContractAddr: "0x45B831727DC96035e6a2f77AAAcE4835195a54Af",
+	ETH1SyncOffset:       new(big.Int).SetInt64(9249887),
+	RegistryContractAddr: "0xd6b633304Db2DD59ce93753FA55076DA367e5b2c",
 	Bootnodes: []string{
 		"enr:-Li4QO86ZMZr_INMW_WQBsP2jS56yjrHnZXxAUOKJz4_qFPKD1Cr3rghQD2FtXPk2_VPnJUi8BBiMngOGVXC0wTYpJGGAYgqnGSNh2F0dG5ldHOIAAAAAAAAAACEZXRoMpDkvpOTAAAQIP__________gmlkgnY0gmlwhArqAsGJc2VjcDI1NmsxoQKNW0Mf-xTXcevRSkZOvoN0Q0T9OkTjGZQyQeOl3bYU3YN0Y3CCE4iDdWRwgg-g",
 		"enr:-Li4QBoH15fXLV78y1_nmD5sODveptALORh568iWLS_eju3SUvF2ZfGE2j-nERKU1zb2g5KlS8L70SRLdRUJ-pHH-fmGAYgvh9oGh2F0dG5ldHOIAAAAAAAAAACEZXRoMpDkvpOTAAAQIP__________gmlkgnY0gmlwhArqAsGJc2VjcDI1NmsxoQO_tV3JP75ZUZPjhOgc2VqEu_FQEMeHc4AyOz6Lz33M2IN0Y3CCE4mDdWRwgg-h",
