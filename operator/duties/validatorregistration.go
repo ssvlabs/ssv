5552b92f59a395cc80996b6c357c122cd0a1842a
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2023-07-05 12:53:00 +0300

    validatorregistration handler

---
+++ operator/duties/validatorregistration.go
@@ -0,0 +1,70 @@
+package duties
+
+import (
+	"context"
+
+	"github.com/attestantio/go-eth2-client/spec/phase0"
+	spectypes "github.com/bloxapp/ssv-spec/types"
+	"go.uber.org/zap"
+
+	"github.com/bloxapp/ssv/logging/fields"
+)
+
+const validatorRegistrationEpochInterval = uint64(10)
+
+type ValidatorRegistrationHandler struct {
+	baseHandler
+
+	validatorsPassedFirstRegistration map[string]struct{}
+}
+
+func NewValidatorRegistrationHandler() *ValidatorRegistrationHandler {
+	return &ValidatorRegistrationHandler{
+		validatorsPassedFirstRegistration: map[string]struct{}{},
+	}
+}
+
+func (h *ValidatorRegistrationHandler) Name() string {
+	return "VALIDATOR_REGISTRATION"
+}
+
+func (h *ValidatorRegistrationHandler) HandleDuties(ctx context.Context, logger *zap.Logger) {
+	logger = logger.With(zap.String("handler", h.Name()))
+	logger.Info("starting duty handler")
+
+	for slot := range h.ticker {
+		shares := h.validatorController.GetOperatorShares()
+
+		sent := 0
+		for _, share := range shares {
+			if !share.HasBeaconMetadata() {
+				continue
+			}
+
+			// if not passed first registration, should be registered within one epoch time in a corresponding slot
+			// if passed first registration, should be registered within validatorRegistrationEpochInterval epochs time in a corresponding slot
+			registrationSlotInterval := h.network.SlotsPerEpoch()
+			if _, ok := h.validatorsPassedFirstRegistration[string(share.ValidatorPubKey)]; ok {
+				registrationSlotInterval *= validatorRegistrationEpochInterval
+			}
+
+			if uint64(share.BeaconMetadata.Index)%registrationSlotInterval != uint64(slot)%registrationSlotInterval {
+				continue
+			}
+
+			pk := phase0.BLSPubKey{}
+			copy(pk[:], share.ValidatorPubKey)
+
+			h.executeDuties(logger, []*spectypes.Duty{{
+				Type:   spectypes.BNRoleValidatorRegistration,
+				PubKey: pk,
+				Slot:   slot,
+				// no need for other params
+			}})
+
+			sent++
+			h.validatorsPassedFirstRegistration[string(share.ValidatorPubKey)] = struct{}{}
+		}
+		logger.Debug("validator registration duties sent", zap.Uint64("slot", uint64(slot)), fields.Count(sent))
+	}
+}
