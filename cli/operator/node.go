92ed66c641b47257d7d7a9500ddd37bc4e0078a7
moshe-blox <moshe@blox.io>
2023-07-09 11:16:42 +0300

    fix

--- cli/operator/node.go
+++ cli/operator/node.go
@@ -210,7 +210,7 @@ var StartNodeCmd = &cobra.Command{
 					// TODO: replace with narrower interface! (instead of accessing the entire PeersIndex)
 					PeersIndex: p2pNetwork.(p2pv1.PeersIndexProvider).PeersIndex(),
 					Network:    p2pNetwork.(p2pv1.HostProvider).Host().Network(),
-					TopicIndex: p2pNetwork.(apinode.TopicIndex),
+					TopicIndex: p2pNetwork.(handlers.TopicIndex),
 				},
 				&handlers.Validators{
 					Shares: nodeStorage.Shares(),
