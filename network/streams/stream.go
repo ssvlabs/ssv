8259133387060f9720c12eea6b39ef1db2ff0cd0
y0sher <lyosher@gmail.com>
2023-07-09 17:20:23 +0300

    add timeout to streams

--- network/streams/stream.go
+++ network/streams/stream.go
@@ -10,14 +10,7 @@ import (
 
 // Stream represents a stream in the system
 type Stream interface {
-	// ID returns the id of the stream
-	ID() string
-
-	io.Closer
-	// CloseWrite closes the stream for writing but leaves it open for
-	// reading. It does not free the stream, a call Close or
-	// Reset is required.
-	CloseWrite() error
+	core.Stream
 
 	// ReadWithTimeout will read bytes from stream and return the result, will return error if timeout or error.
 	// does not close stream when returns
@@ -30,49 +23,34 @@ type Stream interface {
 
 // streamWrapper is a wrapper struct for the core.Stream interface, implements Stream and network.SSVStream
 type streamWrapper struct {
-	s core.Stream
+	core.Stream
 }
 
 // NewStream returns a new instance of stream
 func NewStream(s core.Stream) Stream {
 	return &streamWrapper{
-		s: s,
+		Stream: s,
 	}
-}
-
-// Close closes the stream
-func (ts *streamWrapper) Close() error {
-	return ts.s.Close()
-}
-
-// CloseWrite closes write stream
-func (ts *streamWrapper) CloseWrite() error {
-	return ts.s.CloseWrite()
 }
 
 // ReadWithTimeout reads with timeout
 func (ts *streamWrapper) ReadWithTimeout(timeout time.Duration) ([]byte, error) {
-	if err := ts.s.SetReadDeadline(time.Now().Add(timeout)); err != nil {
+	if err := ts.Stream.SetReadDeadline(time.Now().Add(timeout)); err != nil {
 		return nil, errors.Wrap(err, "could not set read deadline")
 	}
-	return io.ReadAll(ts.s)
+	return io.ReadAll(ts.Stream)
 }
 
 // WriteWithTimeout reads next message with timeout
 func (ts *streamWrapper) WriteWithTimeout(data []byte, timeout time.Duration) error {
-	if err := ts.s.SetWriteDeadline(time.Now().Add(timeout)); err != nil {
+	if err := ts.Stream.SetWriteDeadline(time.Now().Add(timeout)); err != nil {
 		return errors.Wrap(err, "could not set write deadline")
 	}
 
 	n := len(data)
-	bytsWrote, err := ts.s.Write(data)
+	bytsWrote, err := ts.Stream.Write(data)
 	if bytsWrote != n {
 		return errors.Errorf("written bytes (%d) to sync stream doesnt match input data (%d)", bytsWrote, n)
 	}
 	return err
-}
-
-// ID returns the id of the stream
-func (ts *streamWrapper) ID() string {
-	return ts.s.ID()
 }
