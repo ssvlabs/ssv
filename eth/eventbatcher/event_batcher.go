6acb11d55e496b4e380d0209c7f4857753f29a23
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-07-17 17:16:15 +0400

    Add event batcher logging

--- eth/eventbatcher/event_batcher.go
+++ eth/eventbatcher/event_batcher.go
@@ -2,14 +2,27 @@ package eventbatcher
 
 import (
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	"go.uber.org/zap"
+
+	"github.com/bloxapp/ssv/logging/fields"
 )
 
 const defaultEventBuf = 1024
 
-type EventBatcher struct{}
+type EventBatcher struct {
+	logger *zap.Logger
+}
 
-func NewEventBatcher() *EventBatcher {
-	return &EventBatcher{}
+func NewEventBatcher(options ...Option) *EventBatcher {
+	eb := &EventBatcher{
+		logger: zap.NewNop(),
+	}
+
+	for _, opt := range options {
+		opt(eb)
+	}
+
+	return eb
 }
 
 type BlockEvents struct {
@@ -18,9 +31,12 @@ type BlockEvents struct {
 }
 
 func (eb *EventBatcher) BatchEvents(events <-chan ethtypes.Log) <-chan BlockEvents {
+	eb.logger.Debug("starting event batching")
+
 	blockEvents := make(chan BlockEvents, defaultEventBuf)
 	go func() {
 		defer close(blockEvents)
+
 		var currentBlockEvents BlockEvents
 		for event := range events {
 			if currentBlockEvents.BlockNumber == 0 {
@@ -33,13 +49,17 @@ func (eb *EventBatcher) BatchEvents(events <-chan ethtypes.Log) <-chan BlockEven
 					BlockNumber: event.BlockNumber,
 					Events:      []ethtypes.Log{event},
 				}
+				eb.logger.Debug("batched block events", fields.BlockNumber(currentBlockEvents.BlockNumber))
 			} else {
 				currentBlockEvents.Events = append(currentBlockEvents.Events, event)
 			}
 		}
 		if len(currentBlockEvents.Events) != 0 {
 			blockEvents <- currentBlockEvents
+			eb.logger.Debug("batched block events", fields.BlockNumber(currentBlockEvents.BlockNumber))
 		}
+
+		eb.logger.Debug("finished event batching")
 	}()
 
 	return blockEvents
