e1c59fd23716df653d79ea9153933f0c16065e39
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2023-07-20 11:53:47 +0300

    deprecate duty limit

--- operator/node.go
+++ operator/node.go
@@ -44,9 +44,7 @@ type Options struct {
 	Context             context.Context
 	DB                  basedb.IDb
 	ValidatorController validator.Controller
-	// max slots for duty to wait
-	DutyLimit        uint64                      `yaml:"DutyLimit" env:"DUTY_LIMIT" env-default:"32" env-description:"max slots to wait for duty to start"`
-	ValidatorOptions validator.ControllerOptions `yaml:"ValidatorOptions"`
+	ValidatorOptions    validator.ControllerOptions `yaml:"ValidatorOptions"`
 
 	ForkVersion forksprotocol.ForkVersion
 
