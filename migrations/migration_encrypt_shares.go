fa1cbf4c6a4c907b14f7ef84a7ea58734d8d6495
guy muroch <guym@blox.io>
2023-07-31 18:22:48 +0300

    change migration to work with txn

--- migrations/migration_encrypt_shares.go
+++ migrations/migration_encrypt_shares.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha256"
 	"crypto/x509"
 	"fmt"
+	"github.com/bloxapp/ssv/storage/basedb"
 
 	"go.uber.org/zap"
 )
@@ -12,35 +13,37 @@ import (
 var encryptSharesMigration = Migration{
 	Name: "encrypt_shares",
 	Run: func(ctx context.Context, logger *zap.Logger, opt Options, key []byte) error {
-		nodeStorage, err := opt.nodeStorage(logger)
-		if err != nil {
-			return fmt.Errorf("failed to get node storage: %w", err)
-		}
-		operatorKey, found, err := nodeStorage.GetPrivateKey()
-		if err != nil {
-			return fmt.Errorf("failed to get private key: %w", err)
-		}
-		if !found {
-			return nil
-		}
-		signerStorage := opt.signerStorage(logger)
-		accounts, err := signerStorage.ListAccounts()
-		if err != nil {
-			return fmt.Errorf("failed to list accounts: %w", err)
-		}
-		keyBytes := x509.MarshalPKCS1PrivateKey(operatorKey)
-		hash := sha256.Sum256(keyBytes)
-		keyString := fmt.Sprintf("%x", hash)
-		err = signerStorage.SetEncryptionKey(keyString)
-		if err != nil {
-			return fmt.Errorf("failed to set encryption key: %w", err)
-		}
-		for _, account := range accounts {
-			err := signerStorage.SaveAccount(account)
+		return opt.Db.Update(func(txn basedb.Txn) error {
+			nodeStorage, err := opt.nodeStorage(logger)
 			if err != nil {
-				return fmt.Errorf("failed to save account %s: %w", account, err)
+				return fmt.Errorf("failed to get node storage: %w", err)
 			}
-		}
-		return nil
+			operatorKey, found, err := nodeStorage.GetPrivateKey()
+			if err != nil {
+				return fmt.Errorf("failed to get private key: %w", err)
+			}
+			if !found {
+				return nil
+			}
+			signerStorage := opt.signerStorage(logger)
+			accounts, err := signerStorage.ListAccountsTxn(opt.Db)
+			if err != nil {
+				return fmt.Errorf("failed to list accounts: %w", err)
+			}
+			keyBytes := x509.MarshalPKCS1PrivateKey(operatorKey)
+			hash := sha256.Sum256(keyBytes)
+			keyString := fmt.Sprintf("%x", hash)
+			err = signerStorage.SetEncryptionKey(keyString)
+			if err != nil {
+				return fmt.Errorf("failed to set encryption key: %w", err)
+			}
+			for _, account := range accounts {
+				err := signerStorage.SaveAccountTxn(opt.Db, account)
+				if err != nil {
+					return fmt.Errorf("failed to save account %s: %w", account, err)
+				}
+			}
+			return txn.Set(migrationsPrefix, key, migrationCompleted)
+		})
 	},
 }
