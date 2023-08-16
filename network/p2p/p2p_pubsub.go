750353661ee35aa4257b0d48488331ba6d9d445b
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-08-16 20:25:24 +0400

    Add a debug log

--- network/p2p/p2p_pubsub.go
+++ network/p2p/p2p_pubsub.go
@@ -145,6 +145,7 @@ func (n *p2pNetwork) handlePubsubMessages(logger *zap.Logger) func(topic string,
 
 		var decodedMsg *queue.DecodedSSVMessage
 		if msg.ValidatorData != nil {
+			logger.Debug("msg.ValidatorData type: " + fmt.Sprintf("%T", msg.ValidatorData))
 			m, ok := msg.ValidatorData.(*queue.DecodedSSVMessage)
 			if ok {
 				decodedMsg = m
