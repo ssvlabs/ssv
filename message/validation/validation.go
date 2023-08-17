d18386c3731ffd4c125fa58e6ac27b5747db7869
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-08-17 15:24:45 +0400

    Enable event messages validation

--- message/validation/validation.go
+++ message/validation/validation.go
@@ -128,17 +128,15 @@ func (mv *MessageValidator) ValidateMessage(ssvMessage *spectypes.SSVMessage, re
 		return nil, fmt.Errorf("malformed message: %w", err)
 	}
 
-	return msg, nil // TODO: remove
-
 	switch ssvMessage.MsgType {
 	case spectypes.SSVConsensusMsgType:
-		if err := mv.validateConsensusMessage(share, msg, receivedAt); err != nil {
-			return nil, err
-		}
+		//if err := mv.validateConsensusMessage(share, msg, receivedAt); err != nil {
+		//	return nil, err
+		//}
 	case spectypes.SSVPartialSignatureMsgType:
-		if err := mv.validatePartialSignatureMessage(share, msg); err != nil {
-			return nil, err
-		}
+		//if err := mv.validatePartialSignatureMessage(share, msg); err != nil {
+		//	return nil, err
+		//}
 	case ssvmessage.SSVEventMsgType:
 		if err := mv.validateEventMessage(msg); err != nil {
 			return nil, err
