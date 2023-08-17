76210d92827cd818f41147e21a015645a7b7e4e9
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-08-17 19:01:30 +0400

    Remove return

--- message/validation/consensus_validation.go
+++ message/validation/consensus_validation.go
@@ -86,9 +86,6 @@ func (mv *MessageValidator) validateConsensusMessage(share *ssvtypes.SSVShare, m
 		return fmt.Errorf("malfrormed round change justifications: %w", err)
 	}
 
-	// TODO: do other checks
-	return nil
-
 	// TODO: checks for pj and rcj
 	_ = pj
 	_ = rcj
