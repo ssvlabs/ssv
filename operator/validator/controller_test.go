ee1f90cf0396fc970bc3d89001d6c0516451389e
guy muroch <guym@blox.io>
2023-08-22 18:58:39 +0300

    add tests for GetValidator and SetOperatorData

--- operator/validator/controller_test.go
+++ operator/validator/controller_test.go
@@ -3,7 +3,12 @@ package validator
 import (
 	"context"
 	"encoding/hex"
+	"github.com/bloxapp/ssv/network/forks/genesis"
+	"github.com/bloxapp/ssv/operator/validator/mocks"
+	"github.com/bloxapp/ssv/protocol/v2/queue/worker"
+	registrystorage "github.com/bloxapp/ssv/registry/storage"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/golang/mock/gomock"
 	"sync"
 	"testing"
 	"time"
@@ -17,10 +22,8 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 
-	"github.com/bloxapp/ssv/network/forks/genesis"
 	"github.com/bloxapp/ssv/protocol/v2/blockchain/beacon"
 	"github.com/bloxapp/ssv/protocol/v2/message"
-	"github.com/bloxapp/ssv/protocol/v2/queue/worker"
 	"github.com/bloxapp/ssv/protocol/v2/ssv/validator"
 	"github.com/bloxapp/ssv/protocol/v2/types"
 )
@@ -30,6 +33,7 @@ import (
 
 type MockControllerOptions struct {
 	validatorsMap map[string]*validator.Validator
+	operatorData  *registrystorage.OperatorData
 	sharesStorage SharesStorage
 }
 
@@ -82,6 +86,146 @@ func TestHandleNonCommitteeMessages(t *testing.T) {
 	}()
 
 	wg.Wait()
+}
+
+func TestAssertionOperatorData(t *testing.T) {
+	// Setup logger and mock controller
+	logger := logging.TestLogger(t)
+	ctrl := gomock.NewController(t)
+	sharesStorage := mocks.NewMockSharesStorage(ctrl)
+
+	operatorData := &registrystorage.OperatorData{
+		ID:           1,
+		PublicKey:    []byte("samplePublicKeyData"),
+		OwnerAddress: common.Address([]byte("67Ce5c69260bd819B4e0AD13f4b873074D479811")),
+	}
+
+	newOperatorData := &registrystorage.OperatorData{
+		ID:           2,
+		PublicKey:    []byte("samplePublicKeyData2"),
+		OwnerAddress: common.Address([]byte("64Ce5c69260bd819B4e0AD13f4b873074D479811")),
+	}
+
+	// Set up the controller with mock data
+	controllerOptions := MockControllerOptions{
+		sharesStorage: sharesStorage,
+		operatorData:  operatorData,
+	}
+	ctr := setupController(logger, controllerOptions)
+	ctr.SetOperatorData(newOperatorData)
+
+	// Compare the objects
+	require.Equal(t, newOperatorData, ctr.GetOperatorData(), "The operator data did not match the expected value")
+}
+
+func TestGetValidator(t *testing.T) {
+	// Setup logger and mock controller
+	logger := logging.TestLogger(t)
+
+	// Initialize a test validator with the decoded owner address
+	testValidator := &validator.Validator{
+		Share: &types.SSVShare{
+			Metadata: types.Metadata{},
+		},
+	}
+
+	// Set up the controller with mock data
+	controllerOptions := MockControllerOptions{
+		validatorsMap: map[string]*validator.Validator{
+			"0": testValidator,
+		},
+	}
+	ctr := setupController(logger, controllerOptions)
+
+	// Execute the function under test and validate results
+	_, found := ctr.GetValidator("0")
+	require.True(t, found)
+	_, found = ctr.GetValidator("1")
+	require.False(t, found)
+}
+
+func TestGetValidatorStats(t *testing.T) {
+	// Setup logger and mock controller
+	logger := logging.TestLogger(t)
+	ctrl := gomock.NewController(t)
+	sharesStorage := mocks.NewMockSharesStorage(ctrl)
+
+	// Define constants and sample data
+	passedEpoch := phase0.Epoch(1)
+	operatorIds := []uint64{1, 2}
+
+	// Create operators
+	operators := make([]*spectypes.Operator, len(operatorIds))
+	for i, id := range operatorIds {
+		operators[i] = &spectypes.Operator{OperatorID: id}
+	}
+
+	// Create a sample SSVShare slice
+	sharesSlice := []*types.SSVShare{
+		{
+			Share: spectypes.Share{
+				OperatorID: 1,
+				Committee:  operators,
+			},
+			Metadata: types.Metadata{
+				BeaconMetadata: &beacon.ValidatorMetadata{
+					Balance:         0,
+					Status:          3, // ValidatorStatePendingInitialized
+					Index:           0,
+					ActivationEpoch: passedEpoch,
+				},
+			},
+		},
+		{
+			Share: spectypes.Share{
+				OperatorID: 1,
+				Committee:  operators,
+			},
+			Metadata: types.Metadata{
+				BeaconMetadata: &beacon.ValidatorMetadata{
+					Balance:         0,
+					Status:          1, // ValidatorStatePendingInitialized
+					Index:           0,
+					ActivationEpoch: passedEpoch,
+				},
+			},
+		},
+		{
+			Share: spectypes.Share{
+				OperatorID: 2,
+				Committee:  operators,
+			},
+			Metadata: types.Metadata{
+				BeaconMetadata: &beacon.ValidatorMetadata{
+					Balance:         0,
+					Status:          1, // ValidatorStatePendingInitialized
+					Index:           0,
+					ActivationEpoch: passedEpoch,
+				},
+			},
+		},
+	}
+
+	// Set mock expectations
+	sharesStorage.EXPECT().List(nil).Return(sharesSlice).Times(1)
+
+	// Set up the controller with mock data
+	controllerOptions := MockControllerOptions{
+		sharesStorage: sharesStorage,
+		operatorData: &registrystorage.OperatorData{
+			ID:           1,
+			PublicKey:    []byte("samplePublicKeyData"),
+			OwnerAddress: common.Address([]byte("67Ce5c69260bd819B4e0AD13f4b873074D479811")),
+		},
+	}
+	ctr := setupController(logger, controllerOptions)
+
+	// Execute the function under test and validate results
+	allShares, activeShares, operatorShares, err := ctr.GetValidatorStats()
+	require.NoError(t, err, "Failed to get validator stats")
+	require.Equal(t, len(sharesSlice), int(allShares), "Unexpected total shares count")
+	require.Equal(t, 1, int(activeShares), "Unexpected active shares count")
+	require.Equal(t, 2, int(operatorShares), "Unexpected operator shares count")
 }
 
 func TestUpdateFeeRecipient(t *testing.T) {
@@ -198,9 +342,6 @@ func TestGetIndices(t *testing.T) {
 	ctr := setupController(logger, controllerOptions)
 
 	activeIndicesForCurrentEpoch := ctr.ActiveValidatorIndices(currentEpoch)
-	println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<,here")
-	println(len(activeIndicesForCurrentEpoch))
-	println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<,here")
 	require.Equal(t, 2, len(activeIndicesForCurrentEpoch)) // should return only active indices
 
 	activeIndicesForNextEpoch := ctr.ActiveValidatorIndices(currentEpoch + 1)
@@ -210,6 +351,7 @@ func TestGetIndices(t *testing.T) {
 func setupController(logger *zap.Logger, controllerOptions MockControllerOptions) controller {
 	return controller{
 		context:                    context.Background(),
+		operatorData:               controllerOptions.operatorData,
 		sharesStorage:              controllerOptions.sharesStorage,
 		beacon:                     nil,
 		logger:                     logger,
