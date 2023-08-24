05b4c41d0723b3440b3d0224523ce76e83147bd7
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-08-24 16:08:03 +0400

    Revert "Fix a comment"
    This reverts commit 188f86843330d7329932acdf73b3e1fa2c4121f4.

--- message/validation/consensus_validation.go
+++ message/validation/consensus_validation.go
@@ -156,7 +156,7 @@ func (mv *MessageValidator) validateSignerBehavior(state *ConsensusState, signer
 	}
 
 	if msgSlot > signerState.Slot || signedMsg.Message.Round > signerState.Round {
-		// State needs to be reset, and it means that behavioral checks below are not needed.
+		// State needs to be reset, and it means that behavioral checks below would pass.
 		return nil
 	}
 
