2fff9627ed55123fc37040973d76fb86bb6623ca
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-08-24 15:36:50 +0400

    Remove TODO

--- message/validation/consensus_validation.go
+++ message/validation/consensus_validation.go
@@ -155,7 +155,6 @@ func (mv *MessageValidator) validateSignerBehavior(state *ConsensusState, signer
 		return err
 	}
 
-	// Advance slot & round, if needed.
 	if msgSlot > signerState.Slot || signedMsg.Message.Round > signerState.Round {
 		// State needs to be reset, and it means that behavioral checks below would pass.
 		return nil
