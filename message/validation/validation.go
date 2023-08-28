383e31a7b40aa3dbc1ac3f5724883923c5702373
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-08-28 14:39:59 +0400

    Get rid of ErrNonCommitteeOnlySignedMessage

--- message/validation/validation.go
+++ message/validation/validation.go
@@ -162,12 +162,6 @@ func (mv *MessageValidator) ValidateMessage(ssvMessage *spectypes.SSVMessage, re
 		return nil, e
 	}
 
-	if nonCommittee && (ssvMessage.MsgType != spectypes.SSVConsensusMsgType) {
-		e := ErrNonCommitteeOnlySignedMessage
-		e.got = ssvMessage.MsgType
-		return nil, e
-	}
-
 	switch ssvMessage.MsgType {
 	case spectypes.SSVConsensusMsgType:
 		if err := mv.validateConsensusMessage(share, msg, nonCommittee, receivedAt); err != nil {
