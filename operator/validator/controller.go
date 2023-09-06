be5b42b615ecc660e01c4488808e9c0c605654ac
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-09-06 22:06:45 +0400

    Extract validator creation logic from validators map

--- operator/validator/controller.go
+++ operator/validator/controller.go
@@ -670,14 +670,42 @@ func (c *controller) onShareStart(share *ssvtypes.SSVShare) (bool, error) {
 	// Start a committee validator.
 	v, found := c.validatorsMap.GetValidator(hex.EncodeToString(share.ValidatorPubKey))
 	if !found {
-		createdValidator, err := c.validatorsMap.CreateValidator(c.logger.Named("ValidatorsMap"), share, c.validatorOptions, SetupRunners)
-		if err != nil {
-			return false, fmt.Errorf("could not get or create validator: %w", err)
+		if !share.HasBeaconMetadata() {
+			return false, fmt.Errorf("beacon metadata is missing")
 		}
 
-		v = createdValidator
+		// Share context with both the validator and the runners,
+		// so that when the validator is stopped, the runners are stopped as well.
+		ctx, cancel := context.WithCancel(c.context)
+
+		opts := c.validatorOptions
+		opts.SSVShare = share
+
+		opts.DutyRunners = SetupRunners(ctx, c.logger, opts)
+		v = validator.NewValidator(ctx, cancel, opts)
+
+		c.printShare(share, "setup validator done")
+		opts.SSVShare = nil
+
+		c.validatorsMap.CreateValidator(hex.EncodeToString(share.ValidatorPubKey), v)
+	} else {
+		c.printShare(v.Share, "get validator")
 	}
+
 	return c.startValidator(v)
+}
+
+func (c *controller) printShare(s *ssvtypes.SSVShare, msg string) {
+	committee := make([]string, len(s.Committee))
+	for i, c := range s.Committee {
+		committee[i] = fmt.Sprintf(`[OperatorID=%d, PubKey=%x]`, c.OperatorID, c.PubKey)
+	}
+	c.logger.Debug(msg,
+		fields.PubKey(s.ValidatorPubKey),
+		zap.Uint64("node_id", s.OperatorID),
+		zap.Strings("committee", committee),
+		fields.FeeRecipient(s.FeeRecipientAddress[:]),
+	)
 }
 
 func (c *controller) setShareFeeRecipient(share *ssvtypes.SSVShare, getRecipientData GetRecipientDataFunc) error {
--- operator/validatorsmap/validators_map.go
+++ operator/validatorsmap/validators_map.go
@@ -3,7 +3,6 @@ package validatorsmap
 // TODO(nkryuchkov): remove old validator interface(s)
 import (
 	"context"
-	"encoding/hex"
 	"fmt"
 	"sync"
 
@@ -12,7 +11,6 @@ import (
 	"go.uber.org/zap"
 
 	"github.com/bloxapp/ssv/logging/fields"
-	"github.com/bloxapp/ssv/protocol/v2/ssv/runner"
 	"github.com/bloxapp/ssv/protocol/v2/ssv/validator"
 	"github.com/bloxapp/ssv/protocol/v2/types"
 )
@@ -77,39 +75,13 @@ func (vm *ValidatorsMap) GetValidator(pubKey string) (*validator.Validator, bool
 }
 
 // CreateValidator creates a new validator instance
-func (vm *ValidatorsMap) CreateValidator(
-	logger *zap.Logger,
-	share *types.SSVShare,
-	optsTemplate validator.Options,
-	setupRunners func(ctx context.Context, logger *zap.Logger, options validator.Options) runner.DutyRunners,
-) (*validator.Validator, error) {
+// TODO: pass spectypes.ValidatorPK instead of string
+func (vm *ValidatorsMap) CreateValidator(pubKey string, v *validator.Validator) {
 	// main lock
 	vm.lock.Lock()
 	defer vm.lock.Unlock()
 
-	pubKey := hex.EncodeToString(share.ValidatorPubKey)
-	if v, ok := vm.validatorsMap[pubKey]; !ok {
-		if !share.HasBeaconMetadata() {
-			return nil, fmt.Errorf("beacon metadata is missing")
-		}
-		opts := optsTemplate
-		opts.SSVShare = share
-
-		// Share context with both the validator and the runners,
-		// so that when the validator is stopped, the runners are stopped as well.
-		ctx, cancel := context.WithCancel(vm.ctx)
-		opts.DutyRunners = setupRunners(ctx, logger, opts)
-		opts.MessageValidator = optsTemplate.MessageValidator
-		opts.Metrics = optsTemplate.Metrics
-		vm.validatorsMap[pubKey] = validator.NewValidator(ctx, cancel, opts)
-
-		printShare(share, logger, "setup validator done")
-		opts.SSVShare = nil
-	} else {
-		printShare(v.Share, logger, "get validator")
-	}
-
-	return vm.validatorsMap[pubKey], nil
+	vm.validatorsMap[pubKey] = v
 }
 
 // RemoveValidator removes a validator instance from the map
