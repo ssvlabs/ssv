91f1d9037169ad734e0bdb31da6dda3ea3cf1ce1
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-09-13 18:35:09 +0400

    Fix duty fetcher tests

--- operator/duties/dutyfetcher/duty_fetcher_test.go
+++ operator/duties/dutyfetcher/duty_fetcher_test.go
@@ -8,15 +8,18 @@ import (
 
 	eth2apiv1 "github.com/attestantio/go-eth2-client/api/v1"
 	"github.com/attestantio/go-eth2-client/spec/phase0"
+	spectypes "github.com/bloxapp/ssv-spec/types"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zaptest"
 
 	"github.com/bloxapp/ssv/networkconfig"
 	"github.com/bloxapp/ssv/operator/slot_ticker"
-	"github.com/bloxapp/ssv/operator/validatorsmap"
+	"github.com/bloxapp/ssv/operator/storage"
 	"github.com/bloxapp/ssv/protocol/v2/blockchain/beacon"
-	"github.com/bloxapp/ssv/protocol/v2/ssv/validator"
 	ssvtypes "github.com/bloxapp/ssv/protocol/v2/types"
+	"github.com/bloxapp/ssv/storage/basedb"
+	"github.com/bloxapp/ssv/storage/kv"
 )
 
 func TestFetcher_Start(t *testing.T) {
@@ -33,32 +36,41 @@ func TestFetcher_Start(t *testing.T) {
 	beaconNode.EXPECT().SyncCommitteeDuties(gomock.Any(), gomock.Any(), gomock.Any()).Return([]*eth2apiv1.SyncCommitteeDuty{{ValidatorIndex: 12}}, nil).AnyTimes()
 
 	slotTicker := slot_ticker.NewTicker(ctx, networkCfg)
-	vm := validatorsmap.New(ctx, validatorsmap.WithInitialState(map[string]*validator.Validator{
-		"1": {
-			Share: &ssvtypes.SSVShare{
-				Metadata: ssvtypes.Metadata{
-					BeaconMetadata: &beacon.ValidatorMetadata{
-						Status:          eth2apiv1.ValidatorStateActiveOngoing,
-						Index:           11,
-						ActivationEpoch: 0,
-					},
-				},
-			},
-		},
-		"2": {
-			Share: &ssvtypes.SSVShare{
-				Metadata: ssvtypes.Metadata{
-					BeaconMetadata: &beacon.ValidatorMetadata{
-						Status:          eth2apiv1.ValidatorStateActiveOngoing,
-						Index:           12,
-						ActivationEpoch: 0,
-					},
-				},
-			},
-		},
-	}))
 
-	f := New(beaconNode, slotTicker, vm)
+	logger := zaptest.NewLogger(t)
+	db, err := kv.NewInMemory(logger, basedb.Options{})
+	require.NoError(t, err)
+
+	ns, err := storage.NewNodeStorage(logger, db)
+	require.NoError(t, err)
+
+	shares := []*ssvtypes.SSVShare{
+		{
+			Share: spectypes.Share{
+				ValidatorPubKey: []byte{1},
+			},
+			Metadata: ssvtypes.Metadata{
+				BeaconMetadata: &beacon.ValidatorMetadata{
+					Status:          eth2apiv1.ValidatorStateActiveOngoing,
+					Index:           11,
+					ActivationEpoch: 0,
+				},
+			},
+		},
+		{
+			Metadata: ssvtypes.Metadata{
+				BeaconMetadata: &beacon.ValidatorMetadata{
+					Status:          eth2apiv1.ValidatorStateActiveOngoing,
+					Index:           12,
+					ActivationEpoch: 0,
+				},
+			},
+		},
+	}
+
+	require.NoError(t, ns.Shares().Save(nil, shares...))
+
+	f := New(beaconNode, slotTicker, ns.Shares())
 	f.ticker <- 21
 
 	go f.Start(ctx)
@@ -87,32 +99,40 @@ func TestFetcher_ProposerDuty(t *testing.T) {
 
 	slotTicker := slot_ticker.NewTicker(ctx, networkCfg)
 
-	vm := validatorsmap.New(ctx, validatorsmap.WithInitialState(map[string]*validator.Validator{
-		"1": {
-			Share: &ssvtypes.SSVShare{
-				Metadata: ssvtypes.Metadata{
-					BeaconMetadata: &beacon.ValidatorMetadata{
-						Status:          eth2apiv1.ValidatorStateActiveOngoing,
-						Index:           11,
-						ActivationEpoch: 0,
-					},
-				},
-			},
-		},
-		"2": {
-			Share: &ssvtypes.SSVShare{
-				Metadata: ssvtypes.Metadata{
-					BeaconMetadata: &beacon.ValidatorMetadata{
-						Status:          eth2apiv1.ValidatorStateActiveOngoing,
-						Index:           12,
-						ActivationEpoch: 0,
-					},
-				},
-			},
-		},
-	}))
+	logger := zaptest.NewLogger(t)
+	db, err := kv.NewInMemory(logger, basedb.Options{})
+	require.NoError(t, err)
 
-	f := New(beaconNode, slotTicker, vm)
+	ns, err := storage.NewNodeStorage(logger, db)
+	require.NoError(t, err)
+
+	shares := []*ssvtypes.SSVShare{
+		{
+			Share: spectypes.Share{
+				ValidatorPubKey: []byte{1},
+			},
+			Metadata: ssvtypes.Metadata{
+				BeaconMetadata: &beacon.ValidatorMetadata{
+					Status:          eth2apiv1.ValidatorStateActiveOngoing,
+					Index:           11,
+					ActivationEpoch: 0,
+				},
+			},
+		},
+		{
+			Metadata: ssvtypes.Metadata{
+				BeaconMetadata: &beacon.ValidatorMetadata{
+					Status:          eth2apiv1.ValidatorStateActiveOngoing,
+					Index:           12,
+					ActivationEpoch: 0,
+				},
+			},
+		},
+	}
+
+	require.NoError(t, ns.Shares().Save(nil, shares...))
+
+	f := New(beaconNode, slotTicker, ns.Shares())
 	go f.Start(ctx)
 
 	proposerDuties := map[phase0.ValidatorIndex]*eth2apiv1.ProposerDuty{
@@ -149,32 +169,40 @@ func TestFetcher_Start_FetchEpoch_Error(t *testing.T) {
 
 	slotTicker := slot_ticker.NewTicker(ctx, networkCfg)
 
-	vm := validatorsmap.New(ctx, validatorsmap.WithInitialState(map[string]*validator.Validator{
-		"1": {
-			Share: &ssvtypes.SSVShare{
-				Metadata: ssvtypes.Metadata{
-					BeaconMetadata: &beacon.ValidatorMetadata{
-						Status:          eth2apiv1.ValidatorStateActiveOngoing,
-						Index:           11,
-						ActivationEpoch: 0,
-					},
-				},
-			},
-		},
-		"2": {
-			Share: &ssvtypes.SSVShare{
-				Metadata: ssvtypes.Metadata{
-					BeaconMetadata: &beacon.ValidatorMetadata{
-						Status:          eth2apiv1.ValidatorStateActiveOngoing,
-						Index:           12,
-						ActivationEpoch: 0,
-					},
-				},
-			},
-		},
-	}))
+	logger := zaptest.NewLogger(t)
+	db, err := kv.NewInMemory(logger, basedb.Options{})
+	require.NoError(t, err)
 
-	f := New(beaconNode, slotTicker, vm)
+	ns, err := storage.NewNodeStorage(logger, db)
+	require.NoError(t, err)
+
+	shares := []*ssvtypes.SSVShare{
+		{
+			Share: spectypes.Share{
+				ValidatorPubKey: []byte{1},
+			},
+			Metadata: ssvtypes.Metadata{
+				BeaconMetadata: &beacon.ValidatorMetadata{
+					Status:          eth2apiv1.ValidatorStateActiveOngoing,
+					Index:           11,
+					ActivationEpoch: 0,
+				},
+			},
+		},
+		{
+			Metadata: ssvtypes.Metadata{
+				BeaconMetadata: &beacon.ValidatorMetadata{
+					Status:          eth2apiv1.ValidatorStateActiveOngoing,
+					Index:           12,
+					ActivationEpoch: 0,
+				},
+			},
+		},
+	}
+
+	require.NoError(t, ns.Shares().Save(nil, shares...))
+
+	f := New(beaconNode, slotTicker, ns.Shares())
 	go f.Start(ctx)
 
 	go f.Start(ctx)
