6ff150691f85b92e5cbc7fe5dcd164f7278338b7
Matus Kysel <matuskysel1@gmail.com>
2023-09-18 10:46:05 +0200

    feat: new slot_ticker prototype

--- operator/slot_ticker/slotticker.go
+++ operator/slot_ticker/slotticker.go
@@ -86,3 +86,41 @@ func (s *SlotTicker) start(
 		}
 	}()
 }
+
+type SlotTicker2 struct {
+	timer        *time.Timer
+	slotDuration time.Duration
+	genesisTime  time.Time
+}
+
+func NewSlotTicker2(slotDuration time.Duration, genesisTime time.Time) *SlotTicker2 {
+	now := time.Now()
+	timeSinceGenesis := now.Sub(genesisTime)
+
+	var initialDelay time.Duration
+	if timeSinceGenesis < 0 {
+		// Genesis time is in the future
+		initialDelay = -timeSinceGenesis // Wait until the genesis time
+	} else {
+		slotsSinceGenesis := timeSinceGenesis / slotDuration
+		nextSlotStartTime := genesisTime.Add((slotsSinceGenesis + 1) * slotDuration)
+		initialDelay = time.Until(nextSlotStartTime)
+	}
+
+	return &SlotTicker2{
+		timer:        time.NewTimer(initialDelay),
+		slotDuration: slotDuration,
+		genesisTime:  genesisTime,
+	}
+}
+
+func (s *SlotTicker2) Next() (<-chan time.Time, uint64) {
+	timeSinceGenesis := time.Since(s.genesisTime)
+	if timeSinceGenesis < 0 {
+		return s.timer.C, 0
+	}
+	slotNumber := uint64(timeSinceGenesis / s.slotDuration)
+	nextSlotStartTime := s.genesisTime.Add(time.Duration(slotNumber+1) * s.slotDuration)
+	s.timer.Reset(time.Until(nextSlotStartTime))
+	return s.timer.C, slotNumber + 1
+}
---
+++ operator/slot_ticker/slotticker_test.go
@@ -0,0 +1,153 @@
+package slot_ticker
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestSlotTicker2(t *testing.T) {
+	const numTicks = 3
+	slotDuration := 200 * time.Millisecond
+	// Set the genesis time such that we start from slot 1
+	genesisTime := time.Now().Truncate(slotDuration).Add(-slotDuration)
+
+	// Calculate the expected starting slot based on genesisTime
+	timeSinceGenesis := time.Since(genesisTime)
+	expectedSlot := uint64(timeSinceGenesis/slotDuration) + 1
+
+	ticker := NewSlotTicker2(slotDuration, genesisTime)
+
+	for i := 0; i < numTicks; i++ {
+		tickChan, slot := ticker.Next()
+		<-tickChan
+
+		if slot != expectedSlot {
+			t.Errorf("Expected slot %d, but got slot %d", expectedSlot, slot)
+			break
+		}
+		expectedSlot++
+	}
+}
+
+func TestTickerInitialization(t *testing.T) {
+	slotDuration := 200 * time.Millisecond
+	genesisTime := time.Now()
+	ticker := NewSlotTicker2(slotDuration, genesisTime)
+
+	start := time.Now()
+	ch, slot := ticker.Next() // Ignoring the slot number here with "_"
+	<-ch
+
+	// Allow a small buffer (e.g., 10ms) due to code execution overhead
+	buffer := 10 * time.Millisecond
+
+	if elapsed := time.Since(start); elapsed+buffer < slotDuration {
+		t.Errorf("First tick occurred too soon: %v", elapsed)
+	}
+
+	if slot != 1 {
+		t.Errorf("Expected slot 1, but got slot %d", slot)
+	}
+}
+
+func TestSlotNumberConsistency(t *testing.T) {
+	slotDuration := 200 * time.Millisecond
+	genesisTime := time.Now()
+
+	ticker := NewSlotTicker2(slotDuration, genesisTime)
+	var lastSlot uint64
+
+	for i := 0; i < 10; i++ {
+		ch, slot := ticker.Next()
+		<-ch
+		if lastSlot != 0 && slot != lastSlot+1 {
+			t.Errorf("Expected slot %d, got %d", lastSlot+1, slot)
+			break
+		}
+		lastSlot = slot
+	}
+}
+
+func TestGenesisInFuture(t *testing.T) {
+	slotDuration := 200 * time.Millisecond
+	genesisTime := time.Now().Add(1 * time.Second) // Setting genesis time 1s in the future
+
+	ticker := NewSlotTicker2(slotDuration, genesisTime)
+	start := time.Now()
+
+	tickChan, _ := ticker.Next() // Ignoring the slot number with '_'
+	<-tickChan
+
+	// The first tick should occur after the genesis time
+	expectedFirstTickDuration := genesisTime.Sub(start)
+	actualFirstTickDuration := time.Since(start)
+
+	// Allow a small buffer (e.g., 10ms) due to code execution overhead
+	buffer := 10 * time.Millisecond
+
+	if actualFirstTickDuration+buffer < expectedFirstTickDuration {
+		t.Errorf("First tick occurred too soon. Expected at least: %v, but got: %v", expectedFirstTickDuration, actualFirstTickDuration)
+	}
+}
+
+func TestBoundedDrift(t *testing.T) {
+	slotDuration := 20 * time.Millisecond
+	genesisTime := time.Now()
+
+	ticker := NewSlotTicker2(slotDuration, genesisTime)
+	ticks := 100
+
+	start := time.Now()
+	for i := 0; i < ticks; i++ {
+		ch, _ := ticker.Next() // Ignoring the slot number here with "_"
+		<-ch
+	}
+	expectedDuration := time.Duration(ticks) * slotDuration
+	elapsed := time.Since(start)
+
+	// We'll allow a small buffer for drift, say 1%
+	buffer := expectedDuration * 1 / 100
+	if elapsed < expectedDuration-buffer || elapsed > expectedDuration+buffer {
+		t.Errorf("Drifted too far from expected time. Expected: %v, Actual: %v", expectedDuration, elapsed)
+	}
+}
+
+func TestMultipleSlotTickers(t *testing.T) {
+	const (
+		numTickers    = 1000
+		ticksPerTimer = 3
+	)
+
+	slotDuration := 200 * time.Millisecond
+	genesisTime := time.Now()
+
+	// Start the clock to time the full execution of all tickers
+	start := time.Now()
+
+	var wg sync.WaitGroup
+	wg.Add(numTickers)
+
+	for i := 0; i < numTickers; i++ {
+		go func() {
+			defer wg.Done()
+			ticker := NewSlotTicker2(slotDuration, genesisTime)
+			for j := 0; j < ticksPerTimer; j++ {
+				ch, _ := ticker.Next() // Ignoring the slot number here with "_"
+				<-ch
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	// Calculate the total time taken for all tickers to complete their ticks
+	elapsed := time.Since(start)
+	expectedDuration := slotDuration * ticksPerTimer
+
+	// We'll allow a small buffer for drift, say 1%
+	buffer := expectedDuration * 1 / 100
+	if elapsed > expectedDuration+buffer {
+		t.Errorf("Expected all tickers to complete within %v but took %v", expectedDuration, elapsed)
+	}
+}
