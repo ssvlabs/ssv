9e7fc69fd740857fec2b4fa48911a62085d07133
moshe-blox <moshe@blox.io>
2023-10-27 21:46:42 +0300

    remove first registration condition

--- operator/duties/validatorregistration.go
+++ operator/duties/validatorregistration.go
@@ -2,27 +2,20 @@ package duties
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/attestantio/go-eth2-client/spec/phase0"
 	spectypes "github.com/bloxapp/ssv-spec/types"
 	"go.uber.org/zap"
-
-	"github.com/bloxapp/ssv/logging/fields"
 )
 
 const validatorRegistrationEpochInterval = uint64(10)
 
 type ValidatorRegistrationHandler struct {
 	baseHandler
-
-	validatorsPassedFirstRegistration map[string]struct{}
 }
 
 func NewValidatorRegistrationHandler() *ValidatorRegistrationHandler {
-	return &ValidatorRegistrationHandler{
-		validatorsPassedFirstRegistration: map[string]struct{}{},
-	}
+	return &ValidatorRegistrationHandler{}
 }
 
 func (h *ValidatorRegistrationHandler) Name() string {
@@ -40,15 +33,9 @@ func (h *ValidatorRegistrationHandler) HandleDuties(ctx context.Context) {
 		case slot := <-h.ticker:
 			shares := h.validatorController.GetOperatorShares()
 
-			sent := 0
-			sharesChecked := 0
-			noMetadata := 0
-			notThisSlot := 0
-			validators := make(map[string]bool)
+			validators := []phase0.ValidatorIndex{}
 			for _, share := range shares {
-				sharesChecked++
 				if !share.HasBeaconMetadata() {
-					noMetadata++
 					continue
 				}
 
@@ -58,11 +45,8 @@ func (h *ValidatorRegistrationHandler) HandleDuties(ctx context.Context) {
 				registrationSlotInterval *= validatorRegistrationEpochInterval
 
 				if uint64(share.BeaconMetadata.Index)%registrationSlotInterval != uint64(slot)%registrationSlotInterval {
-					validators[fmt.Sprint(share.BeaconMetadata.Index)] = true
-					notThisSlot++
 					continue
 				}
-				validators[fmt.Sprint(share.BeaconMetadata.Index)] = false
 
 				pk := phase0.BLSPubKey{}
 				copy(pk[:], share.ValidatorPubKey)
@@ -74,16 +58,11 @@ func (h *ValidatorRegistrationHandler) HandleDuties(ctx context.Context) {
 					// no need for other params
 				}})
 
-				sent++
-				h.validatorsPassedFirstRegistration[string(share.ValidatorPubKey)] = struct{}{}
+				validators = append(validators, share.BeaconMetadata.Index)
 			}
 			h.logger.Debug("validator registration duties sent",
-				zap.Uint64("shares", uint64(sharesChecked)),
-				zap.Uint64("no_metadata", uint64(noMetadata)),
-				zap.Uint64("not_this_slot", uint64(notThisSlot)),
-				zap.Uint64("slot", uint64(slot)), fields.Count(sent),
-				zap.Any("validators", validators),
-			)
+				zap.Uint64("slot", uint64(slot)),
+				zap.Any("validators", validators))
 
 		case <-h.indicesChange:
 			continue
