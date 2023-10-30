e42787beb307b6c03f77305118826182a559ef08
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-10-30 17:52:16 +0300

    Check BLS only in committee decided

--- message/validation/consensus_validation.go
+++ message/validation/consensus_validation.go
@@ -116,7 +116,7 @@ func (mv *messageValidator) validateConsensusMessage(
 		}
 	}
 
-	if mv.verifySignatures && signedMsg.Message.MsgType == specqbft.CommitMsgType {
+	if mv.verifySignatures && mv.inCommittee(share) && mv.isDecidedMessage(signedMsg) {
 		if err := ssvtypes.VerifyByOperators(signedMsg.Signature, signedMsg, mv.netCfg.Domain, spectypes.QBFTSignatureType, share.Committee); err != nil {
 			signErr := ErrInvalidSignature
 			signErr.innerErr = err
