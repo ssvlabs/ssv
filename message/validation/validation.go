98a3f960f1b38b935f4f0c4127bf6869228fd304
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-11-01 18:06:16 +0300

    Cleanup

--- message/validation/validation.go
+++ message/validation/validation.go
@@ -56,33 +56,6 @@ const (
 	maxDutiesPerEpoch          = 2
 )
 
-// ConsensusID uniquely identifies a public key and role pair to keep track of state.
-type ConsensusID struct {
-	PubKey phase0.BLSPubKey
-	Role   spectypes.BeaconRole
-}
-
-// ConsensusState keeps track of the signers for a given public key and role.
-type ConsensusState struct {
-	// TODO: consider evicting old data to avoid excessive memory consumption
-	Signers *hashmap.Map[spectypes.OperatorID, *SignerState]
-}
-
-// GetSignerState retrieves the state for the given signer.
-// Returns nil if the signer is not found.
-func (cs *ConsensusState) GetSignerState(signer spectypes.OperatorID) *SignerState {
-	signerState, _ := cs.Signers.Get(signer)
-	return signerState
-}
-
-// CreateSignerState initializes and sets a new SignerState for the given signer.
-func (cs *ConsensusState) CreateSignerState(signer spectypes.OperatorID) *SignerState {
-	signerState := &SignerState{}
-	cs.Signers.Set(signer, signerState)
-
-	return signerState
-}
-
 // PubsubMessageValidator defines methods for validating pubsub messages.
 type PubsubMessageValidator interface {
 	ValidatorForTopic(topic string) func(ctx context.Context, p peer.ID, pmsg *pubsub.Message) pubsub.ValidationResult
