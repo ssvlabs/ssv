f2d8e0d50e6acfbd1274a6ebb361e3bfc27660e1
moshe-blox <moshe@blox.io>
2023-11-06 20:46:34 +0200

    deploy fix & new db to ssv-node-3

--- .k8/production/prater/scripts/deploy-cluster-1--4.sh
+++ .k8/production/prater/scripts/deploy-cluster-1--4.sh
@@ -107,7 +107,7 @@ DIR=".k8/production/prater"
 DEPLOY_FILES=(
   # "ssv-node-prater-1-deployment.yml"
   "ssv-node-prater-2-deployment.yml"
-  # "ssv-node-prater-3-deployment.yml"
+  "ssv-node-prater-3-deployment.yml"
   # "ssv-node-prater-4-deployment.yml"
 )
 
