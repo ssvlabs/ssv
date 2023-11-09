0dd8c0d4a01b6d673c950cf52f505846c248ccc8
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-11-09 16:30:26 +0300

    Remove unused argument from SubmitVoluntaryExit

--- beacon/goclient/voluntary_exit.go
+++ beacon/goclient/voluntary_exit.go
@@ -4,6 +4,6 @@ import (
 	"github.com/attestantio/go-eth2-client/spec/phase0"
 )
 
-func (gc *goClient) SubmitVoluntaryExit(voluntaryExit *phase0.SignedVoluntaryExit, sig phase0.BLSSignature) error {
+func (gc *goClient) SubmitVoluntaryExit(voluntaryExit *phase0.SignedVoluntaryExit, _ phase0.BLSSignature) error {
 	return gc.client.SubmitVoluntaryExit(gc.ctx, voluntaryExit)
 }
