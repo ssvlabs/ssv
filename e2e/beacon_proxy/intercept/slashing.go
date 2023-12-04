e232c641e1ed3e17d61472f0fb65a33f02dd2a9c
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2023-12-04 13:19:09 +0400

    Implement blocked epoch

--- e2e/beacon_proxy/intercept/slashing.go
+++ e2e/beacon_proxy/intercept/slashing.go
@@ -7,6 +7,10 @@ import (
 	v1 "github.com/attestantio/go-eth2-client/api/v1"
 	"github.com/attestantio/go-eth2-client/spec"
 	"github.com/attestantio/go-eth2-client/spec/phase0"
+	"go.uber.org/zap"
+
+	"github.com/bloxapp/ssv/logging/fields"
+	"github.com/bloxapp/ssv/networkconfig"
 )
 
 type validatorState struct {
@@ -16,17 +20,31 @@ type validatorState struct {
 	firstBlock           *spec.VersionedBeaconBlock
 }
 
+type epochState struct {
+	defined bool
+	start   phase0.Epoch
+	sleep   phase0.Epoch
+	end     phase0.Epoch
+}
+
 type SlashingInterceptor struct {
+	logger             *zap.Logger
+	netCfg             networkconfig.NetworkConfig
 	validators         map[phase0.ValidatorIndex]*validatorState
 	fakeProposerDuties bool
 	mu                 sync.RWMutex
+	epochState         epochState
 }
 
 func NewSlashingInterceptor(
+	logger *zap.Logger,
+	netCfg networkconfig.NetworkConfig,
 	validators []*v1.Validator,
 	fakeDoubleProposerDuties bool,
 ) *SlashingInterceptor {
 	s := &SlashingInterceptor{
+		logger:             logger,
+		netCfg:             netCfg,
 		validators:         make(map[phase0.ValidatorIndex]*validatorState),
 		fakeProposerDuties: fakeDoubleProposerDuties,
 	}
@@ -38,6 +56,16 @@ func NewSlashingInterceptor(
 	return s
 }
 
+func (s *SlashingInterceptor) awaitStart(startCh <-chan struct{}) {
+	<-startCh
+
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.epochState.defined = true
+	s.epochState.start = s.netCfg.Beacon.EstimatedCurrentEpoch() + 1
+}
+
 func (s *SlashingInterceptor) InterceptAttesterDuties(
 	ctx context.Context,
 	epoch phase0.Epoch,
@@ -46,6 +74,10 @@ func (s *SlashingInterceptor) InterceptAttesterDuties(
 ) ([]*v1.AttesterDuty, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
+
+	if s.isBlockedEpoch(epoch) {
+		return []*v1.AttesterDuty{}, nil
+	}
 
 	for _, duty := range duties {
 		state, ok := s.validators[duty.ValidatorIndex]
@@ -64,10 +96,20 @@ func (s *SlashingInterceptor) InterceptAttestationData(
 	ctx context.Context,
 	slot phase0.Slot,
 	committeeIndex phase0.CommitteeIndex,
-	data phase0.AttestationData,
-) (phase0.AttestationData, error) {
+	data *phase0.AttestationData,
+) (*phase0.AttestationData, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
+
+	epoch := s.netCfg.Beacon.EstimatedEpochAtSlot(slot)
+	if s.isBlockedEpoch(epoch) {
+		s.logger.Warn("unexpected request",
+			fields.Name("InterceptAttestationData"),
+			fields.Slot(slot),
+			fields.Epoch(epoch))
+
+		return nil, nil
+	}
 
 	for _, state := range s.validators {
 		// Skip validators that are not in the requested committee.
@@ -78,7 +120,7 @@ func (s *SlashingInterceptor) InterceptAttestationData(
 
 		// Record the first attestation data.
 		if state.firstAttestationData == nil {
-			state.firstAttestationData = &data
+			state.firstAttestationData = data
 			continue
 		}
 
@@ -98,6 +140,10 @@ func (s *SlashingInterceptor) InterceptProposerDuties(
 ) ([]*v1.ProposerDuty, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
+
+	if s.isBlockedEpoch(epoch) {
+		return []*v1.ProposerDuty{}, nil
+	}
 
 	if !s.fakeProposerDuties {
 		return duties, nil
@@ -130,6 +176,16 @@ func (s *SlashingInterceptor) InterceptBlockProposal(
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
+	epoch := s.netCfg.Beacon.EstimatedEpochAtSlot(slot)
+	if s.isBlockedEpoch(epoch) {
+		s.logger.Warn("unexpected request",
+			fields.Name("InterceptBlockProposal"),
+			fields.Slot(slot),
+			fields.Epoch(epoch))
+
+		return nil, nil
+	}
+
 	for _, state := range s.validators {
 		// Skip forward to the proposer.
 		if state.firstAttesterDuty == nil || state.firstAttesterDuty.Slot != slot {
@@ -147,4 +203,8 @@ func (s *SlashingInterceptor) InterceptBlockProposal(
 		block.Capella.Slot = state.firstBlock.Capella.Slot
 	}
 	return block, nil
+}
+
+func (s *SlashingInterceptor) isBlockedEpoch(epoch phase0.Epoch) bool {
+	return !s.epochState.defined || epoch < s.epochState.start || epoch == s.epochState.sleep || epoch > s.epochState.end
 }
