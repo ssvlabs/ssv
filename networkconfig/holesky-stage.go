b7e5aa4d25639b0df84ead0901f5d6a6d7fc3786
moshe-blox <moshe@blox.io>
2023-12-07 15:39:56 +0200

    update ENR after UDP port opened

--- networkconfig/holesky-stage.go
+++ networkconfig/holesky-stage.go
@@ -16,7 +16,7 @@ var HoleskyStage = NetworkConfig{
 	RegistrySyncOffset:   new(big.Int).SetInt64(84599),
 	RegistryContractAddr: "0x0d33801785340072C452b994496B19f196b7eE15",
 	Bootnodes: []string{
-		"enr:-Li4QAOMksKOEOrBWAN3YJnOdnvze1m6rI8baqiOs4LAEBJvMUPzUnzIIfxw74X7PXSi_Nx-IQzU2GpvlwmSq8Ir8zOGAYxEeMS2h2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__________gmlkgnY0gmlwhDQiKqmJc2VjcDI1NmsxoQP2e508AoA0B-KH-IaAd3nVCfI9q16lNztV-oTpcH72tIN0Y3CCE4mDdWRwgg-h",
+		"enr:-Li4QPnPGESWx2wnu3s2qeu6keFbkaV2M0ZiGHgxxGI9ThP4XSgSaFzl6zYsF1zAdni3Mh04iA6BEZqoC6LZ52UFnwKGAYxEgLqeh2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__________gmlkgnY0gmlwhDQiKqmJc2VjcDI1NmsxoQP2e508AoA0B-KH-IaAd3nVCfI9q16lNztV-oTpcH72tIN0Y3CCE4mDdWRwgg-h",
 	},
 	WhitelistedOperatorKeys:       []string{},
 	PermissionlessActivationEpoch: 10560,
