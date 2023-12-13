e6a38ed319d4a21ff625f1039864dbcf812fc5df
y0sher <lyosher@gmail.com>
2023-12-13 23:32:44 +0200

    try to clean up before exiting to have the exit code.

--- e2e/run.sh
+++ e2e/run.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-cleanup() {
-    code=$?
-    docker compose down
-    exit $code
-}
-trap cleanup EXIT
-
 check_exit_code () {
    if [ $1 -ne 0 ]; then
        echo "Tests failed with exit code $1"
+       docker compose down
        exit $exit_code
    else
        echo "Tests passed successfully"
@@ -20,6 +14,7 @@ check_exit_code () {
 docker compose up -d --build beacon_proxy ssv-node-1 ssv-node-2 ssv-node-3 ssv-node-4
 
 docker compose run --build logs_catcher
+
 check_exit_code $?
 
 # run more tests
