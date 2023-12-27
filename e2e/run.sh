4aa689d2d68109edf17a9759313742ddc23ec0b8
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2023-12-27 17:27:45 +0200

    clean run script

--- e2e/run.sh
+++ e2e/run.sh
@@ -9,11 +9,11 @@ docker compose run --build logs_catcher logs-catcher --mode Slashing
 # Step 3: Stop the services
 docker compose down
 
-# Step 4. Run share_update for non leader
-docker compose run --build share_update share-update
+# Step 4. Run share_update for BlsVerification test
+docker compose run --build share_update
 
 # Step 6: Start the beacon_proxy and ssv-nodes again
 docker compose up -d beacon_proxy ssv-node-1 ssv-node-2 ssv-node-3 ssv-node-4
 
-# Step 7: Run logs_catcher in Mode BlsVerification for non leader
-docker compose run --build logs_catcher logs-catcher --mode BlsVerification
+# Step 7: Run logs_catcher in Mode BlsVerification
+docker compose run logs_catcher logs-catcher --mode BlsVerification
