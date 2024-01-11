7072af717920c8303c683e8a3ce83de00acfc27a
moshe-blox <moshe@blox.io>
2024-01-11 16:42:43 +0200

    Revert "lint fixes"
    This reverts commit 179bec46bd3539e53bda241817f509efd56334b7.

--- eth/ethtest/utils_test.go
+++ eth/ethtest/utils_test.go
@@ -254,7 +254,6 @@ func setupOperatorStorage(
 ) (operatorstorage.Storage, *registrystorage.OperatorData) {
 	if operator == nil {
 		logger.Fatal("empty test operator was passed")
-		return nil, nil // Linter needs to see this.
 	}
 
 	nodeStorage, err := operatorstorage.NewNodeStorage(logger, db)
