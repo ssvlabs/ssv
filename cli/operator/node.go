bab9523114cad3ca3ebfc0de51a4e4b0a0c9091f
moshe-blox <89339422+moshe-blox@users.noreply.github.com>
2024-01-22 15:10:16 +0200

    fix: (SlotTicker) potential double tick (#1244)
    * fix: (SlotTicker) potential double tick
    
    ---------
    
    Co-authored-by: Matus Kysel <MatusKysel@users.noreply.github.com>

--- cli/operator/node.go
+++ cli/operator/node.go
@@ -141,7 +141,10 @@ var StartNodeCmd = &cobra.Command{
 		}
 
 		slotTickerProvider := func() slotticker.SlotTicker {
-			return slotticker.New(networkConfig)
+			return slotticker.New(logger, slotticker.Config{
+				SlotDuration: networkConfig.SlotDurationSec(),
+				GenesisTime:  networkConfig.GetGenesisTime(),
+			})
 		}
 
 		cfg.ConsensusClient.Context = cmd.Context()
