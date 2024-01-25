903558abf613176bb5df713d4fe34b87752f72e2
guym-blox <83158283+guym-blox@users.noreply.github.com>
2024-01-25 15:17:36 +0200

    test upload-artifact

--- e2e/run.sh
+++ e2e/run.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+set -e
+trap 'catch $?' EXIT
+
+# Get the directory of the script itself
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Set LOG_DIR to a 'logs' directory within the same directory as the script
+LOG_DIR="$SCRIPT_DIR/crash-logs"
+
+catch() {
+  if [ "$1" != "0" ]; then
+    echo "Error $1 occurred. Saving logs..."
+    save_logs
+  fi
+}
+
+save_logs() {
+  echo "Creating directory at: $LOG_DIR"
+  mkdir -p "$LOG_DIR"
+  # Saving logs of each container separately
+  docker compose logs ssv-node-1 > "$LOG_DIR/ssv-node-1.txt"
+  docker compose logs ssv-node-2 > "$LOG_DIR/ssv-node-2.txt"
+  docker compose logs ssv-node-3 > "$LOG_DIR/ssv-node-3.txt"
+  docker compose logs ssv-node-4 > "$LOG_DIR/ssv-node-4.txt"
+  docker compose logs ssv-node-4 > "$LOG_DIR/beacon_proxy.txt"
+}
+
 export BEACON_NODE_URL=http://bn-h-2.stage.bloxinfra.com:3502/
 export EXECUTION_NODE_URL=ws://bn-h-2.stage.bloxinfra.com:8557/ws
 
