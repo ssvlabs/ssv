1f24e919807867c121d65076bdbcda83eb48d27b
y0sher <lyosher@gmail.com>
2024-01-30 17:42:40 +0200

    stop run if any command exited with non-zero exit code.

--- e2e/run.sh
+++ e2e/run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#!/bin/bash
+
+# Exit on error
+set -e
 
 # Step 1: Start the beacon_proxy and ssv-node services
 docker compose up -d --build beacon_proxy ssv-node-1 ssv-node-2 ssv-node-3 ssv-node-4
