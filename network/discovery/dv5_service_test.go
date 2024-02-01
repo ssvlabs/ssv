232b3ffa2c4b79ff1e488f1346ece29b2519b29d
pavelkrolevets <pavelkrolevets@gmail.com>
2024-02-01 18:30:13 +0600

    add dv5 service test

--- network/discovery/dv5_service_test.go
+++ network/discovery/dv5_service_test.go
@@ -3,138 +3,605 @@ package discovery
 import (
 	"context"
 	"net"
-	"os"
+	"reflect"
 	"testing"
+	"time"
 
-	spectypes "github.com/bloxapp/ssv-spec/types"
-	"github.com/bloxapp/ssv/network/commons"
-	"github.com/bloxapp/ssv/network/peers"
-	"github.com/bloxapp/ssv/network/peers/connections/mock"
-	"github.com/bloxapp/ssv/network/records"
-	"github.com/bloxapp/ssv/utils"
+	"github.com/ethereum/go-ethereum/p2p/discover"
 	"github.com/ethereum/go-ethereum/p2p/enode"
-	"github.com/pkg/errors"
+	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+
+	spectypes "github.com/bloxapp/ssv-spec/types"
+	"github.com/bloxapp/ssv/logging"
+	"github.com/bloxapp/ssv/network/peers"
+	"github.com/bloxapp/ssv/utils"
 )
 
-func TestCheckPeer(t *testing.T) {
-	var (
-		ctx          = context.Background()
-		logger       = zap.NewNop()
-		myDomainType = spectypes.DomainType{0x1, 0x2, 0x3, 0x4}
-		mySubnets    = mockSubnets(1, 2, 3)
-		tests        = []*checkPeerTest{
-			{
-				name:          "valid",
-				domainType:    &myDomainType,
-				subnets:       mySubnets,
-				expectedError: nil,
-			},
-			{
-				name:          "missing domain type",
-				domainType:    nil,
-				subnets:       mySubnets,
-				expectedError: nil,
-			},
-			{
-				name:          "different domain type",
-				domainType:    &spectypes.DomainType{0x1, 0x2, 0x3, 0x5},
-				subnets:       mySubnets,
-				expectedError: nil,
-			},
-			{
-				name:          "missing subnets",
-				domainType:    &myDomainType,
-				subnets:       nil,
-				expectedError: errors.New("could not read subnets"),
-			},
-			{
-				name:          "inactive subnets",
-				domainType:    &myDomainType,
-				subnets:       mockSubnets(),
-				expectedError: errors.New("zero subnets"),
-			},
-			{
-				name:          "no shared subnets",
-				domainType:    &myDomainType,
-				subnets:       mockSubnets(0, 4, 5),
-				expectedError: errors.New("no shared subnets"),
-			},
-			{
-				name:          "one shared subnet",
-				domainType:    &myDomainType,
-				subnets:       mockSubnets(0, 1, 4),
-				expectedError: nil,
-			},
-			{
-				name:          "two shared subnets",
-				domainType:    &myDomainType,
-				subnets:       mockSubnets(0, 1, 2),
-				expectedError: nil,
-			},
-		}
-	)
-
-	// Create the LocalNode instances for the tests.
-	for _, test := range tests {
-		// Create a random network key.
-		priv, err := utils.ECDSAPrivateKey(logger, "")
-		require.NoError(t, err)
-
-		// Create a temporary directory for storage.
-		tempDir := t.TempDir()
-		defer os.RemoveAll(tempDir)
-
-		localNode, err := records.CreateLocalNode(priv, tempDir, net.ParseIP("127.0.0.1"), 12000, 13000)
-		require.NoError(t, err)
-
-		if test.domainType != nil {
-			err := records.SetDomainTypeEntry(localNode, *test.domainType)
-			require.NoError(t, err)
-		}
-		if test.subnets != nil {
-			err := records.SetSubnetsEntry(localNode, test.subnets)
-			require.NoError(t, err)
-		}
-
-		test.localNode = localNode
+func Test_newDiscV5Service(t *testing.T) {
+	type args struct {
+		pctx     context.Context
+		logger   *zap.Logger
+		discOpts *Options
 	}
-
-	// Run the tests.
-	subnetIndex := peers.NewSubnetsIndex(commons.Subnets())
-	dvs := &DiscV5Service{
-		ctx:        ctx,
-		conns:      &mock.MockConnectionIndex{LimitValue: true},
-		subnetsIdx: subnetIndex,
-		domainType: myDomainType,
-		subnets:    mySubnets,
+	logger := zap.New(zapcore.NewNopCore(), zap.WithFatalHook(zapcore.WriteThenPanic))
+	privKey, err := utils.ECDSAPrivateKey(logger.Named(logging.NameBootNode), "")
+	require.NoError(t, err)
+	tests := []struct {
+		name    string
+		args    args
+		want    Service
+		wantErr bool
+	}{
+		{
+			name: "ok",
+			args: args{
+				pctx:   context.Background(),
+				logger: logger,
+				discOpts: &Options{
+					DiscV5Opts: &DiscV5Options{
+						IP:         "0.0.0.0",
+						BindIP:     "", // net.IPv4zero.String()
+						Port:       3000,
+						TCPPort:    5000,
+						NetworkKey: privKey,
+						Bootnodes:  []string{},
+					},
+				},
+			},
+			want: nil,
+		},
 	}
-
-	for _, test := range tests {
-		err := dvs.checkPeer(logger, PeerEvent{
-			Node: test.localNode.Node(),
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := newDiscV5Service(tt.args.pctx, tt.args.logger, tt.args.discOpts)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("newDiscV5Service() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			require.NotNil(t, got)
 		})
-		if test.expectedError != nil {
-			require.ErrorContains(t, err, test.expectedError.Error(), test.name)
-		} else {
-			require.NoError(t, err, test.name)
-		}
 	}
 }
 
-type checkPeerTest struct {
-	name          string
-	domainType    *spectypes.DomainType
-	subnets       []byte
-	localNode     *enode.LocalNode
-	expectedError error
+func TestDiscV5Service_Close(t *testing.T) {
+	type fields struct {
+		ctx          context.Context
+		cancel       context.CancelFunc
+		dv5Listener  *discover.UDPv5
+		bootnodes    []*enode.Node
+		conns        peers.ConnectionIndex
+		subnetsIdx   peers.SubnetsIndex
+		publishState int32
+		conn         *net.UDPConn
+		domainType   spectypes.DomainType
+		subnets      []byte
+	}
+	// logger := zap.New(zapcore.NewNopCore(), zap.WithFatalHook(zapcore.WriteThenPanic))
+	ctx, cancel := context.WithCancel(context.Background())
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			name: "ok",
+			fields: fields{
+				ctx:    ctx,
+				cancel: cancel,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dvs := &DiscV5Service{
+				ctx:          tt.fields.ctx,
+				cancel:       tt.fields.cancel,
+				dv5Listener:  tt.fields.dv5Listener,
+				bootnodes:    tt.fields.bootnodes,
+				conns:        tt.fields.conns,
+				subnetsIdx:   tt.fields.subnetsIdx,
+				publishState: tt.fields.publishState,
+				conn:         tt.fields.conn,
+				domainType:   tt.fields.domainType,
+				subnets:      tt.fields.subnets,
+			}
+			if err := dvs.Close(); (err != nil) != tt.wantErr {
+				t.Errorf("DiscV5Service.Close() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
 }
 
-func mockSubnets(active ...int) []byte {
-	subnets := make([]byte, commons.Subnets())
-	for _, subnet := range active {
-		subnets[subnet] = 1
+func TestDiscV5Service_Self(t *testing.T) {
+	type fields struct {
+		ctx          context.Context
+		cancel       context.CancelFunc
+		dv5Listener  *discover.UDPv5
+		bootnodes    []*enode.Node
+		conns        peers.ConnectionIndex
+		subnetsIdx   peers.SubnetsIndex
+		publishState int32
+		conn         *net.UDPConn
+		domainType   spectypes.DomainType
+		subnets      []byte
 	}
-	return subnets
+	tests := []struct {
+		name   string
+		fields fields
+		want   *enode.LocalNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dvs := &DiscV5Service{
+				ctx:          tt.fields.ctx,
+				cancel:       tt.fields.cancel,
+				dv5Listener:  tt.fields.dv5Listener,
+				bootnodes:    tt.fields.bootnodes,
+				conns:        tt.fields.conns,
+				subnetsIdx:   tt.fields.subnetsIdx,
+				publishState: tt.fields.publishState,
+				conn:         tt.fields.conn,
+				domainType:   tt.fields.domainType,
+				subnets:      tt.fields.subnets,
+			}
+			if got := dvs.Self(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DiscV5Service.Self() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDiscV5Service_Node(t *testing.T) {
+	type fields struct {
+		ctx          context.Context
+		cancel       context.CancelFunc
+		dv5Listener  *discover.UDPv5
+		bootnodes    []*enode.Node
+		conns        peers.ConnectionIndex
+		subnetsIdx   peers.SubnetsIndex
+		publishState int32
+		conn         *net.UDPConn
+		domainType   spectypes.DomainType
+		subnets      []byte
+	}
+	type args struct {
+		logger *zap.Logger
+		info   peer.AddrInfo
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *enode.Node
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dvs := &DiscV5Service{
+				ctx:          tt.fields.ctx,
+				cancel:       tt.fields.cancel,
+				dv5Listener:  tt.fields.dv5Listener,
+				bootnodes:    tt.fields.bootnodes,
+				conns:        tt.fields.conns,
+				subnetsIdx:   tt.fields.subnetsIdx,
+				publishState: tt.fields.publishState,
+				conn:         tt.fields.conn,
+				domainType:   tt.fields.domainType,
+				subnets:      tt.fields.subnets,
+			}
+			got, err := dvs.Node(tt.args.logger, tt.args.info)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("DiscV5Service.Node() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DiscV5Service.Node() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDiscV5Service_Bootstrap(t *testing.T) {
+	type fields struct {
+		ctx          context.Context
+		cancel       context.CancelFunc
+		dv5Listener  *discover.UDPv5
+		bootnodes    []*enode.Node
+		conns        peers.ConnectionIndex
+		subnetsIdx   peers.SubnetsIndex
+		publishState int32
+		conn         *net.UDPConn
+		domainType   spectypes.DomainType
+		subnets      []byte
+	}
+	type args struct {
+		logger  *zap.Logger
+		handler HandleNewPeer
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dvs := &DiscV5Service{
+				ctx:          tt.fields.ctx,
+				cancel:       tt.fields.cancel,
+				dv5Listener:  tt.fields.dv5Listener,
+				bootnodes:    tt.fields.bootnodes,
+				conns:        tt.fields.conns,
+				subnetsIdx:   tt.fields.subnetsIdx,
+				publishState: tt.fields.publishState,
+				conn:         tt.fields.conn,
+				domainType:   tt.fields.domainType,
+				subnets:      tt.fields.subnets,
+			}
+			if err := dvs.Bootstrap(tt.args.logger, tt.args.handler); (err != nil) != tt.wantErr {
+				t.Errorf("DiscV5Service.Bootstrap() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestDiscV5Service_checkPeer(t *testing.T) {
+	type fields struct {
+		ctx          context.Context
+		cancel       context.CancelFunc
+		dv5Listener  *discover.UDPv5
+		bootnodes    []*enode.Node
+		conns        peers.ConnectionIndex
+		subnetsIdx   peers.SubnetsIndex
+		publishState int32
+		conn         *net.UDPConn
+		domainType   spectypes.DomainType
+		subnets      []byte
+	}
+	type args struct {
+		logger *zap.Logger
+		e      PeerEvent
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dvs := &DiscV5Service{
+				ctx:          tt.fields.ctx,
+				cancel:       tt.fields.cancel,
+				dv5Listener:  tt.fields.dv5Listener,
+				bootnodes:    tt.fields.bootnodes,
+				conns:        tt.fields.conns,
+				subnetsIdx:   tt.fields.subnetsIdx,
+				publishState: tt.fields.publishState,
+				conn:         tt.fields.conn,
+				domainType:   tt.fields.domainType,
+				subnets:      tt.fields.subnets,
+			}
+			if err := dvs.checkPeer(tt.args.logger, tt.args.e); (err != nil) != tt.wantErr {
+				t.Errorf("DiscV5Service.checkPeer() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestDiscV5Service_initDiscV5Listener(t *testing.T) {
+	type fields struct {
+		ctx          context.Context
+		cancel       context.CancelFunc
+		dv5Listener  *discover.UDPv5
+		bootnodes    []*enode.Node
+		conns        peers.ConnectionIndex
+		subnetsIdx   peers.SubnetsIndex
+		publishState int32
+		conn         *net.UDPConn
+		domainType   spectypes.DomainType
+		subnets      []byte
+	}
+	type args struct {
+		logger   *zap.Logger
+		discOpts *Options
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dvs := &DiscV5Service{
+				ctx:          tt.fields.ctx,
+				cancel:       tt.fields.cancel,
+				dv5Listener:  tt.fields.dv5Listener,
+				bootnodes:    tt.fields.bootnodes,
+				conns:        tt.fields.conns,
+				subnetsIdx:   tt.fields.subnetsIdx,
+				publishState: tt.fields.publishState,
+				conn:         tt.fields.conn,
+				domainType:   tt.fields.domainType,
+				subnets:      tt.fields.subnets,
+			}
+			if err := dvs.initDiscV5Listener(tt.args.logger, tt.args.discOpts); (err != nil) != tt.wantErr {
+				t.Errorf("DiscV5Service.initDiscV5Listener() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestDiscV5Service_discover(t *testing.T) {
+	type fields struct {
+		ctx          context.Context
+		cancel       context.CancelFunc
+		dv5Listener  *discover.UDPv5
+		bootnodes    []*enode.Node
+		conns        peers.ConnectionIndex
+		subnetsIdx   peers.SubnetsIndex
+		publishState int32
+		conn         *net.UDPConn
+		domainType   spectypes.DomainType
+		subnets      []byte
+	}
+	type args struct {
+		ctx      context.Context
+		handler  HandleNewPeer
+		interval time.Duration
+		filters  []NodeFilter
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dvs := &DiscV5Service{
+				ctx:          tt.fields.ctx,
+				cancel:       tt.fields.cancel,
+				dv5Listener:  tt.fields.dv5Listener,
+				bootnodes:    tt.fields.bootnodes,
+				conns:        tt.fields.conns,
+				subnetsIdx:   tt.fields.subnetsIdx,
+				publishState: tt.fields.publishState,
+				conn:         tt.fields.conn,
+				domainType:   tt.fields.domainType,
+				subnets:      tt.fields.subnets,
+			}
+			dvs.discover(tt.args.ctx, tt.args.handler, tt.args.interval, tt.args.filters...)
+		})
+	}
+}
+
+func TestDiscV5Service_RegisterSubnets(t *testing.T) {
+	type fields struct {
+		ctx          context.Context
+		cancel       context.CancelFunc
+		dv5Listener  *discover.UDPv5
+		bootnodes    []*enode.Node
+		conns        peers.ConnectionIndex
+		subnetsIdx   peers.SubnetsIndex
+		publishState int32
+		conn         *net.UDPConn
+		domainType   spectypes.DomainType
+		subnets      []byte
+	}
+	type args struct {
+		logger  *zap.Logger
+		subnets []int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dvs := &DiscV5Service{
+				ctx:          tt.fields.ctx,
+				cancel:       tt.fields.cancel,
+				dv5Listener:  tt.fields.dv5Listener,
+				bootnodes:    tt.fields.bootnodes,
+				conns:        tt.fields.conns,
+				subnetsIdx:   tt.fields.subnetsIdx,
+				publishState: tt.fields.publishState,
+				conn:         tt.fields.conn,
+				domainType:   tt.fields.domainType,
+				subnets:      tt.fields.subnets,
+			}
+			if err := dvs.RegisterSubnets(tt.args.logger, tt.args.subnets...); (err != nil) != tt.wantErr {
+				t.Errorf("DiscV5Service.RegisterSubnets() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestDiscV5Service_DeregisterSubnets(t *testing.T) {
+	type fields struct {
+		ctx          context.Context
+		cancel       context.CancelFunc
+		dv5Listener  *discover.UDPv5
+		bootnodes    []*enode.Node
+		conns        peers.ConnectionIndex
+		subnetsIdx   peers.SubnetsIndex
+		publishState int32
+		conn         *net.UDPConn
+		domainType   spectypes.DomainType
+		subnets      []byte
+	}
+	type args struct {
+		logger  *zap.Logger
+		subnets []int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dvs := &DiscV5Service{
+				ctx:          tt.fields.ctx,
+				cancel:       tt.fields.cancel,
+				dv5Listener:  tt.fields.dv5Listener,
+				bootnodes:    tt.fields.bootnodes,
+				conns:        tt.fields.conns,
+				subnetsIdx:   tt.fields.subnetsIdx,
+				publishState: tt.fields.publishState,
+				conn:         tt.fields.conn,
+				domainType:   tt.fields.domainType,
+				subnets:      tt.fields.subnets,
+			}
+			if err := dvs.DeregisterSubnets(tt.args.logger, tt.args.subnets...); (err != nil) != tt.wantErr {
+				t.Errorf("DiscV5Service.DeregisterSubnets() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestDiscV5Service_publishENR(t *testing.T) {
+	type fields struct {
+		ctx          context.Context
+		cancel       context.CancelFunc
+		dv5Listener  *discover.UDPv5
+		bootnodes    []*enode.Node
+		conns        peers.ConnectionIndex
+		subnetsIdx   peers.SubnetsIndex
+		publishState int32
+		conn         *net.UDPConn
+		domainType   spectypes.DomainType
+		subnets      []byte
+	}
+	type args struct {
+		logger *zap.Logger
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dvs := &DiscV5Service{
+				ctx:          tt.fields.ctx,
+				cancel:       tt.fields.cancel,
+				dv5Listener:  tt.fields.dv5Listener,
+				bootnodes:    tt.fields.bootnodes,
+				conns:        tt.fields.conns,
+				subnetsIdx:   tt.fields.subnetsIdx,
+				publishState: tt.fields.publishState,
+				conn:         tt.fields.conn,
+				domainType:   tt.fields.domainType,
+				subnets:      tt.fields.subnets,
+			}
+			dvs.publishENR(tt.args.logger)
+		})
+	}
+}
+
+func TestDiscV5Service_createLocalNode(t *testing.T) {
+	type fields struct {
+		ctx          context.Context
+		cancel       context.CancelFunc
+		dv5Listener  *discover.UDPv5
+		bootnodes    []*enode.Node
+		conns        peers.ConnectionIndex
+		subnetsIdx   peers.SubnetsIndex
+		publishState int32
+		conn         *net.UDPConn
+		domainType   spectypes.DomainType
+		subnets      []byte
+	}
+	type args struct {
+		logger   *zap.Logger
+		discOpts *Options
+		ipAddr   net.IP
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *enode.LocalNode
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dvs := &DiscV5Service{
+				ctx:          tt.fields.ctx,
+				cancel:       tt.fields.cancel,
+				dv5Listener:  tt.fields.dv5Listener,
+				bootnodes:    tt.fields.bootnodes,
+				conns:        tt.fields.conns,
+				subnetsIdx:   tt.fields.subnetsIdx,
+				publishState: tt.fields.publishState,
+				conn:         tt.fields.conn,
+				domainType:   tt.fields.domainType,
+				subnets:      tt.fields.subnets,
+			}
+			got, err := dvs.createLocalNode(tt.args.logger, tt.args.discOpts, tt.args.ipAddr)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("DiscV5Service.createLocalNode() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DiscV5Service.createLocalNode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_newUDPListener(t *testing.T) {
+	type args struct {
+		bindIP  net.IP
+		port    int
+		network string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *net.UDPConn
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := newUDPListener(tt.args.bindIP, tt.args.port, tt.args.network)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("newUDPListener() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("newUDPListener() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
