51424426a95b6939ac5920cc4b56100aae6dcd75
moshe-blox <moshe@blox.io>
2024-02-04 12:19:52 +0200

    revert deployments

--- .k8/production/holesky/scripts/deploy-cluster-1--4.sh
+++ .k8/production/holesky/scripts/deploy-cluster-1--4.sh
@@ -107,8 +107,8 @@ DIR=".k8/production/holesky"
 DEPLOY_FILES=(
   "ssv-node-holesky-1-deployment.yml"
   "ssv-node-holesky-2-deployment.yml"
-  # "ssv-node-holesky-3-deployment.yml"
-  # "ssv-node-holesky-4-deployment.yml"
+  "ssv-node-holesky-3-deployment.yml"
+  "ssv-node-holesky-4-deployment.yml"
 )
 
 if [[ -d $DIR ]]; then
