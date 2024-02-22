61591d513ac17dfc451d9777cb9a7d7202fdc0ea
y0sher <lyosher@gmail.com>
2024-02-22 11:00:58 +0200

    just fallback instead of parallel

--- beacon/goclient/proposer.go
+++ beacon/goclient/proposer.go
@@ -128,8 +128,6 @@ func (gc *goClient) PrsymGetParallelBlocks(slot phase0.Slot, graffitiBytes, rand
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel() // Ensure cleanup on exit
 	blindedCh := make(chan res)
-	fullBlockCh := make(chan res)
-
 	blindedTimer := time.NewTimer(TimeToGetBlindedBlock)
 
 	// Fetch blinded block
@@ -138,16 +136,6 @@ func (gc *goClient) PrsymGetParallelBlocks(slot phase0.Slot, graffitiBytes, rand
 		select {
 		case blindedCh <- res{o, v, e}:
 		case <-ctx.Done(): // timed out
-		}
-	}()
-
-	// Fetch full block
-	go func() {
-		time.Sleep(time.Second / 2) // Wait a bit to let blinded fetch
-		o, v, e := gc.GetBeaconBlock(slot, graffitiBytes, randao)
-		select {
-		case fullBlockCh <- res{o, v, e}:
-		case <-ctx.Done(): // cancelled by blinded
 		}
 	}()
 
@@ -164,9 +152,8 @@ func (gc *goClient) PrsymGetParallelBlocks(slot phase0.Slot, graffitiBytes, rand
 
 	if berr != nil || blindedTimedout {
 		gc.log.Debug("🧊 failed to get blinded block, defaulting to full block", zap.Error(berr))
-		block := <-fullBlockCh
 		cancel() // Cancel blinded fetch so it doesn't hang
-		obj, ver, berr = block.obj, block.ver, block.err
+		obj, ver, berr = gc.GetBeaconBlock(slot, graffitiBytes, randao)
 		if berr != nil {
 			return nil, ver, errors.Wrap(berr, "failed to get beacon block")
 		}
