098300077ff0ece58566dd62ea9e14437d4458e7
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-03-15 15:28:37 +0300

    Revert "Add debug logs"
    This reverts commit 1924ae72f0d1c1ed87f502b01d6094db2b0f0a28.

--- ekm/errors.go
+++ ekm/errors.go
@@ -3,20 +3,15 @@ package ekm
 import (
 	"fmt"
 
-	"github.com/attestantio/go-eth2-client/spec/phase0"
 	"github.com/bloxapp/eth2-key-manager/core"
 )
 
 type SlashableAttestationError struct {
-	Status                 core.VoteDetectionType
-	AttestationSourceEpoch phase0.Epoch
-	AttestationTargetEpoch phase0.Epoch
-	HighestSourceEpoch     phase0.Epoch
-	HighestTargetEpoch     phase0.Epoch
+	Status core.VoteDetectionType
 }
 
 func (se SlashableAttestationError) Error() string {
-	return fmt.Sprintf("slashable attestation (%s), not signing (debug: ASE %v ATE %v HSE %v HTE %v)", se.Status, se.AttestationSourceEpoch, se.AttestationTargetEpoch, se.HighestSourceEpoch, se.HighestTargetEpoch)
+	return fmt.Sprintf("slashable attestation (%s), not signing", se.Status)
 }
 
 type SlashableProposalError struct {
