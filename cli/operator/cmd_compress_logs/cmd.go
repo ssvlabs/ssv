0ce0dca02e2fb3b8d15dc05c6f27fcad7abde40c
Anton Korpusenko <antokorp@gmail.com>
2024-03-21 15:59:18 +0300

    added lumberjack logs rotating & logs compression support

--- cli/operator/cmd_compress_logs/cmd.go
+++ cli/operator/cmd_compress_logs/cmd.go
@@ -1,18 +1,22 @@
 package cmd_compress_logs
 
 import (
+	"archive/tar"
 	"compress/gzip"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
 
 	globalconfig "github.com/bloxapp/ssv/cli/config"
 	"github.com/bloxapp/ssv/logging"
 	"github.com/bloxapp/ssv/utils/commons"
-	"github.com/ilyakaznacheev/cleanenv"
-	"github.com/spf13/cobra"
-	"go.uber.org/zap"
 )
 
 type config struct {
@@ -39,7 +43,7 @@ var CompressLogsCmd = &cobra.Command{
 
 		logsFilePath := cfg.LogFilePath
 
-		fileSizeBytes, err := compressFile(logsFilePath)
+		fileSizeBytes, err := compressLogFiles(logsFilePath)
 		if err != nil {
 			logger.Fatal("logs file compression failed", zap.Error(err))
 		}
@@ -48,32 +52,40 @@ var CompressLogsCmd = &cobra.Command{
 	},
 }
 
-func compressFile(path string) (int64, error) {
-	file, err := os.Open(filepath.Clean(path))
+func compressLogFiles(logConfigPath string) (int64, error) {
+	// Find all log files in the directory of the provided file
+	logFiles, err := getLogFilesAbsPaths(logConfigPath)
 	if err != nil {
 		return 0, err
 	}
-	defer func() {
-		_ = file.Close()
-	}()
 
-	compressedFile, err := os.Create(filepath.Clean(path + compressedFileExtension))
+	// Create a temporary directory
+	tmpDir, err := os.MkdirTemp("", "logs_"+fmt.Sprint(time.Now().Unix()))
+
 	if err != nil {
 		return 0, err
 	}
-	defer func() {
-		_ = compressedFile.Close()
-	}()
-	gzWriter := gzip.NewWriter(compressedFile)
-	if _, err := io.Copy(gzWriter, file); err != nil {
+	defer os.RemoveAll(tmpDir) // clean up
+
+	for _, file := range logFiles {
+		fmt.Println(file)
+	}
+	fmt.Println(tmpDir)
+
+	err = copyFilesToDir(tmpDir, logFiles)
+	if err != nil {
 		return 0, err
 	}
 
-	if err := gzWriter.Close(); err != nil {
+	tarAbsPath, err := compressDirectory(tmpDir, logConfigPath+compressedFileExtension)
+	if err != nil {
 		return 0, err
 	}
 
-	// get the size of the compressed file
+	compressedFile, err := os.Open(tarAbsPath)
+	if err != nil {
+		return 0, err
+	}
 	info, err := compressedFile.Stat()
 	if err != nil {
 		return 0, err
@@ -112,4 +124,148 @@ func setGlobalLogger(cfg *config) (*zap.Logger, error) {
 func init() {
 	globalconfig.ProcessConfigArg(&cfg, &globalArgs, CompressLogsCmd)
 	globalconfig.ProcessHelpCmd(&cfg, &globalArgs, CompressLogsCmd)
+}
+
+func copyFilesToDir(destDir string, files []string) error {
+	// Create the destination directory
+	err := os.MkdirAll(destDir, 0755)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		// Open the source file
+		srcFile, err := os.Open(file)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("Xxxxx", filepath.Base(srcFile.Name()))
+		// Create the destination file
+		destFile, err := os.Create(filepath.Join(destDir, filepath.Base(srcFile.Name())))
+		if err != nil {
+			_ = srcFile.Close()
+			return err
+		}
+
+		// Copy the contents of the source file to the destination file
+		_, err = io.Copy(destFile, srcFile)
+		if err != nil {
+			srcFile.Close()
+			destFile.Close()
+			return err
+		}
+
+		_ = srcFile.Close()
+		_ = destFile.Close()
+	}
+	return nil
+}
+
+func getLogFilesAbsPaths(path string) ([]string, error) {
+	logFileName := getFileNameWithoutExt(path)
+	ext := filepath.Ext(path)
+	absDirPath, err := filepath.Abs(filepath.Dir(path))
+	if err != nil {
+		return nil, err
+	}
+	files, err := os.ReadDir(filepath.Dir(path))
+	if err != nil {
+		return nil, err
+	}
+
+	var logFiles []string
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		fileName := file.Name()
+		filePrefix := strings.TrimSuffix(fileName, filepath.Ext(fileName))
+
+		// filter to have .log files
+		if filepath.Ext(fileName) == ext && strings.Contains(filePrefix, logFileName) {
+			logFiles = append(logFiles, filepath.Join(absDirPath, fileName))
+		}
+	}
+
+	return logFiles, nil
+}
+
+func compressDirectory(srcDir string, tarFileName string) (string, error) {
+	dirName := filepath.Base(srcDir)
+	parentDir := filepath.Dir(srcDir)
+	newSrcDir := filepath.Join(parentDir, dirName)
+
+	tarFile, err := os.Create(tarFileName)
+	if err != nil {
+		return "", err
+	}
+	if err != nil {
+		return "", err
+	}
+	p, err := filepath.Abs(tarFile.Name())
+	if err != nil {
+		return "", err
+	}
+	fmt.Println("tarFile", p)
+	defer tarFile.Close()
+
+	// Create a gzip writer
+	gzWriter := gzip.NewWriter(tarFile)
+	defer gzWriter.Close()
+
+	// Create a tar writer
+	tw := tar.NewWriter(gzWriter)
+	defer tw.Close()
+
+	// recursively walk the directory and write the contents to the tarball
+	err = filepath.Walk(newSrcDir, func(file string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Create a new dir/file header
+		header, err := tar.FileInfoHeader(fi, fi.Name())
+		if err != nil {
+			return err
+		}
+
+		// Update the name to correctly reflect the desired destination when un-taring
+		header.Name = strings.TrimPrefix(filepath.ToSlash(file), parentDir)
+		if err := tw.WriteHeader(header); err != nil {
+			return err
+		}
+
+		// If it's not a dir, write file content
+		if !fi.IsDir() {
+			data, err := os.Open(file)
+			if err != nil {
+				return err
+			}
+			if _, err := io.Copy(tw, data); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	absTarFilePath, err := filepath.Abs(tarFile.Name())
+	if err != nil {
+		return "", err
+	}
+
+	return absTarFilePath, nil
+}
+
+func getFileNameWithoutExt(path string) string {
+	filenameWithExt := filepath.Base(path) // Get the file name with extension
+	extension := filepath.Ext(path)        // Get the file extension
+
+	filename := filenameWithExt[0 : len(filenameWithExt)-len(extension)] // Remove the extension from the filename
+	return filename
 }
--- cli/operator/cmd_compress_logs/cmd_test.go
+++ cli/operator/cmd_compress_logs/cmd_test.go
@@ -1,58 +1,92 @@
 package cmd_compress_logs
 
 import (
-	"compress/gzip"
-	"path/filepath"
-
 	"bufio"
-	"bytes"
+	"compress/gzip"
 	"fmt"
 	"io"
+	"math/rand"
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
+	"time"
 
-	globalconfig "github.com/bloxapp/ssv/cli/config"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+
+	globalconfig "github.com/bloxapp/ssv/cli/config"
 )
+
+func TestGzipDir(t *testing.T) {
+	path := filepath.Clean("/Users/dev/ssv/ssv/logs_xxx/test_logs_dir/test.log")
+	dir := filepath.Dir(path)
+	_, err := compressDirectory(dir, "x.gz")
+	require.NoError(t, err)
+}
+
+func TestGetLogFiles(t *testing.T) {
+	path := filepath.Clean("/Users/dev/ssv/ssv/logs_xxx/test_logs_dir/test.log")
+	files, err := getLogFilesAbsPaths(path)
+	fmt.Println(files)
+	require.NoError(t, err)
+}
+
+func TestCompression(t *testing.T) {
+	path := filepath.Clean("/Users/dev/ssv/ssv/logs_xxx/test_logs_dir/test.log")
+	_, err := compressLogFiles(path)
+	require.NoError(t, err)
+}
+
+func TestGetLogFilesAbsPaths(t *testing.T) {
+	tmpDir, err := os.MkdirTemp("", "test")
+	require.NoError(t, err)
+	defer os.RemoveAll(tmpDir) // clean up
+
+	logFiles := []string{"test.log", "test2.log", "test3.log"}
+	nonLogFiles := []string{"test.txt", "test.csv"}
+	for _, file := range append(logFiles, nonLogFiles...) {
+		_, err := os.Create(filepath.Join(tmpDir, file))
+		require.NoError(t, err)
+	}
+
+	logFilePath := filepath.Join(tmpDir, "test.log")
+	paths, err := getLogFilesAbsPaths(logFilePath)
+	require.NoError(t, err)
+
+	// Check that the function returns the correct paths to all the log files in the directory
+	require.Len(t, paths, len(logFiles))
+	for _, path := range paths {
+		require.Contains(t, logFiles, filepath.Base(path))
+	}
+}
 
 func TestCompressFile(t *testing.T) {
 	// Create a temporary file
-	file, err := os.Create(filepath.Clean("test.log"))
+	fileName := "test.log"
+	err := generateLogFile(uint32(50), fileName)
 	require.NoError(t, err)
-	defer func() {
-		_ = os.Remove(file.Name())
-	}()
 
-	// Write some content to the file
-	_, err = file.WriteString("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-	require.NoError(t, err)
-	_ = file.Close()
-
-	// Get the size of the original file
-	info, err := os.Stat(file.Name())
-	require.NoError(t, err)
-	originalSize := info.Size()
-
-	// Call the compressFile function
-	_, err = compressFile(file.Name())
+	_, err = compressLogFiles(fileName)
 	require.NoError(t, err)
 
 	// Check that the compressed file exists
-	compressedFileName := file.Name() + compressedFileExtension
-	_, err = os.Stat(filepath.Clean(compressedFileName))
-	require.NoError(t, err)
-
-	// Get the size of the compressed file
-	info, err = os.Stat(filepath.Clean(compressedFileName))
-	require.NoError(t, err)
-	compressedSize := info.Size()
-
-	// Check that the compressed file is smaller than the original file
-	require.Less(t, compressedSize, originalSize)
-
-	// Clean up the compressed file
-	_ = os.Remove(compressedFileName)
+	//compressedFileName := file.Name() + compressedFileExtension
+	//_, err = os.Stat(filepath.Clean(compressedFileName))
+	//require.NoError(t, err)
+	//
+	//// Get the size of the compressed file
+	//info, err = os.Stat(filepath.Clean(compressedFileName))
+	//require.NoError(t, err)
+	//compressedSize := info.Size()
+	//
+	//// Check that the compressed file is smaller than the original file
+	//require.Less(t, compressedSize, originalSize)
+	//
+	//// Clean up the compressed file
+	//_ = os.Remove(compressedFileName)
 }
 
 func TestSetGlobalLogger(t *testing.T) {
@@ -78,9 +112,10 @@ func BenchmarkCompressLogs(b *testing.B) {
 	testCases := []struct {
 		SizeInMB int
 	}{
-		{10},
-		{500},
-		{1024},
+		{50},
+		//{10},
+		//{500},
+		//{1024},
 	}
 
 	for _, tc := range testCases {
@@ -89,15 +124,12 @@ func BenchmarkCompressLogs(b *testing.B) {
 
 			gzPath := testLogFilePath + compressedFileExtension
 
-			// generate log file
-			err := generateLogFile(tc.SizeInMB, testLogFilePath)
+			err := generateLogFile(uint32(tc.SizeInMB), testLogFilePath)
 			require.NoError(b, err)
 
-			// compress logs file
-			_, err = compressFile(testLogFilePath)
+			_, err = compressLogFiles(testLogFilePath)
 			require.NoError(b, err)
 
-			// get the size of the compressed file
 			info, err := os.Stat(gzPath)
 			if err != nil {
 				b.Fatal(err)
@@ -117,9 +149,9 @@ func BenchmarkCompressLogs(b *testing.B) {
 			require.True(b, eq, "files are not equal!")
 
 			// delete files
-			if err := deleteFiles(testLogFilePath, gzPath, unzippedFileName); err != nil {
-				b.Fatal(err)
-			}
+
+			require.NoError(b, deleteFilesStartingWithPrefix(filepath.Dir(testLogFilePath), filepath.Base(testLogFilePath)))
+			require.NoError(b, deleteFiles(gzPath, unzippedFileName))
 		})
 	}
 }
@@ -164,46 +196,39 @@ func compareFiles(file1Path, file2Path string) (bool, error) {
 	return true, nil
 }
 
-// used to write to the file in chunks to avoid memory issues with large files
-const chunkSize = 200 * 1024 * 1024 // 200MB
+func generateLogFile(sizeInMB uint32, path string) error {
+	// Create a logger
+	path = filepath.Clean(path)
+	logger, _ := zap.NewProduction()
 
-// generateLogFile generates a valid log file with the given size in MB
-// chunks are used to write the file to avoid memory issues with large files
-func generateLogFile(sizeInMB int, path string) error {
-	file, err := os.Create(filepath.Clean(path))
-	if err != nil {
-		return err
-	}
-	defer func() {
-		_ = file.Close()
-	}()
-
-	var buffer bytes.Buffer
-	var totalSize int64 = 0
-	var i int64 = 0
-
-	for totalSize < int64(sizeInMB)*1024*1024 {
-		// some random dummy log entry
-		entry := fmt.Sprintf("{\"L\":\"DEBUG\",\"T\":\"\",\"N\":\"P2PNetwork\",\"M\":\"subscribing to subnets\",\"subnets\":\"%032d\"}\n", i)
-		buffer.WriteString(entry)
-
-		if buffer.Len() >= chunkSize {
-			// Write buffer to file and clear buffer
-			if _, err := file.WriteString(buffer.String()); err != nil {
-				return err
-			}
-			buffer.Reset()
-		}
-
-		totalSize += int64(len(entry))
-		i++
+	// Create a lumberjack.Logger
+	lumberjackLogger := &lumberjack.Logger{
+		Filename:   path,
+		MaxSize:    20, // megabytes
+		MaxBackups: 20,
+		MaxAge:     28, // days
 	}
 
-	// Write remaining buffer to file
-	if buffer.Len() > 0 {
-		if _, err := file.WriteString(buffer.String()); err != nil {
-			return err
-		}
+	// Wrap the lumberjack.Logger in a zapcore.WriteSyncer
+	syncWriter := zapcore.AddSync(lumberjackLogger)
+
+	// Create a zapcore.Core that writes logs to the lumberjack.Logger
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+		syncWriter,
+		zap.InfoLevel,
+	)
+
+	// Create a zap.Logger that uses the zapcore.Core
+	logger = zap.New(core)
+
+	// Use the logger
+	logger.Info("Generating log file", zap.Uint32("sizeInMB", sizeInMB), zap.String("path", path))
+
+	// Write a large amount of data to the log file
+	for i := uint32(0); i < sizeInMB; i++ {
+		// Write another log message
+		logger.Info("Some dummy log", zap.String("data", generate1MBString()))
 	}
 
 	return nil
@@ -246,6 +271,23 @@ func unzip(src string, dest string) error {
 	return nil
 }
 
+func deleteFilesStartingWithPrefix(dirPath string, prefix string) error {
+	files, err := os.ReadDir(dirPath)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		if strings.HasPrefix(file.Name(), prefix) {
+			if err := os.Remove(filepath.Join(dirPath, file.Name())); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
 func deleteFiles(paths ...string) error {
 	for _, path := range paths {
 		if err := os.Remove(path); err != nil {
@@ -253,4 +295,52 @@ func deleteFiles(paths ...string) error {
 		}
 	}
 	return nil
+}
+
+func generate1MBString() string {
+	const MB = 1 << 20 // 1MB in bytes
+	const charSize = 1 // size in bytes of the character we're appending
+
+	// Create a string builder for efficient string concatenation
+	var b strings.Builder
+	// Preallocate enough memory for the final string
+	b.Grow(MB)
+
+	strSize := MB / charSize
+	// Seed the random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 0; i < strSize; i++ {
+		symbol := rand.Intn('z'-'a') + 'a'
+		b.WriteRune(rune(symbol))
+	}
+
+	return b.String()
+}
+
+// calcSizeOfFilesWithPrefix calculates the total size of files in a directory that have a specific prefix.
+func calcSizeOfFilesWithPrefix(dirPath string, prefix string) (int64, error) {
+	// Read the directory contents
+	files, err := os.ReadDir(dirPath)
+	if err != nil {
+		return 0, err
+	}
+
+	var totalSize int64
+	for _, file := range files {
+		// Check if the file name has the specified prefix
+		if strings.HasPrefix(file.Name(), prefix) {
+			// Get file information to access file size
+			info, err := file.Info()
+			if err != nil {
+				return 0, err
+			}
+			totalSize += info.Size()
+		}
+	}
+
+	// Optionally, print the total size in MB for debugging
+	fmt.Printf("Total size of files with prefix '%s': %d bytes (%.2f MB)\n", prefix, totalSize, float64(totalSize)/1024.0/1024.0)
+
+	return totalSize, nil
 }
