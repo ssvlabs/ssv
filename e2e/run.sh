7289879bd96668f45142b20fb0345aa86616d9b4
guy muroch <guym@blox.io>
2024-03-24 16:07:34 +0200

    rebase to origin/gm/e2e_left_overs_new

--- e2e/run.sh
+++ e2e/run.sh
@@ -48,8 +48,7 @@ save_logs() {
     docker logs "$logs_catcher_container" > "$LOG_DIR/$logs_catcher_container.txt"
   fi
 }
-export BEACON_NODE_URL=http://bn-h-2.stage.bloxinfra.com:3502/
-export EXECUTION_NODE_URL=ws://bn-h-2.stage.bloxinfra.com:8557/ws
+
 # Step 1: Start the beacon_proxy and ssv-node services
 docker compose up -d --build beacon_proxy ssv-node-1 ssv-node-2 ssv-node-3 ssv-node-4
 
