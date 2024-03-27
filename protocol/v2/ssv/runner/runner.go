961d1f505d5bba602d999d93467562333861120d
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2024-03-27 19:42:53 +0200

    align to spec

--- protocol/v2/ssv/runner/runner.go
+++ protocol/v2/ssv/runner/runner.go
@@ -224,15 +224,14 @@ func (b *BaseRunner) basePartialSigMsgProcessing(
 
 // didDecideCorrectly returns true if the expected consensus instance decided correctly
 func (b *BaseRunner) didDecideCorrectly(prevDecided bool, decidedMsg *specqbft.SignedMessage) (bool, error) {
-	decided := decidedMsg != nil
-	decidedRunningInstance := decided && b.State.RunningInstance != nil && decidedMsg.Message.Height == b.State.RunningInstance.GetHeight()
-
-	if !decided {
+	if decidedMsg == nil {
 		return false, nil
 	}
-	if !decidedRunningInstance {
+
+	if b.State.RunningInstance == nil || b.State.RunningInstance.GetHeight() != decidedMsg.Message.Height {
 		return false, errors.New("decided wrong instance")
 	}
+
 	// verify we decided running instance only, if not we do not proceed
 	if prevDecided {
 		return false, nil
