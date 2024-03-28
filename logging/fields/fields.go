82ae3ea41c381e98231f57abf9fdad5d6fa10f8c
moshe-blox <moshe@blox.io>
2024-03-28 16:09:47 +0200

    short numbers

--- logging/fields/fields.go
+++ logging/fields/fields.go
@@ -270,11 +270,11 @@ func Topic(val string) zap.Field {
 }
 
 func ConsensusTime(val time.Duration) zap.Field {
-	return zap.Float64(FieldConsensusTime, val.Seconds())
+	return zap.String(FieldConsensusTime, strconv.FormatFloat(val.Seconds(), 'f', 4, 64))
 }
 
 func SubmissionTime(val time.Duration) zap.Field {
-	return zap.Float64(FieldSubmissionTime, val.Seconds())
+	return zap.String(FieldSubmissionTime, strconv.FormatFloat(val.Seconds(), 'f', 4, 64))
 }
 
 func DutyID(val string) zap.Field {
