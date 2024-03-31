3b089200b1e317460f2af5703e0930e64a02deec
y0sher <lyosher@gmail.com>
2024-03-31 15:26:11 +0000

    add test for the caches

--- operator/keys/rsa_linux_test.go
+++ operator/keys/rsa_linux_test.go
@@ -38,3 +38,26 @@ func Test_VerifyOpenSSLWithOpenSSL(t *testing.T) {
 
 	require.NoError(t, VerifyRSA(pub, msg, sig))
 }
+
+func Test_Caches(t *testing.T) {
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	require.NoError(t, err)
+	msg := []byte("hello")
+	priv := &privateKey{key, nil}
+	sig, err := priv.Sign(msg)
+	require.NoError(t, err)
+
+	pub := priv.Public().(*publicKey)
+
+	require.NoError(t, VerifyRSA(pub, msg, sig))
+
+	// should sign using cache
+	require.NotNil(t, priv.cachedPrivKey)
+
+	sig2, err := priv.Sign(msg)
+	require.NoError(t, err)
+
+	require.NotNil(t, pub.cachedPubkey)
+
+	require.NoError(t, VerifyRSA(pub, msg, sig2))
+}
