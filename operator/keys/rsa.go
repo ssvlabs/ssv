aa3ceb1c2139106bb1bfb60f6c1a0c3b84a9deff
y0sher <lyosher@gmail.com>
2024-03-31 14:11:08 +0300

    openssl without jemalloc

--- operator/keys/rsa.go
+++ operator/keys/rsa.go
@@ -4,12 +4,25 @@ package keys
 
 import (
 	"crypto"
+	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
 )
 
+type privateKey struct {
+	privKey *rsa.PrivateKey
+}
+
 type publicKey struct {
 	pubKey *rsa.PublicKey
+}
+
+func SignRSA(priv *privateKey, data []byte) ([]byte, error) {
+	return rsa.SignPKCS1v15(rand.Reader, priv.privKey, crypto.SHA256, data)
+}
+
+func EncryptRSA(pub *publicKey, data []byte) ([]byte, error) {
+	return rsa.EncryptPKCS1v15(rand.Reader, pub.pubKey, data)
 }
 
 func VerifyRSA(pub *publicKey, data, signature []byte) error {
