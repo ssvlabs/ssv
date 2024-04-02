c6d2fc3aab9c3eae72d0954bebd32e244aae2b89
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-04-02 13:54:22 +0400

    Track commit messages instead of decided in full node

--- protocol/v2/ssv/validator/non_committee_validator.go
+++ protocol/v2/ssv/validator/non_committee_validator.go
@@ -1,6 +1,9 @@
 package validator
 
 import (
+	"fmt"
+	"sort"
+
 	specqbft "github.com/bloxapp/ssv-spec/qbft"
 	spectypes "github.com/bloxapp/ssv-spec/types"
 	"go.uber.org/zap"
@@ -14,9 +17,10 @@ import (
 )
 
 type NonCommitteeValidator struct {
-	Share          *types.SSVShare
-	Storage        *storage.QBFTStores
-	qbftController *qbftcontroller.Controller
+	Share              *types.SSVShare
+	Storage            *storage.QBFTStores
+	qbftController     *qbftcontroller.Controller
+	commitMsgContainer *specqbft.MsgContainer
 }
 
 func NewNonCommitteeValidator(logger *zap.Logger, identifier spectypes.MessageID, opts Options) *NonCommitteeValidator {
@@ -56,32 +60,63 @@ func (ncv *NonCommitteeValidator) ProcessMessage(logger *zap.Logger, msg *queue.
 			logger.Debug("❗ failed to get consensus Message from network Message", zap.Error(err))
 			return
 		}
-		// only supports decided msg's
-		if signedMsg.Message.MsgType != specqbft.CommitMsgType || !ncv.Share.HasQuorum(len(signedMsg.Signers)) {
+		// only supports commit msg's
+		if signedMsg.Message.MsgType != specqbft.CommitMsgType {
 			return
 		}
 
 		logger = logger.With(fields.Height(signedMsg.Message.Height))
 
-		if decided, err := ncv.qbftController.ProcessMsg(logger, signedMsg); err != nil {
+		decided, err := ncv.qbftController.ProcessMsg(logger, signedMsg)
+		if err != nil {
 			logger.Debug("❌ failed to process message",
 				zap.Uint64("msg_height", uint64(signedMsg.Message.Height)),
 				zap.Any("signers", signedMsg.Signers),
 				zap.Error(err))
-		} else if decided != nil {
-			if inst := ncv.qbftController.StoredInstances.FindInstance(signedMsg.Message.Height); inst != nil {
-				logger := logger.With(
+			return
+		}
+
+		if decided == nil {
+			addMsg, err := ncv.commitMsgContainer.AddFirstMsgForSignerAndRound(signedMsg)
+			if err != nil {
+				logger.Debug("❌ could not add commit msg to container",
 					zap.Uint64("msg_height", uint64(signedMsg.Message.Height)),
-					zap.Uint64("ctrl_height", uint64(ncv.qbftController.Height)),
 					zap.Any("signers", signedMsg.Signers),
-				)
-				if err = ncv.qbftController.SaveInstance(inst, signedMsg); err != nil {
-					logger.Debug("❗failed to save instance", zap.Error(err))
-				} else {
-					logger.Debug("💾 saved instance")
-				}
+					zap.Error(err))
+				return
+			}
+			if !addMsg {
+				return
+			}
+
+			signers, commitMsgs := ncv.commitMsgContainer.LongestUniqueSignersForRoundAndRoot(signedMsg.Message.Round, signedMsg.Message.Root)
+			if !ncv.Share.HasQuorum(len(signers)) {
+				return
+			}
+
+			signedMsg, err = aggregateCommitMsgs(commitMsgs)
+			if err != nil {
+				logger.Debug("❌ could not add aggregate commit messages",
+					zap.Uint64("msg_height", uint64(signedMsg.Message.Height)),
+					zap.Any("signers", signedMsg.Signers),
+					zap.Error(err))
+				return
 			}
 		}
+
+		if inst := ncv.qbftController.StoredInstances.FindInstance(signedMsg.Message.Height); inst != nil {
+			logger := logger.With(
+				zap.Uint64("msg_height", uint64(signedMsg.Message.Height)),
+				zap.Uint64("ctrl_height", uint64(ncv.qbftController.Height)),
+				zap.Any("signers", signedMsg.Signers),
+			)
+			if err = ncv.qbftController.SaveInstance(inst, signedMsg); err != nil {
+				logger.Debug("❗failed to save instance", zap.Error(err))
+			} else {
+				logger.Debug("💾 saved instance")
+			}
+		}
+
 		return
 	}
 }
@@ -93,4 +128,27 @@ func nonCommitteeInstanceContainerCapacity(fullNode bool) int {
 		return 2
 	}
 	return 1
+}
+
+func aggregateCommitMsgs(msgs []*specqbft.SignedMessage) (*specqbft.SignedMessage, error) {
+	if len(msgs) == 0 {
+		return nil, fmt.Errorf("can't aggregate zero commit msgs")
+	}
+
+	var ret *specqbft.SignedMessage
+	for _, m := range msgs {
+		if ret == nil {
+			ret = m.DeepCopy()
+		} else {
+			if err := ret.Aggregate(m); err != nil {
+				return nil, fmt.Errorf("could not aggregate commit msg: %w", err)
+			}
+		}
+	}
+
+	sort.Slice(ret.Signers, func(i, j int) bool {
+		return ret.Signers[i] < ret.Signers[j]
+	})
+
+	return ret, nil
 }
