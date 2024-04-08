4b432a2132c5806d4beab82e154e8075c582ef70
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-04-08 18:50:09 +0400

    Debug full buffer [1]

--- protocol/v2/ssv/validator/non_committee_validator.go
+++ protocol/v2/ssv/validator/non_committee_validator.go
@@ -75,6 +75,8 @@ func (ncv *NonCommitteeValidator) ProcessMessage(logger *zap.Logger, msg *queue.
 
 		logger = logger.With(fields.Height(signedMsg.Message.Height))
 
+		return
+
 		addMsg, err := ncv.commitMsgContainer.AddFirstMsgForSignerAndRound(signedMsg)
 		if err != nil {
 			logger.Debug("❌ could not add commit msg to container",
