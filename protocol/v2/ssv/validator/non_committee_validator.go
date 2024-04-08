eff2a059ab2fc22ba02a143e9d208a4c7a97678e
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-04-08 15:05:38 +0400

    Trigger deployment

--- protocol/v2/ssv/validator/non_committee_validator.go
+++ protocol/v2/ssv/validator/non_committee_validator.go
@@ -56,7 +56,7 @@ func (ncv *NonCommitteeValidator) ProcessMessage(logger *zap.Logger, msg *queue.
 	logger = logger.With(fields.PubKey(msg.MsgID.GetPubKey()), fields.Role(msg.MsgID.GetRoleType()))
 
 	if err := validateMessage(ncv.Share.Share, msg); err != nil {
-		logger.Debug("❌ got invalid message", zap.Error(err))
+		logger.Warn("❌ got invalid message", zap.Error(err))
 		return
 	}
 
