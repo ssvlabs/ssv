0075bda64f1b250c3339da4c6cb4a4141bfec4b4
guy muroch <guym@blox.io>
2024-04-11 14:24:49 +0300

    remove unused code

--- network/discovery/dv5_logger.go
+++ network/discovery/dv5_logger.go
@@ -8,27 +8,3 @@ import (
 type dv5Logger struct {
 	logger *zap.Logger // struct logger to implement log.Handler
 }
-
-// Log takes a record and uses the zap.Logger to print it
-//func (dvl *dv5Logger) Log(r *log.Record) error {
-//	logger := dvl.logger.With(zap.Any("context", r.Ctx))
-//	for _, v := range r.Ctx {
-//		logger = dvl.logger.With(zap.Any("v", v))
-//	}
-//	switch r.Lvl {
-//	case log.LvlTrace:
-//		logger.Debug(fmt.Sprintf("TRACE: %s", r.Msg))
-//	case log.LvlDebug:
-//		logger.Debug(r.Msg)
-//	case log.LvlInfo:
-//		logger.Info(r.Msg)
-//	case log.LvlWarn:
-//		logger.Warn(r.Msg)
-//	case log.LvlError:
-//		logger.Error(r.Msg)
-//	case log.LvlCrit:
-//		logger.Fatal(r.Msg)
-//	default:
-//	}
-//	return nil
-//}
