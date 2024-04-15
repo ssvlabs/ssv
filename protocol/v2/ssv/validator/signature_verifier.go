b18903cfb141b3ab3d67567f3cfad84df833e223
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2024-04-15 22:34:51 +0300

    reduce nesting
    Co-authored-by: Nikita Kryuchkov <nkryuchkov10@gmail.com>

--- protocol/v2/ssv/validator/signature_verifier.go
+++ protocol/v2/ssv/validator/signature_verifier.go
@@ -22,20 +22,23 @@ func NewSignatureVerifier() *SignatureVerifier {
 func (s *SignatureVerifier) Verify(msg *spectypes.SignedSSVMessage, operators []*spectypes.Operator) error {
 	// Find operator that matches ID with the signer and verify signature
 	for _, op := range operators {
-		if op.OperatorID == msg.GetOperatorID() {
-			operatorPubKey, ok := s.operatorIDToPubkeyCache.Get(op.OperatorID)
-			if !ok {
-				var err error
-				operatorPubKey, err = keys.PublicKeyFromString(string(op.SSVOperatorPubKey))
-				if err != nil {
-					return fmt.Errorf("could not parse signer public key: %w", err)
-				}
-
-				s.operatorIDToPubkeyCache.Set(op.OperatorID, operatorPubKey)
-			}
-
-			return operatorPubKey.Verify(msg.Data, msg.Signature)
+		if op.OperatorID != msg.GetOperatorID() {
+			continue
 		}
+			
+		operatorPubKey, ok := s.operatorIDToPubkeyCache.Get(op.OperatorID)
+		if !ok {
+			var err error
+			operatorPubKey, err = keys.PublicKeyFromString(string(op.SSVOperatorPubKey))
+			if err != nil {
+				return fmt.Errorf("could not parse signer public key: %w", err)
+			}
+		
+			s.operatorIDToPubkeyCache.Set(op.OperatorID, operatorPubKey)
+		}
+
+		return operatorPubKey.Verify(msg.Data, msg.Signature)
+	}
 	}
 
 	return fmt.Errorf("unknown signer")
