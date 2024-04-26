f55db37a28bad73025f43ababeb91b3cd84dd9cc
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-04-26 19:22:10 +0400

    Cleanup signer state

--- message/validation/alan/signer_state.go
+++ message/validation/alan/signer_state.go
@@ -3,8 +3,6 @@ package msgvalidation
 // signer_state.go describes state of a signer.
 
 import (
-	"time"
-
 	"github.com/attestantio/go-eth2-client/spec/phase0"
 	specqbft "github.com/bloxapp/ssv-spec/alan/qbft"
 )
@@ -12,7 +10,6 @@ import (
 // SignerState represents the state of a signer, including its start time, slot, round,
 // message counts, proposal data, and the number of duties performed in the current epoch.
 type SignerState struct {
-	Start              time.Time
 	Slot               phase0.Slot
 	Round              specqbft.Round
 	MessageCounts      MessageCounts
@@ -24,7 +21,6 @@ type SignerState struct {
 // ResetSlot resets the state's slot, round, message counts, and proposal data to the given values.
 // It also updates the start time to the current time and increments the epoch duties count if it's a new epoch.
 func (s *SignerState) ResetSlot(slot phase0.Slot, round specqbft.Round, newEpoch bool) {
-	s.Start = time.Now()
 	s.Slot = slot
 	s.Round = round
 	s.MessageCounts = MessageCounts{}
@@ -40,7 +36,6 @@ func (s *SignerState) ResetSlot(slot phase0.Slot, round specqbft.Round, newEpoch
 // ResetRound resets the state's round, message counts, and proposal data to the given values.
 // It also updates the start time to the current time.
 func (s *SignerState) ResetRound(round specqbft.Round) {
-	s.Start = time.Now()
 	s.Round = round
 	s.MessageCounts = MessageCounts{}
 	s.ProposalData = nil
