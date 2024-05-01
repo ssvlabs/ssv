cde25f960d4baf70e895c69851ace4f02cbfc2cd
Matus Kysel <matus@ssvlabs.io>
2024-05-01 21:18:28 +0200

    add pre HF runners

---
+++ protocol/v2/ssv/genesis/runner/duty_runners.go
@@ -0,0 +1,12 @@
+package runner
+
+import genesisspectypes "github.com/ssvlabs/ssv-spec-pre-cc/types"
+
+// DutyRunners is a map of duty runners mapped by msg id hex.
+type DutyRunners map[genesisspectypes.BeaconRole]Runner
+
+// DutyRunnerForMsgID returns a Runner from the provided msg ID, or nil if not found
+func (ci DutyRunners) DutyRunnerForMsgID(msgID genesisspectypes.MessageID) Runner {
+	role := msgID.GetRoleType()
+	return ci[role]
+}
