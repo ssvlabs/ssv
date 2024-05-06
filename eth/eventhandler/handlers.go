8e10df2d4c3b0f7072c0d8ffb2d5525a518ed1a1
moshe-blox <moshe@blox.io>
2024-05-06 19:41:15 +0300

    fix: eventHandler quorum calculation bug

--- eth/eventhandler/handlers.go
+++ eth/eventhandler/handlers.go
@@ -276,6 +276,8 @@ func (eh *EventHandler) validatorAddedEventToShare(
 	selfOperatorID := eh.operatorDataStore.GetOperatorID()
 	var shareSecret *bls.SecretKey
 
+	quorum, partialQuorum := ssvtypes.ComputeQuorumAndPartialQuorum(len(event.OperatorIds))
+
 	operators := make([]*spectypes.Operator, 0)
 	committeeIDs := make([]spectypes.OperatorID, 0)
 	committee := make([]*spectypes.CommitteeMember, 0)
@@ -328,19 +330,17 @@ func (eh *EventHandler) validatorAddedEventToShare(
 			}
 		}
 
-		q, pq := ssvtypes.ComputeQuorumAndPartialQuorum(len(committee))
-
 		operators = append(operators, &spectypes.Operator{
 			OperatorID:        operatorID,
 			SSVOperatorPubKey: od.PublicKey,
 			ClusterID:         spectypes.GetClusterID(committeeIDs),
 			Committee:         committee,
-			Quorum:            q,
-			PartialQuorum:     pq,
+			Quorum:            quorum,
+			PartialQuorum:     partialQuorum,
 		})
 	}
 
-	validatorShare.Quorum = operators[0].Quorum
+	validatorShare.Quorum = quorum
 	validatorShare.DomainType = eh.networkConfig.Domain
 	validatorShare.Committee = shareMembers
 	validatorShare.Graffiti = []byte("ssv.network")
