757ec734ccce926b98bb95ef5370cd105494adf4
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-05-09 16:27:40 +0400

    Enable msg validation

--- message/validation/self.go
+++ message/validation/self.go
@@ -3,11 +3,13 @@ package validation
 import (
 	"encoding/hex"
 
+	specqbft "github.com/ssvlabs/ssv-spec/qbft"
 	spectypes "github.com/ssvlabs/ssv-spec/types"
 
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"go.uber.org/zap"
 
+	"github.com/bloxapp/ssv/logging/fields"
 	"github.com/bloxapp/ssv/protocol/v2/ssv/queue"
 )
 
@@ -18,41 +20,38 @@ func (mv *messageValidator) validateSelf(pMsg *pubsub.Message) pubsub.Validation
 		return pubsub.ValidationReject
 	}
 
-	d, err := queue.DecodeSignedSSVMessage(signedSSVMessage)
-	if err != nil {
-		mv.logger.Error("failed to decode signed ssv message", zap.Error(err))
-		return pubsub.ValidationReject
+	decodedMessage := &queue.DecodedSSVMessage{
+		SSVMessage: signedSSVMessage.SSVMessage,
 	}
 
-	//
-	//switch signedSSVMessage.SSVMessage.MsgType {
-	//case spectypes.SSVConsensusMsgType:
-	//	consensusMessage, err := specqbft.DecodeMessage(signedSSVMessage.SSVMessage.Data)
-	//	if err != nil {
-	//		mv.logger.Error("failed to decode consensus message", zap.Error(err))
-	//		return pubsub.ValidationReject
-	//	}
-	//
-	//	decodedMessage.Body = consensusMessage
-	//
-	//case spectypes.SSVPartialSignatureMsgType:
-	//	partialSignatureMessages := &spectypes.PartialSignatureMessages{}
-	//	if err := partialSignatureMessages.Decode(signedSSVMessage.SSVMessage.Data); err != nil {
-	//		mv.logger.Error("failed to decode partial signature messages", zap.Error(err))
-	//		return pubsub.ValidationReject
-	//	}
-	//
-	//	decodedMessage.Body = partialSignatureMessages
-	//
-	//default:
-	//	mv.logger.Error("unsupported message type", fields.MessageType(signedSSVMessage.SSVMessage.MsgType))
-	//}
+	switch signedSSVMessage.SSVMessage.MsgType {
+	case spectypes.SSVConsensusMsgType:
+		consensusMessage, err := specqbft.DecodeMessage(signedSSVMessage.SSVMessage.Data)
+		if err != nil {
+			mv.logger.Error("failed to decode consensus message", zap.Error(err))
+			return pubsub.ValidationReject
+		}
+
+		decodedMessage.Body = consensusMessage
+
+	case spectypes.SSVPartialSignatureMsgType:
+		partialSignatureMessages := &spectypes.PartialSignatureMessages{}
+		if err := partialSignatureMessages.Decode(signedSSVMessage.SSVMessage.Data); err != nil {
+			mv.logger.Error("failed to decode partial signature messages", zap.Error(err))
+			return pubsub.ValidationReject
+		}
+
+		decodedMessage.Body = partialSignatureMessages
+
+	default:
+		mv.logger.Error("unsupported message type", fields.MessageType(signedSSVMessage.SSVMessage.MsgType))
+	}
 
 	mv.logger.Debug("gotp2pmessage",
 		zap.Int("type", int(signedSSVMessage.SSVMessage.MsgType)),
 		zap.String("role", signedSSVMessage.SSVMessage.GetID().GetRoleType().String()),
 		zap.String("id", hex.EncodeToString(signedSSVMessage.SSVMessage.GetID().GetSenderID()[16:])),
 	)
-	pMsg.ValidatorData = d
+	pMsg.ValidatorData = decodedMessage
 	return pubsub.ValidationAccept
 }
