d1e7e423ba5d3a3bd57d739e49a1163154a8430f
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-05-13 18:35:53 +0400

    Fix max msg sizes

--- message/validation/const.go
+++ message/validation/const.go
@@ -11,11 +11,12 @@ const (
 	// clockErrorTolerance is the maximum amount of clock error we expect to see between nodes.
 	clockErrorTolerance = time.Millisecond * 50
 
-	maxConsensusMsgSize                      = 8388608 // TODO: calculate new value
-	maxPartialSignatureMsgSize               = 1952
-	maxPayloadSize                           = maxConsensusMsgSize
-	maxSignedMsgSize                         = 4 + 56 + maxPayloadSize                // Max possible MsgType + MsgID + Data
-	maxEncodedMsgSize                        = maxSignedMsgSize + maxSignedMsgSize/10 // 10% for encoding overhead
+	// TODO: make sure values are correct
+	maxConsensusMsgSize                      = 6291829 // TODO: 8+8+8+56+32+8+13*65536+13*65536 instead?
+	maxPartialSignatureMsgSize               = 8 + 8 + 1000*(96+32+8+8)
+	maxPayloadSize                           = maxConsensusMsgSize                               // max(maxConsensusMsgSize, maxPartialSignatureMsgSize)
+	maxSignedMsgSize                         = 13*256 + 13*8 + 8 + 56 + maxPayloadSize + 5243144 // Max possible MsgType + MsgID + Data
+	maxEncodedMsgSize                        = maxSignedMsgSize + maxSignedMsgSize/10            // 10% for encoding overhead
 	allowedRoundsInFuture                    = 1
 	allowedRoundsInPast                      = 2
 	lateSlotAllowance                        = 2
