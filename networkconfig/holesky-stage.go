2e2d234f688b3bf53596456cd06486ef6aa09bef
moshe-blox <moshe@blox.io>
2024-05-21 11:41:59 +0300

    deploy CS to 61-64

--- networkconfig/holesky-stage.go
+++ networkconfig/holesky-stage.go
@@ -11,11 +11,12 @@ import (
 var HoleskyStage = NetworkConfig{
 	Name:                 "holesky-stage",
 	Beacon:               beacon.NewNetwork(spectypes.HoleskyNetwork),
-	Domain:               [4]byte{0x00, 0x00, 0x31, 0x12},
+	Domain:               [4]byte{0x00, 0x00, 0x31, 0x98},
 	GenesisEpoch:         1,
 	RegistrySyncOffset:   new(big.Int).SetInt64(84599),
 	RegistryContractAddr: "0x0d33801785340072C452b994496B19f196b7eE15",
 	Bootnodes: []string{
 		"enr:-Li4QPnPGESWx2wnu3s2qeu6keFbkaV2M0ZiGHgxxGI9ThP4XSgSaFzl6zYsF1zAdni3Mh04iA6BEZqoC6LZ52UFnwKGAYxEgLqeh2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__________gmlkgnY0gmlwhDQiKqmJc2VjcDI1NmsxoQP2e508AoA0B-KH-IaAd3nVCfI9q16lNztV-oTpcH72tIN0Y3CCE4mDdWRwgg-h",
 	},
+	CommitteeSubnetForkEpoch: 0,
 }
