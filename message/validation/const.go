f2d1a0054cb77dcb87699685ae92cb8795f43150
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-05-22 16:49:17 +0400

    Remove redundant comment

--- message/validation/const.go
+++ message/validation/const.go
@@ -14,14 +14,13 @@ const (
 	// TODO: make sure values are correct
 	maxConsensusMsgSize                      = 6291829 // TODO: 8+8+8+56+32+8+13*65536+13*65536 instead?
 	maxPartialSignatureMsgSize               = 8 + 8 + 1000*(96+32+8+8)
-	maxPayloadSize                           = maxConsensusMsgSize                               // max(maxConsensusMsgSize, maxPartialSignatureMsgSize)
-	maxSignedMsgSize                         = 13*256 + 13*8 + 8 + 56 + maxPayloadSize + 5243144 // Max possible MsgType + MsgID + Data
-	maxEncodedMsgSize                        = maxSignedMsgSize + maxSignedMsgSize/10            // 10% for encoding overhead
+	maxPayloadSize                           = maxConsensusMsgSize // max(maxConsensusMsgSize, maxPartialSignatureMsgSize)
+	maxSignedMsgSize                         = 13*256 + 13*8 + 8 + 56 + maxPayloadSize + 5243144
+	maxEncodedMsgSize                        = maxSignedMsgSize + maxSignedMsgSize/10 // 10% for encoding overhead
 	allowedRoundsInFuture                    = 1
 	allowedRoundsInPast                      = 2
 	lateSlotAllowance                        = 2
 	rsaSignatureSize                         = 256
-	blsSignatureSize                         = 96
 	syncCommitteeSize                        = 512
 	maxSignaturesInSyncCommitteeContribution = 13
 )
