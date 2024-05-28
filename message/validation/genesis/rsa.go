94481f0e7b08e00199b5092e545a60f65584ddd7
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-05-28 15:39:14 +0400

    Use Alan shares in genesis message validation

--- message/validation/genesis/rsa.go
+++ message/validation/genesis/rsa.go
@@ -8,7 +8,8 @@ import (
 	"github.com/ssvlabs/ssv/operator/keys"
 )
 
-func (mv *messageValidator) verifySignature(messageData []byte, operatorID spectypes.OperatorID, signature []byte) error {
+func (mv *messageValidator) verifySignature(msg *spectypes.SignedSSVMessage) error {
+	operatorID := msg.GetOperatorID()
 	operatorPubKey, ok := mv.operatorIDToPubkeyCache.Get(operatorID)
 	if !ok {
 		operator, found, err := mv.nodeStorage.GetOperatorData(nil, operatorID)
@@ -34,7 +35,7 @@ func (mv *messageValidator) verifySignature(messageData []byte, operatorID spect
 		mv.operatorIDToPubkeyCache.Set(operatorID, operatorPubKey)
 	}
 
-	if err := operatorPubKey.Verify(messageData, signature); err != nil {
+	if err := operatorPubKey.Verify(msg.GetData(), msg.Signature[:]); err != nil {
 		e := ErrSignatureVerification
 		e.innerErr = fmt.Errorf("verify opid: %v signature: %w", operatorID, err)
 		return e
