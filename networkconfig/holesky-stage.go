dceb450e229fa4486ae44932bf6750e8c09c47d2
moshe-blox <moshe@blox.io>
2024-05-29 18:16:36 +0300

    deploy new bootnode ENR (new privkey)

--- networkconfig/holesky-stage.go
+++ networkconfig/holesky-stage.go
@@ -16,6 +16,6 @@ var HoleskyStage = NetworkConfig{
 	RegistrySyncOffset:   new(big.Int).SetInt64(84599),
 	RegistryContractAddr: "0x0d33801785340072C452b994496B19f196b7eE15",
 	Bootnodes: []string{
-		"enr:-Li4QNqVtgIyawyzKILhcFRfBR93Qkm2aM-y_Irk-PL7ojMmeS5nYzHurvcpDqhZCUoaP8TLyhiG-8tGALk_AjtOquWGAYxEgLqgh2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__________gmlkgnY0gmlwhDQiKqmJc2VjcDI1NmsxoQP2e508AoA0B-KH-IaAd3nVCfI9q16lNztV-oTpcH72tIN0Y3CCE4mDdWRwgg-h",
+		"enr:-Li4QP4F8hNKamUYYuVsQXNGB4hgQM9t-12_--I9y_Z4N-PCR3cjHuqJJ-FPbG_DqxSyWazkNtP_7zHLbSRDnDGBFHaGAY_E6m1xh2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__________gmlkgnY0gmlwhDQiKqmJc2VjcDI1NmsxoQKnAiuSlgSR8asjCH0aYoVKM8uPbi4noFuFHZHaAHqknYN0Y3CCE4mDdWRwgg-h",
 	},
 }
