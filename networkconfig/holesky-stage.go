9c5193999a55b9e6076420e550bccaaef5787edf
y0sher <lyosher@gmail.com>
2024-06-02 17:19:45 +0300

    remove v5protocol id and change enr

--- networkconfig/holesky-stage.go
+++ networkconfig/holesky-stage.go
@@ -16,6 +16,6 @@ var HoleskyStage = NetworkConfig{
 	RegistrySyncOffset:   new(big.Int).SetInt64(84599),
 	RegistryContractAddr: "0x0d33801785340072C452b994496B19f196b7eE15",
 	Bootnodes: []string{
-		"enr:-Li4QNqVtgIyawyzKILhcFRfBR93Qkm2aM-y_Irk-PL7ojMmeS5nYzHurvcpDqhZCUoaP8TLyhiG-8tGALk_AjtOquWGAYxEgLqgh2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__________gmlkgnY0gmlwhDQiKqmJc2VjcDI1NmsxoQP2e508AoA0B-KH-IaAd3nVCfI9q16lNztV-oTpcH72tIN0Y3CCE4mDdWRwgg-h",
+		"enr:-Li4QCBHHRgmx5fs_9dIFmzZcEpsyyARVCKFhq35cf1VSSoZaK77d8nHmSyEPWOebGRKlYNVXm1RtTTc7TSii90eXseGAY_ZTXR9h2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__________gmlkgnY0gmlwhDQiKqmJc2VjcDI1NmsxoQK3StZ1-s-DSBpQECDB4boxFJ56H3rgxgUUeGO2BFAsgYN0Y3CCE4mDdWRwgg-h",
 	},
 }
