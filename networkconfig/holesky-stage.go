b0beb135df4d627366ad085e4a82d08eea18cea7
y0sher <lyosher@gmail.com>
2024-06-02 19:00:58 +0300

    new format enr

--- networkconfig/holesky-stage.go
+++ networkconfig/holesky-stage.go
@@ -16,6 +16,6 @@ var HoleskyStage = NetworkConfig{
 	RegistrySyncOffset:   new(big.Int).SetInt64(84599),
 	RegistryContractAddr: "0x0d33801785340072C452b994496B19f196b7eE15",
 	Bootnodes: []string{
-		"enr:-Li4QCBHHRgmx5fs_9dIFmzZcEpsyyARVCKFhq35cf1VSSoZaK77d8nHmSyEPWOebGRKlYNVXm1RtTTc7TSii90eXseGAY_ZTXR9h2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__________gmlkgnY0gmlwhDQiKqmJc2VjcDI1NmsxoQK3StZ1-s-DSBpQECDB4boxFJ56H3rgxgUUeGO2BFAsgYN0Y3CCE4mDdWRwgg-h",
+		"enr:-Ja4QMFMjrYCXeunSw96P_hxKOxGddsTzFwxJj8hjzPQC73BN9ng_IE5DQvbUYitXSU_6m8mYsaM15gKhtcm5chUFWCGAY_ZreHKgmlkgnY0gmlwhDQiKqmJc2VjcDI1NmsxoQJ2M-diOqYDd5-7jdnmAz9LP0MLsH6g_r4PCTMPfCuXMINzc3YBg3RjcIITiYN1ZHCCD6E",
 	},
 }
