84cafabcb4ec39184e47c0c119d83e60cdb9978a
y0sher <lyosher@gmail.com>
2024-06-02 13:52:16 +0300

    ignore subnets in disc

--- network/discovery/dv5_service.go
+++ network/discovery/dv5_service.go
@@ -1,9 +1,7 @@
 package discovery
 
 import (
-	"bytes"
 	"context"
-	"fmt"
 	"net"
 	"sync/atomic"
 	"time"
@@ -158,12 +156,12 @@ func (dvs *DiscV5Service) checkPeer(logger *zap.Logger, e PeerEvent) error {
 
 	// Get the peer's subnets, skipping if it has none.
 	nodeSubnets, err := records.GetSubnetsEntry(e.Node.Record())
-	if err != nil {
-		return fmt.Errorf("could not read subnets: %w", err)
-	}
-	if bytes.Equal(zeroSubnets, nodeSubnets) {
-		return errors.New("zero subnets")
-	}
+	//if err != nil {
+	//	return fmt.Errorf("could not read subnets: %w", err)
+	//}
+	//if bytes.Equal(zeroSubnets, nodeSubnets) {
+	//	return errors.New("zero subnets")
+	//}
 
 	dvs.subnetsIdx.UpdatePeerSubnets(e.AddrInfo.ID, nodeSubnets)
 	if !dvs.limitNodeFilter(e.Node) && !dvs.sharedSubnetsFilter(1)(e.Node) {
