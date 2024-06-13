9a2f77c806c2f1e1d0de45610d8e76ad40869d98
moshe-blox <moshe@blox.io>
2024-06-13 10:43:21 +0300

    temporary fix domain type

--- protocol/v2/ssv/runner/attester.go
+++ protocol/v2/ssv/runner/attester.go
@@ -122,10 +122,6 @@ func (r *AttesterRunner) ProcessConsensus(logger *zap.Logger, signedMsg *specqbf
 	if err := r.GetNetwork().Broadcast(msgToBroadcast); err != nil {
 		return errors.Wrap(err, "can't broadcast partial post consensus sig")
 	}
-	logger.Debug("🧩 broadcasted partial post consensus sig",
-		zap.String("domain_type", hex.EncodeToString(r.GetShare().DomainType[:])),
-		zap.Uint64("slot", uint64(decidedValue.Duty.Slot)),
-		zap.Uint64("signer", r.BaseRunner.Share.OperatorID))
 	return nil
 }
 
