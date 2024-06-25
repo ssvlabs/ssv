c1e904e56c71e25257a9aee9399d2842496ed1cc
Anton Korpusenko <antokorp@gmail.com>
2024-06-25 17:54:48 +0300

    Added committee.ConsumeQueue filtering (#1421)
    * added committee_queue filtering
    
    * committeePrioritizer added
    
    * removed height and round from filtering state
    
    * removed comment + fix typo error
    
    * added slot to error msg
    
    * fixed crash on runners map (#1431)
    
    * fixed crash on runners map
    
    * removed overhead allocations and passing runner to ConsumeQueue
    
    * fixed nil pointer usage of queue state
    
    * moved setting the state.slot to queue creation
    
    ---------
    
    Co-authored-by: Matus Kysel <MatusKysel@users.noreply.github.com>

--- protocol/v2/ssv/validator/committee.go
+++ protocol/v2/ssv/validator/committee.go
@@ -124,7 +124,7 @@ func (c *Committee) StartDuty(logger *zap.Logger, duty *spectypes.CommitteeDuty)
 			queueState: &queue.State{
 				HasRunningInstance: false,
 				Height:             qbft.Height(duty.Slot),
-				Slot:               0,
+				Slot:               duty.Slot,
 				//Quorum:             options.SSVShare.Share,// TODO
 			},
 		}
@@ -133,7 +133,7 @@ func (c *Committee) StartDuty(logger *zap.Logger, duty *spectypes.CommitteeDuty)
 
 	logger = c.logger.With(fields.DutyID(fields.FormatCommitteeDutyID(c.Operator.Committee, c.BeaconNetwork.EstimatedEpochAtSlot(duty.Slot), duty.Slot)), fields.Slot(duty.Slot))
 	// TODO alan: stop queue
-	go c.ConsumeQueue(logger, duty.Slot, c.ProcessMessage)
+	go c.ConsumeQueue(logger, duty.Slot, c.ProcessMessage, c.Runners[duty.Slot])
 
 	logger.Info("ℹ️ starting duty processing")
 	return c.Runners[duty.Slot].StartNewDuty(logger, duty)
