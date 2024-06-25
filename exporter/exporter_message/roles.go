58c1f2b1d1383f8408f115f3156804538dbb74ad
guy muroch <guym@blox.io>
2024-06-25 11:53:59 +0300

    add sync committee role to roles in exporter

--- exporter/exporter_message/roles.go
+++ exporter/exporter_message/roles.go
@@ -6,7 +6,7 @@ const (
 	RoleAttester RunnerRole = iota
 	RoleAggregator
 	RoleProposer
-	BNRoleSyncCommittee
+	RoleSyncCommittee
 	RoleSyncCommitteeContribution
 
 	RoleValidatorRegistration
@@ -17,20 +17,22 @@ const (
 // String returns name of the runner role
 func (r RunnerRole) String() string {
 	switch r {
-	case RoleCommittee:
-		return "COMMITTEE_RUNNER"
 	case RoleAttester:
 		return "ATTESTER_RUNNER"
 	case RoleAggregator:
 		return "AGGREGATOR_RUNNER"
 	case RoleProposer:
 		return "PROPOSER_RUNNER"
+	case RoleSyncCommittee:
+		return "SYNC_COMMITTEE"
 	case RoleSyncCommitteeContribution:
 		return "SYNC_COMMITTEE_CONTRIBUTION_RUNNER"
 	case RoleValidatorRegistration:
 		return "VALIDATOR_REGISTRATION_RUNNER"
 	case RoleVoluntaryExit:
 		return "VOLUNTARY_EXIT_RUNNER"
+	case RoleCommittee:
+		return "COMMITTEE_RUNNER"
 	default:
 		return "UNDEFINED"
 	}
@@ -45,6 +47,8 @@ func (r RunnerRole) ToBeaconRole() string {
 		return "AGGREGATOR"
 	case RoleProposer:
 		return "PROPOSER"
+	case RoleSyncCommittee:
+		return "SYNC_COMMITTEE"
 	case RoleSyncCommitteeContribution:
 		return "SYNC_COMMITTEE_CONTRIBUTION"
 	case RoleValidatorRegistration:
