d79b196e0fb0b8a2cd2df25056a23416fbad3102
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2024-07-07 11:16:13 +0300

    Duty Scheduler: Pre-Fork & Fork Support (#1443)
    * implementation
    
    * move alan fork epoch to network config
    
    * add attester only fork test
    
    * remove metadata in tests
    
    * skip to next epoch ticker
    
    * separate genesis tests
    
    * improve log

--- operator/validator/controller.go
+++ operator/validator/controller.go
@@ -8,6 +8,7 @@ import (
 	"sync"
 	"time"
 
+	genesisspectypes "github.com/ssvlabs/ssv-spec-pre-cc/types"
 	"golang.org/x/exp/maps"
 	"golang.org/x/exp/slices"
 
@@ -98,8 +99,6 @@ type Controller interface {
 	CommitteeActiveIndices(epoch phase0.Epoch) []phase0.ValidatorIndex
 	AllActiveIndices(epoch phase0.Epoch, afterInit bool) []phase0.ValidatorIndex
 	GetValidator(pubKey spectypes.ValidatorPK) (*validator.Validator, bool)
-	ExecuteDuty(logger *zap.Logger, duty *spectypes.BeaconDuty)
-	ExecuteCommitteeDuty(logger *zap.Logger, committeeID spectypes.CommitteeID, duty *spectypes.CommitteeDuty)
 	UpdateValidatorMetaDataLoop()
 	StartNetworkHandlers()
 	GetOperatorShares() []*ssvtypes.SSVShare
@@ -117,6 +116,8 @@ type Controller interface {
 	ReactivateCluster(owner common.Address, operatorIDs []uint64, toReactivate []*ssvtypes.SSVShare) error
 	UpdateFeeRecipient(owner, recipient common.Address) error
 	ExitValidator(pubKey phase0.BLSPubKey, blockNumber uint64, validatorIndex phase0.ValidatorIndex) error
+
+	duties.DutyExecutor
 }
 
 type nonCommitteeValidator struct {
@@ -692,6 +693,10 @@ func (c *controller) UpdateValidatorsMetadata(data map[spectypes.ValidatorPK]*be
 // GetValidator returns a validator instance from ValidatorsMap
 func (c *controller) GetValidator(pubKey spectypes.ValidatorPK) (*validator.Validator, bool) {
 	return c.validatorsMap.GetValidator(pubKey)
+}
+
+func (c *controller) ExecuteGenesisDuty(logger *zap.Logger, duty *genesisspectypes.Duty) {
+	panic("implement me")
 }
 
 func (c *controller) ExecuteDuty(logger *zap.Logger, duty *spectypes.BeaconDuty) {
