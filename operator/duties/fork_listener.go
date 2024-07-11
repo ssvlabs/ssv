4c606f60c1194682085c5ea81ba9f7b1a1c372c2
pavelkrolevets <pavelkrolevets@gmail.com>
2024-07-11 15:11:28 +0300

    add fork listener to scheduler

---
+++ operator/duties/fork_listener.go
@@ -0,0 +1,45 @@
+package duties
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+
+	"github.com/ssvlabs/ssv/network"
+)
+
+type ForkListener struct {
+	baseHandler
+	p2pnet network.P2PNetwork
+}
+
+func NewForkListener(p2pnet network.P2PNetwork) *ForkListener {
+	h := &ForkListener{
+		p2pnet: p2pnet,
+	}
+	return h
+}
+
+func (f *ForkListener) HandleDuties(ctx context.Context) {
+	f.logger.Info("starting fork listener")
+	defer f.logger.Info("fork listener exited")
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+
+		case <-f.ticker.Next():
+			slot := f.ticker.Slot()
+			if f.network.AlanForked(slot) {
+				if err := f.p2pnet.UpdateDomainTypeAtFork(f.logger); err != nil {
+					f.logger.Warn("could not update ENR at fork", zap.Error(err))
+				}
+			}
+		}
+	}
+}
+
+func (h *ForkListener) Name() string {
+	return "fork_listener"
+}
