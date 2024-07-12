e0bd122fa3a3f30fa94c1a77ff4de270adb91c6a
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-07-12 15:42:20 -0300

    Use require instead of assert

--- message/validation/consensus_state_test.go
+++ message/validation/consensus_state_test.go
@@ -4,15 +4,15 @@ import (
 	"testing"
 
 	"github.com/attestantio/go-eth2-client/spec/phase0"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestOperatorState(t *testing.T) {
 	t.Run("TestNewOperatorState", func(t *testing.T) {
 		size := phase0.Slot(10)
 		os := newOperatorState(size)
-		assert.NotNil(t, os)
-		assert.Equal(t, len(os.state), int(size))
+		require.NotNil(t, os)
+		require.Equal(t, len(os.state), int(size))
 	})
 
 	t.Run("TestGetAndSet", func(t *testing.T) {
@@ -26,8 +26,8 @@ func TestOperatorState(t *testing.T) {
 		os.Set(slot, epoch, signerState)
 		retrievedState := os.Get(slot)
 
-		assert.NotNil(t, retrievedState)
-		assert.Equal(t, retrievedState.Slot, slot)
+		require.NotNil(t, retrievedState)
+		require.Equal(t, retrievedState.Slot, slot)
 	})
 
 	t.Run("TestGetInvalidSlot", func(t *testing.T) {
@@ -37,7 +37,7 @@ func TestOperatorState(t *testing.T) {
 		slot := phase0.Slot(5)
 		retrievedState := os.Get(slot)
 
-		assert.Nil(t, retrievedState)
+		require.Nil(t, retrievedState)
 	})
 
 	t.Run("TestMaxSlot", func(t *testing.T) {
@@ -49,7 +49,7 @@ func TestOperatorState(t *testing.T) {
 		signerState := &SignerState{Slot: slot}
 
 		os.Set(slot, epoch, signerState)
-		assert.Equal(t, os.MaxSlot(), slot)
+		require.Equal(t, os.MaxSlot(), slot)
 	})
 
 	t.Run("TestDutyCount", func(t *testing.T) {
@@ -62,8 +62,8 @@ func TestOperatorState(t *testing.T) {
 
 		os.Set(slot, epoch, signerState)
 
-		assert.Equal(t, os.DutyCount(epoch), 1)
-		assert.Equal(t, os.DutyCount(epoch-1), 0)
+		require.Equal(t, os.DutyCount(epoch), 1)
+		require.Equal(t, os.DutyCount(epoch-1), 0)
 
 		slot2 := phase0.Slot(6)
 		epoch2 := phase0.Epoch(2)
@@ -71,9 +71,9 @@ func TestOperatorState(t *testing.T) {
 
 		os.Set(slot2, epoch2, signerState2)
 
-		assert.Equal(t, os.DutyCount(epoch2), 1)
-		assert.Equal(t, os.DutyCount(epoch), 1)
-		assert.Equal(t, os.DutyCount(epoch-1), 0)
+		require.Equal(t, os.DutyCount(epoch2), 1)
+		require.Equal(t, os.DutyCount(epoch), 1)
+		require.Equal(t, os.DutyCount(epoch-1), 0)
 	})
 
 	t.Run("TestIncrementLastEpochDuties", func(t *testing.T) {
@@ -84,15 +84,13 @@ func TestOperatorState(t *testing.T) {
 		epoch := phase0.Epoch(1)
 		signerState := &SignerState{Slot: slot}
 
-		// First set
 		os.Set(slot, epoch, signerState)
-		assert.Equal(t, os.DutyCount(epoch), 1)
+		require.Equal(t, os.DutyCount(epoch), 1)
 
-		// Second set within the same epoch
 		slot2 := phase0.Slot(6)
 		signerState2 := &SignerState{Slot: slot2}
 		os.Set(slot2, epoch, signerState2)
 
-		assert.Equal(t, os.DutyCount(epoch), 2)
+		require.Equal(t, os.DutyCount(epoch), 2)
 	})
 }
