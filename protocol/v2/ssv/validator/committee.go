70fd0ff11913e1315955f05970a3df91a94b34c8
rehs0y <lyosher@gmail.com>
2024-07-16 11:39:10 +0000

    fix: pass only relevant duty validator shares into the slot committee runner (#1460)
    * pass only relevant duty validator shares into the slot committee runner
    
    * fix: Remove beacon duties without shares in StartDuty function
    
    The StartDuty function in the Committee struct now removes beacon duties that do not have a corresponding share. This ensures that only relevant duty validator shares are passed into the slot committee runner.
    
    * Stop committee duty if no beacon duties exist.

--- protocol/v2/ssv/validator/committee.go
+++ protocol/v2/ssv/validator/committee.go
@@ -98,12 +98,32 @@ func (c *Committee) StartDuty(logger *zap.Logger, duty *spectypes.CommitteeDuty)
 		return errors.New(fmt.Sprintf("CommitteeRunner for slot %d already exists", duty.Slot))
 	}
 
-	var sharesCopy = make(map[phase0.ValidatorIndex]*spectypes.Share, len(c.Shares))
-	for k, v := range c.Shares {
-		sharesCopy[k] = v
+	validatorShares := make(map[phase0.ValidatorIndex]*spectypes.Share, len(duty.BeaconDuties))
+	toRemove := make([]int, 0)
+	// Remove beacon duties that don't have a share
+	for i, bd := range duty.BeaconDuties {
+		share, ok := c.Shares[bd.ValidatorIndex]
+		if !ok {
+			toRemove = append(toRemove, i)
+			continue
+		}
+		validatorShares[bd.ValidatorIndex] = share
+	}
+	// Remove beacon duties that don't have a share
+	if len(toRemove) > 0 {
+		newDuties, err := removeIndices(duty.BeaconDuties, toRemove)
+		if err != nil {
+			logger.Warn("could not remove beacon duties", zap.Error(err), zap.Ints("indices", toRemove))
+		} else {
+			duty.BeaconDuties = newDuties
+		}
 	}
 
-	r := c.CreateRunnerFn(duty.Slot, sharesCopy)
+	if len(duty.BeaconDuties) == 0 {
+		return errors.New("CommitteeDuty has no valid beacon duties")
+	}
+
+	r := c.CreateRunnerFn(duty.Slot, validatorShares)
 	// Set timeout function.
 	r.GetBaseRunner().TimeoutF = c.onTimeout
 	c.Runners[duty.Slot] = r
