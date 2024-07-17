a3c90814cebec7b1e6bca10ee9b61a9f8be929e3
moshe-blox <moshe@blox.io>
2024-07-17 17:42:22 +0300

    change Alan domain type for holesky-stage

--- networkconfig/holesky-stage.go
+++ networkconfig/holesky-stage.go
@@ -12,7 +12,7 @@ var HoleskyStage = NetworkConfig{
 	Name:                 "holesky-stage",
 	Beacon:               beacon.NewNetwork(spectypes.HoleskyNetwork),
 	GenesisDomainType:    [4]byte{0x00, 0x00, 0x31, 0x12},
-	AlanDomainType:       [4]byte{0x00, 0x00, 0x31, 0x99},
+	AlanDomainType:       [4]byte{0x00, 0x00, 0x31, 0x13},
 	GenesisEpoch:         1,
 	RegistrySyncOffset:   new(big.Int).SetInt64(84599),
 	RegistryContractAddr: "0x0d33801785340072C452b994496B19f196b7eE15",
