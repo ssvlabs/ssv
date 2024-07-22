302b9908c90cf0a6988f679dc6d50e7f449f59ba
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-07-23 01:49:44 +0400

    message validation: add comments when getting first validator index (#1496)
    * message validation: check indices length
    
    * add a comment
    
    * make comment clearer

--- message/validation/common_checks.go
+++ message/validation/common_checks.go
@@ -101,7 +101,9 @@ func (mv *messageValidator) validateBeaconDuty(
 	// Rule: For a proposal duty message, we check if the validator is assigned to it
 	if role == spectypes.RoleProposer {
 		epoch := mv.netCfg.Beacon.EstimatedEpochAtSlot(slot)
-		if mv.dutyStore.Proposer.ValidatorDuty(epoch, slot, indices[0]) == nil {
+		// Non-committee roles always have one validator index.
+		validatorIndex := indices[0]
+		if mv.dutyStore.Proposer.ValidatorDuty(epoch, slot, validatorIndex) == nil {
 			return ErrNoDuty
 		}
 	}
@@ -109,7 +111,9 @@ func (mv *messageValidator) validateBeaconDuty(
 	// Rule: For a sync committee aggregation duty message, we check if the validator is assigned to it
 	if role == spectypes.RoleSyncCommitteeContribution {
 		period := mv.netCfg.Beacon.EstimatedSyncCommitteePeriodAtEpoch(mv.netCfg.Beacon.EstimatedEpochAtSlot(slot))
-		if mv.dutyStore.SyncCommittee.Duty(period, indices[0]) == nil {
+		// Non-committee roles always have one validator index.
+		validatorIndex := indices[0]
+		if mv.dutyStore.SyncCommittee.Duty(period, validatorIndex) == nil {
 			return ErrNoDuty
 		}
 	}
