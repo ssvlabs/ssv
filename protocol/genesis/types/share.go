b8fc5471b8caedde03eba53c450f2d10f6f9fb94
Matus Kysel <matus@ssvlabs.io>
2024-07-24 10:34:53 +0200

    fix quorum

--- protocol/genesis/types/share.go
+++ protocol/genesis/types/share.go
@@ -3,15 +3,18 @@ package types
 import (
 	genesisspectypes "github.com/ssvlabs/ssv-spec-pre-cc/types"
 	spectypes "github.com/ssvlabs/ssv-spec/types"
+	"github.com/ssvlabs/ssv/protocol/v2/types"
 )
 
 func ConvertToGenesisShare(share *spectypes.Share, operator *spectypes.CommitteeMember) *genesisspectypes.Share {
+	q, pc := types.ComputeQuorumAndPartialQuorum(len(share.Committee))
 	genesisShare := &genesisspectypes.Share{
 		OperatorID:          operator.OperatorID,
 		ValidatorPubKey:     share.ValidatorPubKey[:], // Ensure this is necessary; remove if ValidatorPubKey is already a slice.
 		SharePubKey:         share.SharePubKey,
 		Committee:           make([]*genesisspectypes.Operator, 0, len(share.Committee)),
-		Quorum:              operator.GetQuorum(),
+		Quorum:              q,
+		PartialQuorum:       pc,
 		DomainType:          genesisspectypes.DomainType(share.DomainType),
 		FeeRecipientAddress: share.FeeRecipientAddress,
 		Graffiti:            share.Graffiti,
