6468628e1e904c0bc67f3c7a2f2a29e65b93bcb7
moshe-blox <moshe@blox.io>
2024-07-25 17:56:28 +0300

    draft

--- scripts/spec-alignment/differ.sh
+++ scripts/spec-alignment/differ.sh
@@ -3,9 +3,19 @@
 # Get the path to the ssv-spec folder.
 SPEC_PATH=$(go mod download -json github.com/ssvlabs/ssv-spec | jq -r .Dir)
 
+# Get the path to the ssv-spec folder.
+GENESIS_SPEC_PATH=$(go mod download -json github.com/ssvlabs/ssv-spec-pre-cc | jq -r .Dir)
+
 # Run Differ.
 differ \
     --config ./differ.config.yaml \
     --output ./output.diff \
     ../../ \
     $SPEC_PATH
+
+# Run Differ vs. genesis spec.
+differ \
+    --config ./genesis-differ.config.yaml \
+    --output ./output.diff \
+    ../../ \
+    $GENESIS_SPEC_PATH
