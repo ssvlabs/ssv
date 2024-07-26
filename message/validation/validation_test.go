4312c11aed2a22d491eec689434b38f12a8f3f65
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-07-26 14:13:40 -0300

    fix message validation tests

--- message/validation/validation_test.go
+++ message/validation/validation_test.go
@@ -50,6 +50,7 @@ func Test_ValidateSSVMessage(t *testing.T) {
 	require.NoError(t, err)
 
 	netCfg := networkconfig.TestNetwork
+	netCfg.AlanForkEpoch = math.MaxUint64 // use genesis domain to be able to use message templates from spec
 
 	ks := spectestingutils.Testing4SharesSet()
 	shares := generateShares(t, ks, ns, netCfg)
@@ -85,6 +86,11 @@ func Test_ValidateSSVMessage(t *testing.T) {
 					&share1,
 					&share2,
 					&share3,
+				},
+				Indices: []phase0.ValidatorIndex{
+					share1.ValidatorIndex,
+					share2.ValidatorIndex,
+					share3.ValidatorIndex,
 				},
 			}
 		}
@@ -737,6 +743,7 @@ func Test_ValidateSSVMessage(t *testing.T) {
 						ds := dutystore.New()
 						ds.Proposer.Add(spectestingutils.TestingDutyEpoch, spectestingutils.TestingDutySlot, shares.active.ValidatorIndex, &eth2apiv1.ProposerDuty{}, true)
 						ds.SyncCommittee.Add(0, shares.active.ValidatorIndex, &eth2apiv1.SyncCommitteeDuty{}, true)
+						ds.VoluntaryExit.AddDuty(spectestingutils.TestingDutySlot, phase0.BLSPubKey(shares.active.ValidatorPubKey))
 
 						validator := New(netCfg, validatorStore, ds, signatureVerifier).(*messageValidator)
 
