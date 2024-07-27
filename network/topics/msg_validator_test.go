8c444ec60d9984b9a47c2f2003f9455001dec07d
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-07-26 21:09:05 -0300

    fix TestMsgValidator

--- network/topics/msg_validator_test.go
+++ network/topics/msg_validator_test.go
@@ -2,11 +2,6 @@ package topics
 
 import (
 	"context"
-	"crypto"
-	"crypto/rsa"
-	"crypto/sha256"
-	genesisspectypes "github.com/ssvlabs/ssv-spec-pre-cc/types"
-	genesisvalidation "github.com/ssvlabs/ssv/message/validation/genesis"
 	"testing"
 
 	v1 "github.com/attestantio/go-eth2-client/api/v1"
@@ -14,14 +9,18 @@ import (
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	ps_pb "github.com/libp2p/go-libp2p-pubsub/pb"
 	pspb "github.com/libp2p/go-libp2p-pubsub/pb"
-	"github.com/stretchr/testify/require"
-	"go.uber.org/zap/zaptest"
-
+	"github.com/ssvlabs/ssv-spec-pre-cc/types"
 	"github.com/ssvlabs/ssv-spec/qbft"
 	spectypes "github.com/ssvlabs/ssv-spec/types"
 	spectestingutils "github.com/ssvlabs/ssv-spec/types/testingutils"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zaptest"
+
+	"github.com/ssvlabs/ssv/message/signatureverifier"
+	"github.com/ssvlabs/ssv/message/validation"
 	"github.com/ssvlabs/ssv/network/commons"
 	"github.com/ssvlabs/ssv/networkconfig"
+	"github.com/ssvlabs/ssv/operator/duties/dutystore"
 	operatorstorage "github.com/ssvlabs/ssv/operator/storage"
 	beaconprotocol "github.com/ssvlabs/ssv/protocol/v2/blockchain/beacon"
 	ssvtypes "github.com/ssvlabs/ssv/protocol/v2/types"
@@ -51,21 +50,20 @@ func TestMsgValidator(t *testing.T) {
 	}
 	require.NoError(t, ns.Shares().Save(nil, share))
 
-	mv := genesisvalidation.New(networkconfig.TestNetwork, genesisvalidation.WithNodeStorage(ns))
+	committeeID := share.CommitteeID()
+
+	signatureVerifier := signatureverifier.NewSignatureVerifier(ns)
+	mv := validation.New(networkconfig.TestNetwork, ns.ValidatorStore(), dutystore.New(), signatureVerifier)
 	require.NotNil(t, mv)
 
 	slot := networkconfig.TestNetwork.Beacon.GetBeaconNetwork().EstimatedCurrentSlot()
 
 	t.Run("valid consensus msg", func(t *testing.T) {
-		ssvMsg, err := dummySSVConsensusMsg(share.ValidatorPubKey, qbft.Height(slot))
+		ssvMsg, err := dummySSVConsensusMsg(committeeID[:], qbft.Height(slot))
 		require.NoError(t, err)
 
-		_, skByte, err := rsaencryption.GenerateKeys()
-		require.NoError(t, err)
-		operatorPrivateKey, err := rsaencryption.PemToPrivateKey(skByte)
-		require.NoError(t, err)
-
-		operatorId := uint64(1)
+		operatorID := uint64(1)
+		operatorPrivateKey := ks.OperatorKeys[operatorID]
 
 		operatorPubKey, err := rsaencryption.ExtractPublicKey(&operatorPrivateKey.PublicKey)
 		require.NoError(t, err)
@@ -73,30 +71,32 @@ func TestMsgValidator(t *testing.T) {
 		od := &storage.OperatorData{
 			PublicKey:    []byte(operatorPubKey),
 			OwnerAddress: common.Address{},
-			ID:           operatorId,
+			ID:           operatorID,
 		}
 
 		found, err := ns.SaveOperatorData(nil, od)
 		require.False(t, found)
 		require.NoError(t, err)
 
-		encodedMsg, err := commons.EncodeNetworkMsg(ssvMsg)
+		operatorSigner := spectestingutils.NewOperatorSigner(ks, operatorID)
+		sig, err := operatorSigner.SignSSVMessage(ssvMsg)
 		require.NoError(t, err)
 
-		hash := sha256.Sum256(encodedMsg)
-		signature, err := rsa.SignPKCS1v15(nil, operatorPrivateKey, crypto.SHA256, hash[:])
+		signedSSVMessage := &spectypes.SignedSSVMessage{
+			Signatures:  [][]byte{sig},
+			OperatorIDs: []types.OperatorID{operatorID},
+			SSVMessage:  ssvMsg,
+		}
+
+		encodedMsg, err := signedSSVMessage.Encode()
 		require.NoError(t, err)
 
-		var sig []byte
-		copy(sig[:], signature)
-
-		packedPubSubMsgPayload := genesisspectypes.EncodeSignedSSVMessage(encodedMsg, operatorId, sig)
-		topicID := commons.ValidatorTopicID(ssvMsg.GetID().GetDutyExecutorID())
+		topicID := commons.CommitteeTopicID(spectypes.CommitteeID(signedSSVMessage.SSVMessage.GetID().GetDutyExecutorID()[16:]))[0]
 
 		pmsg := &pubsub.Message{
 			Message: &pspb.Message{
-				Topic: &topicID[0],
-				Data:  packedPubSubMsgPayload,
+				Topic: &topicID,
+				Data:  encodedMsg,
 			},
 		}
 		res := mv.Validate(context.Background(), "16Uiu2HAkyWQyCb6reWXGQeBUt9EXArk6h3aq3PsFMwLNq3pPGH1r", pmsg)
@@ -146,12 +146,17 @@ func newPBMsg(data []byte, topic string, from []byte) *pubsub.Message {
 	return pmsg
 }
 
-func dummySSVConsensusMsg(pk spectypes.ValidatorPK, height qbft.Height) (*spectypes.SSVMessage, error) {
-	id := spectypes.NewMsgID(networkconfig.TestNetwork.DomainType(), pk[:], spectypes.RoleCommittee)
-	ks := spectestingutils.Testing4SharesSet()
-	validSignedMessage := spectestingutils.TestingRoundChangeMessageWithHeightAndIdentifier(ks.OperatorKeys[1], 1, height, id[:])
+func dummySSVConsensusMsg(dutyExecutorID []byte, height qbft.Height) (*spectypes.SSVMessage, error) {
+	id := spectypes.NewMsgID(networkconfig.TestNetwork.DomainType(), dutyExecutorID, spectypes.RoleCommittee)
+	qbftMsg := &qbft.Message{
+		MsgType:    qbft.RoundChangeMsgType,
+		Height:     height,
+		Round:      qbft.FirstRound,
+		Identifier: id[:],
+		Root:       spectestingutils.TestingQBFTRootData,
+	}
 
-	encodedSignedMessage, err := validSignedMessage.Encode()
+	encodedQBFTMsg, err := qbftMsg.Encode()
 	if err != nil {
 		return nil, err
 	}
@@ -159,6 +164,6 @@ func dummySSVConsensusMsg(pk spectypes.ValidatorPK, height qbft.Height) (*specty
 	return &spectypes.SSVMessage{
 		MsgType: spectypes.SSVConsensusMsgType,
 		MsgID:   id,
-		Data:    encodedSignedMessage,
+		Data:    encodedQBFTMsg,
 	}, nil
 }
