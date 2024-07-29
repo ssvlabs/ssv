51f3cfe65b828c382cbefb51a810fd45dfa3b738
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-07-29 07:19:29 -0300

    Sort imports

--- integration/qbft/tests/scenario_test.go
+++ integration/qbft/tests/scenario_test.go
@@ -5,8 +5,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ssvlabs/ssv/exporter/convert"
-
 	spec "github.com/attestantio/go-eth2-client/spec/phase0"
 	"github.com/ethereum/go-ethereum/common"
 	spectypes "github.com/ssvlabs/ssv-spec/types"
@@ -15,6 +13,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 
+	"github.com/ssvlabs/ssv/exporter/convert"
 	qbftstorage "github.com/ssvlabs/ssv/ibft/storage"
 	"github.com/ssvlabs/ssv/logging"
 	"github.com/ssvlabs/ssv/logging/fields"
