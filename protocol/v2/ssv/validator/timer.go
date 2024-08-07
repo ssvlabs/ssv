e55874c938250055cbecddbd39e6a7a932a406ca
Anton Korpusenko <antokorp@gmail.com>
2024-08-07 16:18:57 +0300

    added runner is nil check on committee timeout

--- protocol/v2/ssv/validator/timer.go
+++ protocol/v2/ssv/validator/timer.go
@@ -92,6 +92,10 @@ func (v *Committee) onTimeout(logger *zap.Logger, identifier spectypes.MessageID
 		//	return
 		//}
 		dr := v.Runners[phase0.Slot(height)]
+		if dr == nil {
+			logger.Warn("❗no committee runner found for slot", fields.Slot(phase0.Slot(height)))
+			return
+		}
 		hasDuty := dr.HasRunningDuty()
 		if !hasDuty {
 			return
