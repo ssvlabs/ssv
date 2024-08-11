12b3d12fe88e757c37cb542a7efea67d1ee29388
guym-blox <83158283+guym-blox@users.noreply.github.com>
2024-08-11 19:02:43 +0300

    add logic to proposer scheduler to support pre fork (#1603)
    * add logic to proposer scheduler to support pre fork
    
    * refactor
    
    ---------
    
    Co-authored-by: guy muroch <guym@blox.io>
    Co-authored-by: moshe-blox <moshe@blox.io>

--- operator/duties/proposer.go
+++ operator/duties/proposer.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"time"
 
+	genesisspectypes "github.com/ssvlabs/ssv-spec-pre-cc/types"
+
 	eth2apiv1 "github.com/attestantio/go-eth2-client/api/v1"
 	"github.com/attestantio/go-eth2-client/spec/phase0"
 	spectypes "github.com/ssvlabs/ssv-spec/types"
@@ -134,6 +136,18 @@ func (h *ProposerHandler) processExecution(epoch phase0.Epoch, slot phase0.Slot)
 		return
 	}
 
+	if !h.network.PastAlanForkAtEpoch(h.network.Beacon.EstimatedEpochAtSlot(slot)) {
+		toExecute := make([]*genesisspectypes.Duty, 0, len(duties))
+		for _, d := range duties {
+			if h.shouldExecute(d) {
+				toExecute = append(toExecute, h.toGenesisSpecDuty(d, genesisspectypes.BNRoleProposer))
+			}
+		}
+
+		h.dutiesExecutor.ExecuteGenesisDuties(h.logger, toExecute)
+		return
+	}
+
 	// range over duties and execute
 	toExecute := make([]*spectypes.ValidatorDuty, 0, len(duties))
 	for _, d := range duties {
@@ -184,6 +198,15 @@ func (h *ProposerHandler) fetchAndProcessDuties(ctx context.Context, epoch phase
 
 func (h *ProposerHandler) toSpecDuty(duty *eth2apiv1.ProposerDuty, role spectypes.BeaconRole) *spectypes.ValidatorDuty {
 	return &spectypes.ValidatorDuty{
+		Type:           role,
+		PubKey:         duty.PubKey,
+		Slot:           duty.Slot,
+		ValidatorIndex: duty.ValidatorIndex,
+	}
+}
+
+func (h *ProposerHandler) toGenesisSpecDuty(duty *eth2apiv1.ProposerDuty, role genesisspectypes.BeaconRole) *genesisspectypes.Duty {
+	return &genesisspectypes.Duty{
 		Type:           role,
 		PubKey:         duty.PubKey,
 		Slot:           duty.Slot,
