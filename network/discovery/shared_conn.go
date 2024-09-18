a4ea7207b0319d7cb3643a3ccd3d5bcfa4d64fa0
MatheusFranco99 <48058141+MatheusFranco99@users.noreply.github.com>
2024-09-18 21:32:51 +0100

    Update sharedUDPConn due to update of go-ethereum library

--- network/discovery/shared_conn.go
+++ network/discovery/shared_conn.go
@@ -1,25 +1,26 @@
 package discovery
 
 import (
-	"fmt"
 	"net"
+	"net/netip"
 
 	"github.com/ethereum/go-ethereum/p2p/discover"
+	"github.com/pkg/errors"
 )
 
 // sharedUDPConn implements a shared connection. Write sends messages to the underlying connection while read returns
 // messages that were found unprocessable and sent to the unhandled channel by the primary listener.
-// It's copied from https://github.com/ethereum/go-ethereum/blob/v1.13.5/p2p/server.go#L421
+// It's copied from https://github.com/ethereum/go-ethereum/blob/v1.14.8/p2p/server.go#L435
 type sharedUDPConn struct {
 	*net.UDPConn
 	unhandled chan discover.ReadPacket
 }
 
-// ReadFromUDP implements discover.UDPConn
-func (s *sharedUDPConn) ReadFromUDP(b []byte) (n int, addr *net.UDPAddr, err error) {
+// ReadFromUDPAddrPort implements discover.UDPConn
+func (s *sharedUDPConn) ReadFromUDPAddrPort(b []byte) (n int, addr netip.AddrPort, err error) {
 	packet, ok := <-s.unhandled
 	if !ok {
-		return 0, nil, fmt.Errorf("connection was closed")
+		return 0, netip.AddrPort{}, errors.New("connection was closed")
 	}
 	l := len(packet.Data)
 	if l > len(b) {
