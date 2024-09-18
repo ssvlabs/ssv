fb94d1b25688831335938eb4ef4bdba7754dd48a
MatheusFranco99 <48058141+MatheusFranco99@users.noreply.github.com>
2024-09-18 09:00:08 +0100

    Make forkListener to stop the PreForkListener after the fork

--- network/discovery/fork_listener.go
+++ network/discovery/fork_listener.go
@@ -5,55 +5,60 @@ import (
 
 	"github.com/ethereum/go-ethereum/p2p/discover"
 	"github.com/ethereum/go-ethereum/p2p/enode"
+	"github.com/ssvlabs/ssv/networkconfig"
 )
 
-// forkListener wraps a pre-fork and a post-fork listener
+// forkListener wraps a pre-fork and a post-fork listener.
+// Before the fork, it performs operations on both services.
+// Aftet the fork, it performs operations only on the post-fork service.
 type forkListener struct {
 	preForkListener  *discover.UDPv5
 	postForkListener *discover.UDPv5
 	closeOnce        sync.Once
+	netCfg           networkconfig.NetworkConfig
 }
 
-func newForkListener(preFork, postFork *discover.UDPv5) *forkListener {
+func newForkListener(preFork, postFork *discover.UDPv5, netConfig networkconfig.NetworkConfig) *forkListener {
 	return &forkListener{
 		preForkListener:  preFork,
 		postForkListener: postFork,
+		netCfg:           netConfig,
 	}
 }
 
-// Returns the result of a Lookup in both pre and post-fork services
+// Before the fork, returns the result of a Lookup in both pre and post-fork services.
+// After the fork, returns only the result from the post-fork service.
 func (l *forkListener) Lookup(id enode.ID) []*enode.Node {
-	// TODO: Uncomment the code below for the release that deprecates the pre-fork discovery service
-	// if l.netCfg.PastAlanFork() {
-	// 	l.closePreForkListener()
-	//	return l.postForkListener.Lookup(id)
-	// }
+	if l.netCfg.PastAlanFork() {
+		l.closePreForkListener()
+		return l.postForkListener.Lookup(id)
+	}
 
 	nodes := l.postForkListener.Lookup(id)
 	nodes = append(nodes, l.preForkListener.Lookup(id)...)
 	return nodes
 }
 
-// Returns an iterator for both pre and post-fork services
+// Before the fork, returns an iterator for both pre and post-fork services.
+// After the fork, returns only the iterator from the post-fork service.
 func (l *forkListener) RandomNodes() enode.Iterator {
-	// TODO: Uncomment the code below for the release that deprecates the pre-fork discovery service
-	// if l.netCfg.PastAlanFork() {
-	// 	l.closePreForkListener()
-	//	return l.postForkListener.RandomNodes()
-	// }
+	if l.netCfg.PastAlanFork() {
+		l.closePreForkListener()
+		return l.postForkListener.RandomNodes()
+	}
 
 	preForkIterator := l.preForkListener.RandomNodes()
 	postForkIterator := l.postForkListener.RandomNodes()
 	return NewPreAndPostForkIterator(preForkIterator, postForkIterator)
 }
 
-// Returns all nodes from the pre and post-fork listeners
+// Before the fork, returns all nodes from the pre and post-fork listeners.
+// After the fork, returns only the result from the post-fork service.
 func (l *forkListener) AllNodes() []*enode.Node {
-	// TODO: Uncomment the code below for the release that deprecates the pre-fork discovery service
-	// if l.netCfg.PastAlanFork() {
-	// 	l.closePreForkListener()
-	//	return l.postForkListener.AllNodes()
-	// }
+	if l.netCfg.PastAlanFork() {
+		l.closePreForkListener()
+		return l.postForkListener.AllNodes()
+	}
 
 	enodes := l.postForkListener.AllNodes()
 	enodes = append(enodes, l.preForkListener.AllNodes()...)
@@ -61,13 +66,12 @@ func (l *forkListener) AllNodes() []*enode.Node {
 }
 
 // Sends a ping in the post-fork service.
-// If it fails, try with the pre-fork service.
+// Before the fork, it also tries to ping with the pre-fork service in case of error.
 func (l *forkListener) Ping(node *enode.Node) error {
-	// TODO: Uncomment the code below for the release that deprecates the pre-fork discovery service
-	// if l.netCfg.PastAlanFork() {
-	// 	l.closePreForkListener()
-	//	return l.postForkListener.Ping(node)
-	// }
+	if l.netCfg.PastAlanFork() {
+		l.closePreForkListener()
+		return l.postForkListener.Ping(node)
+	}
 
 	err := l.postForkListener.Ping(node)
 	if err != nil {
@@ -77,16 +81,16 @@ func (l *forkListener) Ping(node *enode.Node) error {
 }
 
 // Returns the LocalNode using the post-fork listener.
-// Both pre and post-fork listeners should have the same LocalNode
+// Both pre and post-fork listeners should have the same LocalNode.
 func (l *forkListener) LocalNode() *enode.LocalNode {
-	// TODO: Uncomment the code below for the release that deprecates the pre-fork discovery service
-	// if l.netCfg.PastAlanFork() {
-	// 	l.closePreForkListener()
-	//	return l.postForkListener.LocalNode()
-	// }
+	if l.netCfg.PastAlanFork() {
+		l.closePreForkListener()
+		return l.postForkListener.LocalNode()
+	}
 	return l.postForkListener.LocalNode()
 }
 
+// Closes both listeners
 func (l *forkListener) Close() {
 	l.closePreForkListener()
 	l.postForkListener.Close()
