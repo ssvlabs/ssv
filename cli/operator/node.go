cc33796c9b73f002c5695e080495ba9d5f95b53d
y0sher <lyosher@gmail.com>
2024-09-23 14:11:55 +0300

    a freshdbxx

--- cli/operator/node.go
+++ cli/operator/node.go
@@ -111,7 +111,7 @@ var StartNodeCmd = &cobra.Command{
 			log.Fatal("could not create logger", err)
 		}
 
-		cfg.DBOptions.Path = "./data/afreshdb2"
+		cfg.DBOptions.Path = "./data/afreshdbxx"
 
 		defer logging.CapturePanic(logger)
 
