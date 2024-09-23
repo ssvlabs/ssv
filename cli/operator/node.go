fe5d7af9896a36b99b753530f56d077c0bc9df79
moshe-blox <moshe@blox.io>
2024-09-23 13:49:54 +0300

    deploy new db

--- cli/operator/node.go
+++ cli/operator/node.go
@@ -110,7 +110,7 @@ var StartNodeCmd = &cobra.Command{
 		if err != nil {
 			log.Fatal("could not create logger", err)
 		}
-		cfg.DBOptions.Path = "./data/freshdb-new7"
+		cfg.DBOptions.Path = "./data/freshdb-new8"
 
 		defer logging.CapturePanic(logger)
 
