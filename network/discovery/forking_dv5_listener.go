10792c84a142f1aa32c12aa19e42e6846a7c1980
y0sher <lyosher@gmail.com>
2024-10-07 11:54:42 +0300

    don't close prefork listenr on fork.

--- network/discovery/forking_dv5_listener.go
+++ network/discovery/forking_dv5_listener.go
@@ -42,11 +42,6 @@ func NewForkingDV5Listener(logger *zap.Logger, preFork, postFork Listener, itera
 // Before the fork, returns the result of a Lookup in both pre and post-fork services.
 // After the fork, returns only the result from the post-fork service.
 func (l *forkingDV5Listener) Lookup(id enode.ID) []*enode.Node {
-	if l.netCfg.PastAlanFork() {
-		l.closePreForkListener()
-		return l.postForkListener.Lookup(id)
-	}
-
 	nodes := l.postForkListener.Lookup(id)
 	nodes = append(nodes, l.preForkListener.Lookup(id)...)
 	return nodes
@@ -55,11 +50,6 @@ func (l *forkingDV5Listener) Lookup(id enode.ID) []*enode.Node {
 // Before the fork, returns an iterator for both pre and post-fork services.
 // After the fork, returns only the iterator from the post-fork service.
 func (l *forkingDV5Listener) RandomNodes() enode.Iterator {
-	if l.netCfg.PastAlanFork() {
-		l.closePreForkListener()
-		return l.postForkListener.RandomNodes()
-	}
-
 	fairMix := enode.NewFairMix(l.iteratorTimeout)
 	fairMix.AddSource(&annotatedIterator{l.postForkListener.RandomNodes(), "post"})
 	fairMix.AddSource(&annotatedIterator{l.preForkListener.RandomNodes(), "pre"})
@@ -69,11 +59,6 @@ func (l *forkingDV5Listener) RandomNodes() enode.Iterator {
 // Before the fork, returns all nodes from the pre and post-fork listeners.
 // After the fork, returns only the result from the post-fork service.
 func (l *forkingDV5Listener) AllNodes() []*enode.Node {
-	if l.netCfg.PastAlanFork() {
-		l.closePreForkListener()
-		return l.postForkListener.AllNodes()
-	}
-
 	enodes := l.postForkListener.AllNodes()
 	enodes = append(enodes, l.preForkListener.AllNodes()...)
 	return enodes
@@ -82,11 +67,6 @@ func (l *forkingDV5Listener) AllNodes() []*enode.Node {
 // Sends a ping in the post-fork service.
 // Before the fork, it also tries to ping with the pre-fork service in case of error.
 func (l *forkingDV5Listener) Ping(node *enode.Node) error {
-	if l.netCfg.PastAlanFork() {
-		l.closePreForkListener()
-		return l.postForkListener.Ping(node)
-	}
-
 	err := l.postForkListener.Ping(node)
 	if err != nil {
 		return l.preForkListener.Ping(node)
@@ -97,10 +77,6 @@ func (l *forkingDV5Listener) Ping(node *enode.Node) error {
 // Returns the LocalNode using the post-fork listener.
 // Both pre and post-fork listeners should have the same LocalNode.
 func (l *forkingDV5Listener) LocalNode() *enode.LocalNode {
-	if l.netCfg.PastAlanFork() {
-		l.closePreForkListener()
-		return l.postForkListener.LocalNode()
-	}
 	return l.postForkListener.LocalNode()
 }
 
