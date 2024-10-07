5707e5b5ef4018adb586bfe5025ac75517a05287
moshe-blox <moshe@blox.io>
2024-10-08 01:02:04 +0300

    fork at 84471

--- networkconfig/holesky-stage.go
+++ networkconfig/holesky-stage.go
@@ -13,10 +13,10 @@ var HoleskyStage = NetworkConfig{
 	Beacon:               beacon.NewNetwork(spectypes.HoleskyNetwork),
 	GenesisDomainType:    [4]byte{0x00, 0x00, 0x31, 0x12},
 	AlanDomainType:       [4]byte{0x00, 0x00, 0x31, 0x13},
-	GenesisEpoch:         84471,
+	GenesisEpoch:         1,
 	RegistrySyncOffset:   new(big.Int).SetInt64(84599),
 	RegistryContractAddr: "0x0d33801785340072C452b994496B19f196b7eE15",
-	AlanForkEpoch:        84423,
+	AlanForkEpoch:        84471,
 	Bootnodes: []string{
 		// Public bootnode:
 		// "enr:-Ja4QDYHVgUs9NvlMqq93ot6VNqbmrIlMrwKnq4X3DPRgyUNB4ospDp8ubMvsf-KsgqY8rzpZKy4GbE1DLphabpRBc-GAY_diLjngmlkgnY0gmlwhDQrLYqJc2VjcDI1NmsxoQKnAiuSlgSR8asjCH0aYoVKM8uPbi4noFuFHZHaAHqknYNzc3YBg3RjcIITiYN1ZHCCD6E",
