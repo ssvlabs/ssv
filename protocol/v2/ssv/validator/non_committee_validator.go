5d76b903e9bbb448a970d2756f97a1b71e2c61e2
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-10-11 16:48:33 -0300

    log root on saving

--- protocol/v2/ssv/validator/non_committee_validator.go
+++ protocol/v2/ssv/validator/non_committee_validator.go
@@ -124,6 +124,7 @@ func (ncv *CommitteeObserver) ProcessMessage(msg *queue.SSVMessage) error {
 			logger.Warn("NOT saved participants, roles not found",
 				zap.Uint64("validator_index", uint64(key.ValidatorIndex)),
 				zap.String("msg_id", hex.EncodeToString(msg.MsgID[:])),
+				fields.BlockRoot(key.Root),
 			)
 		}
 
