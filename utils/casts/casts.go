eb8dcfe3c0330ee58f788e108a501919726f7b04
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-10-15 10:48:28 -0300

    fix remaining casting issues between roles

--- utils/casts/casts.go
+++ utils/casts/casts.go
@@ -63,6 +63,27 @@ func RunnerRoleToConvertRole(runnerRole spectypes.RunnerRole) convert.RunnerRole
 	}
 }
 
+func BeaconRoleToRunnerRole(runnerRole spectypes.BeaconRole) spectypes.RunnerRole {
+	switch runnerRole {
+	case spectypes.BNRoleAttester:
+		return spectypes.RoleCommittee
+	case spectypes.BNRoleAggregator:
+		return spectypes.RoleAggregator
+	case spectypes.BNRoleProposer:
+		return spectypes.RoleProposer
+	case spectypes.BNRoleSyncCommittee:
+		return spectypes.RoleCommittee
+	case spectypes.BNRoleSyncCommitteeContribution:
+		return spectypes.RoleSyncCommitteeContribution
+	case spectypes.BNRoleValidatorRegistration:
+		return spectypes.RoleValidatorRegistration
+	case spectypes.BNRoleVoluntaryExit:
+		return spectypes.RoleVoluntaryExit
+	default:
+		return spectypes.RoleUnknown
+	}
+}
+
 // DurationToUint64 returns error if duration is negative and converts time.Duration to uint64 safe otherwise
 func DurationToUint64(t time.Duration) (uint64, error) {
 	if t < 0 {
