58136108cee34f8b720ac3f8731819b4699c41f9
Anatolie Lupacescu <anatolie@ssvlabs.io>
2024-10-22 17:06:14 +0300

    formatting

--- exporter/api/query_handlers.go
+++ exporter/api/query_handlers.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 
 	"github.com/attestantio/go-eth2-client/spec/phase0"
-	spectypes "github.com/ssvlabs/ssv-spec/types"
 	"go.uber.org/zap"
 
+	spectypes "github.com/ssvlabs/ssv-spec/types"
 	"github.com/ssvlabs/ssv/exporter/convert"
 	"github.com/ssvlabs/ssv/ibft/storage"
 	"github.com/ssvlabs/ssv/logging/fields"
