4cd1d128f4a32ab6098be5a6668d921f2ec5fbd6
iurii <sidorenko@ssvlabs.io>
2024-10-25 22:52:30 +0300

    commit to trigger stage deploy

--- protocol/v2/types/operator.go
+++ protocol/v2/types/operator.go
@@ -2,6 +2,7 @@ package types
 
 import spectypes "github.com/ssvlabs/ssv-spec/types"
 
+
 func OperatorIDsFromOperators(operators []*spectypes.Operator) []spectypes.OperatorID {
 	ids := make([]spectypes.OperatorID, len(operators))
 	for i, op := range operators {
