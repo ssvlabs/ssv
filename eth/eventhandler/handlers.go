93b3aa5b13dea40d59906820b5b02e20d89d5220
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-10-31 14:48:35 -0300

    Revert "fix double self calculation logic"
    This reverts commit 444bbc2248d8259dfff54239ddcfc8ad92b45393.

--- eth/eventhandler/handlers.go
+++ eth/eventhandler/handlers.go
@@ -97,6 +97,10 @@ func (eh *EventHandler) handleOperatorAdded(txn basedb.Txn, event *contract.Cont
 
 		var existingOperatorsCount int
 		for _, shareMember := range share.Committee {
+			if shareMember.Signer == event.OperatorId {
+				existingOperatorsCount++
+			}
+
 			_, ok, err := eh.nodeStorage.GetOperatorData(txn, shareMember.Signer)
 			if err != nil {
 				return fmt.Errorf("get operator data: %w", err)
