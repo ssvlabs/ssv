dda71680a9d790ef76e73419b3f02e56a5c30fe3
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-10-31 10:24:17 -0300

    Revert "use mutex instead of txn"
    This reverts commit 5e04deeeeba031025c5b982eeb5ffe90a7f3fed4.

--- ibft/storage/store.go
+++ ibft/storage/store.go
@@ -4,7 +4,6 @@ import (
 	"encoding/binary"
 	"fmt"
 	"slices"
-	"sync"
 
 	"github.com/attestantio/go-eth2-client/spec/phase0"
 	"github.com/pkg/errors"
@@ -43,9 +42,8 @@ func init() {
 // ibftStorage struct
 // instanceType is what separates different iBFT eth2 duty types (attestation, proposal and aggregation)
 type ibftStorage struct {
-	prefix         []byte
-	db             basedb.Database
-	participantsMu sync.Mutex
+	prefix []byte
+	db     basedb.Database
 }
 
 // New create new ibft storage
@@ -58,7 +56,7 @@ func New(db basedb.Database, prefix string) qbftstorage.QBFTStore {
 
 // GetHighestInstance returns the StoredInstance for the highest instance.
 func (i *ibftStorage) GetHighestInstance(identifier []byte) (*qbftstorage.StoredInstance, error) {
-	val, found, err := i.get(highestInstanceKey, identifier[:])
+	val, found, err := i.get(nil, highestInstanceKey, identifier[:])
 	if !found {
 		return nil, nil
 	}
@@ -93,14 +91,14 @@ func (i *ibftStorage) saveInstance(inst *qbftstorage.StoredInstance, toHistory, 
 	}
 
 	if asHighest {
-		err = i.save(value, highestInstanceKey, inst.State.ID)
+		err = i.save(nil, value, highestInstanceKey, inst.State.ID)
 		if err != nil {
 			return errors.Wrap(err, "could not save highest instance")
 		}
 	}
 
 	if toHistory {
-		err = i.save(value, instanceKey, inst.State.ID, uInt64ToByteSlice(uint64(inst.State.Height)))
+		err = i.save(nil, value, instanceKey, inst.State.ID, uInt64ToByteSlice(uint64(inst.State.Height)))
 		if err != nil {
 			return errors.Wrap(err, "could not save historical instance")
 		}
@@ -111,7 +109,7 @@ func (i *ibftStorage) saveInstance(inst *qbftstorage.StoredInstance, toHistory, 
 
 // GetInstance returns historical StoredInstance for the given identifier and height.
 func (i *ibftStorage) GetInstance(identifier []byte, height specqbft.Height) (*qbftstorage.StoredInstance, error) {
-	val, found, err := i.get(instanceKey, identifier[:], uInt64ToByteSlice(uint64(height)))
+	val, found, err := i.get(nil, instanceKey, identifier[:], uInt64ToByteSlice(uint64(height)))
 	if !found {
 		return nil, nil
 	}
@@ -152,17 +150,17 @@ func (i *ibftStorage) CleanAllInstances(msgID []byte) error {
 		return errors.Wrap(err, "failed to remove decided")
 	}
 
-	if err := i.delete(highestInstanceKey, msgID[:]); err != nil {
+	if err := i.delete(nil, highestInstanceKey, msgID[:]); err != nil {
 		return errors.Wrap(err, "failed to remove last decided")
 	}
 	return nil
 }
 
 func (i *ibftStorage) UpdateParticipants(identifier convert.MessageID, slot phase0.Slot, newParticipants []spectypes.OperatorID) (updated bool, err error) {
-	i.participantsMu.Lock()
-	defer i.participantsMu.Unlock()
+	txn := i.db.Begin()
+	defer txn.Discard()
 
-	existingParticipants, err := i.getParticipants(identifier, slot)
+	existingParticipants, err := i.getParticipants(txn, identifier, slot)
 	if err != nil {
 		return false, fmt.Errorf("get participants %w", err)
 	}
@@ -172,8 +170,12 @@ func (i *ibftStorage) UpdateParticipants(identifier convert.MessageID, slot phas
 		return false, nil
 	}
 
-	if err := i.saveParticipants(identifier, slot, mergedParticipants); err != nil {
+	if err := i.saveParticipants(txn, identifier, slot, mergedParticipants); err != nil {
 		return false, fmt.Errorf("save participants: %w", err)
+	}
+
+	if err := txn.Commit(); err != nil {
+		return false, fmt.Errorf("commit transaction: %w", err)
 	}
 
 	return true, nil
@@ -203,11 +205,11 @@ func (i *ibftStorage) GetParticipantsInRange(identifier convert.MessageID, from,
 }
 
 func (i *ibftStorage) GetParticipants(identifier convert.MessageID, slot phase0.Slot) ([]spectypes.OperatorID, error) {
-	return i.getParticipants(identifier, slot)
+	return i.getParticipants(nil, identifier, slot)
 }
 
-func (i *ibftStorage) getParticipants(identifier convert.MessageID, slot phase0.Slot) ([]spectypes.OperatorID, error) {
-	val, found, err := i.get(participantsKey, identifier[:], uInt64ToByteSlice(uint64(slot)))
+func (i *ibftStorage) getParticipants(txn basedb.ReadWriter, identifier convert.MessageID, slot phase0.Slot) ([]spectypes.OperatorID, error) {
+	val, found, err := i.get(txn, participantsKey, identifier[:], uInt64ToByteSlice(uint64(slot)))
 	if err != nil {
 		return nil, err
 	}
@@ -219,12 +221,12 @@ func (i *ibftStorage) getParticipants(identifier convert.MessageID, slot phase0.
 	return operators, nil
 }
 
-func (i *ibftStorage) saveParticipants(identifier convert.MessageID, slot phase0.Slot, operators []spectypes.OperatorID) error {
+func (i *ibftStorage) saveParticipants(txn basedb.ReadWriter, identifier convert.MessageID, slot phase0.Slot, operators []spectypes.OperatorID) error {
 	bytes, err := encodeOperators(operators)
 	if err != nil {
 		return fmt.Errorf("encode operators: %w", err)
 	}
-	if err := i.save(bytes, participantsKey, identifier[:], uInt64ToByteSlice(uint64(slot))); err != nil {
+	if err := i.save(txn, bytes, participantsKey, identifier[:], uInt64ToByteSlice(uint64(slot))); err != nil {
 		return fmt.Errorf("save to DB: %w", err)
 	}
 
@@ -237,16 +239,16 @@ func mergeParticipants(existingParticipants, newParticipants []spectypes.Operato
 	return slices.Compact(allParticipants)
 }
 
-func (i *ibftStorage) save(value []byte, id string, pk []byte, keyParams ...[]byte) error {
+func (i *ibftStorage) save(txn basedb.ReadWriter, value []byte, id string, pk []byte, keyParams ...[]byte) error {
 	prefix := append(i.prefix, pk...)
 	key := i.key(id, keyParams...)
-	return i.db.Set(prefix, key, value)
+	return i.db.Using(txn).Set(prefix, key, value)
 }
 
-func (i *ibftStorage) get(id string, pk []byte, keyParams ...[]byte) ([]byte, bool, error) {
+func (i *ibftStorage) get(txn basedb.ReadWriter, id string, pk []byte, keyParams ...[]byte) ([]byte, bool, error) {
 	prefix := append(i.prefix, pk...)
 	key := i.key(id, keyParams...)
-	obj, found, err := i.db.Get(prefix, key)
+	obj, found, err := i.db.Using(txn).Get(prefix, key)
 	if !found {
 		return nil, found, nil
 	}
@@ -256,10 +258,10 @@ func (i *ibftStorage) get(id string, pk []byte, keyParams ...[]byte) ([]byte, bo
 	return obj.Value, found, nil
 }
 
-func (i *ibftStorage) delete(id string, pk []byte, keyParams ...[]byte) error {
+func (i *ibftStorage) delete(txn basedb.ReadWriter, id string, pk []byte, keyParams ...[]byte) error {
 	prefix := append(i.prefix, pk...)
 	key := i.key(id, keyParams...)
-	return i.db.Delete(prefix, key)
+	return i.db.Using(txn).Delete(prefix, key)
 }
 
 func (i *ibftStorage) key(id string, params ...[]byte) []byte {
