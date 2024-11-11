2ac7d3d72b2b3d7f469f487a5569efa3423e72c1
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-11-11 13:42:34 -0300

    script for custom config generation

---
+++ cmd/cfggen/generate-config.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"encoding/hex"
+	"flag"
+	"log"
+	"math/big"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/attestantio/go-eth2-client/spec/phase0"
+	ethcommon "github.com/ethereum/go-ethereum/common"
+	spectypes "github.com/ssvlabs/ssv-spec/types"
+	"gopkg.in/yaml.v2"
+
+	"github.com/ssvlabs/ssv/networkconfig"
+	"github.com/ssvlabs/ssv/protocol/v2/blockchain/beacon"
+)
+
+const (
+	defaultOutputPath     = "./config/config.local.yaml"
+	defaultLogLevel       = "info"
+	defaultDBPath         = "./data/db"
+	defaultDiscovery      = "mdns"
+	sliceSeparator        = ","
+	configFilePermissions = 0644
+)
+
+var (
+	defaultNetwork = networkconfig.LocalTestnet
+)
+
+func main() {
+	outputPath := flag.String("output-path", defaultOutputPath, "Output path for generated config")
+
+	logLevel := flag.String("log-level", defaultLogLevel, "Log level")
+	dbPath := flag.String("db-path", defaultDBPath, "DB path")
+	discovery := flag.String("discovery", defaultDiscovery, "Discovery")
+	consensusClient := flag.String("consensus-client", "", "Consensus client")
+	executionClient := flag.String("execution-client", "", "Execution client")
+	operatorPrivateKey := flag.String("operator-private-key", "", "Secret key")
+	metricsAPIPort := flag.Int("metrics-api-port", 0, "Metrics API port")
+
+	networkName := flag.String("network-name", defaultNetwork.Name, "Network name")
+	networkGenesisDomain := flag.String("network-genesis-domain", "0x"+hex.EncodeToString(defaultNetwork.GenesisDomainType[:]), "Network genesis domain")
+	networkAlanDomain := flag.String("network-alan-domain", "0x"+hex.EncodeToString(defaultNetwork.AlanDomainType[:]), "Network alan domain")
+	networkGenesisEpoch := flag.Uint64("network-genesis-epoch", uint64(defaultNetwork.GenesisEpoch), "Network genesis epoch")
+	networkRegistrySyncOffset := flag.Uint64("network-registry-sync-offset", defaultNetwork.RegistrySyncOffset.Uint64(), "Network registry sync offset")
+	networkRegistryContractAddr := flag.String("network-registry-contract-addr", defaultNetwork.RegistryContractAddr.String(), "Network registry contract addr")
+	networkBootnodes := flag.String("network-bootnodes", strings.Join(defaultNetwork.Bootnodes, sliceSeparator), "Network bootnodes (comma-separated)")
+	networkDiscoveryProtocolID := flag.String("network-discovery-protocol-id", "0x"+hex.EncodeToString(defaultNetwork.DiscoveryProtocolID[:]), "Network discovery protocol ID")
+	networkAlanForkEpoch := flag.Uint64("network-alan-fork-epoch", uint64(defaultNetwork.AlanForkEpoch), "Network Alan fork epoch")
+
+	beaconNetworkParent := flag.String("beacon-network-parent", string(defaultNetwork.Beacon.(*beacon.Network).Parent), "Beacon network parent")
+	beaconNetworkName := flag.String("beacon-network-name", defaultNetwork.Beacon.(*beacon.Network).Name, "Beacon network name")
+
+	beaconNetworkForkVersion := flag.String("beacon-network-fork-version", "0x"+hex.EncodeToString(defaultNetwork.Beacon.(*beacon.Network).ForkVersionVal[:]), "Beacon network fork version")
+	beaconNetworkMinGenesisTime := flag.Uint64("beacon-network-min-genesis-time", defaultNetwork.Beacon.(*beacon.Network).MinGenesisTimeVal, "Beacon network min genesis time")
+	beaconNetworkSlotDuration := flag.Int64("beacon-network-slot-duration", int64(defaultNetwork.Beacon.(*beacon.Network).SlotDurationVal), "Beacon network slot duration")
+	beaconNetworkSlotsPerEpoch := flag.Uint64("beacon-network-slots-per-epoch", defaultNetwork.Beacon.(*beacon.Network).SlotsPerEpochVal, "Beacon network slots per epoch")
+	beaconNetworkEpochsPerSyncCommitteePeriod := flag.Uint64("beacon-network-epochs-per-sync-committee-period", defaultNetwork.Beacon.(*beacon.Network).EpochsPerSyncCommitteePeriodVal, "Beacon network epochs per sync committee period")
+
+	flag.Parse()
+
+	if *consensusClient == "" {
+		log.Fatalf("The --consensus-client flag is mandatory")
+	}
+
+	if *executionClient == "" {
+		log.Fatalf("The --execution-client flag is mandatory")
+	}
+
+	parsedGenesisDomain, err := hex.DecodeString(strings.TrimPrefix(*networkGenesisDomain, "0x"))
+	if err != nil {
+		log.Fatalf("Failed to decode genesis network domain: %v", err)
+	}
+
+	parsedAlanDomain, err := hex.DecodeString(strings.TrimPrefix(*networkAlanDomain, "0x"))
+	if err != nil {
+		log.Fatalf("Failed to decode alan network domain: %v", err)
+	}
+
+	parsedDiscoveryProtocolID, err := hex.DecodeString(strings.TrimPrefix(*networkDiscoveryProtocolID, "0x"))
+	if err != nil {
+		log.Fatalf("Failed to decode discovery protocol ID: %v", err)
+	}
+
+	var parsedDiscoveryProtocolIDArr [6]byte
+	if len(parsedDiscoveryProtocolID) != 0 {
+		parsedDiscoveryProtocolIDArr = [6]byte(parsedDiscoveryProtocolID)
+	}
+
+	parsedBeaconNetworkForkVersion, err := hex.DecodeString(strings.TrimPrefix(*beaconNetworkForkVersion, "0x"))
+	if err != nil {
+		log.Fatalf("Failed to decode beacon network fork version: %v", err)
+	}
+
+	var parsedBeaconNetworkForkVersionArr [4]byte
+	if len(parsedBeaconNetworkForkVersion) != 0 {
+		parsedBeaconNetworkForkVersionArr = [4]byte(parsedBeaconNetworkForkVersion)
+	}
+
+	var bootnodes []string
+	if *networkBootnodes != "" {
+		bootnodes = strings.Split(*networkBootnodes, sliceSeparator)
+	}
+
+	var config Config
+	config.Global.LogLevel = *logLevel
+	config.DB.Path = *dbPath
+	config.ConsensusClient.Address = *consensusClient
+	config.ExecutionClient.Address = *executionClient
+	config.P2P.Discovery = *discovery
+	config.OperatorPrivateKey = *operatorPrivateKey
+	config.MetricsAPIPort = *metricsAPIPort
+	config.SSV.CustomNetwork = &networkconfig.NetworkConfig{
+		Name: *networkName,
+		Beacon: &beacon.Network{
+			Parent:                          spectypes.BeaconNetwork(*beaconNetworkParent),
+			Name:                            *beaconNetworkName,
+			ForkVersionVal:                  parsedBeaconNetworkForkVersionArr,
+			MinGenesisTimeVal:               *beaconNetworkMinGenesisTime,
+			SlotDurationVal:                 time.Duration(*beaconNetworkSlotDuration),
+			SlotsPerEpochVal:                *beaconNetworkSlotsPerEpoch,
+			EpochsPerSyncCommitteePeriodVal: *beaconNetworkEpochsPerSyncCommitteePeriod,
+		},
+		GenesisDomainType:    spectypes.DomainType(parsedGenesisDomain),
+		AlanDomainType:       spectypes.DomainType(parsedAlanDomain),
+		GenesisEpoch:         phase0.Epoch(*networkGenesisEpoch),
+		RegistrySyncOffset:   new(big.Int).SetUint64(*networkRegistrySyncOffset),
+		RegistryContractAddr: ethcommon.HexToAddress(*networkRegistryContractAddr),
+		Bootnodes:            bootnodes,
+		DiscoveryProtocolID:  parsedDiscoveryProtocolIDArr,
+		AlanForkEpoch:        phase0.Epoch(*networkAlanForkEpoch),
+	}
+
+	data, err := yaml.Marshal(&config)
+	if err != nil {
+		log.Fatalf("Failed to marshal YAML: %v", err)
+	}
+
+	err = os.WriteFile(*outputPath, data, configFilePermissions)
+	if err != nil {
+		log.Fatalf("Failed to write file: %v", err)
+	}
+
+	log.Printf("Saved config into '%s'", *outputPath)
+}
