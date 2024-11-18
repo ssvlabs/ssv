ec66b475db796591c73476e49495f74f27029164
iurii-ssv <183610124+iurii-ssv@users.noreply.github.com>
2024-11-18 18:00:10 +0200

    beacon-client: attestation data request caching (#1798)
    * beacon-client: attestation data request caching
    
    * first review pass
    
    * add unit-tests for GetAttestationData
    
    * 2nd review pass
    
    * minor cleanup
    
    * gotta start ttlcache to prune expired items
    
    * check total number of slots requested
    
    * use singleflight package to hide low-level implementation details
    
    * fix linter
    
    * err must be func-local to not cause races
    
    * omit toolchain directive
    
    * lock Go 1.22.6
    
    * deep copy AttestationData
    
    * reduce attestation data cache TTL to 2 slots because in practice its only fetched during the slot of the duty and not later
    
    * comment
    
    * comment
    
    * comment
    
    * comment
    
    * add clarifying comment about cached items lifetime
    
    ---------
    
    Co-authored-by: moshe-blox <moshe@blox.io>

--- utils/hashmap/hashmap.go
+++ utils/hashmap/hashmap.go
@@ -29,11 +29,18 @@ func (m *Map[Key, Value]) GetOrSet(key Key, value Value) (Value, bool) {
 	return actual.(Value), loaded
 }
 
+func (m *Map[Key, Value]) CompareAndSwap(key Key, old, new Value) (swapped bool) {
+	return m.m.CompareAndSwap(key, old, new)
+}
+
 func (m *Map[Key, Value]) Set(key Key, value Value) {
 	m.m.Store(key, value)
 }
 
 // SlowLen returns the number of elements in the map by iterating over all items.
+// This method call doesn't block other operations (for example Set), hence the
+// resulting length value returned might or might not reflect the outcome of
+// concurrent operations happening with this Map.
 //
 // This implementation is quite expensive. If it becomes a bottleneck,
 // we should consider maintaining an internal atomic counter and
