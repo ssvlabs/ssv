40906b60cae8f3e01e1460252c25e2dc7f12c6c0
oleg-ssvlabs <oleg.iar@ssvlabs.io>
2024-11-20 15:10:56 +0100

    Observability: change the metric instantiation function name

--- observability/metric.go
+++ observability/metric.go
@@ -6,7 +6,7 @@ var (
 	SecondsHistogramBuckets = []float64{0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10}
 )
 
-func GetMetric[T any](metricName string, create func(metricName string) (T, error)) T {
+func NewMetric[T any](metricName string, create func(metricName string) (T, error)) T {
 	logger := zap.L()
 	metric, err := create(metricName)
 	if err != nil {
