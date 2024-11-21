908d02d5796d9f00fab010921f469052e430a96f
moshe-blox <moshe@blox.io>
2024-11-21 18:24:46 +0200

    set fork epoch to 326520

--- networkconfig/mainnet.go
+++ networkconfig/mainnet.go
@@ -14,7 +14,7 @@ var Mainnet = NetworkConfig{
 	GenesisDomainType:    spectypes.GenesisMainnet,
 	AlanDomainType:       spectypes.AlanMainnet,
 	GenesisEpoch:         218450,
-	AlanForkEpoch:        326517, // Nov-25-2024 12:00:23 PM UTC
+	AlanForkEpoch:        326520, // Nov-25-2024 12:00:23 PM UTC
 	RegistrySyncOffset:   new(big.Int).SetInt64(17507487),
 	RegistryContractAddr: "0xDD9BC35aE942eF0cFa76930954a156B3fF30a4E1",
 	Bootnodes: []string{
