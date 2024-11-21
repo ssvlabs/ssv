e072eb521d6b3782ec38beff53105f3acb7a9c0a
oleg-ssvlabs <oleg.iar@ssvlabs.io>
2024-11-21 10:44:30 +0100

    Observability - refactor NewMetric function, other minor improvements

--- eth/eventhandler/observability.go
+++ eth/eventhandler/observability.go
@@ -11,43 +11,35 @@ import (
 )
 
 const (
-	observabilityComponentName      = "github.com/ssvlabs/ssv/eth/eventhandler"
-	observabilityComponentNamespace = "ssv.event_syncer.handler"
+	observabilityName      = "github.com/ssvlabs/ssv/eth/eventhandler"
+	observabilityNamespace = "ssv.event_syncer.handler"
 )
 
 var (
-	meter = otel.Meter(observabilityComponentName)
+	meter = otel.Meter(observabilityName)
 
 	eventsProcessSuccessCounter = observability.NewMetric(
-		fmt.Sprintf("%s.events_processed", observabilityComponentNamespace),
-		func(metricName string) (metric.Int64Counter, error) {
-			return meter.Int64Counter(
-				metricName,
-				metric.WithUnit("{event}"),
-				metric.WithDescription("total number of successfully processed events(logs)"))
-		},
-	)
+		meter.Int64Counter(
+			metricName("events_processed"),
+			metric.WithUnit("{event}"),
+			metric.WithDescription("total number of successfully processed events(logs)")))
 
 	eventsProcessFailureCounter = observability.NewMetric(
-		fmt.Sprintf("%s.events_failed", observabilityComponentNamespace),
-		func(metricName string) (metric.Int64Counter, error) {
-			return meter.Int64Counter(
-				metricName,
-				metric.WithUnit("{event}"),
-				metric.WithDescription("total number of failures during event(log) processing"))
-		},
-	)
+		meter.Int64Counter(
+			metricName("events_failed"),
+			metric.WithUnit("{event}"),
+			metric.WithDescription("total number of failures during event(log) processing")))
 
 	lastProcessedBlockGauge = observability.NewMetric(
-		fmt.Sprintf("%s.last_processed_block", observabilityComponentNamespace),
-		func(metricName string) (metric.Int64Gauge, error) {
-			return meter.Int64Gauge(
-				metricName,
-				metric.WithUnit("{block_number}"),
-				metric.WithDescription("last processed block by event handler"))
-		},
-	)
+		meter.Int64Gauge(
+			metricName("last_processed_block"),
+			metric.WithUnit("{block_number}"),
+			metric.WithDescription("last processed block by event handler")))
 )
+
+func metricName(name string) string {
+	return fmt.Sprintf("%s.%s", observabilityNamespace, name)
+}
 
 func eventNameAttribute(eventName string) attribute.KeyValue {
 	const eventNameAttrName = "ssv.event_syncer.event_name"
--- observability/metric.go
+++ observability/metric.go
@@ -6,13 +6,10 @@ var (
 	SecondsHistogramBuckets = []float64{0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10}
 )
 
-func NewMetric[T any](metricName string, create func(metricName string) (T, error)) T {
+func NewMetric[T any](metric T, err error) T {
 	logger := zap.L()
-	metric, err := create(metricName)
 	if err != nil {
-		logger.Error("failed to instantiate metric",
-			zap.String("metric_name", metricName),
-			zap.Error(err))
+		logger.Error("failed to instantiate metric", zap.Error(err))
 	}
 	return metric
 }
