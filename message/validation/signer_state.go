9dc78feb5f192aa3b3b285b06ff3d60cf7b0f8aa
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2024-11-22 16:20:44 -0300

    add comments for SeenSigners

--- message/validation/signer_state.go
+++ message/validation/signer_state.go
@@ -16,7 +16,12 @@ type SignerState struct {
 	// Storing pointer to byte array instead of slice to reduce memory consumption when we don't need the hash.
 	// A nil slice could be an alternative, but it'd consume more memory, and we'd need to cast [32]byte returned by sha256.Sum256() to slice.
 	HashedProposalData *[32]byte
-	SeenSigners        map[SignersBitMask]struct{}
+	// Max possible map size for committee sizes:
+	//  4 (f=1): C(4,3)+C(4,4)=5
+	//  7 (f=2): C(7,5)+C(7,6)+C(7,7)=29
+	// 10 (f=3): C(10,7)+C(10,8)+C(10,9)+C(10,10)=176
+	// 13 (f=4): C(13,9)+C(13,10)+C(13,11)+C(13,12)+C(13,13)=1093
+	SeenSigners map[SignersBitMask]struct{}
 }
 
 func NewSignerState(slot phase0.Slot, round specqbft.Round) *SignerState {
