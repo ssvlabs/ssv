e4218ba8e9a0fea973a3dae4e22ebe4c8ff7dd40
moshe-blox <moshe@blox.io>
2024-11-24 18:02:57 +0200

    don't fork (until epoch 99999999)

--- networkconfig/mainnet.go
+++ networkconfig/mainnet.go
@@ -14,7 +14,7 @@ var Mainnet = NetworkConfig{
 	GenesisDomainType:    spectypes.GenesisMainnet,
 	AlanDomainType:       spectypes.AlanMainnet,
 	GenesisEpoch:         218450,
-	AlanForkEpoch:        327375, // Nov-25-2024 12:00:23 PM UTC
+	AlanForkEpoch:        99999999, // Nov-25-2024 12:00:23 PM UTC
 	RegistrySyncOffset:   new(big.Int).SetInt64(17507487),
 	RegistryContractAddr: "0xDD9BC35aE942eF0cFa76930954a156B3fF30a4E1",
 	Bootnodes: []string{
