45cfefea5352196037c3147d4555e9f73894a94d
Matus Kysel <matus@ssvlabs.io>
2024-11-27 15:13:54 +0100

    fix(queue): change msg types priorities

--- protocol/v2/ssv/queue/messages.go
+++ protocol/v2/ssv/queue/messages.go
@@ -301,21 +301,3 @@ func scoreCommitteeMessageSubtype(state *State, m *SSVMessage, relativeHeight in
 	}
 	return 0
 }
-
-// scoreCommitteeConsensusType returns an integer score for the type of committee consensus message.
-// When given a non-consensus message, scoreConsensusType returns 0.
-func scoreCommitteeConsensusType(m *SSVMessage) int {
-	if qbftMsg, ok := m.Body.(*specqbft.Message); ok {
-		switch qbftMsg.MsgType {
-		case specqbft.CommitMsgType:
-			return 4
-		case specqbft.RoundChangeMsgType:
-			return 3
-		case specqbft.ProposalMsgType:
-			return 2
-		case specqbft.PrepareMsgType:
-			return 1
-		}
-	}
-	return 0
-}
