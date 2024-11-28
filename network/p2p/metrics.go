1d6bb7a79bb3311e7d0b0a7c76d32ddd32cfcb03
oleg-ssvlabs <oleg.iar@ssvlabs.io>
2024-11-28 12:58:43 +0100

    Observability - instrument P2P Topics component

--- network/p2p/metrics.go
+++ network/p2p/metrics.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/ssvlabs/ssv/logging/fields"
 	"github.com/ssvlabs/ssv/network/peers/connections"
-	"github.com/ssvlabs/ssv/network/topics"
 
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/prometheus/client_golang/prometheus"
@@ -18,7 +17,6 @@ import (
 
 type Metrics interface {
 	connections.Metrics
-	topics.Metrics
 }
 
 var (
