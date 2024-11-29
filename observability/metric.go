132f210452b56427ab13c02b055b77c4ccfd83f5
oleg-ssvlabs <oleg.iar@ssvlabs.io>
2024-11-29 11:56:03 +0100

    Observability - add 0.001 dimension to default seconds histogram bucket

--- observability/metric.go
+++ observability/metric.go
@@ -3,7 +3,7 @@ package observability
 import "go.uber.org/zap"
 
 var (
-	SecondsHistogramBuckets = []float64{0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10}
+	SecondsHistogramBuckets = []float64{0, 0.001, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10}
 )
 
 func NewMetric[T any](metric T, err error) T {
