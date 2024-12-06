92a52845bc2f9aaf8114dc2b7fc0377896b29d45
oleg-ssvlabs <oleg.iar@ssvlabs.io>
2024-12-06 11:20:51 +0100

    Remove some of the unicode charts from traces

--- protocol/v2/ssv/runner/committee.go
+++ protocol/v2/ssv/runner/committee.go
@@ -378,7 +378,7 @@ func (cr *CommitteeRunner) ProcessPostConsensus(ctx context.Context, logger *zap
 	}
 	logger = logger.With(fields.ConsensusTime(cr.measurements.ConsensusTime()))
 
-	eventMsg := "🧩 got partial signatures"
+	eventMsg := "got partial signatures"
 	span.AddEvent(eventMsg)
 	logger.Debug(eventMsg,
 		zap.Bool("quorum", quorum),
@@ -474,7 +474,7 @@ func (cr *CommitteeRunner) ProcessPostConsensus(ctx context.Context, logger *zap
 			specSig := phase0.BLSSignature{}
 			copy(specSig[:], sig)
 
-			eventMsg = "🧩 reconstructed partial signatures committee"
+			eventMsg = "reconstructed partial signatures committee"
 			span.AddEvent(eventMsg)
 			vlogger.Debug(eventMsg, zap.Uint64s("signers", getPostConsensusCommitteeSigners(cr.BaseRunner.State, root)))
 			// Get the beacon object related to root
