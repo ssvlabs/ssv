e5f4019aa2df6f7eb6e58209b2654dde781c8368
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2024-12-08 13:57:38 +0200

    add logs

--- protocol/v2/ssv/runner/committee.go
+++ protocol/v2/ssv/runner/committee.go
@@ -17,6 +17,7 @@ import (
 
 	specqbft "github.com/ssvlabs/ssv-spec/qbft"
 	spectypes "github.com/ssvlabs/ssv-spec/types"
+
 	"github.com/ssvlabs/ssv/logging/fields"
 	"github.com/ssvlabs/ssv/networkconfig"
 	"github.com/ssvlabs/ssv/protocol/v2/blockchain/beacon"
@@ -672,6 +673,7 @@ func (cr *CommitteeRunner) executeDuty(ctx context.Context, logger *zap.Logger, 
 	slot := duty.DutySlot()
 	// We set committeeIndex to 0 for simplicity, there is no need to specify it exactly because
 	// all 64 Ethereum committees assigned to this slot will get the same data to attest for.
+	logger.Debug("BEFORE: getting attestation data", fields.Slot(slot))
 	attData, _, err := cr.GetBeaconNode().GetAttestationData(slot, 0)
 	if err != nil {
 		return errors.Wrap(err, "failed to get attestation data")
@@ -680,6 +682,7 @@ func (cr *CommitteeRunner) executeDuty(ctx context.Context, logger *zap.Logger, 
 		zap.Duration("attestation_data_time", time.Since(start)),
 		fields.Slot(slot),
 	)
+	logger.Debug("AFTER: got attestation data", zap.Any("attestation_data", attData))
 
 	cr.started = time.Now()
 	cr.metrics.StartConsensus()
