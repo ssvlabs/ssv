72fda7642514df784813381665a430980a8d48bf
oleg-ssvlabs <oleg.iar@ssvlabs.io>
2024-12-12 10:02:01 +0100

    Implement validator statuses reporter

--- operator/validator/task_executor.go
+++ operator/validator/task_executor.go
@@ -24,7 +24,7 @@ func (c *controller) taskLogger(taskName string, fields ...zap.Field) *zap.Logge
 func (c *controller) StopValidator(pubKey spectypes.ValidatorPK) error {
 	logger := c.taskLogger("StopValidator", fields.PubKey(pubKey[:]))
 
-	recordValidatorStatus(c.ctx, statusRemoved)
+	// recordValidatorStatus(c.ctx, statusRemoved)
 	c.onShareStop(pubKey)
 
 	logger.Info("removed validator")
