aad7655f8101f3f1ff5bb880d0f38a95b9ba257e
moshe-blox <moshe@blox.io>
2024-12-22 19:09:34 +0200

    copy fixedSubnets slice

--- operator/validator/controller.go
+++ operator/validator/controller.go
@@ -509,11 +509,9 @@ func (c *controller) StartValidators() {
 }
 
 func (c *controller) selfSubnets() records.Subnets {
-	// Get the fixed subnets (e.g., exporter subscribed to all subnets).
-	mySubnets := c.network.FixedSubnets()
-	if mySubnets == nil {
-		mySubnets = make(records.Subnets, networkcommons.Subnets())
-	}
+	// Start off with a copy of the fixed subnets (e.g., exporter subscribed to all subnets).
+	mySubnets := make(records.Subnets, networkcommons.Subnets())
+	copy(mySubnets, c.network.FixedSubnets())
 
 	// Compute the new subnets according to the active committees/validators.
 	myValidators := c.validatorStore.OperatorValidators(c.operatorDataStore.GetOperatorID())
