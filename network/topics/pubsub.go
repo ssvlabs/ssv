a2feeccbdee63b4e40219cae32e95daa97fd97b9
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2024-12-25 18:01:56 +0200

    Update pubsub.go

--- network/topics/pubsub.go
+++ network/topics/pubsub.go
@@ -31,11 +31,11 @@ const (
 // the following are kept in vars to allow flexibility (e.g. in tests)
 const (
 	// validationQueueSize is the size that we assign to the validation queue
-	validationQueueSize = 1024
+	validationQueueSize = 2048
 	// outboundQueueSize is the size that we assign to the outbound message queue
-	outboundQueueSize = 1024
+	outboundQueueSize = 2048
 	// validateThrottle is the amount of goroutines used for pubsub msg validation
-	validateThrottle = 8192
+	validateThrottle = 16384
 	// scoreInspectInterval is the interval for performing score inspect, which goes over all peers scores
 	defaultScoreInspectInterval = 1 * time.Minute
 	// scoreInspectLogFrequency is the frequency of logging the score inspection
