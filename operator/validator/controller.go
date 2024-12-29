6e4775fc23ea920a98b681f2bd2fbd7f0ab04e41
y0sher <lyosher@gmail.com>
2024-12-29 15:49:58 +0200

    refactor: update SetCommitteeSubnet modify and not return. modify usage in controller

--- operator/validator/controller.go
+++ operator/validator/controller.go
@@ -452,9 +452,9 @@ func (c *controller) StartValidators() {
 		return
 	}
 
-	cidInt := new(big.Int)
+	intBuf := new(big.Int)
 
-	mySubnets := c.selfSubnets(cidInt)
+	mySubnets := c.selfSubnets(intBuf)
 
 	var ownShares []*ssvtypes.SSVShare
 	var pubKeysToFetch [][]byte
@@ -462,7 +462,8 @@ func (c *controller) StartValidators() {
 		if c.operatorDataStore.GetOperatorID() != 0 && share.BelongsToOperator(c.operatorDataStore.GetOperatorID()) {
 			ownShares = append(ownShares, share)
 		}
-		subnet := networkcommons.SetCommitteeSubnet(cidInt, share.CommitteeID())
+		networkcommons.SetCommitteeSubnet(intBuf, share.CommitteeID())
+		subnet := intBuf.Uint64()
 		if mySubnets[subnet] != 0 {
 			pubKeysToFetch = append(pubKeysToFetch, share.ValidatorPubKey[:])
 		}
@@ -512,12 +513,12 @@ func (c *controller) StartValidators() {
 }
 
 // selfSubnets calculates the operator's subnets by adding up the fixed subnets and the active committees
-// it recvs big int to avoid unnecessary conversions, if intCid is nil it will allocate a new big int
-func (c *controller) selfSubnets(intCid *big.Int) records.Subnets {
+// it recvs big int buffer for memory reusing, if is nil it will allocate new
+func (c *controller) selfSubnets(buf *big.Int) records.Subnets {
 	// Start off with a copy of the fixed subnets (e.g., exporter subscribed to all subnets).
-	localInt := intCid
-	if localInt == nil {
-		localInt = new(big.Int)
+	localBuf := buf
+	if localBuf == nil {
+		localBuf = new(big.Int)
 	}
 
 	mySubnets := make(records.Subnets, networkcommons.Subnets())
@@ -526,8 +527,8 @@ func (c *controller) selfSubnets(intCid *big.Int) records.Subnets {
 	// Compute the new subnets according to the active committees/validators.
 	myValidators := c.validatorStore.OperatorValidators(c.operatorDataStore.GetOperatorID())
 	for _, v := range myValidators {
-		subnet := networkcommons.SetCommitteeSubnet(localInt, v.CommitteeID())
-		mySubnets[subnet] = 1
+		networkcommons.SetCommitteeSubnet(localBuf, v.CommitteeID())
+		mySubnets[localBuf.Uint64()] = 1
 	}
 
 	return mySubnets
@@ -993,20 +994,21 @@ func (c *controller) UpdateValidatorMetaDataLoop() {
 	const batchSize = 512
 	var sleep = 2 * time.Second
 
-	cidInt := new(big.Int)
+	intBuf := new(big.Int)
 
 	for {
 		// Get the shares to fetch metadata for.
 		start := time.Now()
 
-		mySubnets := c.selfSubnets(cidInt)
+		mySubnets := c.selfSubnets(intBuf)
 		var existingShares, newShares []*ssvtypes.SSVShare
 		c.sharesStorage.Range(nil, func(share *ssvtypes.SSVShare) bool {
 			if share.Liquidated {
 				return true
 			}
 
-			subnet := networkcommons.SetCommitteeSubnet(cidInt, share.CommitteeID())
+			networkcommons.SetCommitteeSubnet(intBuf, share.CommitteeID())
+			subnet := intBuf.Uint64()
 			if mySubnets[subnet] == 0 {
 				return true
 			}
