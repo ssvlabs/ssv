326917b39b292847b4f62a67fe1456f94f8b4b84
iurii <sidorenko@ssvlabs.io>
2025-01-16 12:30:08 +0200

    see if we can read & decode SSZ shares in Badger DB

--- migrations/migration_5_share_gob_to_ssz.go
+++ migrations/migration_5_share_gob_to_ssz.go
@@ -2,10 +2,6 @@ package migrations
 
 import (
 	"context"
-	"fmt"
-	"github.com/davecgh/go-spew/spew"
-
-	"github.com/ssvlabs/ssv/storage/basedb"
 	"go.uber.org/zap"
 )
 
@@ -15,53 +11,53 @@ import (
 var migration_5_change_share_format_from_gob_to_ssz = Migration{
 	Name: "migration_5_change_share_format_from_gob_to_ssz",
 	Run: func(ctx context.Context, logger *zap.Logger, opt Options, key []byte, completed CompletedFunc) error {
-		// storagePrefix is a base prefix we use when storing shares
-		var storagePrefix = []byte("operator/")
-
-		// sets is a bunch of updates this migration will need to perform, we cannot do them all in a
-		// single transaction (because there is a limit on how large a single transaction can be) so
-		// we'll use SetMany func that will split up the data we want to update into batches committing
-		// each batch in a separate transaction. I guess that makes this migration non-atomic.
-		sets := make([]basedb.Obj, 0)
-
-		err := opt.Db.GetAll(append(storagePrefix, sharesPrefixGOB...), func(i int, obj basedb.Obj) error {
-			shareGOB := &storageShareGOB{}
-			if err := shareGOB.Decode(obj.Value); err != nil {
-				// TODO - dumping obj to see why we can't decode it
-				//return fmt.Errorf("decode gob share: %w", err)
-				return fmt.Errorf("decode gob share: %w obj: %s", err, spew.Sdump(obj))
-			}
-			share, err := storageShareGOBToSpecShare(shareGOB)
-			if err != nil {
-				return fmt.Errorf("convert storage share to spec share: %w", err)
-			}
-			shareSSZ := specShareToStorageShareSSZ(share)
-			key := storageKeySSZ(share.ValidatorPubKey[:])
-			value, err := shareSSZ.Encode()
-			if err != nil {
-				return fmt.Errorf("encode ssz share: %w", err)
-			}
-			sets = append(sets, basedb.Obj{
-				Key:   key,
-				Value: value,
-			})
-			return nil
-		})
-		if err != nil {
-			return fmt.Errorf("GetAll: %w", err)
-		}
-
-		if err := opt.Db.SetMany(storagePrefix, len(sets), func(i int) (basedb.Obj, error) {
-			return sets[i], nil
-		}); err != nil {
-			return fmt.Errorf("SetMany: %w", err)
-		}
-
-		// This makes sure migration applies only once.
-		// TODO - uncomment to complete migration
-		//if err := completed(opt.Db); err != nil {
-		//	return fmt.Errorf("complete transaction: %w", err)
+		//// storagePrefix is a base prefix we use when storing shares
+		//var storagePrefix = []byte("operator/")
+		//
+		//// sets is a bunch of updates this migration will need to perform, we cannot do them all in a
+		//// single transaction (because there is a limit on how large a single transaction can be) so
+		//// we'll use SetMany func that will split up the data we want to update into batches committing
+		//// each batch in a separate transaction. I guess that makes this migration non-atomic.
+		//sets := make([]basedb.Obj, 0)
+		//
+		//err := opt.Db.GetAll(append(storagePrefix, sharesPrefixGOB...), func(i int, obj basedb.Obj) error {
+		//	shareGOB := &storageShareGOB{}
+		//	if err := shareGOB.Decode(obj.Value); err != nil {
+		//		// TODO - dumping obj to see why we can't decode it
+		//		//return fmt.Errorf("decode gob share: %w", err)
+		//		return fmt.Errorf("decode gob share: %w obj: %s", err, spew.Sdump(obj))
+		//	}
+		//	share, err := storageShareGOBToSpecShare(shareGOB)
+		//	if err != nil {
+		//		return fmt.Errorf("convert storage share to spec share: %w", err)
+		//	}
+		//	shareSSZ := specShareToStorageShareSSZ(share)
+		//	key := storageKeySSZ(share.ValidatorPubKey[:])
+		//	value, err := shareSSZ.Encode()
+		//	if err != nil {
+		//		return fmt.Errorf("encode ssz share: %w", err)
+		//	}
+		//	sets = append(sets, basedb.Obj{
+		//		Key:   key,
+		//		Value: value,
+		//	})
+		//	return nil
+		//})
+		//if err != nil {
+		//	return fmt.Errorf("GetAll: %w", err)
 		//}
+		//
+		//if err := opt.Db.SetMany(storagePrefix, len(sets), func(i int) (basedb.Obj, error) {
+		//	return sets[i], nil
+		//}); err != nil {
+		//	return fmt.Errorf("SetMany: %w", err)
+		//}
+		//
+		//// This makes sure migration applies only once.
+		//// TODO - uncomment to complete migration
+		////if err := completed(opt.Db); err != nil {
+		////	return fmt.Errorf("complete transaction: %w", err)
+		////}
 
 		return nil
 	},
