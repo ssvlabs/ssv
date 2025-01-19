4a937583fe0f0b658f4e09eaf7345221c411534e
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2025-01-19 13:14:12 +0200

    increase

--- network/topics/container.go
+++ network/topics/container.go
@@ -6,7 +6,7 @@ import (
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 )
 
-const subscriptionBufferSize = 64
+const subscriptionBufferSize = 128
 
 type onTopicJoined func(ps *pubsub.PubSub, topic *pubsub.Topic)
 
