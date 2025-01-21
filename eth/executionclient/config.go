d4c4e1ed0a97b01dd3e322048b50a8570c44730c
zippoxer <moshe@blox.io>
2025-01-21 20:10:44 +0200

    reduce sync distance tolerance to 6

--- eth/executionclient/config.go
+++ eth/executionclient/config.go
@@ -10,5 +10,5 @@ import (
 type ExecutionOptions struct {
 	Addr                  string        `yaml:"ETH1Addr" env:"ETH_1_ADDR" env-required:"true" env-description:"Execution client WebSocket address. Supports multiple comma-separated addresses"`
 	ConnectionTimeout     time.Duration `yaml:"ETH1ConnectionTimeout" env:"ETH_1_CONNECTION_TIMEOUT" env-default:"10s" env-description:"Execution client connection timeout"`
-	SyncDistanceTolerance uint64        `yaml:"ETH1SyncDistanceTolerance" env:"ETH_1_SYNC_DISTANCE_TOLERANCE" env-default:"8" env-description:"The number of out-of-sync blocks we can tolerate"`
+	SyncDistanceTolerance uint64        `yaml:"ETH1SyncDistanceTolerance" env:"ETH_1_SYNC_DISTANCE_TOLERANCE" env-default:"5" env-description:"The number of out-of-sync blocks we can tolerate"`
 }
