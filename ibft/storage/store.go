127aa5e31a6398fac150dc0bf7d2644dcd4f9059
y0sher <lyosher@gmail.com>
2025-02-12 11:15:18 +0200

    simple mtx usage

--- ibft/storage/store.go
+++ ibft/storage/store.go
@@ -28,10 +28,9 @@ const (
 // participantStorage struct
 // instanceType is what separates different iBFT eth2 duty types (attestation, proposal and aggregation)
 type participantStorage struct {
-	prefix         []byte
-	oldPrefix      string // kept back for cleanup
-	db             basedb.Database
-	participantsMu sync.Mutex
+	prefix    []byte
+	oldPrefix string // kept back for cleanup
+	db        basedb.Database
 }
 
 // New create new participant store
@@ -163,9 +162,6 @@ func (i *participantStorage) CleanAllInstances() error {
 }
 
 func (i *participantStorage) SaveParticipants(pk spectypes.ValidatorPK, slot phase0.Slot, newParticipants []spectypes.OperatorID) (updated bool, err error) {
-	i.participantsMu.Lock()
-	defer i.participantsMu.Unlock()
-
 	txn := i.db.Begin()
 	defer txn.Discard()
 
