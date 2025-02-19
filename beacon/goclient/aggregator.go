66d07437df36c5cfe2934a5b2fc224d838fcb386
moshe-blox <moshe@blox.io>
2025-02-19 18:16:59 +0200

    fix: update attestation data index based on data version

--- beacon/goclient/aggregator.go
+++ beacon/goclient/aggregator.go
@@ -34,6 +34,9 @@ func (gc *GoClient) SubmitAggregateSelectionProof(slot phase0.Slot, committeeInd
 	if err != nil {
 		return nil, DataVersionNil, fmt.Errorf("failed to get attestation data: %w", err)
 	}
+	if gc.DataVersion(gc.network.EstimatedEpochAtSlot(attData.Slot)) < spec.DataVersionElectra {
+		attData.Index = committeeIndex
+	}
 
 	// Get aggregate attestation data.
 	root, err := attData.HashTreeRoot()
