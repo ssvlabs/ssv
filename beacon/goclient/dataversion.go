dadb76a0b3b46463ec33ae1a61833cf4e2192ec0
moshe-blox <moshe@blox.io>
2025-02-19 23:19:36 +0200

    refactor: remove unused zap import in GoClient data version

--- beacon/goclient/dataversion.go
+++ beacon/goclient/dataversion.go
@@ -6,7 +6,6 @@ import (
 	"github.com/attestantio/go-eth2-client/api"
 	"github.com/attestantio/go-eth2-client/spec"
 	"github.com/attestantio/go-eth2-client/spec/phase0"
-	"go.uber.org/zap"
 )
 
 func (gc *GoClient) DataVersion(epoch phase0.Epoch) spec.DataVersion {
