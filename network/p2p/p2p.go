8b92fc41f1f8e52ebf5bd21be3fb90ba65045aa4
moshe-blox <moshe@blox.io>
2025-03-02 15:32:26 +0200

    Increase TTL for discovered peers pool to enhance peer discovery reliability

--- network/p2p/p2p.go
+++ network/p2p/p2p.go
@@ -141,7 +141,7 @@ func New(
 		operatorPKHashToPKCache: hashmap.New[string, []byte](),
 		operatorSigner:          cfg.OperatorSigner,
 		operatorDataStore:       cfg.OperatorDataStore,
-		discoveredPeersPool:     ttl.New[peer.ID, discovery.DiscoveredPeer](1*time.Minute, 1*time.Minute),
+		discoveredPeersPool:     ttl.New[peer.ID, discovery.DiscoveredPeer](5*time.Minute, 1*time.Minute),
 		trimmedRecently:         ttl.New[peer.ID, struct{}](30*time.Minute, 5*time.Minute),
 	}
 	if err := n.parseTrustedPeers(); err != nil {
