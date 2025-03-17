a81a266baa4a8e79dd21084314f9588a8e9d1689
Matus Kysel <matus@ssvlabs.io>
2025-03-17 14:17:11 +0100

    feat: add support of hoodi testnet

---
+++ networkconfig/hoodi.go
@@ -0,0 +1,23 @@
+package networkconfig
+
+import (
+	"math/big"
+
+	spectypes "github.com/ssvlabs/ssv-spec/types"
+
+	"github.com/ssvlabs/ssv/protocol/v2/blockchain/beacon"
+)
+
+var Hoodi = NetworkConfig{
+	Name:                 "hoodi",
+	Beacon:               beacon.NewNetwork(spectypes.HoodiNetwork),
+	DomainType:           spectypes.DomainType{0x0, 0x0, 0x5, 0x3},
+	GenesisEpoch:         1,
+	RegistrySyncOffset:   new(big.Int).SetInt64(181612),
+	RegistryContractAddr: "0x38A4794cCEd47d3baf7370CcC43B560D3a1beEFA",
+	DiscoveryProtocolID:  [6]byte{'s', 's', 'v', 'd', 'v', '5'},
+	Bootnodes: []string{
+		// SSV Labs
+		"enr:-Ja4QIKlyNFuFtTOnVoavqwmpgSJXfhSmhpdSDOUhf5-FBr7bBxQRvG6VrpUvlkr8MtpNNuMAkM33AseduSaOhd9IeWGAZWjRbnvgmlkgnY0gmlwhCNVVTCJc2VjcDI1NmsxoQNTTyiJPoZh502xOZpHSHAfR-94NaXLvi5J4CNHMh2tjoNzc3YBg3RjcIITioN1ZHCCD6I",
+	},
+}
