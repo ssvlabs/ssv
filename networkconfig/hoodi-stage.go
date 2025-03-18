84eb3859d9eeba48c71b0ecd8544a7756636abd6
Matus Kysel <MatusKysel@users.noreply.github.com>
2025-03-18 13:28:04 +0100

    Hoodi (#2086)
    * feat: add support of hoodi testnet
    
    * update spec repo
    
    * update contracts
    
    * fix: reduce DefaultHistoricalLogsBatchSize to 200 for Hoodi
    
    ---------
    
    Co-authored-by: zippoxer <moshe@ssvlabs.io>

---
+++ networkconfig/hoodi-stage.go
@@ -0,0 +1,23 @@
+package networkconfig
+
+import (
+	"math/big"
+
+	spectypes "github.com/ssvlabs/ssv-spec/types"
+
+	"github.com/ssvlabs/ssv/protocol/v2/blockchain/beacon"
+)
+
+var HoodiStage = NetworkConfig{
+	Name:                 "hoodi-stage",
+	Beacon:               beacon.NewNetwork(spectypes.HoodiNetwork),
+	DomainType:           [4]byte{0x00, 0x00, 0x31, 0x14},
+	GenesisEpoch:         1,
+	RegistrySyncOffset:   new(big.Int).SetInt64(1004),
+	RegistryContractAddr: "0x868C2789045d7ffC144635Da7D8cC0a974C58f89",
+	DiscoveryProtocolID:  [6]byte{'s', 's', 'v', 'd', 'v', '5'},
+	Bootnodes: []string{
+		// SSV Labs
+		"enr:-Ja4QMME0XoEMoywhjbxvJ_IqFEF184IOQMdweMpZHymLRP2b3gm-XzFgSUuCw4HeZcPV_z6coRINusvqwVEJGxlxaiGAZWjNu0pgmlkgnY0gmlwhAorfUKJc2VjcDI1NmsxoQP_bBE-ZYvaXKBR3dRYMN5K_lZP-q-YsBzDZEtxH_4T_YNzc3YBg3RjcIITioN1ZHCCD6I",
+	},
+}
