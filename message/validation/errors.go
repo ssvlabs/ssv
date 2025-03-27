f8f931fffa009bc4a9937f6ac5bfea2ede4caa30
kchojn <karolchojnowski95@gmail.com>
2025-03-27 12:22:04 +0100

    fix(errors.go): remove unnecessary 'reject' flag from ErrUnknownOperator error to align with other error definitions and improve consistency

--- message/validation/errors.go
+++ message/validation/errors.go
@@ -130,7 +130,7 @@ var (
 	ErrInvalidPartialSignatureTypeCount        = Error{text: "sent more partial signature messages of a certain type than allowed", reject: true}
 	ErrTooManyPartialSignatureMessages         = Error{text: "too many partial signature messages", reject: true}
 	ErrEncodeOperators                         = Error{text: "encode operators", reject: true}
-	ErrUnknownOperator                         = Error{text: "operator is unknown", reject: true}
+	ErrUnknownOperator                         = Error{text: "operator is unknown"}
 	ErrOperatorValidation                      = Error{text: "failed to validate operator data"}
 )
 
