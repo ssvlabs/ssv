a943507b7bc207d4118d1a7644925591ba4d1691
kchojn <karolchojnowski95@gmail.com>
2025-03-28 10:41:31 +0100

    fix lint issues

--- utils/cliflag/cliflag.go
+++ utils/cliflag/cliflag.go
@@ -6,29 +6,30 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const requiredSuffix = " (required)"
+
 // AddPersistentStringFlag adds a string flag to the command
 func AddPersistentStringFlag(c *cobra.Command, flag string, value string, description string, isRequired bool) {
-	req := ""
-	if isRequired {
-		req = " (required)"
-	}
-
-	c.PersistentFlags().String(flag, value, fmt.Sprintf("%s%s", description, req))
-
-	if isRequired {
-		_ = c.MarkPersistentFlagRequired(flag)
-	}
+	c.PersistentFlags().String(flag, value, formatDescription(description, isRequired))
+	markRequired(c, flag, isRequired)
 }
 
 // AddPersistentIntFlag adds a int flag to the command
 func AddPersistentIntFlag(c *cobra.Command, flag string, value uint64, description string, isRequired bool) {
-	req := ""
+	c.PersistentFlags().Uint64(flag, value, formatDescription(description, isRequired))
+	markRequired(c, flag, isRequired)
+}
+
+// formatDescription adds required suffix to description if needed
+func formatDescription(description string, isRequired bool) string {
 	if isRequired {
-		req = " (required)"
+		return fmt.Sprintf("%s%s", description, requiredSuffix)
 	}
+	return description
+}
 
-	c.PersistentFlags().Uint64(flag, value, fmt.Sprintf("%s%s", description, req))
-
+// markRequired marks flag as required if needed, ignoring errors
+func markRequired(c *cobra.Command, flag string, isRequired bool) {
 	if isRequired {
 		_ = c.MarkPersistentFlagRequired(flag)
 	}
