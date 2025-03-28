e2c6033e72328673845fbfaa02fc2d2a8ee0bacb
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2025-03-27 23:02:06 -0300

    set accept header

--- ssvsigner/client.go
+++ ssvsigner/client.go
@@ -144,6 +144,7 @@ func (c *Client) Sign(ctx context.Context, sharePubKey phase0.BLSPubKey, payload
 		Path(pathValidatorsSign + sharePubKey.String()).
 		BodyJSON(payload).
 		Post().
+		Accept("application/json").
 		ToJSON(&resp).
 		Fetch(ctx)
 	if err != nil {
