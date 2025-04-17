17eb427866dae85ee31e00f330d80e89ab1ce2d4
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2025-04-16 22:23:56 -0300

    implement JSON marshaling and YAML marshaling/unmarshaling

--- networkconfig/ssv.go
+++ networkconfig/ssv.go
@@ -1,9 +1,11 @@
 package networkconfig
 
 import (
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"math/big"
+	"strings"
 
 	ethcommon "github.com/ethereum/go-ethereum/common"
 	spectypes "github.com/ssvlabs/ssv-spec/types"
@@ -35,11 +37,12 @@ type SSV interface {
 }
 
 type SSVConfig struct {
-	DomainType           spectypes.DomainType
-	RegistrySyncOffset   *big.Int
-	RegistryContractAddr ethcommon.Address
-	Bootnodes            []string
-	DiscoveryProtocolID  [6]byte
+	DomainType              spectypes.DomainType
+	RegistrySyncOffset      *big.Int
+	RegistryContractAddr    ethcommon.Address
+	Bootnodes               []string
+	DiscoveryProtocolID     [6]byte
+	TotalEthereumValidators int `yaml:"TotalEthereumValidators,omitempty"` // value needs to be maintained — consider getting it from external API with default or per-network value(s) as fallback
 }
 
 func (s SSVConfig) String() string {
@@ -49,6 +52,92 @@ func (s SSVConfig) String() string {
 	}
 
 	return string(marshaled)
+}
+
+type marshaledConfig struct {
+	DomainType              string   `json:"DomainType,omitempty" yaml:"DomainType,omitempty"`
+	RegistrySyncOffset      *big.Int `json:"RegistrySyncOffset,omitempty" yaml:"RegistrySyncOffset,omitempty"`
+	RegistryContractAddr    string   `json:"RegistryContractAddr,omitempty" yaml:"RegistryContractAddr,omitempty"`
+	Bootnodes               []string `json:"Bootnodes,omitempty" yaml:"Bootnodes,omitempty"`
+	DiscoveryProtocolID     string   `json:"DiscoveryProtocolID,omitempty" yaml:"DiscoveryProtocolID,omitempty"`
+	TotalEthereumValidators int      `json:"TotalEthereumValidators,omitempty" yaml:"TotalEthereumValidators,omitempty"`
+}
+
+func (s *SSVConfig) marshal() (marshaledConfig, error) {
+	aux := marshaledConfig{
+		DomainType:              "0x" + hex.EncodeToString(s.DomainType[:]),
+		RegistrySyncOffset:      s.RegistrySyncOffset,
+		RegistryContractAddr:    s.RegistryContractAddr.String(),
+		Bootnodes:               s.Bootnodes,
+		DiscoveryProtocolID:     "0x" + hex.EncodeToString(s.DiscoveryProtocolID[:]),
+		TotalEthereumValidators: s.TotalEthereumValidators,
+	}
+
+	return aux, nil
+}
+
+func (s SSVConfig) MarshalJSON() ([]byte, error) {
+	aux, err := s.marshal()
+	if err != nil {
+		return nil, err
+	}
+
+	return json.Marshal(aux)
+}
+
+func (s SSVConfig) MarshalYAML() (interface{}, error) {
+	aux, err := s.marshal()
+	if err != nil {
+		return nil, err
+	}
+
+	return aux, nil
+}
+
+func (s *SSVConfig) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	aux := &struct {
+		DomainType              string   `yaml:"DomainType,omitempty"`
+		RegistrySyncOffset      *big.Int `yaml:"RegistrySyncOffset,omitempty"`
+		RegistryContractAddr    string   `yaml:"RegistryContractAddr,omitempty"`
+		Bootnodes               []string `yaml:"Bootnodes,omitempty"`
+		DiscoveryProtocolID     string   `yaml:"DiscoveryProtocolID,omitempty"`
+		TotalEthereumValidators int      `yaml:"TotalEthereumValidators,omitempty"`
+	}{}
+
+	if err := unmarshal(aux); err != nil {
+		return err
+	}
+
+	domain, err := hex.DecodeString(strings.TrimPrefix(aux.DomainType, "0x"))
+	if err != nil {
+		return fmt.Errorf("decode domain: %w", err)
+	}
+
+	var domainArr spectypes.DomainType
+	if len(domain) != 0 {
+		domainArr = spectypes.DomainType(domain)
+	}
+
+	discoveryProtocolID, err := hex.DecodeString(strings.TrimPrefix(aux.DiscoveryProtocolID, "0x"))
+	if err != nil {
+		return fmt.Errorf("decode discovery protocol ID: %w", err)
+	}
+
+	var discoveryProtocolIDArr [6]byte
+	if len(discoveryProtocolID) != 0 {
+		discoveryProtocolIDArr = [6]byte(discoveryProtocolID)
+	}
+
+	*s = SSVConfig{
+		DomainType:              domainArr,
+		RegistrySyncOffset:      aux.RegistrySyncOffset,
+		RegistryContractAddr:    ethcommon.HexToAddress(aux.RegistryContractAddr),
+		Bootnodes:               aux.Bootnodes,
+		DiscoveryProtocolID:     discoveryProtocolIDArr,
+		TotalEthereumValidators: aux.TotalEthereumValidators,
+	}
+
+	return nil
 }
 
 func (s SSVConfig) GetDomainType() spectypes.DomainType {
