43a99178df8cb117a7fc5091aaea03fb317b00c7
oleg-ssvlabs <oleg.iar@ssvlabs.io>
2025-04-21 10:27:43 +0200

    Duties map: do not set new map value each time new duties appended

--- operator/duties/committee.go
+++ operator/duties/committee.go
@@ -132,10 +132,11 @@ func (h *CommitteeHandler) addToCommitteeMap(
 				ValidatorDuties: []*spectypes.ValidatorDuty{},
 			},
 		}
+		
+		committeeDutyMap[committee.id] = cd
 	}
 
 	cd.duty.ValidatorDuties = append(cd.duty.ValidatorDuties, specDuty)
-	committeeDutyMap[committee.id] = cd
 }
 
 func (h *CommitteeHandler) toSpecAttDuty(duty *eth2apiv1.AttesterDuty, role spectypes.BeaconRole) *spectypes.ValidatorDuty {
