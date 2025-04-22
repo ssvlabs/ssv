f600586de9fa6b3e26595df34683ed56b7b2b16c
olegshmuelov <45327364+olegshmuelov@users.noreply.github.com>
2025-04-22 13:51:49 +0300

    lint fix

--- operator/validator/metadata/syncer.go
+++ operator/validator/metadata/syncer.go
@@ -14,7 +14,6 @@ import (
 
 	"github.com/ssvlabs/ssv/logging/fields"
 	networkcommons "github.com/ssvlabs/ssv/network/commons"
-	"github.com/ssvlabs/ssv/networkconfig"
 	"github.com/ssvlabs/ssv/protocol/v2/blockchain/beacon"
 	ssvtypes "github.com/ssvlabs/ssv/protocol/v2/types"
 	registrystorage "github.com/ssvlabs/ssv/registry/storage"
@@ -34,7 +33,6 @@ type Syncer struct {
 	logger            *zap.Logger
 	shareStorage      shareStorage
 	validatorStore    selfValidatorStore
-	networkConfig     networkconfig.NetworkConfig
 	beaconNode        beacon.BeaconNode
 	fixedSubnets      networkcommons.Subnets
 	syncInterval      time.Duration
