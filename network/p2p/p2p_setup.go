97255b7f7b5ce0f12c8ca1541ee9988442129581
kchojn <karolchojnowski95@gmail.com>
2025-05-23 20:52:30 +0700

    fix(p2p): resolve scoreParamsFactory is not set error

--- network/p2p/p2p_setup.go
+++ network/p2p/p2p_setup.go
@@ -161,13 +161,16 @@ func (n *p2pNetwork) SetupServices() error {
 	if err := n.setupStreamCtrl(); err != nil {
 		return errors.Wrap(err, "could not setup stream controller")
 	}
+
+	if err := n.setupPeerServices(); err != nil {
+		return errors.Wrap(err, "could not setup peer services")
+	}
+
 	_, err := n.setupPubsub()
 	if err != nil {
 		return errors.Wrap(err, "could not setup topic controller")
 	}
-	if err := n.setupPeerServices(); err != nil {
-		return errors.Wrap(err, "could not setup peer services")
-	}
+
 	if err := n.setupDiscovery(); err != nil {
 		return errors.Wrap(err, "could not setup discovery service")
 	}
