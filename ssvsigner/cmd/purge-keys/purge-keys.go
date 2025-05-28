268de7847d061ccfc178d5ccc0b7ca01c51370cc
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2025-05-28 19:26:19 -0300

    ssvsigner: audit SSV-8

--- ssvsigner/cmd/purge-keys/purge-keys.go
+++ ssvsigner/cmd/purge-keys/purge-keys.go
@@ -16,6 +16,7 @@ import (
 	"go.uber.org/zap"
 
 	"github.com/ssvlabs/ssv/logging/fields"
+
 	ssvsignertls "github.com/ssvlabs/ssv/ssvsigner/tls"
 	"github.com/ssvlabs/ssv/ssvsigner/web3signer"
 )
@@ -56,12 +57,16 @@ func run(logger *zap.Logger, cli CLI) error {
 		zap.Bool("client_tls_enabled", cli.Web3SignerKeystoreFile != "" || cli.Web3SignerServerCertFile != ""),
 	)
 
-	if err := bls.Init(bls.BLS12_381); err != nil {
-		return fmt.Errorf("init BLS: %w", err)
+	if cli.BatchSize <= 0 {
+		return fmt.Errorf("invalid batch size %d, must be > 0", cli.BatchSize)
 	}
 
 	if _, err := url.ParseRequestURI(cli.Web3SignerEndpoint); err != nil {
 		return fmt.Errorf("invalid WEB3SIGNER_ENDPOINT format: %w", err)
+	}
+
+	if err := bls.Init(bls.BLS12_381); err != nil {
+		return fmt.Errorf("init BLS: %w", err)
 	}
 
 	ctx := context.Background()
