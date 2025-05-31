c0304c3259debc1d91e7ce352491e72a9b47e502
kchojn <karolchojnowski95@gmail.com>
2025-05-31 08:09:40 +0200

    fix: revert DefaultHistoricalLogsBatchSize to 200 to prevent websocket read limit errors (#2248)

--- eth/executionclient/defaults.go
+++ eth/executionclient/defaults.go
@@ -11,7 +11,7 @@ const (
 	DefaultHealthInvalidationInterval  = 24 * time.Second // TODO: decide on this value, for now choosing the node prober interval but it should probably be a bit less than block interval
 	DefaultFollowDistance              = 8
 	// TODO ALAN: revert
-	DefaultHistoricalLogsBatchSize = 1000
+	DefaultHistoricalLogsBatchSize = 200
 	defaultLogBuf                  = 8 * 1024
 	maxReconnectionAttempts        = 5000
 	reconnectionBackoffFactor      = 2
