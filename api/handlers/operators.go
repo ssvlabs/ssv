aa68e0b48bd015ebb0d11747ee0283c0e88daa0b
Anatolie Lupacescu <anatolie@ssvlabs.io>
2025-06-03 10:20:41 +0300

    keep all ops per pid

--- api/handlers/operators.go
+++ api/handlers/operators.go
@@ -1,17 +1,20 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/libp2p/go-libp2p/core/peer"
+
+	spectypes "github.com/ssvlabs/ssv-spec/types"
 	"github.com/ssvlabs/ssv/api"
 	"github.com/ssvlabs/ssv/message/validation"
 	"github.com/ssvlabs/ssv/network/peers"
-	"net/http"
 )
 
 type OperatorPeer struct {
-	PeerID     string `json:"peer_id"`
-	OperatorID uint64 `json:"operator_id"`
-	Version    string `json:"version"`
+	PeerID     string   `json:"peer_id"`
+	OperatorID []uint64 `json:"operator_id"`
+	Version    string   `json:"version"`
 }
 
 type OperatorToPeerJSON struct {
@@ -25,20 +28,15 @@ type Operators struct {
 func (h Operators) List(w http.ResponseWriter, r *http.Request) error {
 	opt := &OperatorToPeerJSON{make([]*OperatorPeer, 0)}
 
-	mapcopy := make(map[peer.ID]uint64)
-	validation.PeerIDtoSignerMtx.Lock()
-	for k, v := range validation.PeerIDtoSigner {
-		mapcopy[k] = v
-	}
-	validation.PeerIDtoSignerMtx.Unlock()
-
-	for k, v := range mapcopy {
+	validation.PeerIDtoSigner.Range(func(k peer.ID, v []spectypes.OperatorID) bool {
 		opdata := &OperatorPeer{PeerID: k.String(), OperatorID: v}
 		nodeInfo := h.PeersIndex.NodeInfo(k)
 		if nodeInfo != nil && nodeInfo.Metadata != nil {
 			opdata.Version = nodeInfo.Metadata.NodeVersion
 		}
 		opt.Data = append(opt.Data, opdata)
-	}
+		return true
+	})
+
 	return api.Render(w, r, opt)
 }
--- message/validation/validation.go
+++ message/validation/validation.go
@@ -15,10 +15,10 @@ import (
 	"github.com/jellydator/ttlcache/v3"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/libp2p/go-libp2p/core/peer"
-	spectypes "github.com/ssvlabs/ssv-spec/types"
 	"go.uber.org/zap"
 	"tailscale.com/util/singleflight"
 
+	spectypes "github.com/ssvlabs/ssv-spec/types"
 	"github.com/ssvlabs/ssv/message/signatureverifier"
 	"github.com/ssvlabs/ssv/network/commons"
 	"github.com/ssvlabs/ssv/networkconfig"
@@ -27,6 +27,7 @@ import (
 	ssvtypes "github.com/ssvlabs/ssv/protocol/v2/types"
 	registrystorage "github.com/ssvlabs/ssv/registry/storage"
 	"github.com/ssvlabs/ssv/storage/basedb"
+	"github.com/ssvlabs/ssv/utils/hashmap"
 )
 
 // MessageValidator defines methods for validating pubsub messages.
@@ -51,8 +52,10 @@ type peerIDWithMessageID struct {
 	messageID spectypes.MessageID
 }
 
-var PeerIDtoSignerMtx sync.Mutex
-var PeerIDtoSigner map[peer.ID]spectypes.OperatorID = make(map[peer.ID]spectypes.OperatorID)
+var (
+	PeerIDtoSigner hashmap.Map[peer.ID, []spectypes.OperatorID]
+	peerIDmu       sync.Mutex
+)
 
 type messageValidator struct {
 	logger          *zap.Logger
@@ -159,11 +162,24 @@ func (mv *messageValidator) handlePubsubMessage(pMsg *pubsub.Message, receivedAt
 	}
 
 	pid := pMsg.GetFrom()
-	operator := signedSSVMessage.OperatorIDs[0]
 
-	PeerIDtoSignerMtx.Lock()
-	PeerIDtoSigner[pid] = operator
-	PeerIDtoSignerMtx.Unlock()
+	func() {
+		ops := slices.Clone(signedSSVMessage.OperatorIDs)
+
+		peerIDmu.Lock()
+		defer peerIDmu.Unlock()
+
+		existing, found := PeerIDtoSigner.Get(pid)
+		if found {
+			ops = append(ops, existing...)
+		}
+
+		slices.Sort(ops)
+
+		ops = slices.Compact(ops)
+
+		PeerIDtoSigner.Set(pid, ops)
+	}()
 
 	return mv.handleSignedSSVMessage(signedSSVMessage, pMsg.GetTopic(), pMsg.ReceivedFrom, receivedAt)
 }
