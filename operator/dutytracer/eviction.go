e3742b16532d40ff4058217983c64a0a6c66d77d
Anatolie Lupacescu <anatolie@ssvlabs.io>
2025-06-12 10:38:10 +0300

    check back missed slots

--- operator/dutytracer/eviction.go
+++ operator/dutytracer/eviction.go
@@ -16,82 +16,92 @@ const (
 
 	ttlMapping       = 4
 	ttlCommitteeRoot = 4
+
+	// look back 5 slots
+	depth = 5
 )
 
-func (c *Collector) dumpLinkToDBPeriodically(slot phase0.Slot) (totalSaved int) {
+func (c *Collector) dumpLinkToDBPeriodically(thresholdSlot phase0.Slot) (totalSaved int) {
 	c.validatorIndexToCommitteeLinks.Range(func(index phase0.ValidatorIndex, slotToCommittee *hashmap.Map[phase0.Slot, spectypes.CommitteeID]) bool {
-		committeeID, found := slotToCommittee.Get(slot)
-		if !found {
-			return true
-		}
-
-		if err := c.store.SaveCommitteeDutyLink(slot, index, committeeID); err != nil {
-			c.logger.Error("save validator to committee relations to disk", zap.Error(err))
-			return true
-		}
-
-		totalSaved++
-
-		slotToCommittee.Delete(slot)
-
-		return true
-	})
-
-	return
-}
-
-func (c *Collector) dumpCommitteeToDBPeriodically(slot phase0.Slot) (totalSaved int) {
-	c.committeeTraces.Range(func(key spectypes.CommitteeID, slotToTraceMap *hashmap.Map[phase0.Slot, *committeeDutyTrace]) bool {
-		trace, found := slotToTraceMap.Get(slot)
-		if !found {
-			return true
-		}
-
-		if err := c.store.SaveCommitteeDuty(trace.trace()); err != nil {
-			c.logger.Error("save committee duty to disk", zap.Error(err))
-			return true
-		}
-
-		totalSaved++
-
-		slotToTraceMap.Delete(slot)
-
-		return true
-	})
-
-	return
-}
-
-func (c *Collector) dumpValidatorToDBPeriodically(slot phase0.Slot) (totalSaved int) {
-	c.validatorTraces.Range(func(pk spectypes.ValidatorPK, slotToTraceMap *hashmap.Map[phase0.Slot, *validatorDutyTrace]) bool {
-		trace, found := slotToTraceMap.Get(slot)
-		if !found {
-			return true
-		}
-
-		for _, role := range trace.roleTraces() {
-			// TODO(me): confirm it makes sense
-			// in case some duties do not have the validator index set
-			if role.Validator == 0 {
-				c.logger.Info("got trace with missing validator index", fields.Validator(pk[:]), fields.Slot(slot))
-				index, found := c.validators.ValidatorIndex(pk)
-				if !found {
-					continue
-				}
-
-				role.Validator = index
+		// we look back up to `depth` slots to ensure we evict previously skipped or missed duties
+		for d := range depth {
+			slot := thresholdSlot - phase0.Slot(d)
+			committeeID, found := slotToCommittee.Get(slot)
+			if !found {
+				continue
 			}
 
-			if err := c.store.SaveValidatorDuty(role); err != nil {
-				c.logger.Error("save validator duties to disk", zap.Error(err))
+			if err := c.store.SaveCommitteeDutyLink(slot, index, committeeID); err != nil {
+				c.logger.Error("save validator to committee relations to disk", zap.Error(err))
 				return true
 			}
 
 			totalSaved++
+
+			slotToCommittee.Delete(slot)
 		}
+		return true
+	})
 
-		slotToTraceMap.Delete(slot)
+	return
+}
 
+func (c *Collector) dumpCommitteeToDBPeriodically(thresholdSlot phase0.Slot) (totalSaved int) {
+	c.committeeTraces.Range(func(key spectypes.CommitteeID, slotToTraceMap *hashmap.Map[phase0.Slot, *committeeDutyTrace]) bool {
+		// we look back up to `depth` slots to ensure we evict previously skipped or missed duties
+		for d := range depth {
+			slot := thresholdSlot - phase0.Slot(d)
+			trace, found := slotToTraceMap.Get(slot)
+			if !found {
+				continue
+			}
+
+			if err := c.store.SaveCommitteeDuty(trace.trace()); err != nil {
+				c.logger.Error("save committee duty to disk", zap.Error(err))
+				return true
+			}
+
+			totalSaved++
+
+			slotToTraceMap.Delete(slot)
+		}
+		return true
+	})
+
+	return
+}
+
+func (c *Collector) dumpValidatorToDBPeriodically(thresholdSlot phase0.Slot) (totalSaved int) {
+	c.validatorTraces.Range(func(pk spectypes.ValidatorPK, slotToTraceMap *hashmap.Map[phase0.Slot, *validatorDutyTrace]) bool {
+		// we look back up to `depth` slots to ensure we evict previously skipped or missed duties
+		for d := range depth {
+			slot := thresholdSlot - phase0.Slot(d)
+			trace, found := slotToTraceMap.Get(slot)
+			if !found {
+				continue
+			}
+
+			for _, role := range trace.roleTraces() {
+				if role.Validator == 0 {
+					c.logger.Info("got trace with missing validator index", fields.Validator(pk[:]), fields.Slot(slot))
+					index, found := c.validators.ValidatorIndex(pk)
+					if !found {
+						continue
+					}
+
+					role.Validator = index
+				}
+
+				if err := c.store.SaveValidatorDuty(role); err != nil {
+					c.logger.Error("save validator duties to disk", zap.Error(err))
+					return true
+				}
+
+				totalSaved++
+			}
+
+			slotToTraceMap.Delete(slot)
+		}
 		return true
 	})
 
