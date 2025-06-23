0e97fdbf0a10e3a86aee042e2b250b6425bed6ae
Karol Chojnowski <karolchojnowski95@gmail.com>
2025-06-23 18:04:03 +0700

    refactor(migration): improve error handling and completion logic in migration function for better clarity and reliability

--- migrations/migration_7_derive_signer_key_with_hkdf.go
+++ migrations/migration_7_derive_signer_key_with_hkdf.go
@@ -11,13 +11,25 @@ import (
 
 var migration_7_derive_signer_key_with_hkdf = Migration{
 	Name: "migration_7_derive_signer_key_with_hkdf",
-	Run: func(ctx context.Context, logger *zap.Logger, opt Options, key []byte, completed CompletedFunc) error {
-		return opt.Db.Update(func(txn basedb.Txn) error {
+	Run: func(ctx context.Context, logger *zap.Logger, opt Options, key []byte, completed CompletedFunc) (err error) {
+		defer func() {
+			if err != nil {
+				return // cannot complete migration successfully
+			}
+			// complete migration, this makes sure migration applies only once
+			if err = completed(opt.Db); err != nil {
+				err = fmt.Errorf("complete transaction: %w", err)
+				return
+			}
+		}()
+
+		err = opt.Db.Update(func(txn basedb.Txn) error {
 			if opt.OperatorPrivKey == nil {
 				// No migration needed if using remote signer.
 				return nil
 			}
 
+			// Set storage key to old format so we can decrypt stored data with it.
 			signerStorage := opt.signerStorage(logger)
 			signerStorage.SetEncryptionKey(opt.OperatorPrivKey.EKMHash())
 
@@ -28,26 +40,26 @@ var migration_7_derive_signer_key_with_hkdf = Migration{
 
 			if len(accounts) == 0 {
 				logger.Info("no accounts to migrate")
-				return completed(txn)
+				return nil
 			}
 
+			// Set storage key to new format so we can re-encrypt stored data with it.
 			encryptionKey, err := opt.OperatorPrivKey.EKMEncryptionKey()
 			if err != nil {
 				return fmt.Errorf("failed to get encryption key: %w", err)
 			}
-
-			// re-encryption with the new algorithm
 			signerStorage.SetEncryptionKey(encryptionKey)
 
 			for _, account := range accounts {
 				err := signerStorage.SaveAccountTxn(txn, account)
 				if err != nil {
-					return fmt.Errorf("failed to re-encrypt account %s: %w", account.ID(), err)
+					return fmt.Errorf("failed to save account %s: %w", account.ID(), err)
 				}
 			}
 
 			logger.Info("re-encrypted accounts with HKDF-derived key", zap.Int("count", len(accounts)))
-			return completed(txn)
+			return nil
 		})
+		return err
 	},
 }
