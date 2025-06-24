8f884d915bfa071bf8c62bd9c400ba575db69c62
oleg-ssvlabs <oleg.iar@ssvlabs.io>
2025-06-24 11:58:04 +0200

    Logger nil check

--- observability/logger.go
+++ observability/logger.go
@@ -5,6 +5,9 @@ import "go.uber.org/zap"
 var logger *zap.Logger
 
 func initLogger(l *zap.Logger) *zap.Logger {
+	if l == nil {
+		l = zap.NewNop()
+	}
 	logger = l.Named("Observability")
 	return logger
 }
