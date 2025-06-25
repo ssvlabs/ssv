716f5e9a97e0861e0a835ade00acebbf95c18160
iurii <sidorenko@ssvlabs.io>
2025-06-25 12:10:29 +0300

    fasttrack: gas-limit: default to 36

--- networkconfig/test-network.go
+++ networkconfig/test-network.go
@@ -20,5 +20,6 @@ var TestNetwork = NetworkConfig{
 		Bootnodes: []string{
 			"enr:-Li4QFIQzamdvTxGJhvcXG_DFmCeyggSffDnllY5DiU47pd_K_1MRnSaJimWtfKJ-MD46jUX9TwgW5Jqe0t4pH41RYWGAYuFnlyth2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__________gmlkgnY0gmlwhCLdu_SJc2VjcDI1NmsxoQN4v-N9zFYwEqzGPBBX37q24QPFvAVUtokIo1fblIsmTIN0Y3CCE4uDdWRwgg-j",
 		},
+		GasLimit36Epoch: 0,
 	},
 }
