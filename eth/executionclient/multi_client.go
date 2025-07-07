e0b799657a32e25b954ab2267feba0924e80a44f
Karol Chojnowski <karolchojnowski95@gmail.com>
2025-07-07 12:30:40 +0700

    fix(multi_client.go): update log message from "stream logs interrupted" to "stream logs stopped" for clarity

--- eth/executionclient/multi_client.go
+++ eth/executionclient/multi_client.go
@@ -271,7 +271,7 @@ func (mc *MultiClient) StreamLogs(ctx context.Context, fromBlock uint64) <-chan 
 				_, err := mc.call(contextWithMethod(ctx, "StreamLogs"), f, 0)
 				if err != nil {
 					if isInterruptedError(err) {
-						mc.logger.Debug("stream logs interrupted", zap.Error(err))
+						mc.logger.Debug("stream logs stopped", zap.Error(err))
 					} else {
 						// NOTE: There are unit tests that trigger Fatal and override its behavior.
 						// Therefore, the code must call `return` afterward.
