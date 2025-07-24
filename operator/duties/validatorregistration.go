93af35ed8df842dabf92de72cbf783523a624631
iurii <sidorenko@ssvlabs.io>
2025-07-24 11:36:24 +0300

    increase frequency

--- operator/duties/validatorregistration.go
+++ operator/duties/validatorregistration.go
@@ -55,7 +55,7 @@ func (h *ValidatorRegistrationHandler) HandleDuties(ctx context.Context) {
 
 			var vrs []ValidatorRegistration
 			for _, share := range shares {
-				if !share.IsParticipatingAndAttesting(epoch + phase0.Epoch(frequencyEpochs)) {
+				if !share.IsParticipatingAndAttesting(epoch) {
 					// Only attesting validators are eligible for registration duties.
 					continue
 				}
