228518ade0d4379919986ab70f902d55d3f4ec0c
iurii <sidorenko@ssvlabs.io>
2025-08-11 13:35:22 +0300

    log additional details (epoch/slot/duty-id) in some places

--- operator/validator/controller.go
+++ operator/validator/controller.go
@@ -637,14 +637,16 @@ func (c *controller) GetValidator(pubKey spectypes.ValidatorPK) (*validator.Vali
 }
 
 func (c *controller) ExecuteDuty(ctx context.Context, duty *spectypes.ValidatorDuty) {
+	dutyEpoch := c.networkConfig.EstimatedEpochAtSlot(duty.Slot)
 	dutyID := fields.FormatDutyID(c.networkConfig.EstimatedEpochAtSlot(duty.Slot), duty.Slot, duty.Type, duty.ValidatorIndex)
 	ctx, span := tracer.Start(traces.Context(ctx, dutyID),
 		observability.InstrumentName(observabilityNamespace, "execute_duty"),
 		trace.WithAttributes(
-			observability.CommitteeIndexAttribute(duty.CommitteeIndex),
-			observability.BeaconSlotAttribute(duty.Slot),
-			observability.BeaconRoleAttribute(duty.Type),
 			observability.RunnerRoleAttribute(duty.RunnerRole()),
+			observability.BeaconRoleAttribute(duty.Type),
+			observability.CommitteeIndexAttribute(duty.CommitteeIndex),
+			observability.BeaconEpochAttribute(dutyEpoch),
+			observability.BeaconSlotAttribute(duty.Slot),
 			observability.ValidatorPublicKeyAttribute(duty.PubKey),
 			observability.ValidatorIndexAttribute(duty.ValidatorIndex),
 			observability.DutyIDAttribute(dutyID),
@@ -652,10 +654,18 @@ func (c *controller) ExecuteDuty(ctx context.Context, duty *spectypes.ValidatorD
 		trace.WithLinks(trace.LinkFromContext(ctx)))
 	defer span.End()
 
+	logger := c.logger.
+		With(fields.Role(duty.RunnerRole())).
+		With(fields.Epoch(dutyEpoch)).
+		With(fields.Slot(duty.Slot)).
+		With(fields.ValidatorIndex(duty.ValidatorIndex)).
+		With(fields.Validator(duty.PubKey[:])).
+		With(fields.DutyID(dutyID))
+
 	v, ok := c.GetValidator(spectypes.ValidatorPK(duty.PubKey))
 	if !ok {
 		eventMsg := fmt.Sprintf("could not find validator: %s", duty.PubKey.String())
-		c.logger.Warn(eventMsg)
+		logger.Warn(eventMsg)
 		span.AddEvent(eventMsg)
 		span.SetStatus(codes.Ok, "")
 		return
@@ -663,7 +673,7 @@ func (c *controller) ExecuteDuty(ctx context.Context, duty *spectypes.ValidatorD
 
 	span.AddEvent("executing validator duty")
 	if err := v.ExecuteDuty(ctx, duty); err != nil {
-		c.logger.Error("could not execute validator duty", zap.Error(err))
+		logger.Error("could not execute validator duty", zap.Error(err))
 		span.SetStatus(codes.Error, err.Error())
 		return
 	}
@@ -684,21 +694,30 @@ func (c *controller) ExecuteCommitteeDuty(ctx context.Context, committeeID spect
 		committee = append(committee, operator.OperatorID)
 	}
 
-	dutyID := fields.FormatCommitteeDutyID(committee, c.networkConfig.EstimatedEpochAtSlot(duty.Slot), duty.Slot)
+	dutyEpoch := c.networkConfig.EstimatedEpochAtSlot(duty.Slot)
+	dutyID := fields.FormatCommitteeDutyID(committee, dutyEpoch, duty.Slot)
 	ctx, span := tracer.Start(traces.Context(ctx, dutyID),
 		observability.InstrumentName(observabilityNamespace, "execute_committee_duty"),
 		trace.WithAttributes(
+			observability.RunnerRoleAttribute(duty.RunnerRole()),
+			observability.BeaconEpochAttribute(dutyEpoch),
 			observability.BeaconSlotAttribute(duty.Slot),
 			observability.CommitteeIDAttribute(committeeID),
-			observability.RunnerRoleAttribute(duty.RunnerRole()),
 			observability.DutyIDAttribute(dutyID),
 		),
 		trace.WithLinks(trace.LinkFromContext(ctx)))
 	defer span.End()
 
+	logger := c.logger.
+		With(fields.Role(duty.RunnerRole())).
+		With(fields.Epoch(dutyEpoch)).
+		With(fields.Slot(duty.Slot)).
+		With(fields.CommitteeID(committeeID)).
+		With(fields.DutyID(dutyID))
+
 	span.AddEvent("executing committee duty")
 	if err := cm.ExecuteDuty(ctx, duty); err != nil {
-		c.logger.Error("could not execute committee duty", zap.Error(err))
+		logger.Error("could not execute committee duty", zap.Error(err))
 		span.SetStatus(codes.Error, err.Error())
 		return
 	}
