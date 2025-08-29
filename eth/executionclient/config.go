97d0ceec27397c0a7f1b7b3e765ca2fb4e6d8d5d
iurii <sidorenko@ssvlabs.io>
2025-08-29 14:33:09 +0300

    align default timeout values

--- eth/executionclient/config.go
+++ eth/executionclient/config.go
@@ -9,6 +9,6 @@ import (
 // Options contains config configurations related to Ethereum execution client.
 type Options struct {
 	Addr                  string        `yaml:"ETH1Addr" env:"ETH_1_ADDR" env-required:"true" env-description:"Execution client WebSocket URL(s). Multiple clients are supported via semicolon-separated URLs (e.g. 'ws://localhost:8546;ws://localhost:8547')"`
-	ConnectionTimeout     time.Duration `yaml:"ETH1ConnectionTimeout" env:"ETH_1_CONNECTION_TIMEOUT" env-default:"10s" env-description:"Timeout for execution client requests"`
+	ConnectionTimeout     time.Duration `yaml:"ETH1ConnectionTimeout" env:"ETH_1_CONNECTION_TIMEOUT" env-default:"15s" env-description:"Timeout for execution client requests"`
 	SyncDistanceTolerance uint64        `yaml:"ETH1SyncDistanceTolerance" env:"ETH_1_SYNC_DISTANCE_TOLERANCE" env-default:"5" env-description:"Maximum number of blocks behind head considered in-sync"`
 }
