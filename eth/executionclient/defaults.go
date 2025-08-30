ceaec4d49570e673d1050e47ecc629d2ed8ebbfa
iurii <sidorenko@ssvlabs.io>
2025-08-30 15:32:26 +0300

    Lower default timeouts for CL down to 10s

--- eth/executionclient/defaults.go
+++ eth/executionclient/defaults.go
@@ -5,7 +5,7 @@ import (
 )
 
 const (
-	DefaultReqTimeout                  = 15 * time.Second
+	DefaultReqTimeout                  = 10 * time.Second
 	DefaultReconnectionInitialInterval = 1 * time.Second
 	DefaultReconnectionMaxInterval     = 64 * time.Second
 	DefaultHealthInvalidationInterval  = 24 * time.Second // TODO: decide on this value, for now choosing the node prober interval but it should probably be a bit less than block interval
