83fdc331065af0fbc30fb8205be3af4ac3da946a
iurii <sidorenko@ssvlabs.io>
2025-11-06 14:01:16 +0200

    use consistent naming

--- protocol/v2/ssv/runner/measurements.go
+++ protocol/v2/ssv/runner/measurements.go
@@ -20,54 +20,54 @@ func newMeasurementsStore() *dutyMeasurements {
 	return &dutyMeasurements{}
 }
 
-func (cm *dutyMeasurements) PreConsensusTime() time.Duration {
-	return cm.preConsensusDuration
+func (dm *dutyMeasurements) PreConsensusTime() time.Duration {
+	return dm.preConsensusDuration
 }
 
-func (cm *dutyMeasurements) ConsensusTime() time.Duration {
-	return cm.consensusDuration
+func (dm *dutyMeasurements) ConsensusTime() time.Duration {
+	return dm.consensusDuration
 }
 
-func (cm *dutyMeasurements) PostConsensusTime() time.Duration {
-	return cm.postConsensusDuration
+func (dm *dutyMeasurements) PostConsensusTime() time.Duration {
+	return dm.postConsensusDuration
 }
 
-func (cm *dutyMeasurements) TotalConsensusTime() time.Duration {
-	return cm.preConsensusDuration + cm.consensusDuration + cm.postConsensusDuration
+func (dm *dutyMeasurements) TotalConsensusTime() time.Duration {
+	return dm.preConsensusDuration + dm.consensusDuration + dm.postConsensusDuration
 }
 
-func (cm *dutyMeasurements) TotalDutyTime() time.Duration {
-	return cm.dutyDuration
+func (dm *dutyMeasurements) TotalDutyTime() time.Duration {
+	return dm.dutyDuration
 }
 
-func (cm *dutyMeasurements) StartPreConsensus() {
-	cm.preConsensusStart = time.Now()
+func (dm *dutyMeasurements) StartPreConsensus() {
+	dm.preConsensusStart = time.Now()
 }
 
-func (cm *dutyMeasurements) EndPreConsensus() {
-	cm.preConsensusDuration = time.Since(cm.preConsensusStart)
+func (dm *dutyMeasurements) EndPreConsensus() {
+	dm.preConsensusDuration = time.Since(dm.preConsensusStart)
 }
 
-func (cm *dutyMeasurements) StartConsensus() {
-	cm.consensusStart = time.Now()
+func (dm *dutyMeasurements) StartConsensus() {
+	dm.consensusStart = time.Now()
 }
 
-func (cm *dutyMeasurements) EndConsensus() {
-	cm.consensusDuration = time.Since(cm.consensusStart)
+func (dm *dutyMeasurements) EndConsensus() {
+	dm.consensusDuration = time.Since(dm.consensusStart)
 }
 
-func (cm *dutyMeasurements) StartPostConsensus() {
-	cm.postConsensusStart = time.Now()
+func (dm *dutyMeasurements) StartPostConsensus() {
+	dm.postConsensusStart = time.Now()
 }
 
-func (cm *dutyMeasurements) EndPostConsensus() {
-	cm.postConsensusDuration = time.Since(cm.postConsensusStart)
+func (dm *dutyMeasurements) EndPostConsensus() {
+	dm.postConsensusDuration = time.Since(dm.postConsensusStart)
 }
 
-func (cm *dutyMeasurements) StartDutyFlow() {
-	cm.dutyStart = time.Now()
+func (dm *dutyMeasurements) StartDutyFlow() {
+	dm.dutyStart = time.Now()
 }
 
-func (cm *dutyMeasurements) EndDutyFlow() {
-	cm.dutyDuration = time.Since(cm.dutyStart)
+func (dm *dutyMeasurements) EndDutyFlow() {
+	dm.dutyDuration = time.Since(dm.dutyStart)
 }
