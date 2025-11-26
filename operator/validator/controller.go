64f0b4085be683af74c61796f9cd1c4855c09b12
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2025-11-26 18:57:24 +0300

    improve the comment

--- operator/validator/controller.go
+++ operator/validator/controller.go
@@ -92,7 +92,7 @@ type ControllerOptions struct {
 	WorkersCount                 int    `yaml:"MsgWorkersCount" env:"MSG_WORKERS_COUNT" env-default:"256" env-description:"Number of message processing workers"`
 	QueueBufferSize              int    `yaml:"MsgWorkerBufferSize" env:"MSG_WORKER_BUFFER_SIZE" env-default:"65536" env-description:"Size of message worker queue buffer"`
 	GasLimit                     uint64 `yaml:"ExperimentalGasLimit" env:"EXPERIMENTAL_GAS_LIMIT" env-description:"Gas limit for MEV block proposals (must match across committee, otherwise MEV fails). Do not change unless you know what you're doing"`
-	MajorityForkProtectionStrict bool   `yaml:"MajorityForkProtectionStrict" env:"MAJORITY_FORK_PROTECTION_STRICT" env-description:"Check if the attestation target root matches the proposed one. It should improve Ethereum network split detection in case of global network incidents. However, it can worsen the performance on reorgs."`
+	MajorityForkProtectionStrict bool   `yaml:"MajorityForkProtectionStrict" env:"MAJORITY_FORK_PROTECTION_STRICT" env-description:"Refuse to sign attestations with different target roots than your own. This does not increase validator safety. Instead, this is potentially more beneficial for Ethereum (area of active research), at the cost of potentially decrease validator performance during transitory disagreements between operators (e.g., when some of the operators' Beacon nodes are behind, and during re-orgs)."`
 }
 
 type Nonce uint16
