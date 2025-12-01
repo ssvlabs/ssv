b3e1a1b374a8db858f0851ea36137c39cd10928c
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2025-12-02 01:33:35 +0300

    forks @ 58375

--- networkconfig/hoodi-stage.go
+++ networkconfig/hoodi-stage.go
@@ -20,7 +20,7 @@ var HoodiStageSSV = &SSV{
 	Forks: SSVForks{
 		Alan:                0,
 		GasLimit36:          0,
-		NetworkTopology:     58360,
-		AggregatorCommittee: 58360,
+		NetworkTopology:     58375,
+		AggregatorCommittee: 58375,
 	},
 }
