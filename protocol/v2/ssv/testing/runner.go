fcfcc36deb6b83606bbbea0cd54aabb86e27ae4d
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2025-12-03 16:28:26 +0300

    partially fix ssv mapping tests

--- protocol/v2/ssv/testing/runner.go
+++ protocol/v2/ssv/testing/runner.go
@@ -33,6 +33,10 @@ var CommitteeRunnerWithShareMap = func(logger *zap.Logger, shareMap map[phase0.V
 	return baseRunnerWithShareMap(logger, spectypes.RoleCommittee, shareMap)
 }
 
+var AggregatorCommitteeRunner = func(logger *zap.Logger, keySet *spectestingutils.TestKeySet) runner.Runner {
+	return baseRunner(logger, spectypes.RoleAggregatorCommittee, keySet)
+}
+
 var AggregatorCommitteeRunnerWithShareMap = func(logger *zap.Logger, shareMap map[phase0.ValidatorIndex]*spectypes.Share) runner.Runner {
 	return baseRunnerWithShareMap(logger, spectypes.RoleAggregatorCommittee, shareMap)
 }
