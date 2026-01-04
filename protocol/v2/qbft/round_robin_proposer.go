23e35da349a1a26979bdfa862ea0be9270630347
Julien Harbulot <julien.gitcommit+ssv@gmail.com>
2026-01-04 19:42:24 +0400

    Add new proposer round-robin algorithm for Boole fork

--- protocol/v2/qbft/round_robin_proposer.go
+++ protocol/v2/qbft/round_robin_proposer.go
@@ -5,6 +5,13 @@ import (
 	spectypes "github.com/ssvlabs/ssv-spec/types"
 )
 
+func ForkAwareRoundRobinProposer(state *specqbft.State, round specqbft.Round, booleFork bool) spectypes.OperatorID {
+	if booleFork {
+		return RoundRobinProposer_Boole(state, round)
+	}
+	return RoundRobinProposer(state, round)
+}
+
 // RoundRobinProposer returns the proposer for the round.
 // Each new height starts with the first proposer and increments by 1 with each following round.
 // Each new height has a different first round proposer which is +1 from the previous height.
@@ -16,5 +23,22 @@ func RoundRobinProposer(state *specqbft.State, round specqbft.Round) spectypes.O
 	}
 
 	index := (firstRoundIndex + uint64(round) - uint64(specqbft.FirstRound)) % uint64(len(state.CommitteeMember.Committee))
+	return state.CommitteeMember.Committee[index].OperatorID
+}
+
+// RoundRobinProposer_Boole returns the proposer for the round.
+// Each new height starts with the first proposer and increments by 1 with each following round.
+// Each new height has a different first round proposer which is +1 from the previous height.
+// Also, the current Ethereum epoch is taken into account to introduce variability through epochs
+// (mostly for committees with 4 operators, as 32%4 = 0 as the epochs would "repeat" otherwise).
+// First height starts with index 0
+func RoundRobinProposer_Boole(state *specqbft.State, round specqbft.Round) spectypes.OperatorID {
+	firstRoundIndex := uint64(0)
+	if state.Height != specqbft.FirstHeight {
+		firstRoundIndex += uint64(state.Height) % uint64(len(state.CommitteeMember.Committee))
+	}
+	ethEpoch := uint64(state.Height) / 32
+
+	index := (firstRoundIndex + uint64(round) - uint64(specqbft.FirstRound) + ethEpoch) % uint64(len(state.CommitteeMember.Committee))
 	return state.CommitteeMember.Committee[index].OperatorID
 }
