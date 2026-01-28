f5c1f8fe707d1dc6e0714fb9b79bb80d50463082
Roy-blox <roy@ssvlabs.io>
2026-01-28 16:37:50 +0200

    Set boole fork to 0

--- networkconfig/hoodi-stage.go
+++ networkconfig/hoodi-stage.go
@@ -2,7 +2,6 @@ package networkconfig
 
 import (
 	"math/big"
-	"math"
 
 	ethcommon "github.com/ethereum/go-ethereum/common"
 )
@@ -21,6 +20,6 @@ var HoodiStageSSV = &SSV{
 	Forks: SSVForks{
 		Alan:       0,
 		GasLimit36: 0,
-		Boole:      math.MaxUint64,
+		Boole:      0,
 	},
 }
