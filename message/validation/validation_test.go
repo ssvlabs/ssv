aa62a2c4c4ba30ecc985e23cd8f01a27fdaa10d7
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2026-02-11 14:08:37 +0300

    fix unit tests after pulling changes from the base branch

--- message/validation/validation_test.go
+++ message/validation/validation_test.go
@@ -498,7 +498,7 @@ func Test_ValidateSSVMessage(t *testing.T) {
 
 			badIdentifier := spectypes.NewMsgID(postBooleCfg.DomainType, shares.active.ValidatorPubKey[:], ssvtypes.RoleAggregator)
 			signedSSVMessage := generateSignedMessage(leaderCtx, ks, badIdentifier, slot)
-			booleTopicID := shares.active.BooleCommitteeSubnet().BooleTopic(postBooleCfg.Beacon.Name)
+			booleTopicID := shares.active.BooleCommitteeSubnet().BooleTopic(postBooleCfg.SSV.Name)
 
 			receivedAt := postBooleCfg.SlotStartTime(slot)
 			_, err = validator.handleSignedSSVMessage(signedSSVMessage, booleTopicID, peerID, receivedAt)
@@ -512,7 +512,7 @@ func Test_ValidateSSVMessage(t *testing.T) {
 
 			badIdentifier := spectypes.NewMsgID(postBooleCfg.DomainType, shares.active.ValidatorPubKey[:], ssvtypes.RoleSyncCommitteeContribution)
 			signedSSVMessage := generateSignedMessage(leaderCtx, ks, badIdentifier, slot)
-			booleTopicID := shares.active.BooleCommitteeSubnet().BooleTopic(postBooleCfg.Beacon.Name)
+			booleTopicID := shares.active.BooleCommitteeSubnet().BooleTopic(postBooleCfg.SSV.Name)
 
 			receivedAt := postBooleCfg.SlotStartTime(slot)
 			_, err = validator.handleSignedSSVMessage(signedSSVMessage, booleTopicID, peerID, receivedAt)
@@ -1240,7 +1240,7 @@ func Test_ValidateSSVMessage(t *testing.T) {
 			leader := qbft.RoundRobinProposer(specqbft.Height(postSlot), specqbft.FirstRound, committeeInfo.committee, postBooleCfg)
 			signedSSVMessage := generateSignedMessageWithLeader(ks, msgID, postSlot, leader)
 			receivedAt := postBooleCfg.SlotStartTime(postSlot + 35)
-			booleTopicID := shares.active.BooleCommitteeSubnet().BooleTopic(postBooleCfg.Beacon.Name)
+			booleTopicID := shares.active.BooleCommitteeSubnet().BooleTopic(postBooleCfg.SSV.Name)
 
 			_, err = validator.handleSignedSSVMessage(signedSSVMessage, booleTopicID, peerID, receivedAt)
 			require.ErrorContains(t, err, ErrLateSlotMessage.Error())
