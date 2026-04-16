2206824b68d193b2bf9446fc3f8a564bea3f10fd
Ovidiu Olteanu <ovidiu@ssvlabs.io>
2026-04-16 18:30:37 +0300

    qa: migration test hoodi stage contract

--- networkconfig/hoodi-stage.go
+++ networkconfig/hoodi-stage.go
@@ -10,7 +10,7 @@ var HoodiStageSSV = &SSV{
 	Name:                 "hoodi-stage",
 	DomainType:           [4]byte{0x00, 0x00, 0x31, 0x14},
 	RegistrySyncOffset:   new(big.Int).SetInt64(1004),
-	RegistryContractAddr: ethcommon.HexToAddress("0x0aaace4e8affc47c6834171c88d342a4abd8f105"),
+	RegistryContractAddr: ethcommon.HexToAddress("0x2427C1E38222FFCE38B73777e87F61B45494C4A2"),
 	DiscoveryProtocolID:  [6]byte{'s', 's', 'v', 'd', 'v', '5'},
 	Bootnodes: []string{
 		// SSV Labs
