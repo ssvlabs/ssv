fdb8c1051305528c64700ff61a1c161fffd7681c
Roy-blox <roy@ssvlabs.io>
2026-05-24 10:51:19 +0300

    networkconfig: point hoodi-stage at new registry contract for exit test

--- networkconfig/hoodi-stage.go
+++ networkconfig/hoodi-stage.go
@@ -10,7 +10,7 @@ var HoodiStageSSV = &SSV{
 	Name:                 "hoodi-stage",
 	DomainType:           [4]byte{0x00, 0x00, 0x31, 0x14},
 	RegistrySyncOffset:   new(big.Int).SetInt64(1004),
-	RegistryContractAddr: ethcommon.HexToAddress("0x0aaace4e8affc47c6834171c88d342a4abd8f105"),
+	RegistryContractAddr: ethcommon.HexToAddress("0xc07B3E9671f884FDa67E1e7D43d952E0e1369fd8"),
 	DiscoveryProtocolID:  [6]byte{'s', 's', 'v', 'd', 'v', '5'},
 	Bootnodes: []string{
 		// SSV Labs
