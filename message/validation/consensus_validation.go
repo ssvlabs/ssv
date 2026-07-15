26cc4404a0d5112853e6c52afee025a7b80ff27e
Momčilo Miladinović <momcilo@ssvlabs.io>
2026-07-15 11:22:06 +0200

    lint: drop redundant qbft import alias (QF1008)

--- message/validation/consensus_validation.go
+++ message/validation/consensus_validation.go
@@ -17,7 +17,7 @@ import (
 	spectypes "github.com/ssvlabs/ssv-spec/types"
 
 	"github.com/ssvlabs/ssv/protocol/v2/message"
-	qbft "github.com/ssvlabs/ssv/protocol/v2/qbft"
+	"github.com/ssvlabs/ssv/protocol/v2/qbft"
 	"github.com/ssvlabs/ssv/protocol/v2/qbft/roundtimer"
 	ssvtypes "github.com/ssvlabs/ssv/protocol/v2/types"
 )
